418c062dbac170bc1cb0e8aa271161db4d07c0fa
Dominik Guzei <dominik.guzei@gmail.com>
2016-10-18 12:36:20 +0200

    add wallet send form validator

--- app/components/wallet/WalletSendForm.js
+++ app/components/wallet/WalletSendForm.js
@@ -1,13 +1,15 @@
 // @flow
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { observer } from 'mobx-react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import MobxReactForm from 'mobx-react-form';
 import styles from './WalletSendForm.scss';
 
 @observer
 export default class WalletSendForm extends Component {
   render() {
+    const { validator } = this.props;
     return (
       <div className={styles.component}>
 
@@ -15,24 +17,39 @@ export default class WalletSendForm extends Component {
 
           <TextField
             className={styles.textField}
-            hintText="Bitcoin address"
             floatingLabelText="Receiver"
+            hintText="Bitcoin address"
+            value={validator.$('receiver').value}
+            errorText={validator.$('receiver').error}
+            onChange={validator.$('receiver').onChange}
+            onFocus={validator.$('receiver').onFocus}
+            onBlur={validator.$('receiver').onBlur}
             floatingLabelFixed
             fullWidth
           />
 
           <TextField
             className={styles.textField}
-            hintText="Amount in $"
             floatingLabelText="Amount"
+            hintText="Amount in $"
+            value={validator.$('amount').value}
+            errorText={validator.$('amount').error}
+            onChange={validator.$('amount').onChange}
+            onFocus={validator.$('amount').onFocus}
+            onBlur={validator.$('amount').onBlur}
             floatingLabelFixed
             fullWidth
           />
 
           <TextField
             className={styles.textField}
-            hintText="You can add a message if you want"
             floatingLabelText="Description"
+            hintText="You can add a message if you want"
+            value={validator.$('description').value}
+            errorText={validator.$('description').error}
+            onChange={validator.$('description').onChange}
+            onFocus={validator.$('description').onFocus}
+            onBlur={validator.$('description').onBlur}
             floatingLabelFixed
             multiLine
             fullWidth
@@ -43,6 +60,7 @@ export default class WalletSendForm extends Component {
         <RaisedButton
           className={styles.sendButton}
           label="Send"
+          onClick={validator.onSubmit}
           primary
           fullWidth
         />
@@ -51,3 +69,7 @@ export default class WalletSendForm extends Component {
     );
   }
 }
+
+WalletSendForm.propTypes = {
+  validator: PropTypes.instanceOf(MobxReactForm),
+};
