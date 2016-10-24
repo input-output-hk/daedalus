ecc937d7c5ae3da7518964d2d5624daa94b6fb9b
Dominik Guzei <dominik.guzei@gmail.com>
2016-10-24 12:09:43 +0200

    enable easy testing during development
    We can re-introduce a static test run on the 
    packaged app later on. I already know that this
    works well, since this was the first thing I was
    able to get work ;)

--- features/support/electron.js
+++ features/support/electron.js
@@ -5,10 +5,15 @@ export default function () {
   this.Before(async function() {
     this.app = new Application({
       path: electronPath,
-      args: ['.'],
+      args: ['./electron/main.testing'],
+      env: {
+        HOT: 1,
+        NODE_ENV: 'test'
+      }
     });
     await this.app.start();
     this.client = this.app.client;
+    this.browserWindow = this.app.browserWindow;
   });
   this.After(function() {
     return this.app.stop();
