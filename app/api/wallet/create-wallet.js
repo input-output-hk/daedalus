af983d5ac127269756406b1ee3875221a1aa5ed1
Dominik Guzei <dominik.guzei@gmail.com>
2016-11-15 16:45:09 +0100

    remove console log

--- app/api/wallet/create-wallet.js
+++ app/api/wallet/create-wallet.js
@@ -4,7 +4,6 @@ export const createPersonalWallet = (data: {
   name: string,
   currency: string
 }) => new Promise((resolve) => {
-  console.log(data);
   resolve(Object.assign({}, data, {
     type: 'personal',
     address: '16CCkRJ8sdok7FicNuuNdNv1bG9QVegkA7',
