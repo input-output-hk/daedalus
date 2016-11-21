316b76fdc72af9eef25d9fe1cfddce9ffaef497a
Dominik Guzei <dominik.guzei@gmail.com>
2016-11-21 11:23:49 +0100

    add croatian local-data for react-intl

--- app/index.js
+++ app/index.js
@@ -5,13 +5,14 @@ import { addLocaleData } from 'react-intl';
 import { MemoryRouter as Router } from 'react-router';
 import en from 'react-intl/locale-data/en';
 import de from 'react-intl/locale-data/de';
+import hr from 'react-intl/locale-data/hr';
 import App from './App';
 import state from './state/index';
 import AppController from './controllers/AppController';
 import './styles/index.global.scss';
 
 // https://github.com/yahoo/react-intl/wiki#loading-locale-data
-addLocaleData([en, de]);
+addLocaleData([en, de, hr]);
 
 const appState = state();
 const controller = new AppController(appState);
