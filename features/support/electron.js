f3fce7aea10807c3fe6bc31a32c3d62a0b9bb3a2
Dominik Guzei <dominik.guzei@gmail.com>
2016-11-22 14:32:18 +0100

    increase maximum wait time for app startup

--- features/support/electron.js
+++ features/support/electron.js
@@ -2,7 +2,7 @@ import { Application } from 'spectron';
 import electronPath from 'electron';
 
 export default function () {
-  this.Before(async function() {
+  this.Before({ timeout: 20 * 1000 }, async function() {
     this.app = new Application({
       path: electronPath,
       args: ['./electron/main.testing'],
