0606f709fc26b9626c835ab2666426b3e1fe5dd0
Dominik Guzei <dominik.guzei@gmail.com>
2016-11-23 16:02:52 +0100

    adds missing wallet steps to repo

--- features/step_definitions/wallets-steps.js
+++ features/step_definitions/wallets-steps.js
@@ -2,26 +2,30 @@ import { expect } from 'chai';
 
 export default function () {
 
-  this.Given(/^I have the following wallets:$/, async function (table) {
+  this.Given(/^I have a wallet$/, async function () {
     const result = await this.client.execute(function() {
-      const browserRequire = require;
-      return browserRequire('./api/data/wallets.json')
+      const wallet = daedalus.api.data.createWallet();
+      daedalus.controller.wallets.loadWallets();
+      return wallet;
     });
-    console.log(result);
+    this.wallet = result.value;
   });
 
   this.Given(/^I am on the wallet send screen$/, async function() {
-    this.client.url('/wallet/')
+    await this.client.waitForVisible('.WalletNavigation_sendLink');
+    return this.client.execute(function(route) {
+      daedalus.state.router.transitionTo(route);
+    }, `/wallet/${this.wallet.address}/send`);
   });
 
   this.When(/^I submit the wallet send form$/, async function () {
-    const submitButton = '.WalletSendForm_sendButton';
+    const submitButton = '.WalletSendForm_submitButton';
     await this.client.waitForVisible(submitButton);
     return this.client.click(submitButton);
   });
 
-  this.Then(/^I should see the following error messages:$/, async function (data) {
-    const errorsOnScreen = await this.client.getText('.WalletSendForm_textField > :last-child');
+  this.Then(/^I should see the following error messages on the wallet send form:$/, async function (data) {
+    const errorsOnScreen = await this.client.getText('.WalletSendForm_fields .input_error');
     const errors = data.hashes();
     for (let i=0; i < errors.length; i++) {
       expect(errorsOnScreen[i]).to.equal(errors[i].message);
