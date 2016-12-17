c09eb6e6adcce0894f53cc94397acbeccfd901ec
Darko Mijić <darko.mijic@gmail.com>
2016-12-17 14:22:37 +0100

    Resize window after successful login

--- app/reactions/LoginReaction.js
+++ app/reactions/LoginReaction.js
@@ -1,4 +1,5 @@
 // @flow
+import { ipcRenderer } from 'electron';
 import BaseReaction from './BaseReaction';
 
 export default class LoginReaction extends BaseReaction {
@@ -7,6 +8,8 @@ export default class LoginReaction extends BaseReaction {
     if (isLoggedIn && !isLoggingIn) {
       this.appController.user.loadUser();
       this.appController.wallets.loadWallets();
+      // TODO: move window resizing to more appropriate place
+      ipcRenderer.send('resize-window', { width: 1024, height: 768, animate: true });
     }
   }
 }
