62559c4d27d5c125b956ead4929f36572f97b682
Darko Mijić <darko.mijic@gmail.com>
2016-12-21 10:27:19 +0100

    Automatically login in development mode

--- app/reactions/LoginReaction.js
+++ app/reactions/LoginReaction.js
@@ -12,5 +12,15 @@ export default class LoginReaction extends Reaction {
       // TODO: move window resizing to more appropriate place
       ipcRenderer.send('resize-window', { width: 1024, height: 768, animate: !environment.isTest() });
     }
+    // TODO: environment is 'development' when tests start and than it changes to test
+    // so this reaction fires in test mode without this timeout
+    if (environment.isDev() && !isLoggedIn && !isLoggingIn) {
+      setTimeout(() => {
+        if (environment.isDev()) {
+          const { email, passwordHash } = this.appController.api.repository.user.profile;
+          this.appController.user.login({ email, passwordHash });
+        }
+      }, 1000);
+    }
   }
 }
