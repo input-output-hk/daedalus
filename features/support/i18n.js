cd1ae54cbac8edebb168b0107f65ced4438bf316
Darko Mijić <darko.mijic@gmail.com>
2017-01-10 07:58:31 +0100

    Fix code styling

--- features/support/i18n.js
+++ features/support/i18n.js
@@ -2,7 +2,7 @@ export default function () {
   this.Before(function() {
     this.intl = async (translationId, translationValues = {}) => {
       const translation = await this.client.execute(function(id, values) {
-        const IntlProvider  = require('react-intl').IntlProvider;
+        const IntlProvider = require('react-intl').IntlProvider;
         const locale = daedalus.stores.app.currentLocale;
         const messages = daedalus.translations;
         const intlProvider = new IntlProvider({ locale, messages: messages[locale] }, {});
