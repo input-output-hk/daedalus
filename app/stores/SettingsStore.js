cc3c69bf41f949437dbb5761e8d3fb06e33ca927
Dominik Guzei <dominik.guzei@gmail.com>
2017-01-18 19:11:25 +0100

    refactor to class initializer syntax in settins store

--- app/stores/SettingsStore.js
+++ app/stores/SettingsStore.js
@@ -13,30 +13,30 @@ export default class SettingsStore extends Store {
 
   constructor(...args) {
     super(...args);
-    this.actions.startEditingSettingsField.listen(this._startEditingSettingsField.bind(this));
-    this.actions.stopEditingSettingsField.listen(this._stopEditingSettingsField.bind(this));
-    this.actions.cancelEditingSettingsField.listen(this._cancelEditingSettingsField.bind(this));
+    this.actions.startEditingSettingsField.listen(this._startEditingSettingsField);
+    this.actions.stopEditingSettingsField.listen(this._stopEditingSettingsField);
+    this.actions.cancelEditingSettingsField.listen(this._cancelEditingSettingsField);
   }
 
   @computed get termsOfUse() {
     return this.termsOfUseRequest.execute().result;
   }
 
-  @action _startEditingSettingsField(params) {
+  @action _startEditingSettingsField = (params) => {
     this.settingsFieldBeingEdited = params.field;
-  }
+  };
 
-  @action _stopEditingSettingsField() {
+  @action _stopEditingSettingsField = () => {
     if (this.settingsFieldBeingEdited) {
       this.lastUpdatedSettingsField = this.settingsFieldBeingEdited;
     }
     this.settingsFieldBeingEdited = null;
-  }
+  };
 
-  @action _cancelEditingSettingsField() {
+  @action _cancelEditingSettingsField = () => {
     this.lastUpdatedSettingsField = null;
     this.settingsFieldBeingEdited = null;
-  }
+  };
 
   isValidName(name: string) {
     return isLength(name, { min: 2, max: 30 });
