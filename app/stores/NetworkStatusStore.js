e9f28e577ac968630cf2be8bffc81060fb19e0da
Dominik Guzei <dominik.guzei@gmail.com>
2017-01-20 15:31:44 +0000

    remove ui hack for connection status

--- app/stores/NetworkStatusStore.js
+++ app/stores/NetworkStatusStore.js
@@ -6,10 +6,8 @@ import Request from './lib/Request';
 export default class NetworkStatusStore extends Store {
 
   POLLING_INTERVAL = 5000; // How often we poll for network status
-  UPDATE_TIMEOUT = 2000; // Simulates a "connecting" status for the user (we have too low latency)
 
   @observable isCardanoConnected = false;
-  @observable isCardanoConnecting = false;
   @observable cardanoStatusRequest = new Request(this.api, 'getWallets');
 
   constructor(...args) {
@@ -24,15 +22,9 @@ export default class NetworkStatusStore extends Store {
   }
 
   @action _updateCardanoStatus = () => {
-    this.cardanoStatusRequest.execute();
-    this.isCardanoConnecting = true;
-    // Since we have very low latency we would only be in "connecting" state for a few milliseconds
-    setTimeout(action(() => {
-      this.cardanoStatusRequest
-        .then(action(() => { this.isCardanoConnected = true; }))
-        .catch(action(() => { this.isCardanoConnected = false; }));
-      this.isCardanoConnecting = false;
-    }), this.UPDATE_TIMEOUT);
+    this.cardanoStatusRequest.execute()
+      .then(action(() => { this.isCardanoConnected = true; }))
+      .catch(action(() => { this.isCardanoConnected = false; }));
   };
 
 }
