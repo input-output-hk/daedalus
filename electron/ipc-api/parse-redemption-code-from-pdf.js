d52360787ff53b1e3dd9af8aefb4e5e78fa9240b
Dominik Guzei <dominik.guzei@gmail.com>
2017-01-27 16:10:51 +0000

    small refactoring

--- electron/ipc-api/parse-redemption-code-from-pdf.js
+++ electron/ipc-api/parse-redemption-code-from-pdf.js
@@ -1,20 +1,20 @@
 import { PDFExtract } from 'pdf.js-extract';
 import { ipcMain } from 'electron';
 
-const CHANNEL = 'parse-redemption-code-from-pdf';
+const CHANNEL_NAME = 'parse-redemption-code-from-pdf';
 
 export const PARSE_REDEMPTION_CODE = {
-  REQUEST: CHANNEL,
-  SUCCESS: `${CHANNEL}-success`,
-  ERROR: `${CHANNEL}-error`,
+  REQUEST: CHANNEL_NAME,
+  SUCCESS: `${CHANNEL_NAME}-success`,
+  ERROR: `${CHANNEL_NAME}-error`,
 };
 
 export default () => {
-  ipcMain.on(CHANNEL, (event, pdfPath) => {
+  ipcMain.on(PARSE_REDEMPTION_CODE.REQUEST, (event, pdfPath) => {
     const sender = event.sender;
     try {
       const pdfExtract = new PDFExtract();
-      pdfExtract.extract(pdfPath, {}, function (err, data) {
+      pdfExtract.extract(pdfPath, {}, function (error, data) {
         if (error) sender.send(PARSE_REDEMPTION_CODE.ERROR, error);
         sender.send(PARSE_REDEMPTION_CODE.SUCCESS, data.pages[0].content[8].str);
       });
