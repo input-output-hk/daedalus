201ca70684d5329b4289d31ceb277cb3df30fecb
Dominik Guzei <dominik.guzei@gmail.com>
2017-02-07 16:54:10 +0100

    implements new wallet nav design

--- app/components/wallet/navigation/WalletNavigation.js
+++ app/components/wallet/navigation/WalletNavigation.js
@@ -3,14 +3,20 @@ import React, { Component, PropTypes } from 'react';
 import { observer } from 'mobx-react';
 import { defineMessages, intlShape } from 'react-intl';
 import styles from './WalletNavigation.scss';
-import WalletNavHomeButton from './WalletHomeButton';
 import WalletNavButton from './WalletNavButton';
-import sendIcon from '../../../assets/images/send-ic.svg';
-import sendIconActive from '../../../assets/images/send-white-ic.svg';
-import receiveIcon from '../../../assets/images/receive-ic.svg';
-import receiveIconActive from '../../../assets/images/receive-white-ic.svg';
+import summaryIcon from '../../../assets/images/wallet-nav/summary-ic-light.svg';
+import summaryIconActive from '../../../assets/images/wallet-nav/summary-ic-dark.svg';
+import sendIcon from '../../../assets/images/wallet-nav/send-ic-light.svg';
+import sendIconActive from '../../../assets/images/wallet-nav/send-ic-dark.svg';
+import receiveIcon from '../../../assets/images/wallet-nav/receive-ic-light.svg';
+import receiveIconActive from '../../../assets/images/wallet-nav/receive-ic-dark.svg';
 
 const messages = defineMessages({
+  summary: {
+    id: 'wallet.navigation.summary',
+    defaultMessage: '!!!Summary',
+    description: 'Label for the "summary" nav button in the wallet navigation.'
+  },
   send: {
     id: 'wallet.navigation.send',
     defaultMessage: '!!!send',
@@ -41,22 +47,22 @@ export default class WalletNavigation extends Component {
   };
 
   render() {
-    const { wallet, isActiveNavItem, onNavItemClick } = this.props;
+    const { isActiveNavItem, onNavItemClick } = this.props;
     const { intl } = this.context;
     return (
       <div className={styles.component}>
 
-        <div className={styles.homeLink}>
-          <WalletNavHomeButton
-            walletName={wallet.name}
-            amount={wallet.amount}
-            currency={wallet.currency}
+        <div className={styles.navItem}>
+          <WalletNavButton
+            label={intl.formatMessage(messages.summary)}
+            normalIcon={summaryIcon}
+            activeIcon={summaryIconActive}
             isActive={isActiveNavItem('home')}
             onClick={() => onNavItemClick('home')}
           />
         </div>
 
-        <div className={styles.sendLink}>
+        <div className={styles.navItem}>
           <WalletNavButton
             label={intl.formatMessage(messages.send)}
             normalIcon={sendIcon}
@@ -66,7 +72,7 @@ export default class WalletNavigation extends Component {
           />
         </div>
 
-        <div className={styles.receiveLink}>
+        <div className={styles.navItem}>
           <WalletNavButton
             label={intl.formatMessage(messages.receive)}
             normalIcon={receiveIcon}
