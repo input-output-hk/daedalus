95f9f2fa92f0c28064a12249ac3124d42aa24341
Domen Kožar <domen@dev.si>
2017-02-10 15:56:40 +0100

    [CSL-737] Use cardano-launcher to spin Daedalus and cardano-sl

--- installers/WindowsInstaller.hs
+++ installers/WindowsInstaller.hs
@@ -1,9 +1,42 @@
 module WindowsInstaller where
 
 import           Data.Maybe (fromMaybe)
+import qualified Data.List as L
 import           Development.NSIS
 import           System.Environment (lookupEnv)
 import           Turtle (echo, procs)
+
+shortcutParameters :: String
+shortcutParameters = L.intercalate " " $
+  [ "--node $INSTDIR\\cardano-node.exe"
+  , "--wallet $INSTDIR\\Daedalus.exe"
+  , "--updater C:/TODO" -- TODO
+  , "--node-timeout 5"
+  , (" -n " ++ (L.intercalate " -n " nodeArgs))
+  ] 
+    where
+      appdata = "$APPDATA"
+      nodeArgs = [
+        "--listen", "0.0.0.0:12100",
+        "--peer", "35.156.182.24:3000/MHdrsP-oPf7UWl0007QuXnLK5RD=",
+        "--peer", "54.183.103.204:3000/MHdrsP-oPf7UWl0077QuXnLK5RD=",
+        "--peer", "52.53.231.169:3000/MHdrsP-oPf7UWl0127QuXnLK5RD=",
+        "--peer", "35.157.41.94:3000/MHdrsP-oPf7UWl0057QuXnLK5RD=",
+        "--log-config", "log-config-prod.yaml",
+        "--keyfile", appdata ++ "\\Daedalus\\Secrets\\secret.key",
+        "--logs-prefix", appdata ++ "\\Daedalus\\Logs",
+        "--db-path", appdata ++ "\\Daedalus\\DB",
+        "--wallet-db-path", appdata ++ "\\Daedalus\\Wallet",
+        "--wallet"
+        ]
+
+daedalusShortcut :: [Attrib]
+daedalusShortcut =
+    [ Target "$INSTDIR\\cardano-launcher.exe"
+    , Parameters (str shortcutParameters)
+    , IconFile "$INSTDIR\\Daedalus.exe"
+    , IconIndex 0
+    ]
 
 writeNSIS :: IO ()
 writeNSIS = do
@@ -27,12 +60,9 @@ writeNSIS = do
         createDirectory "$APPDATA\\Daedalus\\Wallet" 
         createDirectory "$APPDATA\\Daedalus\\Logs"
         createDirectory "$APPDATA\\Daedalus\\Secrets"
-        createShortcut "$DESKTOP\\Daedalus.lnk"
-          [ Target "$INSTDIR\\Daedalus.exe"
-          , IconFile "$INSTDIR\\Daedalus.exe"
-          , IconIndex 0
-          ]
+        createShortcut "$DESKTOP\\Daedalus.lnk" daedalusShortcut
         file [] "cardano-node.exe"
+        file [] "cardano-launcher.exe"
         file [] "log-config-prod.yaml"
         file [] "version.txt"
         file [Recursive] "dlls\\"
@@ -49,8 +79,7 @@ writeNSIS = do
         createDirectory "$SMPROGRAMS/Daedalus"
         createShortcut "$SMPROGRAMS/Daedalus/Uninstall Daedalus.lnk" 
           [Target "$INSTDIR/uninstall.exe", IconFile "$INSTDIR/uninstall.exe", IconIndex 0]
-        createShortcut "$SMPROGRAMS/Daedalus/Daedalus.lnk"
-          [Target "$INSTDIR/Daedalus.exe", IconFile "$INSTDIR/Daedalus.exe", IconIndex 0]
+        createShortcut "$SMPROGRAMS/Daedalus/Daedalus.lnk" daedalusShortcut
 
     uninstall $ do
       -- Remove registry keys
