b02f8041b1af873f7f1a014cb027837e4a1d992a
Darko Mijić <darko.mijic@gmail.com>
2017-02-28 16:18:30 +0100

    WIP: use syncStatus endpoint to get the initial local and network difficulty

--- app/stores/NetworkStatusStore.js
+++ app/stores/NetworkStatusStore.js
@@ -1,6 +1,7 @@
 // @flow
 import { observable, action, computed } from 'mobx';
 import Store from './lib/Store';
+import Request from './lib/Request';
 
 export default class NetworkStatusStore extends Store {
 
@@ -9,7 +10,7 @@ export default class NetworkStatusStore extends Store {
   @observable localDifficulty = 0;
   @observable networkDifficulty = 0;
   @observable isLoadingWallets = true;
-
+  @observable networkDifficultyRequest = new Request(this.api, 'getSyncProgress');
   @observable _localDifficultyStartedWith = null;
 
   setup() {
@@ -28,8 +29,13 @@ export default class NetworkStatusStore extends Store {
     if (this.networkDifficulty > 0 && this._localDifficultyStartedWith !== null) {
       const relativeLocal = this.localDifficulty - this._localDifficultyStartedWith;
       const relativeNetwork = this.networkDifficulty - this._localDifficultyStartedWith;
-      // In case node when node is in sync after first local difficulty messages
-      // local and network difficulty will be the same
+      // In case node is in sync after first local difficulty messages
+      // local and network difficulty will be the same (0)
+      console.log('networkDifficulty', this.networkDifficulty);
+      console.log('localDifficulty', this.localDifficulty);
+      console.log('relativeLocal', relativeLocal);
+      console.log('relativeNetwork', relativeNetwork);
+
       if (relativeLocal === relativeNetwork) return 100;
       return relativeLocal / relativeNetwork * 100;
     }
@@ -44,6 +50,14 @@ export default class NetworkStatusStore extends Store {
     return !this.isConnecting && this.syncPercentage >= 100;
   }
 
+  @action _setInitialDifficulty = async () => {
+    const initialDifficulty = await this.networkDifficultyRequest.execute();
+    this._localDifficultyStartedWith = initialDifficulty.localDifficulty;
+    this.localDifficulty = initialDifficulty.localDifficulty;
+    this.networkDifficulty = initialDifficulty.networkDifficulty;
+    console.log('INITIAL', initialDifficulty);
+  };
+
   @action _listenToServerStatusNotifications() {
     this.api.notify((message) => {
       if (message === 'ConnectionClosed') {
@@ -52,7 +66,9 @@ export default class NetworkStatusStore extends Store {
       }
       switch (message.tag) {
         case 'ConnectionOpened':
-          this.isConnected = true; break;
+          this._setInitialDifficulty();
+          this.isConnected = true;
+          break;
         case 'NetworkDifficultyChanged':
           this.networkDifficulty = message.contents.getChainDifficulty;
           this.isConnected = true;
@@ -61,9 +77,9 @@ export default class NetworkStatusStore extends Store {
         case 'LocalDifficultyChanged':
           const difficulty = message.contents.getChainDifficulty;
           this.localDifficulty = difficulty;
-          if (this._localDifficultyStartedWith === null) {
-            this._localDifficultyStartedWith = difficulty;
-          }
+          // if (this._localDifficultyStartedWith === null) {
+          //   this._localDifficultyStartedWith = difficulty;
+          // }
           break;
         case 'ConnectionClosedReconnecting':
           this.isConnected = false;
@@ -89,6 +105,7 @@ export default class NetworkStatusStore extends Store {
   _redirectToLoadingWhenDisconnected = () => {
     if (!this.isConnected) {
       this._localDifficultyStartedWith = null;
+      this._setInitialDifficulty();
       this.actions.goToRoute({ route: '/' });
     }
   };
