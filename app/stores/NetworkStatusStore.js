e204b8173e0705f0e31b51ba0b93d29abfcafcc1
Darko Mijić <darko.mijic@gmail.com>
2017-03-01 12:34:29 +0100

    Handle case when local difficulty is greater then network difficulty

--- app/stores/NetworkStatusStore.js
+++ app/stores/NetworkStatusStore.js
@@ -36,7 +36,7 @@ export default class NetworkStatusStore extends Store {
       console.log('relativeLocal', relativeLocal);
       console.log('relativeNetwork', relativeNetwork);
 
-      if (relativeLocal === relativeNetwork) return 100;
+      if (relativeLocal >= relativeNetwork) return 100;
       return relativeLocal / relativeNetwork * 100;
     }
     return 0;
@@ -77,9 +77,6 @@ export default class NetworkStatusStore extends Store {
         case 'LocalDifficultyChanged':
           const difficulty = message.contents.getChainDifficulty;
           this.localDifficulty = difficulty;
-          // if (this._localDifficultyStartedWith === null) {
-          //   this._localDifficultyStartedWith = difficulty;
-          // }
           break;
         case 'ConnectionClosedReconnecting':
           this.isConnected = false;
