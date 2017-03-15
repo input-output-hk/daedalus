f959f4e1248dadf00cad83499cfa9917b194ef32
Darko Mijić <darko.mijic@gmail.com>
2017-03-15 10:17:32 +0100

    [DAEF-90] Configure react intl translation manager

---
+++ translations/translation-runner.js
@@ -0,0 +1,8 @@
+const manageTranslations = require('react-intl-translations-manager').default;
+
+manageTranslations({
+  messagesDirectory: 'translations/messages',
+  translationsDirectory: 'app/i18n/locales',
+  singleMessagesFile: true,
+  languages: ['en-US', 'hr-HR', 'de-DE']
+});
