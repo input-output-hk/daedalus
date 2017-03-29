4fb6645fef1a0190b496ad0602e9bb3314497bef
Dominik Guzei <dominik.guzei@gmail.com>
2017-03-29 15:03:22 +0200

    [DAEF-125] improve the way we construct dynamic route paths

--- app/Routes.js
+++ app/Routes.js
@@ -15,19 +15,38 @@ import WalletSettingsPage from './containers/wallet/WalletSettingsPage';
 import NoWalletsPage from './containers/wallet/NoWalletsPage';
 import LanguageSelectionPage from './containers/profile/LanguageSelectionPage';
 
+export const ROUTES = {
+  ROOT: '/',
+  STAKING: '/staking',
+  ADA_REDEMPTION: '/ada-redemption',
+  NO_WALLETS: '/no-wallets',
+  PROFILE: {
+    LANGUAGE_SELECTION: '/profile/language-selection',
+  },
+  WALLETS: {
+    ROOT: '/wallets',
+    PAGE: '/wallets/:id/:page',
+    SUMMARY: '/wallets/:id/summary',
+    TRANSACTIONS: '/wallets/:id/transactions',
+    SEND: '/wallets/:id/send',
+    RECEIVE: '/wallets/:id/receive',
+    SETTINGS: '/wallets/:id/settings',
+  },
+};
+
 export default (
   <div>
-    <Route path="/" component={LoadingPage} />
-    <Route path="/profile/language-selection" component={LanguageSelectionPage} />
-    <Route path="/staking" component={StakingPage} />
-    <Route path="/ada-redemption" component={AdaRedemptionPage} />
-    <Route path="/no-wallets" component={NoWalletsPage} />
-    <Route path="/wallets" component={Wallet}>
-      <Route path=":id/summary" component={WalletSummaryPage} />
-      <Route path=":id/transactions" component={WalletTransactionsPage} />
-      <Route path=":id/send" component={WalletSendPage} />
-      <Route path=":id/receive" component={WalletReceivePage} />
-      <Route path=":id/settings" component={WalletSettingsPage} />
+    <Route path={ROUTES.ROOT} component={LoadingPage} />
+    <Route path={ROUTES.PROFILE.LANGUAGE_SELECTION} component={LanguageSelectionPage} />
+    <Route path={ROUTES.STAKING} component={StakingPage} />
+    <Route path={ROUTES.ADA_REDEMPTION} component={AdaRedemptionPage} />
+    <Route path={ROUTES.NO_WALLETS} component={NoWalletsPage} />
+    <Route component={Wallet}>
+      <Route path={ROUTES.WALLETS.SUMMARY} component={WalletSummaryPage} />
+      <Route path={ROUTES.WALLETS.TRANSACTIONS} component={WalletTransactionsPage} />
+      <Route path={ROUTES.WALLETS.SEND} component={WalletSendPage} />
+      <Route path={ROUTES.WALLETS.RECEIVE} component={WalletReceivePage} />
+      <Route path={ROUTES.WALLETS.SETTINGS} component={WalletSettingsPage} />
     </Route>
   </div>
 );
