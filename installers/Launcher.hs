99ffb2a2410482346db3bca89a7dd902ed88d4b7
Domen Kožar <domen@dev.si>
2017-04-17 13:27:24 +0200

    [DEVOPS-41] testnet settings

--- installers/Launcher.hs
+++ installers/Launcher.hs
@@ -25,7 +25,7 @@ launcherArgs launcher = unwords $
     where
       nodeArgs = [
         "--listen", "127.0.0.1:12100",
-        "--report-server", "http://35.157.192.243:8080",
+        "--report-server", "http://52.59.7.118:8080",
         "--log-config", "log-config-prod.yaml",
         "--update-latest-path", quote (installerPath launcher),
         "--keyfile", quote (runtimePath launcher <> "Secrets" <> (pathSeparator : "secret.key")),
@@ -33,7 +33,7 @@ launcherArgs launcher = unwords $
         "--db-path", quote (runtimePath launcher <> "DB-0.2"),
         "--wallet-db-path", quote (runtimePath launcher <> "Wallet-0.2"),
         "--peers-file", "ip-dht-mappings",
-        "--system-start", "1492348550",
+        "--system-start", "1492428658",
         "--wallet",
         "--explicit-initial"
         ]
