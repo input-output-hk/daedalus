c814b69160b9cd8230639c5b1c62ff294a6bb20d
nglumac <niglumac@gmail.com>
2017-04-19 14:52:00 +0200

    [DAEF-164] Add missing translation for Ada Redemption Certificate Parse Error message

--- app/i18n/global-messages.js
+++ app/i18n/global-messages.js
@@ -21,6 +21,11 @@ export default defineMessages({
     defaultMessage: '!!!Invalid phrase entered, please check.',
     description: 'Error message shown when invalid bip39 mnemonic was entered.'
   },
+  invalidAdaRedemptionCertificate: {
+    id: 'global.errors.AdaRedemptionCertificateParseError',
+    defaultMessage: '!!!The ADA redemption code could not be parsed from the given document.',
+    description: 'Error message shown when invalid Ada redemption certificate was uploaded.',
+  },
   languageEnglish: {
     id: 'global.language.english',
     defaultMessage: '!!!English',
