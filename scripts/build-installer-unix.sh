0e3cfa20e58891866cef257b503a682d251b0ea6
Jacob Mitchell <jacob.mitchell@iohk.io>
2017-05-10 16:57:37 -0700

    Use POSIX-friendly shell string substitution.

--- scripts/build-installer-unix.sh
+++ scripts/build-installer-unix.sh
@@ -59,7 +59,7 @@ upload_s3=
 test_install=
 
 daedalus_version="$1"; argnz "product version"; shift
-cardano_branch="$1";   argnz "Cardano SL branch to build Daedalus with"; shift
+cardano_branch="$(printf '%s' "$1" | tr '/' '-')"; argnz "Cardano SL branch to build Daedalus with"; shift
 
 case "$(uname -s)" in
         Darwin ) os=osx;   key=macos.p12;;
