1e0f6bf3bd0c681c6baaec1b0cd96ab67bdf1215
Tomislav Horaček <tomislav.horacek@thespian.hr>
2017-05-11 10:45:28 +0200

    [DAEF-191] Introduce Terms-of-use text as reusable component

--- app/components/profile/terms-of-use/TermsOfUseForm.js
+++ app/components/profile/terms-of-use/TermsOfUseForm.js
@@ -5,7 +5,7 @@ import Button from 'react-toolbox/lib/button/Button';
 import { defineMessages, intlShape } from 'react-intl';
 import CheckboxWithLongLabel from '../../widgets/forms/CheckboxWithLongLabel';
 import LocalizableError from '../../../i18n/LocalizableError';
-import termsOfUseMessages from '../../../i18n/termsOfUse';
+import TermsOfUseText from './TermsOfUseText';
 import styles from './TermsOfUseForm.scss';
 
 const messages = defineMessages({
@@ -55,38 +55,10 @@ export default class TermsOfUseForm extends Component {
       <div className={styles.component}>
         <div className={styles.centeredBox}>
 
-          <div className={styles.terms}>
-            <h1>{intl.formatMessage(termsOfUseMessages.label)}</h1>
-            <p>{intl.formatMessage(termsOfUseMessages.firstParagraph)}</p>
-            <p>{intl.formatMessage(termsOfUseMessages.secondParagraph)}</p>
-            <ol>
-              <li>
-                <h2>{intl.formatMessage(termsOfUseMessages.firstNumberedTitle)}</h2>
-                <ul>
-                  <li>
-                    <strong>{intl.formatMessage(termsOfUseMessages.firstSubtitle)}</strong>
-                    {intl.formatMessage(termsOfUseMessages.firstListText)}
-                  </li>
-                  <li>
-                    <strong>{intl.formatMessage(termsOfUseMessages.secondSubtitle)}</strong>
-                    {intl.formatMessage(termsOfUseMessages.secondListText)}
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <h2>{intl.formatMessage(termsOfUseMessages.secondNumberedTitle)}</h2>
-                <ul>
-                  <li>
-                    <strong>{intl.formatMessage(termsOfUseMessages.thirdSubtitle)}</strong>
-                    {intl.formatMessage(termsOfUseMessages.thirdListText)}
-                  </li>
-                </ul>
-              </li>
-            </ol>
-            <p>{intl.formatMessage(termsOfUseMessages.lastParagraph)}</p>
+          <TermsOfUseText />
 
+          <div className={styles.checkbox}>
             <CheckboxWithLongLabel
-              className={styles.checkbox}
               label={intl.formatMessage(messages.checkboxLabel)}
               onChange={this.toggleAcceptance.bind(this)}
               checked={areTermsOfUseAccepted}
--- app/components/settings/categories/TermsOfUseSettings.js
+++ app/components/settings/categories/TermsOfUseSettings.js
@@ -1,51 +1,16 @@
 // @flow
 import React, { Component } from 'react';
-import { intlShape } from 'react-intl';
 import { observer } from 'mobx-react';
-import classnames from 'classnames';
+import TermsOfUseText from '../../profile/terms-of-use/TermsOfUseText';
 import styles from './TermsOfUseSettings.scss';
-import termsOfUseMessages from '../../../i18n/termsOfUse';
 
 @observer
 export default class TermsOfUseSettings extends Component {
 
-  static contextTypes = {
-    intl: intlShape.isRequired,
-  };
-
   render() {
-    const { intl } = this.context;
-    const componentClassNames = classnames([styles.component, styles.termsOfUse]);
     return (
-      <div className={componentClassNames}>
-        <h1>{intl.formatMessage(termsOfUseMessages.label)}</h1>
-        <p>{intl.formatMessage(termsOfUseMessages.firstParagraph)}</p>
-        <p>{intl.formatMessage(termsOfUseMessages.secondParagraph)}</p>
-        <ol>
-          <li>
-            <h2>{intl.formatMessage(termsOfUseMessages.firstNumberedTitle)}</h2>
-            <ul>
-              <li>
-                <strong>{intl.formatMessage(termsOfUseMessages.firstSubtitle)}</strong>
-                {intl.formatMessage(termsOfUseMessages.firstListText)}
-              </li>
-              <li>
-                <strong>{intl.formatMessage(termsOfUseMessages.secondSubtitle)}</strong>
-                {intl.formatMessage(termsOfUseMessages.secondListText)}
-              </li>
-            </ul>
-          </li>
-          <li>
-            <h2>{intl.formatMessage(termsOfUseMessages.secondNumberedTitle)}</h2>
-            <ul>
-              <li>
-                <strong>{intl.formatMessage(termsOfUseMessages.thirdSubtitle)}</strong>
-                {intl.formatMessage(termsOfUseMessages.thirdListText)}
-              </li>
-            </ul>
-          </li>
-        </ol>
-        <p>{intl.formatMessage(termsOfUseMessages.lastParagraph)}</p>
+      <div className={styles.component}>
+        <TermsOfUseText />
       </div>
     );
   }
