06684b86b04e52d8a7a97c90fe32d0f896221df6
nglumac <niglumac@gmail.com>
2017-06-29 20:27:21 +0200

    [DAEF-255] Add missing input amount skin translations

--- app/components/wallet/skins/AmountInputSkin.js
+++ app/components/wallet/skins/AmountInputSkin.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { defineMessages, intlShape } from 'react-intl';
+import BigNumber from 'bignumber.js';
 import InputSkin from 'react-polymorph/lib/skins/simple/InputSkin';
 import styles from './AmountInputSkin.scss';
 
-export default (props) => (
-  <div className={styles.root}>
-    <InputSkin {...props} />
-    {!props.error && (
-      <span className={styles.fees}>
-        + {props.fees} of fees
-      </span>
-    )}
-    <span className={styles.total}>
-      {!props.error && `= ${props.total} `}{props.currency}
-    </span>
-  </div>
-);
+const messages = defineMessages({
+  feesLabel: {
+    id: 'wallet.amountInput.feesLabel',
+    defaultMessage: '!!!+ {amount} of fees',
+    description: 'Label for the "+ 12.042481 of fees" message above amount input field.'
+  },
+});
+
+export default class AmountInputSkin extends Component {
+
+  props: {
+    currency: string,
+    fees: BigNumber,
+    total: BigNumber,
+    error: boolean,
+  };
+
+  static contextTypes = {
+    intl: intlShape.isRequired,
+  };
+
+  render() {
+    const { error, fees, total, currency } = this.props;
+    const { intl } = this.context;
+
+    return (
+      <div className={styles.root}>
+        <InputSkin {...this.props} />
+        {!error && (
+          <span className={styles.fees}>
+            {intl.formatMessage(messages.feesLabel, { amount: fees })}
+          </span>
+        )}
+        <span className={styles.total}>
+          {!error && `= ${total} `}{currency}
+        </span>
+      </div>
+    );
+  }
+
+}
