7274477f01a3d3ef181a026fbb7f39931aa6b1e8
Domen Kožar <domen@dev.si>
2017-07-05 11:32:24 +0200

    New staging deploy, bump start time, reduce initial nodes

--- installers/Launcher.hs
+++ installers/Launcher.hs
@@ -25,7 +25,7 @@ launcherArgs launcher = unwords $
     where
       nodeArgs = [
         "--listen", "127.0.0.1:12100",
-        "--report-server", "http://35.156.85.198:8080",
+        "--report-server", "http://report-server.aws.iohkdev.io:8080",
         "--log-config", "log-config-prod.yaml",
         "--update-latest-path", quote (installerPath launcher),
         "--keyfile", quote (runtimePath launcher <> "Secrets" <> (pathSeparator : "secret.key")),
@@ -34,7 +34,7 @@ launcherArgs launcher = unwords $
         "--wallet-db-path", quote (runtimePath launcher <> "Wallet-0.4"),
         "--kademlia-peers-file", "ip-dht-mappings",
         "--kademlia-explicit-initial",
-        "--system-start", "1498743070",
+        "--system-start", "1499246772",
         "--wallet"
         ]
 
