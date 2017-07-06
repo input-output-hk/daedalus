4a7bf58d3baf1aec034fec8af993b0b9c1aeef82
Domen Kožar <domen@dev.si>
2017-07-06 16:10:06 +0200

    Update settings for 0.5

--- installers/Launcher.hs
+++ installers/Launcher.hs
@@ -24,18 +24,17 @@ launcherArgs launcher = unwords $
   ]
     where
       nodeArgs = [
-        "--listen", "127.0.0.1:12100",
-        "--report-server", "http://52.59.7.118:8080",
+        "--report-server", "http://report-server.aws.iohk.io:8080",
         "--log-config", "log-config-prod.yaml",
         "--update-latest-path", quote (installerPath launcher),
         "--keyfile", quote (runtimePath launcher <> "Secrets" <> (pathSeparator : "secret.key")),
         "--logs-prefix", quote (runtimePath launcher <> "Logs"),
-        "--db-path", quote (runtimePath launcher <> "DB-0.4"),
-        "--wallet-db-path", quote (runtimePath launcher <> "Wallet-0.4"),
-        "--peers-file", "ip-dht-mappings",
-        "--system-start", "1492428658",
+        "--db-path", quote (runtimePath launcher <> "DB-0.5"),
+        "--wallet-db-path", quote (runtimePath launcher <> "Wallet-0.5"),
+        "--kademlia-peers-file", "ip-dht-mappings",
+        "--system-start", "1499360281",
         "--wallet",
-        "--explicit-initial"
+        "--static-peers"
         ]
 
 quote :: String -> String
