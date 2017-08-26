1ab2cd2d55c256baa71b54b1fcf1e97335280aca
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-08-26 21:49:37 +0300

    launcher:  update flags

--- installers/Launcher.hs
+++ installers/Launcher.hs
@@ -31,23 +31,18 @@ launcherArgs launcher = unwords $
     where
       nodeArgs = [
         "--report-server", "http://report-server.aws.iohk.io:8080",
-        "--listen", "127.0.0.1:12100",
         "--log-config", "log-config-prod.yaml",
         "--update-latest-path", quote (installerPath launcher),
         "--keyfile", quote (runtimePath launcher <> "Secrets-0.5" <> (pathSeparator : "secret.key")),
         "--logs-prefix", quote (runtimePath launcher <> "Logs"),
         "--db-path", quote (runtimePath launcher <> "DB-0.5"),
         "--wallet-db-path", quote (runtimePath launcher <> "Wallet-0.5"),
-        "--kademlia-peers-file", "ip-dht-mappings",
-        "--kademlia-explicit-initial",
         "--update-server", "http://localhost:8080/",
         "--system-start", "1499360281",
-        "--wallet",
         "--update-with-package",
         "--tlscert", quote (runtimePath launcher <> tlsPath <> "server" <> (pathSeparator : "server.crt")),
         "--tlskey", quote (runtimePath launcher <> tlsPath <> "server" <> (pathSeparator : "server.key")),
-        "--tlsca", quote (runtimePath launcher <> tlsPath <> "ca" <> (pathSeparator : "ca.crt")),
-        "--static-peers"
+        "--tlsca", quote (runtimePath launcher <> tlsPath <> "ca" <> (pathSeparator : "ca.crt"))
         ]
       tlsPath = "tls" <> (pathSeparator : [])
       -- NOTE: looks like windows *.bat file is cut of on 1024 characters per line. This is a workaround
