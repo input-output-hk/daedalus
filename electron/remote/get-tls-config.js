d82cea802fbf39121d6d3ae3dbba6f2bb5bba2ee
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-08-27 19:34:32 +0300

    [DAEF-437]  Attempt to correct frontend's idea of TLS CA certificate location

--- electron/remote/get-tls-config.js
+++ electron/remote/get-tls-config.js
@@ -2,5 +2,5 @@ import path from 'path';
 import ClientApi from 'daedalus-client-api';
 
 export default function getTlsConfig() {
-  return ClientApi.tlsInit(path.join(__dirname, '../../tls/ca.crt'));
+  return ClientApi.tlsInit(path.join(__dirname, '../../tls/ca/ca.crt'));
 }
