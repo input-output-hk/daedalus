721c837b53b53cbbae6b75257aedb30bd5324444
Dominik Guzei <dominik.guzei@gmail.com>
2017-09-12 15:36:23 +0200

    [DAEF-464] fix eslint issues

--- app/api/js-api/lib/request.js
+++ app/api/js-api/lib/request.js
@@ -51,9 +51,9 @@ export const request = (httpOptions: RequestOptions, queryParams?: {}) => (
         };
       }
     }
-    const request = https.request(options);
-    if (hasRequestBody) { request.write(requestBody); }
-    request.on('response', (response) => {
+    const httpsRequest = https.request(options);
+    if (hasRequestBody) { httpsRequest.write(requestBody); }
+    httpsRequest.on('response', (response) => {
       let body = '';
       // Cardano-sl returns chunked requests, so we need to concat them
       response.on('data', (chunk) => (body += chunk));
@@ -75,7 +75,7 @@ export const request = (httpOptions: RequestOptions, queryParams?: {}) => (
         }
       });
     });
-    request.on('error', (error) => reject(error));
-    request.end();
+    httpsRequest.on('error', (error) => reject(error));
+    httpsRequest.end();
   })
 );
