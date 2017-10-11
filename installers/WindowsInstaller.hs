b11777a920053e786b160b9cdd19cd965ef3e1d2
michael bishop <michael.bishop@iohk.io>
2017-10-11 20:19:45 -0300

    re-sync configuration.yaml
    revert 6f211f359faa985c584b5dc6c6646f045cf81930
    configuration.yaml came from cardano 824b2303a5dd0058e617a351a1667fa66973d919:node/configuration.yaml

--- installers/WindowsInstaller.hs
+++ installers/WindowsInstaller.hs
@@ -145,7 +145,7 @@ writeInstallerNSIS fullVersion = do
         file [] "client.conf"
         file [] "wallet-topology.yaml"
         file [] "configuration.yaml"
-        file [] "mainnet-genesis.json"
+        file [] "mainnet-genesis-dryrun-with-stakeholders.json"
         writeFileLines "$INSTDIR\\daedalus.bat" (map str launcherScript)
         file [Recursive] "dlls\\"
         file [Recursive] "libressl\\"
