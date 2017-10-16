b149c6863eed035da22349ce5affe52d22ce71cf
nikolaglumac <niglumac@gmail.com>
2017-10-16 12:26:26 +0200

    Get account balances endpoint integration

---
+++ app/api/etc/getEtcAccountBalance.js
@@ -0,0 +1,22 @@
+// @flow
+import { request } from './lib/request';
+import { ETC_API_HOST, ETC_API_PORT } from './index';
+
+export type GetEtcAccountBalanceParams = [string, 'latest' | 'earliest' | 'pending'];
+
+export type GetEtcAccountBalanceResponse = {
+  result: string,
+};
+
+export const getEtcAccountBalance = (params: GetEtcAccountBalanceParams): Promise<GetEtcAccountBalanceResponse> => (
+  request({
+    hostname: ETC_API_HOST,
+    method: 'POST',
+    path: '/',
+    port: ETC_API_PORT,
+  }, {
+    jsonrpc: '2.0',
+    method: 'eth_getBalance',
+    params
+  })
+);
