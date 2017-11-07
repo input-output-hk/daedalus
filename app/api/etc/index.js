fe525f9313d27ab6587f2308edd7c3d7b98deaf1
Dominik Guzei <dominik.guzei@gmail.com>
2017-11-07 12:12:29 +0100

    connect to local mantis instance by default

--- app/api/etc/index.js
+++ app/api/etc/index.js
@@ -40,8 +40,8 @@ import { getEtcEstimatedGas } from './getEtcEstimatedGas';
  * mantis client which is used as backend for ETC blockchain.
  */
 
-export const ETC_API_HOST = 'ec2-52-30-28-57.eu-west-1.compute.amazonaws.com';
-// export const ETC_API_HOST = '127.0.0.1';
+// export const ETC_API_HOST = 'ec2-52-30-28-57.eu-west-1.compute.amazonaws.com';
+export const ETC_API_HOST = '127.0.0.1';
 export const ETC_API_PORT = 8546;
 
 export type GetWalletsResponse = Array<Wallet>;
