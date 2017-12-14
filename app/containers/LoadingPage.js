7cbd390fcf5d78c8a89a29e6440af0fcf214adaa
Stefan Malzner <stefan@adlk.io>
2017-12-14 14:57:43 +0100

    Add ada logo

--- app/containers/LoadingPage.js
+++ app/containers/LoadingPage.js
@@ -5,8 +5,8 @@ import { defineMessages } from 'react-intl';
 import CenteredLayout from '../components/layout/CenteredLayout';
 import Loading from '../components/loading/Loading';
 import type { StoresMap } from '../stores/index';
+import adaLogo from '../assets/images/ada-logo.inline.svg';
 import cardanoLogo from '../assets/images/cardano-logo.inline.svg';
-import cardanoLogoWhite from '../assets/images/cardano-logo-white.inline.svg';
 
 export const messages = defineMessages({
   loadingWalletData: {
@@ -31,8 +31,8 @@ export default class LoadingPage extends Component<Props> {
     return (
       <CenteredLayout>
         <Loading
-          currencyIcon={cardanoLogo}
-          currencyIconWhite={cardanoLogoWhite}
+          currencyIcon={adaLogo}
+          nodeIcon={cardanoLogo}
           isSyncing={isSyncing}
           isConnecting={isConnecting}
           syncPercentage={syncPercentage}
