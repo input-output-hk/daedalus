0d964e5453ce318f9856a26d6de1359998befc52
Dominik Guzei <dominik.guzei@gmail.com>
2017-12-14 17:15:00 +0100

    [DDW-59] make light blue theme the default for ETC build

--- app/stores/ProfileStore.js
+++ app/stores/ProfileStore.js
@@ -82,7 +82,10 @@ export default class SettingsStore extends Store {
   @computed get currentTheme(): string {
     const { result } = this.getThemeRequest.execute();
     if (this.isCurrentThemeSet) return result;
-    return environment.isMainnet() ? THEMES.DARK_BLUE : THEMES.LIGHT_BLUE; // default
+    if (environment.isAdaApi()) {
+      return environment.isMainnet() ? THEMES.DARK_BLUE : THEMES.LIGHT_BLUE; // defaults
+    }
+    return THEMES.LIGHT_BLUE; // default for ETC
   }
 
   @computed get isCurrentThemeSet(): boolean {
