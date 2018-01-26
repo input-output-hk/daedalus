dc20e0fd9cf5f02c4ff909ef3a3bae38ff1aaf16
michael bishop <michael.bishop@iohk.io>
2018-01-26 10:42:32 -0400

    bump windows mantis version

--- scripts/build-installer-win64-mantis.bat
+++ scripts/build-installer-win64-mantis.bat
@@ -1,5 +1,5 @@
 @echo downloading mantis
-curl --location https://s3-eu-west-1.amazonaws.com/iohk.mantis.installer/daedalus-rc1/windows/mantis-daedalus-rc1-win.zip -o mantis.zip
+curl --location https://s3-eu-west-1.amazonaws.com/iohk.mantis.installer/daedalus-rc1/windows/mantis-daedalus-1.0-win.zip -o mantis.zip
 @if %errorlevel% neq 0 (@echo FAILED: couldn't obtain the mantis package
     exit /b 1)
 7z x mantis.zip -y
