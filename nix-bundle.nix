a20f895697ca9f67b0719e177b7a20264d53c0d2
michael bishop <michael.bishop@iohk.io>
2018-02-20 20:09:38 -0400

    add an installer bundle nix-build release.nix -A bundle.installerBundle
    when ran, it puts a copy of daedalus in $HOME which is still sandboxed

--- nix-bundle.nix
+++ nix-bundle.nix
@@ -1,4 +1,4 @@
-{ pkgs, daedalus, nix-bundle, coreutils, utillinux, procps }:
+{ pkgs, daedalus, nix-bundle, coreutils, utillinux, procps, lib, gnutar, xz, pv }:
 
 let
   bundle = import (pkgs.fetchFromGitHub {
@@ -21,9 +21,73 @@ let
 
   exec ${daedalus}/bin/daedalus
   '';
+  namespaceHelper = pkgs.writeScriptBin "namespaceHelper" ''
+    #!/bin/sh
+
+    set -x
+    set -e
+    cd "$(dirname "''${BASH_SOURCE[0]}")"
+
+    exec .${bundle.nix-user-chroot}/bin/nix-user-chroot -n ./nix -c -m /home:/home -m /etc:/host-etc -p DISPLAY -p HOME -p XAUTHORITY -- ${wrapper}/bin/daedalus-wrapper
+  '';
   foo = bundle.nix-bootstrap {
     target = "${wrapper}";
     run = "/bin/daedalus-wrapper";
     nixUserChrootFlags = "-c -m /home:/home -m /etc:/host-etc -p DISPLAY -p HOME -p XAUTHORITY";
   };
-in foo
+  daedalusPackage = pkgs.callPackage (pkgs.path + "/nixos/lib/make-system-tarball.nix") {
+    fileName = "daedalusPackage";
+    contents = [];
+    storeContents = [
+      {
+        object = namespaceHelper;
+        symlink = "/namespaceHelper";
+      }
+    ];
+  };
+  installer = pkgs.writeScriptBin "daedalus-installer" ''
+    #!${pkgs.stdenv.shell}
+
+    set -e
+    export PATH=${lib.concatMapStringsSep ":" (x: "${x}/bin") [ coreutils gnutar xz pv ]}
+
+    test -z "$XDG_DATA_HOME" && { XDG_DATA_HOME="''${HOME}/.local/share"; }
+    export DAEDALUS_DIR="''${XDG_DATA_HOME}/Daedalus"
+
+    echo now installing daedalus...
+
+    if [ -d "''${DAEDALUS_DIR}/unpack" ]; then
+      chmod -R u+w "''${DAEDALUS_DIR}/unpack"
+      rm -rf "''${DAEDALUS_DIR}/unpack"
+    fi
+
+    mkdir -pv "''${DAEDALUS_DIR}/unpack"
+
+    pv ${daedalusPackage}/tarball/daedalusPackage.tar.xz | tar -C "''${DAEDALUS_DIR}/unpack" -xJ
+
+    pushd "''${DAEDALUS_DIR}/unpack"
+    rm namespaceHelper
+    ln -s .${builtins.unsafeDiscardStringContext namespaceHelper}/bin/namespaceHelper namespaceHelper
+    popd
+
+    if [ -d "''${DAEDALUS_DIR}/installation" ]; then
+      mv "''${DAEDALUS_DIR}/installation" "''${DAEDALUS_DIR}/garbage"
+    fi
+    mv "''${DAEDALUS_DIR}/unpack" "''${DAEDALUS_DIR}/installation"
+
+    if [ -d "''${DAEDALUS_DIR}/garbage" ]; then
+      echo cleaning up old version...
+      chmod -R u+w "''${DAEDALUS_DIR}/garbage"
+      rm -rf "''${DAEDALUS_DIR}/garbage"
+    fi
+
+    echo installation finished, daedalus is now in "''${DAEDALUS_DIR}/installation"
+  '';
+  installerBundle = bundle.nix-bootstrap {
+    target = "${installer}";
+    run = "/bin/daedalus-installer";
+    nixUserChrootFlags = "-c -m /home:/home -p HOME";
+  };
+in {
+  inherit foo installer namespaceHelper installerBundle;
+}
