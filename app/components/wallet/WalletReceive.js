c4b0978e3e02052e3658573cbfb85e6f6eadbf01
Dominik Guzei <dominik.guzei@gmail.com>
2018-02-21 13:38:19 +0100

    [DDW-124] dont show validation error after clearing the generate wallet address form

--- app/components/wallet/WalletReceive.js
+++ app/components/wallet/WalletReceive.js
@@ -111,8 +111,9 @@ export default class WalletReceive extends Component<Props, State> {
     },
   }, {
     options: {
+      validationDebounceWait: 0, // Disable debounce to avoid error state after clearing
       validateOnChange: true,
-      validationDebounceWait: 250,
+      showErrorsOnClear: false,
     },
   });
 
@@ -123,13 +124,7 @@ export default class WalletReceive extends Component<Props, State> {
         const { spendingPassword } = form.values();
         const password = walletHasPassword ? spendingPassword : null;
         this.props.onGenerateAddress(password);
-
-        // We need to disable on-change validation before reseting the form in order to
-        // avoid debounced validation being called straight after the form is reset
-        form.state.options.set({ validateOnChange: false });
-        form.reset();
-        form.showErrors(false);
-        form.state.options.set({ validateOnChange: true });
+        form.clear();
       },
       onError: () => {}
     });
