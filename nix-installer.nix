09bd89442dd6e5e8df84203dcb8da0fc5bf78275
Michael Bishop <michael.bishop@iohk.io>
2018-03-08 11:45:59 -0400

    undo a debug change

--- nix-installer.nix
+++ nix-installer.nix
@@ -1,13 +1,12 @@
 { installationSlug ? "nix-install" }:
 let
   pkgs = import (import ./fetchNixpkgs.nix (builtins.fromJSON (builtins.readFile ./nixpkgs-src.json))) { config = {}; overlays = []; };
-  nix-bundle2 = import (pkgs.fetchFromGitHub {
+  nix-bundle = import (pkgs.fetchFromGitHub {
     owner = "input-output-hk";
     repo = "nix-bundle";
     rev = "630e89d1d16083";
     sha256 = "1s9vzlsfxd2ym8jzv2p64j6jlwr9cmir45mb12yzzjr4dc91xk8x";
   }) { nixpkgs = pkgs; };
-  nix-bundle = import ../nix-bundle { nixpkgs = pkgs; };
   installerBundle = nix-bundle.nix-bootstrap {
     target = "${installer}";
     run = "/bin/installer";
