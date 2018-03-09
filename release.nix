3bcad85eb4ba487afa9b3f0ecac4443176f967d4
Michael Bishop <michael.bishop@iohk.io>
2018-03-09 18:15:45 -0400

    add hydra metrics and build products

--- release.nix
+++ release.nix
@@ -1,6 +1,12 @@
 with import ./. {};
-{
-  inherit daedalus tarballInstaller;
-  installerBundle = bundle.installerBundle;
-  inherit newBundle;
+let
+  wrappedBundle = pkgs.runCommand "daedaus-installer" {} ''
+    mkdir -pv $out/nix-support
+    echo "file daedalus-installer ${newBundle}" >> $out/nix-support/hydra-build-products
+    size="$(stat ${newBundle} --printf="%s")"
+    echo installerSize $(($size / 1024 / 1024)) MB >> $out/nix-support/hydra-metrics
+  '';
+in {
+  inherit daedalus;
+  installer = wrappedBundle;
 }
