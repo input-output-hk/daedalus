8080f6e698963d7a0a437f1cb7cc493faddb67b0
Michael Bishop <michael.bishop@iohk.io>
2018-03-14 10:29:15 -0300

    apply all the changes requested in the review

--- installers/nix/nix-installer.nix
+++ installers/nix/nix-installer.nix
@@ -1,12 +1,6 @@
-{ installationSlug ? "nix-install", installedPackages, postInstall ? null }:
+{ installationSlug ? "nix-install", installedPackages, postInstall ? null, nix-bundle }:
 let
-  pkgs = import (import ./fetchNixpkgs.nix (builtins.fromJSON (builtins.readFile ./nixpkgs-src.json))) { config = {}; overlays = []; };
-  nix-bundle = import (pkgs.fetchFromGitHub {
-    owner = "input-output-hk";
-    repo = "nix-bundle";
-    rev = "630e89d1d16083";
-    sha256 = "1s9vzlsfxd2ym8jzv2p64j6jlwr9cmir45mb12yzzjr4dc91xk8x";
-  }) { nixpkgs = pkgs; };
+  pkgs = import (import ../../fetchNixpkgs.nix (builtins.fromJSON (builtins.readFile ../../nixpkgs-src.json))) { config = {}; overlays = []; };
   installerBundle = nix-bundle.nix-bootstrap {
     target = "${installer}";
     run = "/bin/installer";
