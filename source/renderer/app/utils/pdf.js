f60d9f39cfe35ec76b8c3ffdc0a2ccdaafc13f23
nikolaglumac <niglumac@gmail.com>
2018-03-16 15:05:07 +0100

    [DDW-132] Fix eslint warnings

--- source/renderer/app/utils/pdf.js
+++ source/renderer/app/utils/pdf.js
@@ -54,11 +54,11 @@ export const downloadPaperWalletCertificate = (
   const height = 841.98;
   const doc = new PDFDocument({
     size: [width, height],
-    margins : {
+    margins: {
       top: 0,
       bottom: 0,
       left: 0,
-      right: 0
+      right: 0,
     },
     info: {
       Title: 'Daedalus - Paper wallet certificate',
@@ -71,24 +71,24 @@ export const downloadPaperWalletCertificate = (
   SVGtoPDF(doc, paperWalletPage1, 0, 0, { precision: 10 });
 
   // rotation of the document to print rotated content
-  doc.rotate(180, { origin: [width/2, height/2] });
+  doc.rotate(180, { origin: [width / 2, height / 2] });
 
   // page one content
   doc.fontSize(10).fillColor(titleColor);
   doc.text(intl.formatMessage(messages.walletAddressLabel), 0, 160, { width: 595, align: 'center' });
   doc.fillColor(defautlColor);
-  doc.image(qrCodeImage, (width / 2) - 80/2, 180, { fit: [80, 80] });
+  doc.image(qrCodeImage, (width / 2) - 80 / 2, 180, { fit: [80, 80] });
   doc.text(address, (width - 250) / 2, 274, { width: 250, align: 'center' });
 
   // rotation of the document back to normal after the rotated content has been printed
-  doc.rotate(-180, { origin: [width/2, height/2] });
+  doc.rotate(-180, { origin: [width / 2, height / 2] });
 
   // second page
   doc.addPage();
 
   // same deal as first page
   SVGtoPDF(doc, paperWalletPage2, 0, 0);
-  doc.rotate(180, { origin: [width/2, height/2] });
+  doc.rotate(180, { origin: [width / 2, height / 2] });
   doc.fillColor(titleColor);
   doc.text(intl.formatMessage(messages.shieldedRecoveryPhraseLabel), 0, 550, { width: 595, align: 'center' });
   doc.fillColor(defautlColor);
@@ -97,7 +97,7 @@ export const downloadPaperWalletCertificate = (
   doc.text(intl.formatMessage(messages.passwordLabel), 0, 655, { width: 595, align: 'center' });
   doc.fontSize(8).fillColor(defautlColor);
   doc.text(`Daedalus ${intl.formatMessage(messages.releaseVersion)}#${environment.build}`, 0, 695, { width: 595, align: 'center' });
-  doc.rotate(-180, { origin: [width/2, height/2] });
+  doc.rotate(-180, { origin: [width / 2, height / 2] });
 
   try {
     doc.pipe(fs.createWriteStream(filePath));
