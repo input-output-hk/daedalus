448af702de009208f33647e5cb5d923f38d9fc41
nikolaglumac <niglumac@gmail.com>
2018-03-16 14:24:00 +0100

    [DDW-169] Fix certificate path

--- source/main/utils/setupTls.js
+++ source/main/utils/setupTls.js
@@ -2,13 +2,16 @@ import path from 'path';
 import fs from 'fs';
 import Log from 'electron-log';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 /**
  * Here we are reading the TLS certificate from the file system
  * and make it available to render processes via a global variable
  * so that it can be used in HTTP and Websocket connections.
  */
 export const setupTls = () => {
-  const pathToCertificate = path.join(process.cwd(), 'tls', 'ca', 'ca.crt');
+  const caProductionPath = path.join(process.cwd(), 'tls', 'ca', 'ca.crt');
+  const pathToCertificate = isProd ? caProductionPath : path.join(process.cwd(), 'tls', 'ca.crt');
 
   try {
     Log.info('Using certificates from: ' + pathToCertificate);
