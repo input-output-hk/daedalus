397c19a2111fb2d8e9d8d93002eda2bb22dad2ca
nikolaglumac <niglumac@gmail.com>
2018-04-11 18:11:56 +0200

    [DDW-220] Fix bug with validation of repeated password field

--- source/renderer/app/components/wallet/file-import/WalletFileImportDialog.js
+++ source/renderer/app/components/wallet/file-import/WalletFileImportDialog.js
@@ -128,7 +128,9 @@ export default class WalletFileImportDialog extends Component<Props, State> {
         validators: [({ field, form }) => {
           if (!this.state.createPassword) return [true];
           const repeatPasswordField = form.$('repeatPassword');
-          if (repeatPasswordField.value.length > 0) repeatPasswordField.validate(form);
+          if (repeatPasswordField.value.length > 0) {
+            repeatPasswordField.validate({ showErrors: true });
+          }
           return [
             isValidWalletPassword(field.value),
             this.context.intl.formatMessage(globalMessages.invalidWalletPassword)
