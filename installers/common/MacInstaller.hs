35965ca02eab6e9cab3cffb7f13b94e80528d0b4
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-04-27 15:43:33 +0100

    WIP [CSL-2406] installers: darwin cert generation

--- installers/common/MacInstaller.hs
+++ installers/common/MacInstaller.hs
@@ -146,8 +146,8 @@ makeComponentRoot Options{..} appRoot darwinConfig@DarwinConfig{..} = do
   echo "~~~ Preparing files ..."
   ver <- case oBackend of
     Cardano bridge -> do
-      -- Executables
-      forM ["cardano-launcher", "cardano-node"] $ \f -> do
+      -- Executables (from daedalus-bridge)
+      forM ["cardano-launcher", "cardano-node", "cardano-x509-certificates"] $ \f ->
         cp (bridge </> "bin" </> f) (dir </> f)
 
       -- Config files (from daedalus-bridge)
