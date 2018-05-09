7808b0f11fccf754343c26a103c1d91621188351
Michael Bishop <michael.bishop@iohk.io>
2018-05-09 09:45:36 -0300

    [CSL-2503] auto-configure launcher config in nix-shell

--- shell.nix
+++ shell.nix
@@ -4,17 +4,26 @@ in
 { system ? builtins.currentSystem
 , config ? {}
 , pkgs ? (import (localLib.fetchNixPkgs) { inherit system config; })
+, cluster ? "staging"
 }:
 
 with pkgs;
 
-stdenv.mkDerivation {
+let
+  daedalusPkgs = import ./. { inherit cluster; };
+in stdenv.mkDerivation {
   name = "daedalus";
+  LAUNCHER_CONFIG = "${daedalusPkgs.daedalus.cfg}/etc/launcher-config.yaml";
+  DAEDALUS_CONFIG = "${daedalusPkgs.daedalus.cfg}/etc/";
+  shellHook = ''
+    mkdir -p Secrets
+  '';
 
   buildInputs = [
     nix bash binutils coreutils curl gnutar
     git python27 curl electron nodejs-8_x
     nodePackages.node-gyp nodePackages.node-pre-gyp
     gnumake yarn
+    daedalusPkgs.daedalus-bridge
   ];
 }
