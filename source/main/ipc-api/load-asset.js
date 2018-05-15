fbd8ac7762b81e3c97651b77252a486ba95beba6
Dominik Guzei <dominik.guzei@gmail.com>
2018-05-15 15:59:05 +0200

    [DDW-249] use node dirname for loading assets

--- source/main/ipc-api/load-asset.js
+++ source/main/ipc-api/load-asset.js
@@ -7,7 +7,7 @@ import type { LoadAssetRequest } from '../../common/ipc-api/load-asset';
 
 export default () => {
   ipcMain.on(LOAD_ASSET_CHANNEL, ({ sender }, request: LoadAssetRequest) => {
-    const assetPath = path.resolve(`./dist/renderer/${request.fileName}`);
+    const assetPath = path.resolve(__dirname, `../renderer/${request.fileName}`);
     fs.readFile(assetPath, 'base64', (error, data) => {
       if (error) {
         sender.send(LOAD_ASSET_CHANNEL, false, error);
