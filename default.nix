23c407d05c7b77744ff613ca9f6012bc42f08e8e
Samuel Leathers <samuel.leathers@iohk.io>
2018-05-16 01:05:36 -0400

    [DEVOPS-842] hydra CI tests for flow and eslint

--- default.nix
+++ default.nix
@@ -11,8 +11,28 @@ in
 let
   installPath = ".daedalus";
   cardanoPkgs = import ./cardano-sl.nix {
-    inherit system config pkgs;
+    inherit system config;
   };
+  cleanSourceFilter = with pkgs.stdenv;
+    name: type: let baseName = baseNameOf (toString name); in ! (
+      # Filter out .git repo
+      (type == "directory" && baseName == ".git") ||
+      # Filter out editor backup / swap files.
+      lib.hasSuffix "~" baseName ||
+      builtins.match "^\\.sw[a-z]$" baseName != null ||
+      builtins.match "^\\..*\\.sw[a-z]$" baseName != null ||
+
+      # Filter out locally generated/downloaded things.
+      baseName == "dist" ||
+      baseName == "node_modules" ||
+
+      # Filter out the files which I'm editing often.
+      lib.hasSuffix ".nix" baseName ||
+      lib.hasSuffix ".dhall" baseName ||
+      lib.hasSuffix ".hs" baseName ||
+      # Filter out nix-build result symlinks
+      (type == "symlink" && lib.hasPrefix "result" baseName)
+    );
   packages = self: {
     inherit cluster pkgs version;
     inherit (cardanoPkgs) daedalus-bridge;
@@ -20,6 +40,12 @@ let
     daedalus-installer = self.callPackage ./installers/default.nix {};
     daedalus = self.callPackage ./installers/nix/linux.nix {};
     rawapp = self.callPackage ./yarn2nix.nix { api = "ada"; };
+    source = builtins.filterSource cleanSourceFilter ./.;
+
+    tests = {
+      runFlow = self.callPackage ./tests/flow.nix {};
+      runLint = self.callPackage ./tests/lint.nix {};
+    };
     nix-bundle = import (pkgs.fetchFromGitHub {
       owner = "matthewbauer";
       repo = "nix-bundle";
