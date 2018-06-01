782f0550167af3a73476a57189e09b3b8d7e5caa
MarcusHurney <marcushurney@gmail.com>
2018-06-01 16:16:02 +0700

    [DDW-245] Adds themeId prop for Checkbox component within TinySwitch.js

--- source/renderer/app/components/widgets/forms/TinySwitch.js
+++ source/renderer/app/components/widgets/forms/TinySwitch.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Checkbox } from 'react-polymorph/lib/components';
 import { SwitchSkin } from 'react-polymorph/lib/skins/simple';
+import { IDENTIFIERS } from 'react-polymorph/lib/themes/API';
 import styles from './TinySwitch.scss';
 
 type Props = {
@@ -15,6 +16,7 @@ export default class TinySwitch extends Component<Props> {
     return (
       <Checkbox
         className={styles.component}
+        themeId={IDENTIFIERS.SWITCH}
         skin={SwitchSkin}
         checked={this.props.checked}
         onChange={this.props.onChange}
