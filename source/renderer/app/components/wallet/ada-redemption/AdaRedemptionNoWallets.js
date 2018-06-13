838dd6b7f55d458cab2e4c32370be5cad671e9b7
Danilo Prates <daniloprates@gmail.com>
2018-06-13 10:53:39 -0300

    [DDW-289] Correct design and text

--- source/renderer/app/components/wallet/ada-redemption/AdaRedemptionNoWallets.js
+++ source/renderer/app/components/wallet/ada-redemption/AdaRedemptionNoWallets.js
@@ -2,19 +2,26 @@
 import React, { Component } from 'react';
 import { defineMessages, intlShape } from 'react-intl';
 import SVGInline from 'react-svg-inline';
+import Button from 'react-polymorph/lib/components/Button';
+import SimpleButtonSkin from 'react-polymorph/lib/skins/simple/raw/ButtonSkin';
 
 import styles from './AdaRedemptionNoWallets.scss';
-import cross from '../../../assets/images/close-cross.inline.svg';
+import icon from '../../../assets/images/attention-big-thin.inline.svg';
 
 const messages = defineMessages({
-  warning: {
-    id: 'wallet.redeem.noWallets.warning',
-    defaultMessage: '!!!Redeeming Ada is not possible because you don\'t have any wallet.',
-    description: '"No wallets" warning on the Ada Redemption Page.'
+  headLine: {
+    id: 'wallet.redeem.noWallets.headLine',
+    defaultMessage: '!!!Ada redemption is not available because you don\'t have any wallets.',
+    description: '"No wallets" headLine on the Ada Redemption Page.'
+  },
+  instructions: {
+    id: 'wallet.redeem.noWallets.instructions',
+    defaultMessage: '!!!Create a new wallet (or restore an existing one), come back here and choose it for Ada redemption.',
+    description: '"No wallets" instructions on the Ada Redemption Page.'
   },
   createWalletLink: {
     id: 'wallet.redeem.noWallets.createWalletLink',
-    defaultMessage: '!!!Click here to create one',
+    defaultMessage: '!!!Create your first wallet.',
     description: 'URL for the "FAQ on Daedalus website" link in the FAQ section on the support settings page',
   },
 });
@@ -34,17 +41,16 @@ export default class AdaRedemptionNoWallets extends Component<Props> {
 
     return (
       <div className={styles.component}>
-        <SVGInline svg={cross} className={styles.icon} />
-        <div>
-          <p>{intl.formatMessage(messages.warning)}</p>
-          <p>
-            <button
-              onClick={this.props.onGoToCreateWalletClick}
-            >
-              {intl.formatMessage(messages.createWalletLink)}
-            </button>
-          </p>
-        </div>
+        <SVGInline svg={icon} className={styles.icon} />
+        <h1>{intl.formatMessage(messages.headLine)}</h1>
+        <p>{intl.formatMessage(messages.instructions)}</p>
+        <p>
+          <Button
+            onClick={this.props.onGoToCreateWalletClick}
+            label={intl.formatMessage(messages.createWalletLink)}
+            skin={<SimpleButtonSkin />}
+          />
+        </p>
       </div>
     );
   }
