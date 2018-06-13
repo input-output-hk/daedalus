420ec526201bc6bf628561ee84bc7dbd863865ba
MarcusHurney <marcushurney@gmail.com>
2018-06-12 23:54:02 -0700

    [DDW-298] Implements steps and scenarios for node update notification

--- features/node-update-notification.feature
+++ features/node-update-notification.feature
@@ -4,28 +4,21 @@ Feature: Node Update Notification
   Background:
     Given I have completed the basic setup
     And I have the following wallets:
-      | name        |
+      | name   |
       | Test wallet |
-
-  Scenario: User accepts a node update notification
-    Given I am on the "Test wallet" wallet summary screen
-    And when I make a node update available
-    Then I should see the unexpanded node update notification
-    And when I click the node update notification's toggle button
-    Then I should see the expanded node update notification's message
-    Then I should see the expanded node update notification's accept button
-    Then I should see the expanded node update notification's postpone button
-    And when I click the accept button
-    Then I should not see the node update notification anymore
-    And the Daedalus window should close
+    When I am on the "Test wallet" wallet "summary" screen
+    When I make a node update available
+    Then I should see the node update notification component
+    Then I should see the notification's title bar
+    Then I should see the correct version in the notification's title bar
+    Then I should see the notification's toggle button
+    Then I should see the notification's update message
+    Then I should see the notification's accept button
+    Then I should see the notification's postpone button
 
   Scenario: User postpones a node update notification
-    Given I am on the "Test wallet" wallet summary screen
-    And when I make a node update available
-    Then I should see the unexpanded node update notification
-    And when I click the node update notification's toggle button
-    Then I should see the expanded node update notification's message
-    Then I should see the expanded node update notification's accept button
-    Then I should see the expanded node update notification's postpone button
-    And when I click the postpone button
-    Then I should not see the node update notification anymore
+    When I click the notification's postpone button
+    Then I should not see the notification component anymore
+
+  Scenario: User accepts a node update notification
+    When I click the notification's accept button
--- features/step_definitions/node-update-notification-steps.js
+++ features/step_definitions/node-update-notification-steps.js
@@ -1,23 +1,15 @@
-import { Given, When, Then } from 'cucumber';
-import { getWalletByName } from '../support/helpers/wallets-helpers';
-import { navigateTo } from '../support/helpers/route-helpers';
+import { When, Then } from 'cucumber';
+import { expect } from 'chai';
 
 const NODE_UPDATE_COMPONENT = '.NodeUpdateNotification_component';
 const TITLE_BAR = '.NodeUpdateNotification_titleBar';
 const TOGGLE_BUTTON = '.NodeUpdateNotification_toggleButton';
 const UPDATE_MESSAGE = '.NodeUpdateNotification_message';
 const ACTIONS = '.NodeUpdateNotification_actions';
-const ACCEPT_BTN = '.NodeUpdateNotification_acceptButton.SimpleButton_root';
-const DENY_BTN = '.NodeUpdateNotification_denyButton.SimpleButton_root';
+const ACCEPT_BTN = '.NodeUpdateNotification_acceptButton';
+const DENY_BTN = '.NodeUpdateNotification_denyButton';
 
-// BEGIN ACCEPT UPDATE -----------
-
-Given(/^I am on the "([^"]*)" summary screen$/, async function (walletName) {
-  const wallet = getWalletByName.call(this, walletName);
-  await navigateTo.call(this, `/wallets/${wallet.id}/summary`);
-});
-
-When(/^I make a mock node update available$/, async function () {
+When(/^I make a node update available$/, async function () {
   await this.client.executeAsync((nextVersion, done) => {
     daedalus.api.ada.setNextUpdate(nextVersion)
       .then(() => daedalus.stores.NodeUpdateStore.refreshNextUpdate())
@@ -26,78 +18,54 @@ When(/^I make a mock node update available$/, async function () {
   }, { version: 50 });
 });
 
-Then(/^I should see the unexpanded node update notification$/, function () {
-  return this.client.waitForVisible(`${NODE_UPDATE_COMPONENT} ${TITLE_BAR}`);
+Then(/^I should see the node update notification component$/, async function () {
+  await this.client.waitForVisible(`${NODE_UPDATE_COMPONENT}`);
 });
 
-When(/^I click the node update notification's toggle button$/, async function () {
-  const buttonSelector = `${NODE_UPDATE_COMPONENT} ${TITLE_BAR} ${TOGGLE_BUTTON}`;
-  await this.client.waitForVisible(buttonSelector);
-  await this.client.click(buttonSelector);
+Then(/^I should see the notification's title bar$/, async function () {
+  await this.client.waitForVisible(`${NODE_UPDATE_COMPONENT} ${TITLE_BAR}`);
 });
 
-Then(/^I should see the expanded node update notification's message$/, async function () {
-  return this.client.waitForVisible(`${NODE_UPDATE_COMPONENT} ${UPDATE_MESSAGE}`);
+Then(/^I should see the correct version in the notification's title bar$/, async function () {
+  // const message = defineMessages({
+  //   titleWithVersion: {
+  //     id: 'cardano.node.update.notification.titleWithVersion',
+  //     defaultMessage: '!!!Cardano-Core update v{version} is available',
+  //     description: 'Cardano-Core update notification with version.'
+  //   }
+  // });
+  const version = 50;
+  const titleBarSelector = `${NODE_UPDATE_COMPONENT} ${TITLE_BAR}`;
+  await this.client.waitForText(titleBarSelector);
+  const versionText = await this.client.getText(titleBarSelector);
+  const expectedVersionText = `Daedalus and Cardano node update #${version} is available.`;
+  expect(versionText).to.equal(expectedVersionText);
 });
 
-Then(/^I should see the expanded node update notification's accept button$/, async function () {
-  return this.client.waitForVisible(`${NODE_UPDATE_COMPONENT} ${ACTIONS} ${ACCEPT_BTN}`);
+Then(/^I should see the notification's toggle button$/, async function () {
+  await this.client.waitForVisible(`${NODE_UPDATE_COMPONENT} ${TITLE_BAR} ${TOGGLE_BUTTON}`);
 });
 
-Then(/^I should see the expanded node update notification's postpone button$/, async function () {
-  return this.client.waitForVisible(`${NODE_UPDATE_COMPONENT} ${ACTIONS} ${DENY_BTN}`);
+Then(/^I should see the notification's update message$/, async function () {
+  await this.client.waitForVisible(`${NODE_UPDATE_COMPONENT} ${UPDATE_MESSAGE}`);
 });
 
-When(/^I click the accept button$/, async function () {
-  await this.client.click(`${NODE_UPDATE_COMPONENT} ${ACTIONS} ${ACCEPT_BTN}`);
+Then(/^I should see the notification's accept button/, async function () {
+  await this.client.waitForVisible(`${NODE_UPDATE_COMPONENT} ${ACTIONS} ${ACCEPT_BTN}`);
 });
 
-Then(/^I should not see the node update notification anymore$/, function () {
-  return this.client.waitForVisible(NODE_UPDATE_COMPONENT, null, true);
+Then(/^I should see the notification's postpone button$/, async function () {
+  await this.client.waitForVisible(`${NODE_UPDATE_COMPONENT} ${ACTIONS} ${DENY_BTN}`);
 });
 
-// BEGIN POSTPONE UPDATE -----------
-
-Given(/^I am on the "([^"]*)" summary screen$/, async function (walletName) {
-  const wallet = getWalletByName.call(this, walletName);
-  await navigateTo.call(this, `/wallets/${wallet.id}/summary`);
+When(/^I click the notification's postpone button$/, async function () {
+  await this.waitAndClick(`${NODE_UPDATE_COMPONENT} ${ACTIONS} ${DENY_BTN}`);
 });
 
-When(/^I make a mock node update available$/, async function () {
-  await this.client.executeAsync((nextVersion, done) => {
-    daedalus.api.ada.setNextUpdate(nextVersion)
-      .then(() => daedalus.stores.NodeUpdateStore.refreshNextUpdate())
-      .then(done)
-      .catch((error) => done(error));
-  }, { version: 50 });
+When(/^I click the notification's accept button$/, async function () {
+  await this.waitAndClick(`${NODE_UPDATE_COMPONENT} ${ACTIONS} ${ACCEPT_BTN}`);
 });
 
-Then(/^I should see the unexpanded node update notification$/, function () {
-  return this.client.waitForVisible(`${NODE_UPDATE_COMPONENT} ${TITLE_BAR}`);
-});
-
-When(/^I click the node update notification's toggle button$/, async function () {
-  const buttonSelector = `${NODE_UPDATE_COMPONENT} ${TITLE_BAR} ${TOGGLE_BUTTON}`;
-  await this.client.waitForVisible(buttonSelector);
-  await this.client.click(buttonSelector);
-});
-
-Then(/^I should see the expanded node update notification's message$/, async function () {
-  return this.client.waitForVisible(`${NODE_UPDATE_COMPONENT} ${UPDATE_MESSAGE}`);
-});
-
-Then(/^I should see the expanded node update notification's accept button$/, async function () {
-  return this.client.waitForVisible(`${NODE_UPDATE_COMPONENT} ${ACTIONS} ${ACCEPT_BTN}`);
-});
-
-Then(/^I should see the expanded node update notification's postpone button$/, async function () {
-  return this.client.waitForVisible(`${NODE_UPDATE_COMPONENT} ${ACTIONS} ${DENY_BTN}`);
-});
-
-When(/^I click the postpone button$/, async function () {
-  await this.client.click(`${NODE_UPDATE_COMPONENT} ${ACTIONS} ${DENY_BTN}`);
-});
-
-Then(/^I should not see the node update notification anymore$/, function () {
-  return this.client.waitForVisible(NODE_UPDATE_COMPONENT, null, true);
+Then(/^I should not see the notification component anymore$/, async function () {
+  await this.client.waitForVisible(NODE_UPDATE_COMPONENT, null, true);
 });
