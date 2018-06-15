0022f163cb9f901378cca6d482f132f91ee2294f
Danilo Prates <daniloprates@gmail.com>
2018-06-15 11:04:57 -0300

    [DDW-302] Re-enabling deletion

--- features/step_definitions/wallets-limit.js
+++ features/step_definitions/wallets-limit.js
@@ -6,7 +6,7 @@ import { MAX_ADA_WALLETS_COUNT } from '../../source/renderer/app/config/numbersC
 
 Given('I create wallets until I reach the maximum number permitted', async function () {
   const wallets = [...Array(MAX_ADA_WALLETS_COUNT)].map((x, i) => ({
-    name: `Wallet ${i}`,
+    name: `Wallet ${i + 1}`,
     password: ''
   }));
   await createWallets(createRawTable(wallets), this);
@@ -17,14 +17,18 @@ When('I should see maximum number of wallets in the wallets list', async functio
   expect(wallets.value.length).to.equal(MAX_ADA_WALLETS_COUNT);
 });
 
-Then('The buttons in the Add Wallet screen should be disabled', async function () {
+Then(/^The buttons in the Add Wallet screen should be (disabled|enabled)/, async function (state) {
+  const isDisabled = state === 'disabled' ? 'true' : null;
   const buttonsAreDisabled = await this.client
     .getAttribute('.WalletAdd_buttonsContainer .BigButtonForDialogs_component', 'disabled');
 
+  // Excludes the Join shared wallet button
+  buttonsAreDisabled.splice(1, 1);
+
   expect(buttonsAreDisabled)
     .to.be.an('array')
-    .that.include('true')
-    .and.not.include('false');
+    .that.include(isDisabled)
+    .and.not.include(!isDisabled);
 
 });
 
@@ -32,4 +36,3 @@ Then('I should see a disclaimer saying I have reached the maximum number of wall
   const disclaimer = await this.client.getText('.WalletAdd_notification');
   expect(disclaimer.replace(/\n/, ' ')).to.equal(`You have reached the maximum of ${MAX_ADA_WALLETS_COUNT} wallets. No more wallets can be added.`);
 });
-
--- features/wallets-limit.feature
+++ features/wallets-limit.feature
@@ -10,10 +10,12 @@ Feature: Wallet Settings
     And The buttons in the Add Wallet screen should be disabled
     And I should see a disclaimer saying I have reached the maximum number of wallets
 
-  # Scenario: User deletes one wallet and re-enable its Adding new wallets
-  #   Given I delete the wallet "wallet 20"
-  #   And I go to the Add wallet screen
-  #   Then The buttons should be disabled
-  #   And I should see a text saying I have reached the maximum number of wallets
-
-
+  Scenario: User deletes one wallet and re-enable its Adding new wallets
+    Given I am on the "Wallet 20" wallet "settings" screen
+    When I click on delete wallet button
+    And I see delete wallet dialog
+    And I click on the "Make sure you have access to backup before continuing" checkbox
+    And I enter "Wallet 20" as name of the wallet to confirm
+    And I submit the delete wallet dialog
+    And I click on the add wallet button in the sidebar
+    Then The buttons in the Add Wallet screen should be enabled
