13d208982c2ed9efbb37545ebb5053d224a19737
MarcusHurney <marcushurney@gmail.com>
2018-06-20 18:47:56 -0700

    [DDW-298] Changes app.quit back to process.exit in kill-process callback

--- source/main/ipc-api/kill-process.js
+++ source/main/ipc-api/kill-process.js
@@ -1,10 +1,10 @@
 // @flow
-import { ipcMain, app } from 'electron';
+import { ipcMain } from 'electron';
 
 export default () => {
   ipcMain.on('kill-process', () => {
-    app.quit();
     // if (event.sender !== mainWindow.webContents) return;
     // TODO: fix this (are we ensuring that this message is only allowed from mainWindow?)
+    process.exit(20);
   });
 };
