1a3b024f67bbb28214661e144f043595fa3ecfcd
nikolaglumac <niglumac@gmail.com>
2018-07-03 21:04:41 +0200

    [DDW-345] Code style improvements

--- source/common/environment.js
+++ source/common/environment.js
@@ -12,7 +12,7 @@ if (module && module.require) {
 const osNames = {
   darwin: 'macOS',
   win32: 'Windows',
-  linux: 'Linux'
+  linux: 'Linux',
 };
 
 const platform = os.platform();
@@ -27,7 +27,7 @@ const environment = Object.assign({
   API: process.env.API || 'ada',
   API_VERSION,
   MOBX_DEV_TOOLS: process.env.MOBX_DEV_TOOLS,
-  current: process.env.NODE_ENV || '',
+  current: process.env.NODE_ENV || 'development',
   REPORT_URL: process.env.REPORT_URL || 'http://report-server.awstest.iohkdev.io:8080/',
   isDev: () => environment.current === environment.DEVELOPMENT,
   isTest: () => environment.current === environment.TEST,
@@ -37,9 +37,13 @@ const environment = Object.assign({
   isEtcApi: () => environment.API === 'etc',
   build,
   buildNumber: uniq([API_VERSION, build]).join('.'),
-  getBuildLabel: (addCurrentEvironment?: boolean) => `Daedalus (${environment.version}#${environment.buildNumber}) ${addCurrentEvironment ? environment.current : ''}`,
+  getBuildLabel: () => {
+    let buildLabel = `Daedalus (${environment.version}#${environment.buildNumber})`;
+    if (!environment.isProduction()) buildLabel += ` ${environment.current}`;
+    return buildLabel;
+  },
   platform,
-  os: osNames[platform] || '',
+  os: osNames[platform] || platform,
   version,
 }, remote ? remote.getGlobal('env') : process.env);
 
