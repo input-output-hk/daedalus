2aa3d0f5d7859447adb87d2d0511401fd68ae0f9
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-07-09 11:40:08 +1000

    nix: Pass system and config arguments to cardano-sl

--- cardano-sl.nix
+++ cardano-sl.nix
@@ -12,6 +12,7 @@ let
   # we allow on purpose for cardano-sl to have it's own nixpkgs to avoid rebuilds
   cardano-sl-src = builtins.fromJSON (builtins.readFile ./cardano-sl-src.json);
   cardano-sl-pkgs = import (pkgs.fetchgit cardano-sl-src) {
+    inherit system config;
     gitrev = cardano-sl-src.rev;
   };
 
