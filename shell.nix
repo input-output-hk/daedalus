77514c636d17cb6e60467ea20605530dc1c0bed9
Samuel Leathers <samuel.leathers@iohk.io>
2018-07-30 15:52:57 -0400

    [DDW-342] Update shell.nix to have fixYarnLock attribute

--- shell.nix
+++ shell.nix
@@ -6,15 +6,35 @@ in
 , pkgs ? (import (localLib.fetchNixPkgs) { inherit system config; })
 }:
 
-with pkgs;
+let
+  yarn = pkgs.yarn.override { inherit nodejs; };
+  nodejs = pkgs.nodejs-8_x;
+  daedalusShell = pkgs.stdenv.mkDerivation {
+    name = "daedalus";
 
-stdenv.mkDerivation {
-  name = "daedalus";
-
-  buildInputs = [
-    nix bash binutils coreutils curl gnutar
-    git python27 curl electron nodejs-8_x
-    nodePackages.node-gyp nodePackages.node-pre-gyp
-    gnumake yarn
-  ];
-}
+    buildInputs = [ nodejs yarn ] ++ (with pkgs; [
+      nix bash binutils coreutils curl gnutar
+      git python27 curl electron
+      nodePackages.node-gyp nodePackages.node-pre-gyp
+      gnumake
+    ]);
+  };
+  fixYarnLock = pkgs.stdenv.mkDerivation {
+    name = "fix-yarn-lock";
+    buildInputs = [ nodejs yarn pkgs.git ];
+    shellHook = ''
+      git diff > pre-yarn.diff
+      yarn
+      git diff > post-yarn.diff
+      diff pre-yarn.diff post-yarn.diff > /dev/null
+      if [ $? != 0 ]
+      then
+        echo "Changes by yarn have been made. Please commit them."
+      else
+        echo "No changes were made."
+      fi
+      rm pre-yarn.diff post-yarn.diff
+      exit
+    '';
+  };
+in daedalusShell // { inherit fixYarnLock; }
