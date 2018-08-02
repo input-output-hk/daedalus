1574f660a0e7e8d3ebdf657e26d18295b8923d2e
MarcusHurney <marcushurney@gmail.com>
2018-08-01 22:44:12 -0700

    [DDW-356] Adds v1 request helper to getAdaWalletAccounts.js

--- source/renderer/app/api/ada/getAdaWalletAccounts.js
+++ source/renderer/app/api/ada/getAdaWalletAccounts.js
@@ -1,6 +1,6 @@
 // @flow
 import type { AdaAccounts } from './types';
-import { request } from './lib/request';
+import { request } from './lib/v1/request';
 
 export type GetAdaWalletAccountsParams = {
   ca: string,
