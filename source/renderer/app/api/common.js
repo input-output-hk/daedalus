57627f92353d33b8a352dc1710f5bc0a92be2f91
MarcusHurney <marcushurney@gmail.com>
2018-08-07 18:43:04 -0700

    [DDW-358] Sets oldPassword as optional param of UpdateWalletPasswordRequest type

--- source/renderer/app/api/common.js
+++ source/renderer/app/api/common.js
@@ -89,7 +89,7 @@ export type CreateWalletRequest = {
 
 export type UpdateWalletPasswordRequest = {
   walletId: string,
-  oldPassword: ?string,
+  oldPassword?: string,
   newPassword: ?string,
 };
 
