f08992ba7a08d3acf0c0e60e44fd2bc183fb15f8
MarcusHurney <marcushurney@gmail.com>
2018-08-16 18:08:09 -0700

    [DDW-385] Adds urls for staging and testnet

--- source/renderer/app/config/urlsConfig.js
+++ source/renderer/app/config/urlsConfig.js
@@ -1,1 +1,3 @@
-export const ADA_EXPLORER_URL = 'https://cardanoexplorer.com';
+export const MAIN_NET_EXPLORER_URL = 'https://cardanoexplorer.com';
+export const STAGING_EXPLORER_URL = 'http://cardano-explorer.awstest.iohkdev.io';
+export const TEST_NET_EXPLORER_URL = 'http://cardano-explorer.cardano-testnet.iohkdev.io';
