f0e0b9d8c59bfe0c90bde4ea32d2abbfb561640f
darko-mijic <c14836616b554fbbceb8110ee036b75d5d1a9e9e>
2018-08-17 14:02:46 +0200

    [DDW-346] Add testnet label to paper wallet certificate

--- source/renderer/app/utils/paperWalletPdfGenerator.js
+++ source/renderer/app/utils/paperWalletPdfGenerator.js
@@ -5,7 +5,9 @@ import { defineMessages } from 'react-intl';
 import fs from 'fs';
 import paperWalletFontPath from '../assets/pdf/paper-wallet-certificate-font.ttf';
 import paperWalletPage1Path from '../assets/pdf/paper-wallet-certificate-page-1.png';
+import paperWalletPage1PathTestnet from '../assets/pdf/paper-wallet-certificate-page-1-testnet.png';
 import paperWalletPage2Path from '../assets/pdf/paper-wallet-certificate-page-2.png';
+import paperWalletPage2PathTestnet from '../assets/pdf/paper-wallet-certificate-page-2-testnet.png';
 import paperWalletCertificateBgPath from '../assets/pdf/paper-wallet-certificate-background.png';
 import environment from '../../../common/environment';
 import { loadAssetChannel } from '../ipc/loadAsset';
@@ -48,7 +50,7 @@ type DownloadPaperWalletCertificateParams = {
 export const downloadPaperWalletCertificate = async (
   { address, mnemonics, intl, filePath }: DownloadPaperWalletCertificateParams
 ) => {
-  const { version, build } = environment;
+  const { version, build, isMainnet } = environment;
   const daedalusInfo =
     `Daedalus ${version}#${build}`;
   const qrCodeImage = qr.imageSync(address, { type: 'png', size: 10, ec_level: 'L', margin: 0 });
@@ -97,7 +99,9 @@ export const downloadPaperWalletCertificate = async (
   doc.image(backgroundUri, 0, 0, { fit: [width, height] });
 
   // first page
-  const page1Uri = await loadImageUriFromPath(paperWalletPage1Path);
+  const page1Uri = await loadImageUriFromPath(
+    isMainnet() ? paperWalletPage1Path : paperWalletPage1PathTestnet
+  );
   doc.image(page1Uri, 0, 0, { fit: [width, height] });
   doc.rotate(180, { origin: [width / 2, height / 2] });
   doc.fillColor(textColor);
@@ -110,7 +114,9 @@ export const downloadPaperWalletCertificate = async (
 
   // second page
   doc.addPage();
-  const page2Uri = await loadImageUriFromPath(paperWalletPage2Path);
+  const page2Uri = await loadImageUriFromPath(
+    isMainnet() ? paperWalletPage2Path : paperWalletPage2PathTestnet
+  );
   doc.image(page2Uri, 0, 0, { fit: [width, height] });
   doc.rotate(180, { origin: [width / 2, height / 2] });
   doc.fillColor(textColor);
