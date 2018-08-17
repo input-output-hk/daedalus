12d7feaaad178d6928673165c826c690457f1761
MarcusHurney <marcushurney@gmail.com>
2018-08-16 18:10:30 -0700

    [DDW-385] Adds getNetworkExplorerUrl utility function

---
+++ source/renderer/app/utils/ada/network.js
@@ -0,0 +1,17 @@
+// @flow
+import {
+  MAIN_NET_EXPLORER_URL,
+  STAGING_EXPLORER_URL,
+  TEST_NET_EXPLORER_URL
+} from '../../config/urlsConfig';
+import environment from '../../../../common/environment';
+
+const { isMainnet, isStaging, isTestnet } = environment;
+
+export const getNetworkExplorerUrl = () => {
+  let explorerUrl;
+  if (isMainnet()) { explorerUrl = MAIN_NET_EXPLORER_URL; }
+  if (isStaging()) { explorerUrl = STAGING_EXPLORER_URL; }
+  if (isTestnet()) { explorerUrl = TEST_NET_EXPLORER_URL; }
+  return explorerUrl;
+};
