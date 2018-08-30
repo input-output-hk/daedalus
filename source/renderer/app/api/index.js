2ef67f77e847d1300435bfe77c7cea434c3a1069
Dominik Guzei <dominik.guzei@gmail.com>
2018-08-30 17:45:04 +0200

    [DDW-405] fixed tls-auth param names

--- source/renderer/app/api/index.js
+++ source/renderer/app/api/index.js
@@ -15,8 +15,8 @@ export const setupApi = (): Api => ({
   ada: new AdaApi(environment.isTest(), {
     port: environment.WALLET_PORT,
     ca: remote.getGlobal('ca'),
-    clientKey: remote.getGlobal('clientKey'),
-    clientCert: remote.getGlobal('clientCert'),
+    key: remote.getGlobal('clientKey'),
+    cert: remote.getGlobal('clientCert'),
   }),
   etc: new EtcApi(),
   localStorage: new LocalStorageApi(),
