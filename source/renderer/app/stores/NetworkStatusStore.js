243e46161598f7c710d3d47405e06057db8df882
nikolaglumac <niglumac@gmail.com>
2018-08-31 10:51:46 +0200

    [DDW-321] Improve hasBlockSyncingStart logic

--- source/renderer/app/stores/NetworkStatusStore.js
+++ source/renderer/app/stores/NetworkStatusStore.js
@@ -93,7 +93,7 @@ export default class NetworkStatusStore extends Store {
   }
 
   @computed get hasBlockSyncingStarted(): boolean {
-    return this.syncProgress >= 1;
+    return this.syncProgress > 0;
   }
 
   @computed get relativeSyncBlocksDifference(): number {
