874eac4b9a132d540391d6410e7cfd3c7ee0ea61
MarcusHurney <marcushurney@gmail.com>
2018-09-12 02:03:19 -0700

    [DDW-321] Moves common and wallet errors into their respective files

---
+++ source/renderer/app/api/common/errors.js
@@ -0,0 +1,57 @@
+import { defineMessages } from 'react-intl';
+import LocalizableError from '../../i18n/LocalizableError';
+import globalMessages from '../../i18n/global-messages';
+
+const messages = defineMessages({
+  genericApiError: {
+    id: 'api.errors.GenericApiError',
+    defaultMessage: '!!!An error occurred, please try again later.',
+    description: 'Generic error message.'
+  },
+  incorrectWalletPasswordError: {
+    id: 'api.errors.IncorrectPasswordError',
+    defaultMessage: '!!!Incorrect wallet password.',
+    description: '"Incorrect wallet password." error message.'
+  },
+  reportRequestError: {
+    id: 'api.errors.ReportRequestError',
+    defaultMessage: '!!!There was a problem sending the support request.',
+    description: '"There was a problem sending the support request." error message'
+  },
+});
+
+export class GenericApiError extends LocalizableError {
+  constructor() {
+    super({
+      id: messages.genericApiError.id,
+      defaultMessage: messages.genericApiError.defaultMessage,
+    });
+  }
+}
+
+export class IncorrectWalletPasswordError extends LocalizableError {
+  constructor() {
+    super({
+      id: messages.incorrectWalletPasswordError.id,
+      defaultMessage: messages.incorrectWalletPasswordError.defaultMessage,
+    });
+  }
+}
+
+export class ReportRequestError extends LocalizableError {
+  constructor() {
+    super({
+      id: messages.reportRequestError.id,
+      defaultMessage: messages.reportRequestError.defaultMessage,
+    });
+  }
+}
+
+export class InvalidMnemonicError extends LocalizableError {
+  constructor() {
+    super({
+      id: globalMessages.invalidMnemonic.id,
+      defaultMessage: globalMessages.invalidMnemonic.defaultMessage,
+    });
+  }
+}
---
+++ source/renderer/app/api/wallets/errors.js
@@ -0,0 +1,19 @@
+import { defineMessages } from 'react-intl';
+import LocalizableError from '../../i18n/LocalizableError';
+
+const messages = defineMessages({
+  walletAlreadyRestoredError: {
+    id: 'api.errors.WalletAlreadyRestoredError',
+    defaultMessage: '!!!Wallet you are trying to restore already exists.',
+    description: '"Wallet you are trying to restore already exists." error message.'
+  },
+});
+
+export class WalletAlreadyRestoredError extends LocalizableError {
+  constructor() {
+    super({
+      id: messages.walletAlreadyRestoredError.id,
+      defaultMessage: messages.walletAlreadyRestoredError.defaultMessage,
+    });
+  }
+}
