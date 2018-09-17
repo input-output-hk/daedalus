fccf0537ddb96522ba4e8355c0acf6bfbaa0a1f5
Danilo Prates <daniloprates@gmail.com>
2018-09-17 11:00:40 -0300

    [DDW-413] Allowed files logic

--- source/main/config.js
+++ source/main/config.js
@@ -6,3 +6,10 @@ export const APP_NAME = 'Daedalus';
 export const runtimeFolderPath = getRuntimeFolderPath(process.platform, process.env, APP_NAME);
 export const appLogsFolderPath = launcherConfig.logsPrefix || path.join(runtimeFolderPath, 'Logs');
 export const pubLogsFolderPath = path.join(appLogsFolderPath, 'pub');
+
+export const ALLOWED_LOGS = [
+  'Daedalus.log',
+  'launcher',
+];
+export const ALLOWED_NODE_LOGS = new RegExp(/(node.json-)([0-9]{14})/);
+export const MAX_NODE_LOGS_ALLOWED = 3;
