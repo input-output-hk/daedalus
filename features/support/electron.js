461b52b9eb93ad554a74c1a3559ce417ae1f8858
Danilo Prates <daniloprates@gmail.com>
2018-09-24 09:51:16 -0300

    [DDW-417] Removes unecessary export

--- features/support/electron.js
+++ features/support/electron.js
@@ -17,7 +17,7 @@ const printMainProcessLogs = () => (
     })
 );
 
-export const startApp = async () => {
+const startApp = async () => {
   const app = new Application({
     path: electronPath,
     args: ['./dist/main/index.js'],
