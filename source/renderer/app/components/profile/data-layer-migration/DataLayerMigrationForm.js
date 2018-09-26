7343c6bf2b7b226e05762575dfb1655e0a3e9e89
Danilo Prates <daniloprates@gmail.com>
2018-09-26 15:39:51 -0300

    [DDW-417] Final copy 2

--- source/renderer/app/components/profile/data-layer-migration/DataLayerMigrationForm.js
+++ source/renderer/app/components/profile/data-layer-migration/DataLayerMigrationForm.js
@@ -3,34 +3,34 @@ import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 import { Button } from 'react-polymorph/lib/components/Button';
 import { ButtonSkin } from 'react-polymorph/lib/skins/simple/ButtonSkin';
-import { defineMessages, intlShape } from 'react-intl';
+import { defineMessages, intlShape, FormattedHTMLMessage } from 'react-intl';
 import LocalizableError from '../../../i18n/LocalizableError';
 import styles from './DataLayerMigrationForm.scss';
 
 const messages = defineMessages({
   title: {
     id: 'profile.dataLayerMigration.title',
-    defaultMessage: '!!!New data layer migration',
+    defaultMessage: '!!!Wallet data migration',
     description: 'Title for the Data Layer Migration screen.'
   },
   content1: {
     id: 'profile.dataLayerMigration.content1',
-    defaultMessage: '!!!You have installed a version of Daedalus which changes how wallet data is stored and managed. Because of this all of your wallets need to be restored and synchronized with the complete history of Cardano blockchain.',
+    defaultMessage: '!!!You have installed a version of Daedalus that changes how wallet data is stored and managed. Because of this, all of your wallets need to be restored and synchronized with the complete history of the Cardano blockchain.',
     description: 'Content for the Data Layer Migration screen.'
   },
   content2: {
     id: 'profile.dataLayerMigration.content2',
-    defaultMessage: '!!!This is an automatic process and does not require any actions on your behalf.',
+    defaultMessage: '!!!This is an automatic process and does not require any action on your behalf.',
     description: 'Content for the Data Layer Migration screen.'
   },
   content3: {
     id: 'profile.dataLayerMigration.content3',
-    defaultMessage: '!!!Your transaction history and used addresses will be appearing in your wallets as they are recovered during the restoration process. Addresses which were not used will not be recovered since there are not recorded on the blockchain. If funds are sent to those addresses you will receive them and those addresses will then appear in your wallet.',
+    defaultMessage: '!!!Your transaction history and used addresses will appear in your wallets as they are recovered during the restoration process. Addresses that were not used will not be recovered because they are not recorded on the blockchain. If funds were sent to those addresses you will receive the funds and those addresses will appear in your wallet.',
     description: 'Content for the Data Layer Migration screen.'
   },
   submitLabel: {
     id: 'profile.dataLayerMigration.submitLabel',
-    defaultMessage: '!!!Migrate Wallets',
+    defaultMessage: '!!!Start migration',
     description: 'Submit label for the Data Layer Migration screen.'
   },
 });
@@ -61,7 +61,10 @@ export default class DataLayerMigrationForm extends Component<Props> {
         <h1 className={styles.title}>{intl.formatMessage(messages.title)}</h1>
 
         <p className={styles.content1}>
-          {intl.formatMessage(messages.content1)}
+          {/* intl.formatMessage(messages.content1) */}
+          <FormattedHTMLMessage
+            {...messages.content1}
+          />
         </p>
         <p className={styles.content2}>
           {intl.formatMessage(messages.content2)}
