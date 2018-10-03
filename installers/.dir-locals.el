c733ac9d1b68a56f9f7ff6c2ddbb01536273f5b3
Kosyrev Serge <serge.kosyrev@iohk.io>
2018-10-03 19:57:46 +0300

    .dir-locals.el:  update

--- installers/.dir-locals.el
+++ installers/.dir-locals.el
@@ -1,2 +1,6 @@
-((nil . ((dante-target . "exe:make-installer")
-         )))
+((nil
+  .(
+    (dante-repl-command-line "cabal" "repl" "lib:daedalus-installer")
+    (haskell-interactive-tgt              . "lib:daedalus-installer")
+    (dante-target                         . "lib:daedalus-installer")
+    )))
