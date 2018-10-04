90f2bf1b3fd669c4e136593ace8dbefc5255f760
Danilo Prates <daniloprates@gmail.com>
2018-10-03 21:01:43 -0300

    [DDW-423] Adjustments

--- source/common/environment.js
+++ source/common/environment.js
@@ -24,7 +24,6 @@ const environment = Object.assign({
   TEST: 'test',
   PRODUCTION: 'production',
   NETWORK: process.env.NETWORK || 'development',
-  API: 'ada',
   API_VERSION,
   MOBX_DEV_TOOLS: process.env.MOBX_DEV_TOOLS,
   current: process.env.NODE_ENV || 'development',
