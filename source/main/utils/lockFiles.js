b27a76e710a0f95a4fa2f6bee67ed87f78d6858c
Dominik Guzei <dominik.guzei@gmail.com>
2018-10-11 22:12:13 +0200

    [DDW-436] handle common failure cases in lockfile logic

--- source/main/utils/lockFiles.js
+++ source/main/utils/lockFiles.js
@@ -1,8 +1,9 @@
 // @flow
+import fs from 'fs';
 import path from 'path';
 import { lockSync, unlockSync } from 'lockfile';
 import { launcherConfig } from '../config';
-import { getProcessName } from './processes';
+import { getProcessName, getProcessesByName } from './processes';
 
 const getLockFilePath = async (): Promise<string> => {
   const processName = await getProcessName(process.pid);
@@ -10,12 +11,20 @@ const getLockFilePath = async (): Promise<string> => {
 };
 
 export const acquireDaedalusInstanceLock = async () => {
+  const lockFilePath = await getLockFilePath();
   try {
-    lockSync(await getLockFilePath());
+    lockSync(lockFilePath);
   } catch (error) {
-    console.log('Error while trying to acquire lock for Daedalus instance:');
     if (error.code === 'EEXIST') {
-      throw new Error('Another Daedalus instance is already running.');
+      console.log('Lockfile already exists. Checking for other Daedalus instance.');
+      const processName = await getProcessName(process.pid);
+      const processesWithSameName = await getProcessesByName(processName);
+      if (processesWithSameName.length > 1) {
+        throw new Error('Another Daedalus instance is already running.');
+      }
+      console.log('No other Daedalus instance running. Recreating the lockfile.');
+      fs.unlinkSync(lockFilePath);
+      await acquireDaedalusInstanceLock();
     } else {
       throw error;
     }
--- source/main/utils/processes.js
+++ source/main/utils/processes.js
@@ -3,12 +3,27 @@ import psList from 'ps-list';
 export type Process = {
   pid: number,
   name: string,
+  cmd: string,
+  ppid?: number,
+  cpu: number,
+  memore: number,
 };
 
-export const getProcessName = async (processId: number): Promise<string> => {
+export const getProcessById = async (processId: number): Promise<Process> => {
   // retrieves all running processes
   const processes: Array<Process> = await psList();
   // filters running processes against previous PID
   const matches: Array<Process> = processes.filter(({ pid }) => processId === pid);
-  return matches.length > 0 ? matches[0].name : Promise.reject();
+  return matches.length > 0 ? matches[0] : Promise.reject();
+};
+
+export const getProcessName = async (processId: number) => (
+  (await getProcessById(processId)).name
+);
+
+export const getProcessesByName = async (processName: string): Promise<Array<Process>> => {
+  // retrieves all running processes
+  const processes: Array<Process> = await psList();
+  // filters running processes against previous PID
+  return processes.filter(({ name }) => processName === name);
 };
