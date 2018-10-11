6444ede474e03e6c252f99661d8a450bed1eeb0e
MarcusHurney <marcushurney@gmail.com>
2018-10-11 02:53:35 -0700

    [DDW-262] Corrects logic in getNetworkExplorerUrl utility function

--- source/renderer/app/utils/network.js
+++ source/renderer/app/utils/network.js
@@ -11,8 +11,8 @@ export const getNetworkExplorerUrl = (network: string) => {
   const isTestnet = (network === 'testnet');
 
   let explorerUrl = MAINNET_EXPLORER_URL;
-  if (isMainnet()) { explorerUrl = MAINNET_EXPLORER_URL; }
-  if (isStaging()) { explorerUrl = STAGING_EXPLORER_URL; }
-  if (isTestnet()) { explorerUrl = TESTNET_EXPLORER_URL; }
+  if (isMainnet) { explorerUrl = MAINNET_EXPLORER_URL; }
+  if (isStaging) { explorerUrl = STAGING_EXPLORER_URL; }
+  if (isTestnet) { explorerUrl = TESTNET_EXPLORER_URL; }
   return explorerUrl; // sets default to mainnet incase env.NETWORK is undefined
 };
