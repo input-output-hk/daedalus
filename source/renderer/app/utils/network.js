ad410c34344fdc0bdddedb3746a987131f91504d
MarcusHurney <marcushurney@gmail.com>
2018-10-12 01:00:48 -0700

    [DDW-262] Uses imported environment constants in getNetworkExplorerUrl

--- source/renderer/app/utils/network.js
+++ source/renderer/app/utils/network.js
@@ -4,15 +4,12 @@ import {
   STAGING_EXPLORER_URL,
   TESTNET_EXPLORER_URL
 } from '../config/urlsConfig';
+import { MAINNET, STAGING, TESTNET } from '../../../common/environment';
 
-export const getNetworkExplorerUrl = (network: string) => {
-  const isMainnet = (network === 'mainnet');
-  const isStaging = (network === 'staging');
-  const isTestnet = (network === 'testnet');
-
+export const getNetworkExplorerUrl = (network: string): string => {
   let explorerUrl = MAINNET_EXPLORER_URL;
-  if (isMainnet) { explorerUrl = MAINNET_EXPLORER_URL; }
-  if (isStaging) { explorerUrl = STAGING_EXPLORER_URL; }
-  if (isTestnet) { explorerUrl = TESTNET_EXPLORER_URL; }
+  if (network === MAINNET) { explorerUrl = MAINNET_EXPLORER_URL; }
+  if (network === STAGING) { explorerUrl = STAGING_EXPLORER_URL; }
+  if (network === TESTNET) { explorerUrl = TESTNET_EXPLORER_URL; }
   return explorerUrl; // sets default to mainnet incase env.NETWORK is undefined
 };
