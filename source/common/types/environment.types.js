5da578833f84bebbf14b92f27f526a5e5ec68ce6
MarcusHurney <marcushurney@gmail.com>
2018-10-11 19:07:40 -0700

    [DDW-262] Adds Environment flow type definition

---
+++ source/common/types/environment.types.js
@@ -0,0 +1,27 @@
+// @flow
+export type Environment = {
+  DEVELOPMENT: string,
+  TEST: string,
+  PRODUCTION: string,
+  NETWORK: string,
+  API_VERSION: string,
+  MOBX_DEV_TOOLS: ?string,
+  current: string,
+  REPORT_URL: string,
+  isDev: boolean,
+  isTest: boolean,
+  isProduction: boolean,
+  isMainnet: boolean,
+  isStaging: boolean,
+  isTestnet: boolean,
+  build: string,
+  buildNumber: string,
+  buildLabel: string,
+  platform: string,
+  os: string,
+  installerVersion: string,
+  version: string,
+  isWindows: boolean,
+  isMacOS: boolean,
+  isLinux: boolean
+};
