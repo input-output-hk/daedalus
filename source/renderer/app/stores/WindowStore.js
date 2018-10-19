aaa528897d3936cf68f4630fa830b9605bc79630
Kosyrev Serge <serge.kosyrev@iohk.io>
2018-10-19 17:17:14 +0300

    [DEVOPS-1086]  provide an action to trigger application shutdown from the renderer CLI

--- source/renderer/app/stores/WindowStore.js
+++ source/renderer/app/stores/WindowStore.js
@@ -7,12 +7,16 @@ export default class WindowStore extends Store {
 
   setup() {
     this.actions.window.resizeWindow.listen(this._resizeWindow);
+    this.actions.window.quitWindow.listen(this._quitWindow);
   }
 
   // PRIVATE
 
   _resizeWindow = ({ width, height }: { width: number, height: number }) => {
     ipcRenderer.send('resize-window', { width, height, animate: !environment.isTest() });
+  };
+  _quitWindow = ({ }: { }) => {
+    ipcRenderer.send('quit-window', { });
   };
 
 }
