e8eb9d9f4f1c37fdd31979d878b3f944dfdcbadd
nikolaglumac <niglumac@gmail.com>
2018-10-24 14:20:42 +0200

    [DDW-395] Improve Support window styling and positioning

--- source/main/windows/support.js
+++ source/main/windows/support.js
@@ -7,11 +7,29 @@ import RendererErrorHandler from '../utils/rendererErrorHandler';
 const rendererErrorHandler = new RendererErrorHandler();
 
 export const createSupportWindow = (unsetSupportWindow) => {
+  const mainWindow = BrowserWindow.getFocusedWindow();
+  const mainWindowPosition = mainWindow.getPosition();
+  const mainWindowX = mainWindowPosition[0];
+  const mainWindowY = mainWindowPosition[1];
+  const mainWindowSize = mainWindow.getSize();
+  const mainWindowWidth = mainWindowSize[0];
+  const mainWindowHeight = mainWindowSize[1];
+
+  const supportWindowWidth = 375;
+  const supportWindowHeight = 598;
+  const supportWindowX = mainWindowX + mainWindowWidth - supportWindowWidth - 10;
+  const supportWindowY = mainWindowY + mainWindowHeight - supportWindowHeight - 10;
+
   const windowOptions = {
     show: false,
-    width: 375,
-    height: 598,
+    frame: false,
+    width: supportWindowWidth,
+    height: supportWindowHeight,
+    x: supportWindowX,
+    y: supportWindowY,
+    parent: mainWindow,
     resizable: false,
+    transparent: true,
     webPreferences: {
       webviewTag: false,
     }
@@ -26,7 +44,6 @@ export const createSupportWindow = (unsetSupportWindow) => {
 
   rendererErrorHandler.setup(window, createSupportWindow);
 
-
   if (environment.isDev()) {
     window.webContents.openDevTools();
     // Focus the main window after dev tools opened
@@ -40,9 +57,6 @@ export const createSupportWindow = (unsetSupportWindow) => {
 
   window.loadURL(`file://${__dirname}/../renderer/support.html`);
   window.on('page-title-updated', event => { event.preventDefault(); });
-
-  const title = 'Support';
-  window.setTitle(title);
 
   window.webContents.on('did-finish-load', () => {
     if (environment.isTest()) {
