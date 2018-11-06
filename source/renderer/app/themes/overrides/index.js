46d38bece17220bd5e8e713e7a642a1ab9f46b3a
MarcusHurney <marcushurney@gmail.com>
2018-11-05 17:37:07 -0800

    [DDW-376] Creates overrides directory for react-polymorph components. Adds Buttonoverrides.

---
+++ source/renderer/app/themes/overrides/index.js
@@ -0,0 +1,8 @@
+import { IDENTIFIERS } from 'react-polymorph/lib/themes/API';
+import ButtonOverrides from './SimpleButton.scss';
+
+const { BUTTON } = IDENTIFIERS;
+
+export const themeOverrides = {
+  [BUTTON]: ButtonOverrides
+};
