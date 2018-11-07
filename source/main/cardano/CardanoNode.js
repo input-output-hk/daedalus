3c4577577674675a8b7f3569ea1d0397c3398744
Dominik Guzei <dominik.guzei@gmail.com>
2018-11-07 14:53:44 +0100

    [DDW-462] use taskkill to kill processes with pid on windows

--- source/main/cardano/CardanoNode.js
+++ source/main/cardano/CardanoNode.js
@@ -1,6 +1,6 @@
 // @flow
 import Store from 'electron-store';
-import type { ChildProcess, spawn } from 'child_process';
+import type { ChildProcess, spawn, exec } from 'child_process';
 import type { WriteStream } from 'fs';
 import { toInteger } from 'lodash';
 import environment from '../../common/environment';
@@ -527,13 +527,17 @@ export class CardanoNode {
   _killProcessWithName = async (pid: number, name: string): Promise<void> => {
     const { _config } = this;
     try {
-      process.kill(pid);
+      if (!environment.isWindows()) {
+        process.kill(pid);
+      } else {
+        exec(`taskkill /PID ${pid} /T /F`);
+      }
       await promisedCondition(() => !this._isProcessRunning(pid, name), _config.killTimeout);
       this._log.info(`CardanoNode: successfuly killed ${name} process (PID: ${pid})`);
       return Promise.resolve();
     } catch (error) {
       this._log.info(
-        `CardanoNode: _killPreviousProcess returned an error attempting to kill ${name}
+        `CardanoNode: _killProcessWithName returned an error attempting to kill ${name}
         process (PID: ${pid}). Error: ${JSON.stringify(error)}`
       );
       return Promise.reject(error);
