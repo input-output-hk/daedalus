10cfe1c7ce40847d44018f0428aa1acbfaf4dc6b
Dominik Guzei <dominik.guzei@gmail.com>
2018-12-03 12:09:08 +0100

    [DDW-262] fix nuke-ref for 0.js in main

--- yarn2nix.nix
+++ yarn2nix.nix
@@ -45,7 +45,7 @@ yarn2nix.mkYarnPackage {
     cp ${newPackagePath} $out/share/daedalus/package.json
     ${nukeReferences}/bin/nuke-refs $out/share/daedalus/main/index.js.map
     ${nukeReferences}/bin/nuke-refs $out/share/daedalus/main/preload.js.map
-    ${nukeReferences}/bin/nuke-refs $out/share/daedalus/main/0.index.js.map
+    ${nukeReferences}/bin/nuke-refs $out/share/daedalus/main/0.js.map
     ${nukeReferences}/bin/nuke-refs $out/share/daedalus/renderer/index.js.map
     ${nukeReferences}/bin/nuke-refs $out/share/daedalus/renderer/styles.css.map
   '';
