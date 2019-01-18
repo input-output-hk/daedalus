8966d8d9f991ffc377f1b181c9863fdf30f70bf3
MarcusHurney <marcushurney@gmail.com>
2019-01-17 20:17:09 -0800

    [DDW-515] Adds getLocale utility function

---
+++ source/main/utils/getLocale.js
@@ -0,0 +1,15 @@
+// @flow
+import Store from 'electron-store';
+
+const store = new Store();
+
+export const getLocale = (network: string) => {
+  const english = 'en-US';
+  try {
+    const locale = store.get(`${network}-USER-LOCALE`);
+    if (locale !== null && locale !== '') { return locale; }
+    return english;
+  } catch (error) {
+    return english;
+  }
+};
