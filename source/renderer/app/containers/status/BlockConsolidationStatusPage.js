bb9e958fc10ff6fd5a2c7796d051a497bd536090
Danilo Prates <daniloprates@gmail.com>
2019-01-24 13:20:58 -0200

    [DDW-557] Removes shell from renderer

--- source/renderer/app/containers/status/BlockConsolidationStatusPage.js
+++ source/renderer/app/containers/status/BlockConsolidationStatusPage.js
@@ -1,12 +1,9 @@
 // @flow
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
-import { ROUTES } from '../../routes-config';
 import type { InjectedProps } from '../../types/injectedPropsType';
 import BlockConsolidationStatus from '../../components/status/BlockConsolidationStatus';
 import { getCurrentEpoch } from '../../utils/network';
-
-const shell = require('electron').shell;
 
 @inject('stores', 'actions') @observer
 export default class BlockConsolidationStatusPage extends Component<InjectedProps> {
@@ -16,27 +13,18 @@ export default class BlockConsolidationStatusPage extends Component<InjectedProp
     this.props.actions.networkStatus.getNumberOfEpochsConsolidated.trigger();
   }
 
-  handleClose = () => {
-    const { actions } = this.props;
-    actions.router.goToRoute.trigger({ route: ROUTES.ROOT });
-  };
-
-  handleExternalLinkClick = (url: string, event?: MouseEvent) => {
-    if (event) event.preventDefault();
-    shell.openExternal(url);
-  };
-
   render() {
 
-    const { epochsConsolidated, syncProgress, systemStartTime } =
-      this.props.stores.networkStatus;
+    const { app, networkStatus } = this.props.stores;
+    const { epochsConsolidated, syncProgress, systemStartTime } = networkStatus;
+    const { openExternalLink } = app;
 
     return (
       <BlockConsolidationStatus
         currentEpoch={getCurrentEpoch(systemStartTime)}
         epochsConsolidated={epochsConsolidated}
         epochsSynced={syncProgress}
-        onExternalLinkClick={this.handleExternalLinkClick}
+        onExternalLinkClick={openExternalLink}
       />
     );
   }
