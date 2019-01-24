14dd14bfaea550b9b3c7c8ffefd5eb20f41a2243
MarcusHurney <marcushurney@gmail.com>
2019-01-23 16:25:22 -0800

    [DDW-540] Adds listener in preload.js disabling contextmenu for non-selectable elements

--- source/main/preload.js
+++ source/main/preload.js
@@ -54,4 +54,16 @@ process.once('loaded', () => {
   global.eval = () => {
     throw new Error('This app does not support window.eval().');
   };
+
+  // elements containing data that can be copied using the context menu (right click)
+  // must have an attribute set as follows: data-type="selectable" or be an input element
+  global.document.addEventListener('contextmenu', event => {
+    const targetData = Object.values(event.target.dataset);
+    const dataIsSelectable = targetData.includes('selectable');
+    const targetIsInput = event.target.nodeName === 'INPUT';
+
+    if (dataIsSelectable || targetIsInput) { return true; }
+
+    event.preventDefault();
+  }, false);
 });
