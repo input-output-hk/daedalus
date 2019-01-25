20cd8d6b0dcee8c64fade6765c880655b44247bd
Nikola Glumac <niglumac@gmail.com>
2019-01-25 10:58:44 +0100

    [DDW-557] Fix development cluster epoch configuration

--- source/renderer/app/config/epochsConfig.js
+++ source/renderer/app/config/epochsConfig.js
@@ -9,9 +9,9 @@ export const START_TIME_DEVELOPMENT = 1541808003;
 export const SLOT_DURATION_MAINNET = 20; // unit: seconds
 export const SLOT_DURATION_STAGING = 20; // unit: seconds
 export const SLOT_DURATION_TESTNET = 20; // unit: seconds
-export const SLOT_DURATION_DEVELOPMENT = 20; // unit: seconds
+export const SLOT_DURATION_DEVELOPMENT = 7; // unit: seconds
 
 export const EPOCH_LENGTH_BASE_MAINNET = 2160;
 export const EPOCH_LENGTH_BASE_STAGING = 2160;
 export const EPOCH_LENGTH_BASE_TESTNET = 2160;
-export const EPOCH_LENGTH_BASE_DEVELOPMENT = 6;
+export const EPOCH_LENGTH_BASE_DEVELOPMENT = 2;
