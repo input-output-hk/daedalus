9f01ab41fc6ce2863087d4d568d9deee46d8d601
Danilo Prates <daniloprates@gmail.com>
2019-01-30 13:28:42 -0200

    [DDW-572] Fix broken Windows launcher config

--- source/main/utils/getNumberOfEpochsConsolidated.js
+++ source/main/utils/getNumberOfEpochsConsolidated.js
@@ -1,14 +1,19 @@
 // @flow
 import fs from 'fs';
 import path from 'path';
-import { nodeDbPath } from '../config';
+import { appFolderPath } from '../config';
 import { getNumberOfEpochsConsolidatedChannel } from '../ipc/getNumberOfEpochsConsolidated.ipc';
 import type { GetNumberOfEpochsConsolidatedChannelResponse } from '../../common/ipc/api';
+import { environment } from '../environment';
+
+const { isLinux } = environment;
 
 export const getNumberOfEpochsConsolidated = () => {
   getNumberOfEpochsConsolidatedChannel
     .onRequest((): Promise<GetNumberOfEpochsConsolidatedChannelResponse> => {
-      const epochsPath = path.join(nodeDbPath, 'epochs');
+      const epochsPath = isLinux
+        ? path.join(appFolderPath, 'DB', 'epochs')
+        : path.join(appFolderPath, 'DB-1.0', 'epochs');
       let latestConsolidatedEpoch = 0;
       if (fs.existsSync(epochsPath)) {
         const epochfiles = fs
