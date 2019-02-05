75c00ba0c82f24dc50bec2b2aec0de326f7c985e
MarcusHurney <marcushurney@gmail.com>
2019-02-04 20:51:39 -0800

    [DDW-517] Adjusts logging types

--- source/common/types/logging.types.js
+++ source/common/types/logging.types.js
@@ -1,7 +1,31 @@
 // @flow
-export type MessageContext = {
+export type FormatMessageContextParams = {
   appName: string,
   electronProcess: string,
   level: string,
   network: string,
+};
+
+export type ConstructMessageBodyParams = {
+  at?: string,
+  env?: string,
+  ns?: Array<?string>,
+  data?: Object,
+  app?: Array<?string>,
+  msg?: string,
+  pid?: number | string,
+  sev?: string,
+  thread?: number | string
+};
+
+export type MessageBody = {
+  at: string,
+  env: string,
+  ns: Array<?string>,
+  data: Object,
+  app: Array<?string>,
+  msg: string,
+  pid: number | string,
+  sev: string,
+  thread: number | string
 };
