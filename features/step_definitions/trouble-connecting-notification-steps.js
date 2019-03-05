c240d87ede4d14876f8c33b27a2defb25745a8de
MarcusHurney <marcushurney@gmail.com>
2019-03-04 17:25:24 -0800

    [DDW-458] Adds feature and steps for network connection lost notification test

--- features/step_definitions/trouble-connecting-notification-steps.js
+++ features/step_definitions/trouble-connecting-notification-steps.js
@@ -1,33 +1,12 @@
-import { Given, Then, When } from 'cucumber';
-import { expect } from 'chai';
-import { refreshClient } from '../support/helpers/app-helpers';
+import { Then, When } from 'cucumber';
 import { waitUntilTextInSelector } from '../support/helpers/shared-helpers';
 
-const UNRECOVERABLE = 'unrecoverable';
-const WAS_CONNECTED = false;
-
-Given(/^Daedalus is running$/, function () {
-  expect(this.app.isRunning()).to.be.true;
-});
-
-When(/^I refresh the main window$/, async function () {
-  await refreshClient(this.client);
-});
-
-Then(/^I should see the loading screen with "([^"]*)"$/, async function (message) {
-  await waitUntilTextInSelector(this.client, {
-    selector: '.Loading_connecting h1',
-    text: message
-  });
-});
-
-When(/^I set the node to disconnected$/, async function () {
-  await this.client.executeAsync((done) => {
-    daedalus.stores.networkStatus._setDisconnected(WAS_CONNECTED)
-      .then(() => daedalus.stores.networkStatus._handleCardanoNodeStateChange(UNRECOVERABLE))
+When(/^I set the node subscription status to subscribing$/, async function () {
+  await this.client.executeAsync((subscriptionStatus, done) => {
+    daedalus.api.ada.setSubscriptionStatus(subscriptionStatus)
       .then(done)
       .catch((error) => done(error));
-  });
+  }, {});
 });
 
 Then(/^I should see the loading screen with report connecting issue text "([^"]*)"$/, async function (message) {
@@ -36,3 +15,7 @@ Then(/^I should see the loading screen with report connecting issue text "([^"]*
     text: message
   });
 });
+
+// When(/^I set isConnected to false$/, () => {
+//   daedalus.stores.networkStatus.isConnected = false;
+// });
--- features/trouble-connecting-notification.feature
+++ features/trouble-connecting-notification.feature
@@ -1,10 +1,8 @@
 Feature: Trouble Connecting Notification
-  
-  @slow @restartApp
-  Scenario: Trouble connecting notification is shown when node is not connected
-    Given Daedalus is running
-    When I refresh the main window
-    Then I should see the loading screen with "Connecting to network"
-    When I set the node to disconnected
-    Then I should see the loading screen with report connecting issue text "Having trouble connecting to network?"
-  
+
+  Background:
+    Given I have completed the basic setup
+
+  Scenario: Network connection lost notification is shown when node was previously connected and becomes disconnected
+    When I set the node subscription status to subscribing
+    Then I should see the loading screen with "Network connection lost - reconnecting"
