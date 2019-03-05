8ca04b76c885b430a84ecd3edafc3ad414884076
Samuel Leathers <samuel.leathers@iohk.io>
2019-03-05 04:56:46 -0500

    test with 10 minute wait time

--- source/main/config.js
+++ source/main/config.js
@@ -72,9 +72,9 @@ export const frontendOnlyMode = !launcherConfig.frontendOnlyMode;
 // CardanoNode config
 export const NODE_STARTUP_TIMEOUT = 5000;
 export const NODE_STARTUP_MAX_RETRIES = 5;
-export const NODE_SHUTDOWN_TIMEOUT = environment.isTest ? 5000 : 10000;
-export const NODE_KILL_TIMEOUT = environment.isTest ? 5000 : 10000;
-export const NODE_UPDATE_TIMEOUT = environment.isTest ? 10000 : 60000;
+export const NODE_SHUTDOWN_TIMEOUT = environment.isTest ? 5000 : 600000;
+export const NODE_KILL_TIMEOUT = environment.isTest ? 5000 : 600000;
+export const NODE_UPDATE_TIMEOUT = environment.isTest ? 10000 : 600000;
 
 /* eslint-disable max-len */
 export const DISK_SPACE_REQUIRED = 2 * 1073741274; // 2 GB | unit: bytes
