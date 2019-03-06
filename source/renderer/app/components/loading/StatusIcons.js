8924835eacc816dfe4572ff8335dca97f2c73637
Danilo Prates <daniloprates@gmail.com>
2019-03-06 12:40:17 -0300

    [DDW-460] Tooltip - wip

--- source/renderer/app/components/loading/StatusIcons.js
+++ source/renderer/app/components/loading/StatusIcons.js
@@ -1,70 +1,97 @@
 // @flow
 import React, { Component } from 'react';
 import SVGInline from 'react-svg-inline';
-// import { Tooltip } from 'react-polymorph/lib/components/Tooltip';
-// import { TooltipSkin } from 'react-polymorph/lib/skins/simple/TooltipSkin';
-// import { IDENTIFIERS } from 'react-polymorph/lib/themes/API';
+import { Tooltip } from 'react-polymorph/lib/components/Tooltip';
+import { TooltipSkin } from 'react-polymorph/lib/skins/simple/TooltipSkin';
 import classNames from 'classnames';
 import styles from './StatusIcons.scss';
-import isNodeRunningIcon from '../../assets/images/is-node-running-icon.inline.svg';
+import { CardanoNodeStates } from '../../../../common/types/cardano-node.types';
+import nodeStateIcon from '../../assets/images/node-state-icon.inline.svg';
 import isNodeRespondingIcon from '../../assets/images/is-node-responding-icon.inline.svg';
 import isNodeSubscribedIcon from '../../assets/images/is-node-subscribed-icon.inline.svg';
-import isNodeSyncingIcon from '../../assets/images/is-node-syncing-icon.inline.svg';
 import isNodeTimeCorrectIcon from '../../assets/images/is-node-time-correct-icon.inline.svg';
+import isNodeSyncingIcon from '../../assets/images/is-node-syncing-icon.inline.svg';
 
 type Props = {
-  isNodeRunning?: boolean,
+  nodeState: string,
   isNodeResponding?: boolean,
   isNodeSubscribed?: boolean,
-  isNodeSyncing?: boolean,
   isNodeTimeCorrect?: boolean,
+  isNodeSyncing?: boolean,
+};
+
+const STATUS_CLASSNAMES: Object = {
+  [CardanoNodeStates.STARTING]: 'unloaded',
+  [CardanoNodeStates.RUNNING]: 'on',
+  [CardanoNodeStates.EXITING]: 'unloaded',
+  [CardanoNodeStates.STOPPING]: 'unloaded',
+  [CardanoNodeStates.STOPPED]: 'unloaded',
+  [CardanoNodeStates.UPDATING]: 'unloaded',
+  [CardanoNodeStates.UPDATED]: 'unloaded',
+  [CardanoNodeStates.CRASHED]: 'off',
+  [CardanoNodeStates.ERRORED]: 'off',
+  [CardanoNodeStates.UNRECOVERABLE]: 'off',
+  true: 'on',
+  false: 'off',
+  undefined: 'unloaded',
+};
+
+const PARAM_PRETTY_NAME: Object = {
+  isNodeResponding: 'responding',
+  isNodeSubscribed: 'subscribed',
+  isNodeSyncing: 'syncing',
+  isNodeTimeCorrect: 'correct',
 };
 
 export default class StatusIcon extends Component<Props> {
-
-  statusClassNames: Object = {
-    true: 'on',
-    false: 'off',
-    undefined: 'unloaded'
+  getTip = (paramName: string) => {
+    const paramValue: boolean = this.props[paramName];
+    let status = paramValue ? ' is ' : ' is not ';
+    let paramPrettyName = PARAM_PRETTY_NAME[paramName];
+    if (paramName === 'nodeState') {
+      status = 'is';
+      paramPrettyName = paramValue;
+    }
+    let nodePrettyName = paramName !== 'isNodeTimeCorrect'
+      ? `Node ${status} `
+      : `Node time ${status} `;
+    if (typeof paramValue === 'undefined') {
+      nodePrettyName = 'Checking if node is ';
+    }
+    return [nodePrettyName, <b key="param">{paramPrettyName}</b>];
   };
 
-  getClassName = (param?: boolean) => {
-    const { isNodeRunning, isNodeSyncing } = this.props;
+  getClassName = (paramName: string) => {
+    const { nodeState, isNodeSyncing } = this.props;
+    const paramValue = this.props[paramName];
     // If {!isNodeRunning} it displays the icons without opacity
     // Whether {isNodeSyncing} it displays the icons for syncing or loading screen
-    let status = this.statusClassNames[param];
-    if (!isNodeRunning) status = 'unknown';
+    let status = STATUS_CLASSNAMES[paramValue];
+    if (paramName !== 'nodeState' && nodeState !== CardanoNodeStates.RUNNING) {
+      status = 'unknown';
+    }
     return classNames([
       styles[`icon-${status}`],
-      isNodeSyncing ? styles.syncing : styles.loading
+      isNodeSyncing ? styles.syncing : styles.loading,
     ]);
   };
 
-  render() {
-    const {
-      isNodeRunning,
-      isNodeResponding,
-      isNodeSubscribed,
-      isNodeSyncing,
-      isNodeTimeCorrect
-    } = this.props;
+  getIconWithToolTip = (icon: string, param: string) => (
+    <Tooltip skin={TooltipSkin} tip={this.getTip(param)}>
+      <SVGInline svg={icon} className={this.getClassName(param)} />
+    </Tooltip>
+  );
 
+  render() {
     return (
       <div className={styles.component}>
-        {/*
-          <Tooltip
-            themeId={IDENTIFIERS.TOOLTIP}
-            skin={TooltipSkin}
-            tip="Description of the child element"
-          >
-            hover over me
-          </Tooltip>
-        */}
-        <SVGInline svg={isNodeRunningIcon} className={this.getClassName(isNodeRunning)} />
-        <SVGInline svg={isNodeRespondingIcon} className={this.getClassName(isNodeResponding)} />
-        <SVGInline svg={isNodeSubscribedIcon} className={this.getClassName(isNodeSubscribed)} />
-        <SVGInline svg={isNodeTimeCorrectIcon} className={this.getClassName(isNodeTimeCorrect)} />
-        <SVGInline svg={isNodeSyncingIcon} className={this.getClassName(isNodeSyncing)} />
+        {[
+          this.getIconWithToolTip(nodeStateIcon, 'nodeState'),
+          this.getIconWithToolTip(isNodeRespondingIcon, 'isNodeResponding'),
+          this.getIconWithToolTip(isNodeSubscribedIcon, 'isNodeSubscribed'),
+          this.getIconWithToolTip(isNodeTimeCorrectIcon, 'isNodeTimeCorrect'),
+          this.getIconWithToolTip(isNodeSyncingIcon, 'isNodeSyncing'),
+        ]}
       </div>
     );
   }
