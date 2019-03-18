95823a501c15158afc0d48411d9d22c29f2d280f
Dominik Guzei <dominik.guzei@gmail.com>
2019-03-18 15:45:02 +0100

    [DDW-458] fix formatting

--- features/step_definitions/block-consolidation-page-steps.js
+++ features/step_definitions/block-consolidation-page-steps.js
@@ -2,91 +2,149 @@ import { When, Then } from 'cucumber';
 import { expect } from 'chai';
 
 const BLOCK_CONSOLIDATION_COMPONENT = '.BlockConsolidationStatus_component';
-const BLOCK_CONSOLIDATION_EXPLANATION = '.BlockConsolidationStatus_content p:nth-child(3)';
+const BLOCK_CONSOLIDATION_EXPLANATION =
+  '.BlockConsolidationStatus_content p:nth-child(3)';
 const EPOCHS_CONSOLIDATION_STATUS = '.BlockConsolidationStatus_epochs p span';
-const EPOCHS_CONSOLIDATED = '.BlockConsolidationStatus_indicatorEpochsConsolidated p';
+const EPOCHS_CONSOLIDATED =
+  '.BlockConsolidationStatus_indicatorEpochsConsolidated p';
 const TRAILING_BY_2_EPOCH = '.BlockConsolidationStatus_indicatorEpochsBehind p';
 const MAXIMUM_EPOCH = '.BlockConsolidationStatus_fullEpoch';
 const SYNC_PROGRESS = '.BlockConsolidationStatus_indicatorEpochsSynced p span';
 
-When(/^I toggle the Block Consolidation Status Page$/, async function () {
-  await this.client.execute(() => daedalus.actions.app.toggleBlockConsolidationStatusScreen.trigger());
+When(/^I toggle the Block Consolidation Status Page$/, async function() {
+  await this.client.execute(() =>
+    daedalus.actions.app.toggleBlockConsolidationStatusScreen.trigger()
+  );
 });
 
-Then(/^the Block Consolidation Status Page is (hidden|visible)/, async function (state) {
+Then(/^the Block Consolidation Status Page is (hidden|visible)/, async function(
+  state
+) {
   const isVisible = state === 'visible';
-  return await this.client.waitForVisible(BLOCK_CONSOLIDATION_COMPONENT, null, !isVisible);
+  return await this.client.waitForVisible(
+    BLOCK_CONSOLIDATION_COMPONENT,
+    null,
+    !isVisible
+  );
 });
 
-Then(/^the page accurately renders an explanation of how block consolidation works in file storage$/, async function () {
-  await this.client.waitForText(BLOCK_CONSOLIDATION_EXPLANATION);
-  const explanationText = await this.client.getText(BLOCK_CONSOLIDATION_EXPLANATION);
+Then(
+  /^the page accurately renders an explanation of how block consolidation works in file storage$/,
+  async function() {
+    await this.client.waitForText(BLOCK_CONSOLIDATION_EXPLANATION);
+    const explanationText = await this.client.getText(
+      BLOCK_CONSOLIDATION_EXPLANATION
+    );
 
-  const { value: { currentEpoch } } = await this.client.executeAsync(done => (
-    done({ currentEpoch: daedalus.stores.networkStatus.currentEpoch })
-  ));
+    const {
+      value: { currentEpoch },
+    } = await this.client.executeAsync(done =>
+      done({ currentEpoch: daedalus.stores.networkStatus.currentEpoch })
+    );
 
-  expect(explanationText).to.equal(`Blocks for the current epoch (${currentEpoch}) and the previous epoch (${Math.max(currentEpoch - 1, 0)}) are stored as one file per block. All previous epochs will be consolidated to two files per epoch.`);
-});
+    expect(explanationText).to.equal(
+      `Blocks for the current epoch (${currentEpoch}) and the previous epoch (${Math.max(
+        currentEpoch - 1,
+        0
+      )}) are stored as one file per block. All previous epochs will be consolidated to two files per epoch.`
+    );
+  }
+);
 
-Then(/^the page accurately renders epochs consolidated out of the total in the main blocks graphic$/, async function () {
-  await this.client.waitForText(EPOCHS_CONSOLIDATION_STATUS);
-  const consolidationStatus = await this.client.getText(EPOCHS_CONSOLIDATION_STATUS);
+Then(
+  /^the page accurately renders epochs consolidated out of the total in the main blocks graphic$/,
+  async function() {
+    await this.client.waitForText(EPOCHS_CONSOLIDATION_STATUS);
+    const consolidationStatus = await this.client.getText(
+      EPOCHS_CONSOLIDATION_STATUS
+    );
 
-  const { value: { epochsConsolidated, currentEpoch } } = await this.client.executeAsync(done => (
-    done({
-      epochsConsolidated: daedalus.stores.networkStatus.epochsConsolidated,
-      currentEpoch: daedalus.stores.networkStatus.currentEpoch,
-    })
-  ));
+    const {
+      value: { epochsConsolidated, currentEpoch },
+    } = await this.client.executeAsync(done =>
+      done({
+        epochsConsolidated: daedalus.stores.networkStatus.epochsConsolidated,
+        currentEpoch: daedalus.stores.networkStatus.currentEpoch,
+      })
+    );
 
-  expect(consolidationStatus).to.equal(`${epochsConsolidated} of ${currentEpoch}\nepochs consolidated`);
-});
+    expect(consolidationStatus).to.equal(
+      `${epochsConsolidated} of ${currentEpoch}\nepochs consolidated`
+    );
+  }
+);
 
-Then(/^the page accurately renders epochs consolidated above the progress bar$/, async function () {
-  await this.client.waitForText(EPOCHS_CONSOLIDATED);
-  const consolidationStatus = await this.client.getText(EPOCHS_CONSOLIDATED);
+Then(
+  /^the page accurately renders epochs consolidated above the progress bar$/,
+  async function() {
+    await this.client.waitForText(EPOCHS_CONSOLIDATED);
+    const consolidationStatus = await this.client.getText(EPOCHS_CONSOLIDATED);
 
-  const { value: { epochsConsolidated } } = await this.client.executeAsync(done => (
-    done({ epochsConsolidated: daedalus.stores.networkStatus.epochsConsolidated })
-  ));
+    const {
+      value: { epochsConsolidated },
+    } = await this.client.executeAsync(done =>
+      done({
+        epochsConsolidated: daedalus.stores.networkStatus.epochsConsolidated,
+      })
+    );
 
-  expect(consolidationStatus).to.equal(`${epochsConsolidated} epochs consolidated`);
-});
+    expect(consolidationStatus).to.equal(
+      `${epochsConsolidated} epochs consolidated`
+    );
+  }
+);
 
-Then(/^the page accurately renders the epoch trailing 2 behind the current epoch above the progress bar$/, async function () {
-  await this.client.waitForText(TRAILING_BY_2_EPOCH);
-  const trailingEpochText = await this.client.getText(TRAILING_BY_2_EPOCH);
+Then(
+  /^the page accurately renders the epoch trailing 2 behind the current epoch above the progress bar$/,
+  async function() {
+    await this.client.waitForText(TRAILING_BY_2_EPOCH);
+    const trailingEpochText = await this.client.getText(TRAILING_BY_2_EPOCH);
 
-  const { value: { currentEpoch } } = await this.client.executeAsync(done => (
-    done({ currentEpoch: daedalus.stores.networkStatus.currentEpoch })
-  ));
+    const {
+      value: { currentEpoch },
+    } = await this.client.executeAsync(done =>
+      done({ currentEpoch: daedalus.stores.networkStatus.currentEpoch })
+    );
 
-  expect(trailingEpochText).to.equal(`epoch ${Math.max(currentEpoch - 2, 0)}`);
-});
+    expect(trailingEpochText).to.equal(
+      `epoch ${Math.max(currentEpoch - 2, 0)}`
+    );
+  }
+);
 
-Then(/^the page accurately renders the current epoch signifying the max end of the progress bar$/, async function () {
-  await this.client.waitForText(MAXIMUM_EPOCH);
-  const currentEpochText = await this.client.getText(MAXIMUM_EPOCH);
+Then(
+  /^the page accurately renders the current epoch signifying the max end of the progress bar$/,
+  async function() {
+    await this.client.waitForText(MAXIMUM_EPOCH);
+    const currentEpochText = await this.client.getText(MAXIMUM_EPOCH);
 
-  const { value: { currentEpoch } } = await this.client.executeAsync(done => (
-    done({ currentEpoch: daedalus.stores.networkStatus.currentEpoch })
-  ));
+    const {
+      value: { currentEpoch },
+    } = await this.client.executeAsync(done =>
+      done({ currentEpoch: daedalus.stores.networkStatus.currentEpoch })
+    );
 
-  expect(currentEpochText).to.equal(`epoch ${currentEpoch}`);
-});
+    expect(currentEpochText).to.equal(`epoch ${currentEpoch}`);
+  }
+);
 
-Then(/^the page accurately renders the node's sync progress as a percentage below the progress bar$/, async function () {
-  await this.client.waitForText(SYNC_PROGRESS);
+Then(
+  /^the page accurately renders the node's sync progress as a percentage below the progress bar$/,
+  async function() {
+    await this.client.waitForText(SYNC_PROGRESS);
 
-  const { value: { syncProgress } } = await this.client.executeAsync(done => {
-    daedalus.stores.networkStatus._updateNetworkStatus()
-      .then(() => (
-        done({ syncProgress: daedalus.stores.networkStatus.syncProgress })
-      ))
-      .catch((error) => done(error));
-  });
+    const {
+      value: { syncProgress },
+    } = await this.client.executeAsync(done => {
+      daedalus.stores.networkStatus
+        ._updateNetworkStatus()
+        .then(() =>
+          done({ syncProgress: daedalus.stores.networkStatus.syncProgress })
+        )
+        .catch(error => done(error));
+    });
 
-  const blocksSyncedText = await this.client.getText(SYNC_PROGRESS);
-  expect(blocksSyncedText).to.equal(`${syncProgress}% blocks synced`);
-});
+    const blocksSyncedText = await this.client.getText(SYNC_PROGRESS);
+    expect(blocksSyncedText).to.equal(`${syncProgress}% blocks synced`);
+  }
+);
--- features/step_definitions/trouble-connecting-notification-steps.js
+++ features/step_definitions/trouble-connecting-notification-steps.js
@@ -5,51 +5,69 @@ const LOADING_COMPONENT = '.Loading_component';
 const REPORT_ISSUE_TEXT_H1 = '.Loading_reportIssueText';
 const REPORT_ISSUE_BUTTON = '.Loading_reportIssueButton';
 
-Then(/^I should not see the loading screen$/, async function () {
+Then(/^I should not see the loading screen$/, async function() {
   await this.client.waitForVisible(LOADING_COMPONENT, null, true);
 });
 
-Then(/^I should see the report issue notification displaying "([^"]*)"$/, async function (text) {
-  await waitUntilTextInSelector(this.client, {
-    selector: REPORT_ISSUE_TEXT_H1,
-    text
-  });
-});
+Then(
+  /^I should see the report issue notification displaying "([^"]*)"$/,
+  async function(text) {
+    await waitUntilTextInSelector(this.client, {
+      selector: REPORT_ISSUE_TEXT_H1,
+      text,
+    });
+  }
+);
 
-Then(/^I should not see the report issue notification$/, async function () {
+Then(/^I should not see the report issue notification$/, async function() {
   await this.client.waitForVisible(REPORT_ISSUE_TEXT_H1, null, true);
 });
 
-Then(/^The report issue button should be (hidden|visible)$/, async function (state) {
+Then(/^The report issue button should be (hidden|visible)$/, async function(
+  state
+) {
   const waitForHidden = state === 'hidden';
   await this.client.waitForVisible(REPORT_ISSUE_BUTTON, null, waitForHidden);
 });
 
-When(/^I set both local and network block heights to a static, equal number$/, async function () {
-  await this.client.executeAsync((done) => {
-    daedalus.api.ada.setNetworkBlockHeight(150)
-      .then(() => daedalus.api.ada.setLocalBlockHeight(150))
-      .then(done)
-      .catch((error) => done(error));
-  });
-});
+When(
+  /^I set both local and network block heights to a static, equal number$/,
+  async function() {
+    await this.client.executeAsync(done => {
+      daedalus.api.ada
+        .setNetworkBlockHeight(150)
+        .then(() => daedalus.api.ada.setLocalBlockHeight(150))
+        .then(done)
+        .catch(error => done(error));
+    });
+  }
+);
 
-When(/^I set the node subscription status to (subscribing|subscribed)$/, async function (state) {
-  const subscriptionState = state === 'subscribed' ? { status: 'subscribed' } : {};
+When(
+  /^I set the node subscription status to (subscribing|subscribed)$/,
+  async function(state) {
+    const subscriptionState =
+      state === 'subscribed' ? { status: 'subscribed' } : {};
 
-  await this.client.executeAsync((subscriptionStatus, done) => {
-    daedalus.api.ada.setSubscriptionStatus(subscriptionStatus)
-      .then(done)
-      .catch((error) => done(error));
-  }, subscriptionState);
-});
+    await this.client.executeAsync((subscriptionStatus, done) => {
+      daedalus.api.ada
+        .setSubscriptionStatus(subscriptionStatus)
+        .then(done)
+        .catch(error => done(error));
+    }, subscriptionState);
+  }
+);
 
-When(/^I reconnect local and network block heights to the node$/, async function () {
-  await this.client.executeAsync((done) => {
-    daedalus.api.ada.setNetworkBlockHeight(null)
-      .then(() => daedalus.api.ada.setLocalBlockHeight(null))
-      .then(() => daedalus.stores.networkStatus._updateNetworkStatus())
-      .then(done)
-      .catch((error) => done(error));
-  });
-});
+When(
+  /^I reconnect local and network block heights to the node$/,
+  async function() {
+    await this.client.executeAsync(done => {
+      daedalus.api.ada
+        .setNetworkBlockHeight(null)
+        .then(() => daedalus.api.ada.setLocalBlockHeight(null))
+        .then(() => daedalus.stores.networkStatus._updateNetworkStatus())
+        .then(done)
+        .catch(error => done(error));
+    });
+  }
+);
