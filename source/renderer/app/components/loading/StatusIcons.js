18c371d552790487eb716e444416c40c54c25cab
Danilo Prates <daniloprates@gmail.com>
2019-03-19 11:24:20 -0300

    [DDW-460] Simplify the tooltip text function

--- source/renderer/app/components/loading/StatusIcons.js
+++ source/renderer/app/components/loading/StatusIcons.js
@@ -18,122 +18,122 @@ import type { CardanoNodeState } from '../../../../common/types/cardano-node.typ
 const messages = defineMessages({
   nodeIsRunning: {
     id: 'status.icons.nodeIsRunning',
-    defaultMessage: 'Node is running!',
+    defaultMessage: '!!!Node is running!',
     description: 'Message "Node is running" on the status icon tooltip',
   },
   nodeIsStarting: {
     id: 'status.icons.nodeIsStarting',
-    defaultMessage: 'Node is starting!',
+    defaultMessage: '!!!Node is starting!',
     description: 'Message "Node is starting" on the status icon tooltip',
   },
   nodeIsExiting: {
     id: 'status.icons.nodeIsExiting',
-    defaultMessage: 'Node is exiting!',
+    defaultMessage: '!!!Node is exiting!',
     description: 'Message "Node is exiting" on the status icon tooltip',
   },
   nodeIsStopping: {
     id: 'status.icons.nodeIsStopping',
-    defaultMessage: 'Node is stopping!',
+    defaultMessage: '!!!Node is stopping!',
     description: 'Message "Node is stopping" on the status icon tooltip',
   },
   nodeHasStopped: {
     id: 'status.icons.nodeHasStopped',
-    defaultMessage: 'Node has stopped!',
+    defaultMessage: '!!!Node has stopped!',
     description: 'Message "Node has stopped" on the status icon tooltip',
   },
   nodeIsUpdating: {
     id: 'status.icons.nodeIsUpdating',
-    defaultMessage: 'Node is updating!',
+    defaultMessage: '!!!Node is updating!',
     description: 'Message "Node is updating" on the status icon tooltip',
   },
   nodeHasBeenUpdated: {
     id: 'status.icons.nodeHasBeenUpdated',
-    defaultMessage: 'Node has been updated!',
+    defaultMessage: '!!!Node has been updated!',
     description: 'Message "Node has been updated" on the status icon tooltip',
   },
   nodeHasCrashed: {
     id: 'status.icons.nodeHasCrashed',
-    defaultMessage: 'Node has crashed!',
+    defaultMessage: '!!!Node has crashed!',
     description: 'Message "Node has crashed" on the status icon tooltip',
   },
   nodeHasErrored: {
     id: 'status.icons.nodeHasErrored',
-    defaultMessage: 'Node has errored!',
+    defaultMessage: '!!!Node has errored!',
     description: 'Message "Node has errored" on the status icon tooltip',
   },
   nodeIsUnrecoverable: {
     id: 'status.icons.nodeIsUnrecoverable',
-    defaultMessage: 'Node is unrecoverable!',
+    defaultMessage: '!!!Node is unrecoverable!',
     description: 'Message "Node is unrecoverable" on the status icon tooltip',
-  },
-  nodeIsResponding: {
-    id: 'status.icons.nodeIsResponding',
-    defaultMessage: 'Node is responding!',
-    description: 'Message "Node is responding" on the status icon tooltip',
-  },
-  nodeIsNotResponding: {
-    id: 'status.icons.nodeIsNotResponding',
-    defaultMessage: 'Node is not responding!',
-    description: 'Message "Node is not responding" on the status icon tooltip',
-  },
-  checkingIfNodeIsResponding: {
-    id: 'status.icons.checkingIfNodeIsResponding',
-    defaultMessage: 'Checking if Node is responding!',
-    description:
-      'Message "Checking if Node is responding" on the status icon tooltip',
-  },
-  nodeIsSubscribed: {
-    id: 'status.icons.nodeIsSubscribed',
-    defaultMessage: 'Node is subscribed!',
-    description: 'Message "Node is subscribed" on the status icon tooltip',
-  },
-  nodeIsNotSubscribed: {
-    id: 'status.icons.nodeIsNotSubscribed',
-    defaultMessage: 'Node is not subscribed!',
-    description: 'Message "Node is not subscribed" on the status icon tooltip',
   },
   checkYourInternetConnection: {
     id: 'status.icons.checkYourInternetConnection',
-    defaultMessage: 'Check your Internet connection!',
+    defaultMessage: '!!!Check your Internet connection!',
     description:
       'Message "Check your Internet connection" on the status icon tooltip',
   },
-  checkingIfNodeIsSubscribed: {
-    id: 'status.icons.checkingIfNodeIsSubscribed',
-    defaultMessage: 'Checking if Node is subscribed!',
+  isNodeRespondingOn: {
+    id: 'status.icons.isNodeRespondingOn',
+    defaultMessage: '!!!Node is responding!',
+    description: 'Message "Node is responding" on the status icon tooltip',
+  },
+  isNodeRespondingOff: {
+    id: 'status.icons.isNodeRespondingOff',
+    defaultMessage: '!!!Node is not responding!',
+    description: 'Message "Node is not responding" on the status icon tooltip',
+  },
+  isNodeRespondingLoading: {
+    id: 'status.icons.isNodeRespondingLoading',
+    defaultMessage: '!!!Checking if Node is responding!',
+    description:
+      'Message "Checking if Node is responding" on the status icon tooltip',
+  },
+  isNodeSubscribedOn: {
+    id: 'status.icons.isNodeSubscribedOn',
+    defaultMessage: '!!!Node is subscribed!',
+    description: 'Message "Node is subscribed" on the status icon tooltip',
+  },
+  isNodeSubscribedOff: {
+    id: 'status.icons.isNodeSubscribedOff',
+    defaultMessage: '!!!Node is not subscribed!',
+    description: 'Message "Node is not subscribed" on the status icon tooltip',
+  },
+  isNodeSubscribedLoading: {
+    id: 'status.icons.isNodeSubscribedLoading',
+    defaultMessage: '!!!Checking if Node is subscribed!',
     description:
       'Message "Checking if Node is subscribed" on the status icon tooltip',
   },
-  nodeTimeIsCorrect: {
-    id: 'status.icons.nodeTimeIsCorrect',
-    defaultMessage: 'Node time is correct!',
+  isNodeTimeCorrectOn: {
+    id: 'status.icons.isNodeTimeCorrectOn',
+    defaultMessage: '!!!Node time is correct!',
     description: 'Message "Node time is correct" on the status icon tooltip',
   },
-  nodeTimeIsNotCorrect: {
-    id: 'status.icons.nodeTimeIsNotCorrect',
-    defaultMessage: 'Node time is not correct!',
+  isNodeTimeCorrectOff: {
+    id: 'status.icons.isNodeTimeCorrectOff',
+    defaultMessage: '!!!Node time is not correct!',
     description:
       'Message "Node time is not correct" on the status icon tooltip',
   },
-  checkingIfNodeTimeIsCorrect: {
-    id: 'status.icons.checkingIfNodeTimeIsCorrect',
-    defaultMessage: 'Checking if Node time is correct!',
+  isNodeTimeCorrectLoading: {
+    id: 'status.icons.isNodeTimeCorrectLoading',
+    defaultMessage: '!!!Checking if Node time is correct!',
     description:
       'Message "Checking if Node time is correct" on the status icon tooltip',
   },
-  nodeIsSyncing: {
-    id: 'status.icons.nodeIsSyncing',
-    defaultMessage: 'Node is syncing!',
+  isNodeSyncingOn: {
+    id: 'status.icons.isNodeSyncingOn',
+    defaultMessage: '!!!Node is syncing!',
     description: 'Message "Node is syncing" on the status icon tooltip',
   },
-  nodeIsNotSyncing: {
-    id: 'status.icons.nodeIsNotSyncing',
-    defaultMessage: 'Node is not syncing!',
+  isNodeSyncingOff: {
+    id: 'status.icons.isNodeSyncingOff',
+    defaultMessage: '!!!Node is not syncing!',
     description: 'Message "Node is not syncing" on the status icon tooltip',
   },
-  checkingIfNodeIsSyncing: {
-    id: 'status.icons.checkingIfNodeIsSyncing',
-    defaultMessage: 'Checking if Node is syncing!',
+  isNodeSyncingLoading: {
+    id: 'status.icons.isNodeSyncingLoading',
+    defaultMessage: '!!!Checking if Node is syncing!',
     description:
       'Message "Checking if Node is syncing" on the status icon tooltip',
   },
@@ -146,6 +146,8 @@ type Props = {
   isNodeTimeCorrect?: boolean,
   isNodeSyncing?: boolean,
 };
+
+type TipParamValue = true | false | null | string;
 
 const STATUS_CLASSNAMES: Object = {
   [CardanoNodeStates.STARTING]: 'unloaded',
@@ -163,7 +165,7 @@ const STATUS_CLASSNAMES: Object = {
   undefined: 'unloaded',
 };
 
-const MESSAGES = {
+const NODE_STATE_MESSAGES = {
   [CardanoNodeStates.RUNNING]: messages.nodeIsRunning,
   [CardanoNodeStates.STARTING]: messages.nodeIsStarting,
   [CardanoNodeStates.EXITING]: messages.nodeIsExiting,
@@ -174,20 +176,13 @@ const MESSAGES = {
   [CardanoNodeStates.CRASHED]: messages.nodeHasCrashed,
   [CardanoNodeStates.ERRORED]: messages.nodeHasErrored,
   [CardanoNodeStates.UNRECOVERABLE]: messages.nodeIsUnrecoverable,
-  nodeStatenull: messages.nodeIsStarting,
-  isNodeRespondingtrue: messages.nodeIsResponding,
-  isNodeRespondingfalse: messages.nodeIsNotResponding,
-  isNodeRespondingundefined: messages.checkingIfNodeIsResponding,
-  isNodeSubscribedtrue: messages.nodeIsSubscribed,
-  isNodeSubscribedfalse: messages.nodeIsNotSubscribed,
-  isNodeSubscribednull: messages.checkYourInternetConnection,
-  isNodeSubscribedundefined: messages.checkingIfNodeIsSubscribed,
-  isNodeTimeCorrecttrue: messages.nodeTimeIsCorrect,
-  isNodeTimeCorrectfalse: messages.nodeTimeIsNotCorrect,
-  isNodeTimeCorrectundefined: messages.checkingIfNodeTimeIsCorrect,
-  isNodeSyncingtrue: messages.nodeIsSyncing,
-  isNodeSyncingfalse: messages.nodeIsNotSyncing,
-  isNodeSyncingundefined: messages.checkingIfNodeIsSyncing,
+};
+
+const VARIABLE_VALUES = {
+  true: 'On',
+  false: 'Off',
+  undefined: 'Loading',
+  null: 'IsStarting',
 };
 
 export default class StatusIcon extends Component<Props> {
@@ -195,10 +190,13 @@ export default class StatusIcon extends Component<Props> {
     intl: intlShape.isRequired,
   };
 
-  getTip = (paramName: string, paramValue?: boolean) => {
-    let paramPrefix = paramName;
-    if (paramName === 'nodeState' && paramValue) paramPrefix = '';
-    const message = MESSAGES[`${paramPrefix}${String(paramValue)}`];
+  getTip = (paramName: string, paramValue: TipParamValue) => {
+    let message;
+    if (paramName === 'nodeState' && paramValue) {
+      message = NODE_STATE_MESSAGES[String(paramValue)];
+    } else {
+      message = messages[`${paramName}${VARIABLE_VALUES[String(paramValue)]}`];
+    }
     return message && <FormattedHTMLMessage {...message} />;
   };
 
