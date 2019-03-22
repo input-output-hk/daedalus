a834bbf7e3f4e2fca6551a346b0cd94ae24cd7fe
Nikola Glumac <niglumac@gmail.com>
2019-03-22 15:33:42 +0100

    [DDW-590] Fix failing acceptance tests

--- source/renderer/app/config/timingConfig.js
+++ source/renderer/app/config/timingConfig.js
@@ -14,6 +14,6 @@ export const ALLOWED_TIME_DIFFERENCE = 15 * 1000000; // 15 seconds | unit: micro
 export const MAX_ALLOWED_STALL_DURATION = isTest ? 15000 : 2 * 60 * 1000; // 2 minutes / 15 seconds (isTest = true) | unit: milliseconds
 export const NETWORK_STATUS_REQUEST_TIMEOUT = 30 * 1000; // 30 seconds | unit: milliseconds
 export const NETWORK_STATUS_POLL_INTERVAL = 2000; // 2 seconds | unit: milliseconds
-export const NTP_IGNORE_CHECKS_GRACE_PERIOD = isTest ? 1000 : 35 * 1000; // 35 seconds | unit: milliseconds
+export const NTP_IGNORE_CHECKS_GRACE_PERIOD = isTest ? 500 : 35 * 1000; // 35 seconds | unit: milliseconds
 export const EPOCH_DATA_UPDATE_INTERVAL = 10 * 1000; // 10 seconds | unit: milliseconds
 /* eslint-disable max-len */
