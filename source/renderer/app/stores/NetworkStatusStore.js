99d39eba77e4fdf339163a6d62418246d801de93
Nikola Glumac <niglumac@gmail.com>
2019-03-27 14:06:13 +0100

    [DDW-589] Update tlsConfig with 1 second delay

--- source/renderer/app/stores/NetworkStatusStore.js
+++ source/renderer/app/stores/NetworkStatusStore.js
@@ -255,8 +255,12 @@ export default class NetworkStatusStore extends Store {
       return Promise.resolve();
     Logger.info('NetworkStatusStore: received tls config from main process');
     this.api.ada.setRequestConfig(config);
-    this._tlsConfig = config;
-    return Promise.resolve();
+    return new Promise(resolve => {
+      setTimeout(() => {
+        this._tlsConfig = config;
+        resolve();
+      }, 1000);
+    });
   };
 
   _handleCardanoNodeStateChange = async (state: CardanoNodeState) => {
