1313eabe2efa3972b04c4aec6aa00518b5f22ccb
Danilo Prates <daniloprates@gmail.com>
2019-04-02 16:32:18 -0300

    [DDW-582] Tooltip text with intl

--- source/renderer/app/components/wallet/settings-utxo/WalletSettingsUtxoTooltip.js
+++ source/renderer/app/components/wallet/settings-utxo/WalletSettingsUtxoTooltip.js
@@ -1,8 +1,19 @@
 // @flow
 import React, { Component } from 'react';
+import classnames from 'classnames';
 import { observer } from 'mobx-react';
+import { defineMessages, intlShape, FormattedHTMLMessage } from 'react-intl';
 import { get } from 'lodash';
 import styles from './WalletSettingsUtxoTooltip.scss';
+
+export const messages = defineMessages({
+  tooltip: {
+    id: 'wallet.settings.utxos.tooltip',
+    defaultMessage:
+      '!!!<b>{walletUtxosAmount}</b> UTxOs containing <br /> <span> between <b>{prettyPreviousWalletAmount}</b> and </span> <b>{prettyWalletAmount}</b> ADA',
+    description: 'Tooltip for the "Wallet Utxos" screen.',
+  },
+});
 
 type Props = {
   payload?: Array<{
@@ -16,6 +27,10 @@ type Props = {
 
 @observer
 export default class WalletSettingsUtxoTooltip extends Component<Props> {
+  static contextTypes = {
+    intl: intlShape.isRequired,
+  };
+
   getPreviousAmount = (walletAmount: number) => {
     if (walletAmount === 45000000000) return 10000000000;
     if (walletAmount === 0.00001) return null;
@@ -28,16 +43,22 @@ export default class WalletSettingsUtxoTooltip extends Component<Props> {
     const previousWalletAmount = this.getPreviousAmount(walletAmount);
     const prettyWalletAmount = getPrettyAmount(walletAmount);
     const prettyPreviousWalletAmount = getPrettyAmount(previousWalletAmount);
+    const componentStyles = classnames([
+      styles.component,
+      !previousWalletAmount ? styles.noPreviousWalletAmount : null,
+    ]);
+
     return (
-      <div className={styles.component}>
+      <div className={componentStyles}>
         <p>
-          <b>{walletUtxosAmount}</b> UTxOs containing <br />
-          {!!previousWalletAmount && (
-            <span>
-              between <b>${prettyPreviousWalletAmount}</b> and{' '}
-            </span>
-          )}
-          <b>{prettyWalletAmount}</b> ADA
+          <FormattedHTMLMessage
+            {...messages.tooltip}
+            values={{
+              walletUtxosAmount,
+              prettyPreviousWalletAmount,
+              prettyWalletAmount,
+            }}
+          />
         </p>
       </div>
     );
