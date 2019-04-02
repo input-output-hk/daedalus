900e3d0c94f044e56bfd736c5e678c48917124d2
Danilo Prates <daniloprates@gmail.com>
2019-04-02 15:32:37 -0300

    [DDW-582] Empty wallet scenario

--- source/renderer/app/components/wallet/settings-utxo/WalletUtxoSettings.js
+++ source/renderer/app/components/wallet/settings-utxo/WalletUtxoSettings.js
@@ -1,7 +1,8 @@
 // @flow
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { observer } from 'mobx-react';
 import BigNumber from 'bignumber.js';
+import classnames from 'classnames';
 import { defineMessages, intlShape, FormattedHTMLMessage } from 'react-intl';
 import {
   BarChart,
@@ -32,6 +33,12 @@ export const messages = defineMessages({
       '!!!This wallet contains <b>{formattedWalletAmount} ADA</b> on <b>{walletUtxosAmount} UTxOs</b> (unspent transaction outputs). Examine the histogram below to see the distribution of UTxOs with different amounts of ada.',
     description: 'Description for the "Wallet Utxos" screen.',
   },
+  emptyWallet: {
+    id: 'wallet.settings.utxos.emptyWallet',
+    defaultMessage:
+      '!!!This wallet is empty so it does not contain any UTxOs (unspent transaction outputs).',
+    description: 'Empty wallet description for the "Wallet Utxos" screen.',
+  },
   labelX: {
     id: 'wallet.settings.utxos.labelX',
     defaultMessage: '!!!amount',
@@ -61,75 +68,91 @@ export default class WalletUtxoSettings extends Component<Props> {
     const { intl } = this.context;
     const { walletAmount, walletUtxosAmount, chartData } = this.props;
     const formattedWalletAmount = walletAmount.toFormat(DECIMAL_PLACES_IN_ADA);
+    const isEmpty = walletUtxosAmount === 0;
+    const componentStyles = classnames([
+      styles.component,
+      isEmpty ? styles.isEmpty : null,
+    ]);
+
     return (
-      <div className={styles.component}>
+      <div className={componentStyles}>
         <h1>{intl.formatMessage(messages.title)}</h1>
 
-        <p>
-          <FormattedHTMLMessage
-            {...messages.description}
-            values={{ formattedWalletAmount, walletUtxosAmount }}
-          />
-        </p>
+        {!isEmpty ? (
+          <Fragment>
+            <p>
+              <FormattedHTMLMessage
+                {...messages.description}
+                values={{ formattedWalletAmount, walletUtxosAmount }}
+              />
+            </p>
 
-        <ResponsiveContainer
-          height={280}
-          className={styles.responsiveContainer}
-        >
-          <BarChart data={chartData} barGap={30} barCategoryGap={4}>
-            <CartesianGrid
-              horizontal={false}
-              vertical={false}
-              fill="rgba(68, 91, 124, 0.06)"
-            />
-            <XAxis
-              dataKey="walletAmount"
-              interval={0}
-              axisLine={false}
-              tickLine={false}
-              tick={(props: TickProps) => (
-                <Tick
-                  {...props}
-                  textAnchor="start"
-                  getPrettyAmount={this.props.getPrettyAmount}
-                  vertical
+            <ResponsiveContainer
+              height={280}
+              className={styles.responsiveContainer}
+            >
+              <BarChart data={chartData} barGap={30} barCategoryGap={4}>
+                <CartesianGrid
+                  horizontal={false}
+                  vertical={false}
+                  fill="rgba(68, 91, 124, 0.06)"
                 />
-              )}
-              className={styles.xAxis}
-              y={0}
-            >
-              <Label
-                value={intl.formatMessage(messages.labelX)}
-                offset={20}
-                position="insideBottomRight"
-                className={styles.xAxisLabel}
-              />
-            </XAxis>
-            <YAxis
-              dataKey="walletUtxosAmount"
-              axisLine={false}
-              tickLine={false}
-              allowDecimals={false}
-              domain={[0, 'dataMax']}
-              tick={(props: TickProps) => <Tick {...props} textAnchor="end" />}
-            >
-              <Label
-                value={intl.formatMessage(messages.labelY)}
-                offset={-20}
-                position="insideTopLeft"
-                className={styles.yAxisLabel}
-              />
-            </YAxis>
-            <Tooltip
-              cursor={<Cursor />}
-              isAnimationActive={false}
-              content={
-                <CustomTooltip getPrettyAmount={this.props.getPrettyAmount} />
-              }
-            />
-            <Bar dataKey="walletUtxosAmount" className={styles.bar} />
-          </BarChart>
-        </ResponsiveContainer>
+                <XAxis
+                  dataKey="walletAmount"
+                  interval={0}
+                  axisLine={false}
+                  tickLine={false}
+                  tick={(props: TickProps) => (
+                    <Tick
+                      {...props}
+                      textAnchor="start"
+                      getPrettyAmount={this.props.getPrettyAmount}
+                      vertical
+                    />
+                  )}
+                  className={styles.xAxis}
+                  y={0}
+                >
+                  <Label
+                    value={intl.formatMessage(messages.labelX)}
+                    offset={20}
+                    position="insideBottomRight"
+                    className={styles.xAxisLabel}
+                  />
+                </XAxis>
+                <YAxis
+                  dataKey="walletUtxosAmount"
+                  axisLine={false}
+                  tickLine={false}
+                  allowDecimals={false}
+                  domain={[0, 'dataMax']}
+                  tick={(props: TickProps) => (
+                    <Tick {...props} textAnchor="end" />
+                  )}
+                >
+                  <Label
+                    value={intl.formatMessage(messages.labelY)}
+                    offset={-20}
+                    position="insideTopLeft"
+                    className={styles.yAxisLabel}
+                  />
+                </YAxis>
+                <Tooltip
+                  cursor={<Cursor />}
+                  isAnimationActive={false}
+                  content={
+                    <CustomTooltip
+                      getPrettyAmount={this.props.getPrettyAmount}
+                    />
+                  }
+                />
+                <Bar dataKey="walletUtxosAmount" className={styles.bar} />
+              </BarChart>
+            </ResponsiveContainer>
+          </Fragment>
+        ) : (
+          <p>{intl.formatMessage(messages.emptyWallet)}</p>
+        )}
       </div>
     );
   }
