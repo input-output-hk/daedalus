fb4050cbbcf216b0b127dc572845300addd00491
MarcusHurney <marcushurney@gmail.com>
2019-04-04 00:44:34 -0700

    [DDW-604] Adds flow types for creating a theme

---
+++ source/renderer/app/themes/types.js
@@ -0,0 +1,96 @@
+// @flow
+// TODO: Finish ReactPolymorphTheme definition
+export type ReactPolymorphTheme = {
+  autocomplete: {
+    '--rp-autocomplete-bg-color': string,
+    '--rp-autocomplete-border': string,
+    '--rp-autocomplete-border-color-opened': string,
+    '--rp-autocomplete-input-text-color': string,
+    '--rp-autocomplete-placeholder-color': string,
+    '--rp-autocomplete-selected-word-box-bg-color': string,
+    '--rp-autocomplete-selected-word-text-color': string,
+    '--rp-autocomplete-selected-words-font-family': string,
+  },
+  bubble: {
+    '--rp-bubble-bg-color': string,
+    '--rp-bubble-border-color': string,
+    '--rp-bubble-border-radius': string,
+  },
+  button: {
+    '--rp-button-bg-color': string,
+    '--rp-button-bg-color-active': string,
+    '--rp-button-bg-color-disabled': string,
+    '--rp-button-bg-color-hover': string,
+    '--rp-button-font-family': string,
+    '--rp-button-height': string,
+    '--rp-button-line-height': string,
+    '--rp-button-padding': string,
+    '--rp-button-text-color': string,
+    '--rp-button-text-transform': string,
+    '--rp-button-width': string,
+  },
+  checkbox: {
+    '--rp-checkbox-border': string,
+    '--rp-checkbox-border-color-disabled': string,
+    '--rp-checkbox-check-bg-color': string,
+    '--rp-checkbox-label-text-color': string,
+    '--rp-checkbox-label-text-color-disabled': string,
+  },
+  formfield: {},
+  input: {},
+  modal: {},
+  options: {},
+  select: {},
+  switch: {},
+  textarea: {},
+};
+
+export type ThemeColors = {
+  error: {
+    dark: string,
+    light: string,
+    regular: string,
+    ultralight: string,
+  },
+  primary: {
+    active: string,
+    background: string,
+    border: string,
+    disabled: string,
+    focus: string,
+    hover: string,
+    outline: string,
+    placeholder: string,
+    text: string,
+  },
+  secondary: {
+    active: string,
+    background: string,
+    border: string,
+    disabled: string,
+    focus: string,
+    hover: string,
+    outline: string,
+    placeholder: string,
+    text: string,
+  },
+};
+
+export type ThemeFonts = {
+  black: string,
+  bold: string,
+  heavy: string,
+  light: string,
+  medium: string,
+  mono: string,
+  regular: string,
+  semibold: string,
+  thin: string,
+  ultralight: string,
+};
+
+export type CreateThemeParams = {
+  colors: ThemeColors,
+  fonts: ThemeFonts,
+  config?: Object,
+};
