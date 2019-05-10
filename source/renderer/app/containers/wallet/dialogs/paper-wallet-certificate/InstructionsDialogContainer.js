c4d5afb6377e5405098fe77ee725180776fae130
Danilo Prates <daniloprates@gmail.com>
2019-05-10 15:31:42 -0300

    [DDW-637] Error debugging 2

--- source/renderer/app/containers/wallet/dialogs/paper-wallet-certificate/InstructionsDialogContainer.js
+++ source/renderer/app/containers/wallet/dialogs/paper-wallet-certificate/InstructionsDialogContainer.js
@@ -1,14 +1,28 @@
 // @flow
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
+import { defineMessages, intlShape } from 'react-intl';
+import moment from 'moment';
 import InstructionsDialog from '../../../../components/wallet/paper-wallet-certificate/InstructionsDialog';
 import type { InjectedDialogContainerProps } from '../../../../types/injectedPropsType';
+import { generateFileNameWithTimestamp } from '../../../../../../common/utils/files';
+
+const messages = defineMessages({
+  timestamp: {
+    id: 'paper.wallet.create.certificate.instructions.dialog.timestamp',
+    defaultMessage: '!!!MMMM D, YYYY - h:mm A',
+    description: 'Timestamp for the Paper Wallet PDF content.',
+  },
+});
 
 type Props = InjectedDialogContainerProps;
 
 @inject('stores', 'actions')
 @observer
 export default class InstructionsDialogContainer extends Component<Props> {
+  static contextTypes = {
+    intl: intlShape.isRequired,
+  };
   static defaultProps = {
     actions: null,
     stores: null,
@@ -17,35 +31,55 @@ export default class InstructionsDialogContainer extends Component<Props> {
   };
 
   onPrint = () => {
-    let filePath;
+    const { intl } = this.context;
+    const date = moment();
+    const locale = this.props.stores.profile.currentLocale;
+
+    moment.updateLocale(locale, {
+      longDateFormat: {
+        pdfTimestamp: intl.formatMessage(messages.timestamp),
+      },
+    });
+    const localizedDateFormat = moment()
+      .localeData()
+      .longDateFormat('pdfTimestamp');
+    const timestamp = moment(date).format(localizedDateFormat);
+    const name = generateFileNameWithTimestamp({
+      prefix: 'paper-wallet-certificate',
+      date,
+      fileType: '',
+      isUTC: false,
+    });
 
     try {
       // TODO: refactor this direct access to the dialog api
-      filePath = global.dialog.showSaveDialog({
-        defaultPath: 'paper-wallet-certificate.pdf',
+      const filePath = global.dialog.showSaveDialog({
+        defaultPath: `${name}.pdf`,
         filters: [
           {
-            name: 'paper-wallet-certificate',
+            name,
             extensions: ['pdf'],
           },
         ],
       });
+
+      // if cancel button is clicked or path is empty
+      if (!filePath) return;
+
+      this.props.actions.wallets.generateCertificate.trigger({
+        filePath,
+        timestamp,
+      });
     } catch (error) {
-      console.log('ERROR: -----');
+      console.log('ERROR: -----------');
       console.log(error);
 
-      // if (error && error.syscall && error.syscall === 'open') {
       alert(
         'Please, close the existing PDF file or save with a different name'
-      ); // eslint-disable-line
-      // }
+      );
+
       return false;
     }
-
-    // if cancel button is clicked or path is empty
-    if (!filePath) return false;
-
-    return this.props.actions.wallets.generateCertificate.trigger({ filePath });
   };
 
   render() {
