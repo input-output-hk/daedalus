3d621ce03998bb19b926aef1d623e0ff10a28628
Yakov Karavelov <yakov.karavelov@iohk.io>
2019-05-16 16:59:51 +0300

    [DDW-646] Make knob on delegation story in storybook

--- storybook/stories/Delegation.stories.js
+++ storybook/stories/Delegation.stories.js
@@ -1,17 +1,26 @@
 // @flow
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs, number } from '@storybook/addon-knobs';
 import StoryDecorator from './support/StoryDecorator';
 import Delegation from '../../source/renderer/app/components/staking/Delegation';
 
 const timeLeft = (78 * 60 + 10) * 60 * 1000;
 
 storiesOf('Delegation', module)
-  .addDecorator(story => <StoryDecorator>{story()}</StoryDecorator>)
+  .addDecorator((story, context) => (
+    <StoryDecorator>{withKnobs(story, context)}</StoryDecorator>
+  ))
   // ====== Stories ======
 
   .add('Start', () => (
     <div>
-      <Delegation currentLocale="en-US" timeLeft={timeLeft} />
+      <Delegation
+        currentLocale="en-US"
+        timeLeft={number('Time Left (miliseconds)', timeLeft, {
+          min: 0,
+          step: 1000,
+        })}
+      />
     </div>
   ));
