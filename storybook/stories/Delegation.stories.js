183f97a77917881618af4cd78ddab1dc45f5e297
Yakov Karavelov <yakov.karavelov@iohk.io>
2019-05-16 17:28:28 +0300

    [DDW-646] Update story name and make screen as full with sidebar on delegation story in storybook

--- storybook/stories/Delegation.stories.js
+++ storybook/stories/Delegation.stories.js
@@ -2,18 +2,34 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, number } from '@storybook/addon-knobs';
+import StoryLayout from './support/StoryLayout';
+import StoryProvider from './support/StoryProvider';
 import StoryDecorator from './support/StoryDecorator';
+import { CATEGORIES_BY_NAME } from '../../source/renderer/app/config/sidebarConfig';
 import Delegation from '../../source/renderer/app/components/staking/Delegation';
 
 const timeLeft = (78 * 60 + 10) * 60 * 1000;
 
-storiesOf('Delegation', module)
-  .addDecorator((story, context) => (
-    <StoryDecorator>{withKnobs(story, context)}</StoryDecorator>
-  ))
+storiesOf('DelegationScreens', module)
+  .addDecorator((story, context) => {
+    const storyWithKnobs = withKnobs(story, context);
+
+    return (
+      <StoryDecorator>
+        <StoryProvider>
+          <StoryLayout
+            activeSidebarCategory={CATEGORIES_BY_NAME.DELEGATION.route}
+            storyName={context.story}
+          >
+            {storyWithKnobs}
+          </StoryLayout>
+        </StoryProvider>
+      </StoryDecorator>
+    );
+  })
   // ====== Stories ======
 
-  .add('Start', () => (
+  .add('Start of decentralisation notification', () => (
     <div>
       <Delegation
         currentLocale="en-US"
