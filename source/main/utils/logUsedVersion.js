d4c7ca3f083a24a9a0c22ecaa82e931e4f2e1b78
Tomislav Horaček <tomislav.horacek@thespian.hr>
2019-05-27 17:10:09 +0200

    [DDW-655] Extending version logs and including into compressed logs file

--- source/main/utils/logUsedVersion.js
+++ source/main/utils/logUsedVersion.js
@@ -3,19 +3,25 @@ import fs from 'fs';
 
 export const logUsedVersion = async (version: string, logFilePath: string) => {
   let usedVersions = null;
+  const currentVersionData = {
+    version, // e.g. "0.13.2"
+    date: new Date().toISOString(), // e.g. "2018-12-11T144501.0177"
+  }
+
   try {
     // Load existing file
     const rawContent = await fs.promises.readFile(logFilePath, 'utf8');
     usedVersions = JSON.parse(rawContent);
+    const alreadyLogged = usedVersions.some(item => item.version === version)
     // Add current version if it has not yet been saved
-    if (!usedVersions.includes(version)) {
-      usedVersions.push(version);
+    if (!alreadyLogged) {
+      usedVersions.push(currentVersionData);
     }
   } catch (error) {
     // The file doesn't exist
     if (error.code === 'ENOENT') {
       // Start with this version
-      usedVersions = [version];
+      usedVersions = [currentVersionData];
     }
   }
   if (usedVersions) {
