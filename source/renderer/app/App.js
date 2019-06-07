d19c959c0b600d3074977c4d606d1748acc81080
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2019-06-07 12:04:07 +0200

    [DDW-654] Network status screen renaming - fixes

--- source/renderer/app/App.js
+++ source/renderer/app/App.js
@@ -4,7 +4,7 @@ import { Provider, observer } from 'mobx-react';
 import { ThemeProvider } from 'react-polymorph/lib/components/ThemeProvider';
 import DevTools from 'mobx-react-devtools';
 import { Router } from 'react-router';
-import { defineMessages, IntlProvider, intlShape } from 'react-intl';
+import { IntlProvider } from 'react-intl';
 import { Routes } from './Routes';
 import { daedalusTheme } from './themes/daedalus';
 import { themeOverrides } from './themes/overrides/index.js';
@@ -15,27 +15,6 @@ import ThemeManager from './ThemeManager';
 import AboutDialog from './containers/static/AboutDialog';
 import DaedalusDiagnosticsDialog from './containers/status/DaedalusDiagnosticsDialog';
 import GenericNotificationContainer from './containers/notifications/GenericNotificationContainer';
-import GenericNotification from './components/notifications/GenericNotification';
-import { DOWNLOAD_LOGS_SUCCESS_DURATION } from './config/timingConfig';
-import { i18nContext } from './utils/i18nContext';
-
-export const messages = defineMessages({
-  downloadLogsSuccess: {
-    id: 'loading.screen.reportIssue.downloadLogsSuccessMessage',
-    defaultMessage: '!!!Logs successfully downloaded',
-    description: 'Success message for download logs.',
-  },
-  downloadLogsProgress: {
-    id: 'loading.screen.reportIssue.downloadLogsProgressMessage',
-    defaultMessage: '!!!Preparing logs for download',
-    description: 'Progress message for download logs.',
-  },
-});
-
-const DOWNLOAD_LOGS_PROGRESS_NOTIFICATION_ID =
-  'loading-page-download-logs-progress';
-const DOWNLOAD_LOGS_SUCCESS_NOTIFICATION_ID =
-  'loading-page-download-logs-success';
 
 @observer
 export default class App extends Component<{
@@ -43,10 +22,6 @@ export default class App extends Component<{
   actions: ActionsMap,
   history: Object,
 }> {
-  static contextTypes = {
-    intl: intlShape.isRequired,
-  };
-
   componentWillMount() {
     // loads app's global environment variables into AppStore via ipc
     this.props.actions.app.initAppEnvironment.trigger();
@@ -56,14 +31,9 @@ export default class App extends Component<{
     const { app } = stores;
     const { isAboutDialogOpen, isDaedalusDiagnosticsDialogOpen } = app;
     const locale = stores.profile.currentLocale;
-    const intl = i18nContext(locale);
     const mobxDevTools = global.environment.mobxDevTools ? <DevTools /> : null;
     const { currentTheme } = stores.profile;
     const themeVars = require(`./themes/daedalus/${currentTheme}.js`).default;
-
-    // Notification component z-index
-    const notificationOrder = 99999;
-
     return (
       <Fragment>
         <ThemeManager variables={themeVars} />
@@ -79,42 +49,7 @@ export default class App extends Component<{
                   <DaedalusDiagnosticsDialog />
                 )}
                 {isAboutDialogOpen && <AboutDialog />}
-                {
-                  <GenericNotificationContainer>
-                    <GenericNotification
-                      id={DOWNLOAD_LOGS_PROGRESS_NOTIFICATION_ID}
-                      show={stores.uiNotifications.isOpen(
-                        DOWNLOAD_LOGS_PROGRESS_NOTIFICATION_ID
-                      )}
-                      actionToListenAndOpen={actions.profile.downloadLogs}
-                      actionToListenAndClose={
-                        actions.profile.downloadLogsSuccess
-                      }
-                      icon="spinner"
-                      hasEllipsis
-                      hasCloseButton
-                      order={notificationOrder}
-                    >
-                      {intl.formatMessage(messages.downloadLogsProgress)}
-                    </GenericNotification>
-                    <GenericNotification
-                      id={DOWNLOAD_LOGS_SUCCESS_NOTIFICATION_ID}
-                      duration={DOWNLOAD_LOGS_SUCCESS_DURATION}
-                      show={stores.uiNotifications.isOpen(
-                        DOWNLOAD_LOGS_SUCCESS_NOTIFICATION_ID
-                      )}
-                      actionToListenAndOpen={
-                        actions.profile.downloadLogsSuccess
-                      }
-                      actionToListenAndClose={actions.profile.downloadLogs}
-                      icon="success"
-                      hasCloseButton
-                      order={notificationOrder}
-                    >
-                      {intl.formatMessage(messages.downloadLogsSuccess)}
-                    </GenericNotification>
-                  </GenericNotificationContainer>
-                }
+                {<GenericNotificationContainer />}
               </Fragment>
             </IntlProvider>
           </ThemeProvider>
--- source/renderer/app/containers/notifications/GenericNotificationContainer.js
+++ source/renderer/app/containers/notifications/GenericNotificationContainer.js
@@ -1,23 +1,79 @@
 // @flow
-import React, { Component, Children } from 'react';
+import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
+import { defineMessages, intlShape } from 'react-intl';
 import type { InjectedContainerProps } from '../../types/injectedPropsType';
+import { DOWNLOAD_LOGS_SUCCESS_DURATION } from '../../config/timingConfig';
+import GenericNotification from '../../components/notifications/GenericNotification';
+
+export const messages = defineMessages({
+  downloadLogsSuccess: {
+    id: 'loading.screen.reportIssue.downloadLogsSuccessMessage',
+    defaultMessage: '!!!Logs successfully downloaded',
+    description: 'Success message for download logs.',
+  },
+  downloadLogsProgress: {
+    id: 'loading.screen.reportIssue.downloadLogsProgressMessage',
+    defaultMessage: '!!!Preparing logs for download',
+    description: 'Progress message for download logs.',
+  },
+});
+
+const DOWNLOAD_LOGS_PROGRESS_NOTIFICATION_ID =
+  'loading-page-download-logs-progress';
+const DOWNLOAD_LOGS_SUCCESS_NOTIFICATION_ID =
+  'loading-page-download-logs-success';
 
 @inject('stores', 'actions')
 @observer
 export default class GenericNotificationContainer extends Component<InjectedContainerProps> {
+  static contextTypes = {
+    intl: intlShape.isRequired,
+  };
+
   static defaultProps = { actions: null, stores: null };
 
   render() {
-    const { actions, children } = this.props;
+    const { actions, stores } = this.props;
+    const { intl } = this.context;
+    // Notification component z-index
+    const notificationOrder = 99999;
 
-    const propsForChildren = {
-      openNotification: actions.notifications.open,
-      closeNotification: actions.notifications.closeActiveNotification,
-    };
-
-    return Children.map(children, child =>
-      React.cloneElement(child, propsForChildren)
+    return (
+      <div>
+        <GenericNotification
+          id={DOWNLOAD_LOGS_PROGRESS_NOTIFICATION_ID}
+          show={stores.uiNotifications.isOpen(
+            DOWNLOAD_LOGS_PROGRESS_NOTIFICATION_ID
+          )}
+          actionToListenAndOpen={actions.profile.downloadLogs}
+          actionToListenAndClose={actions.profile.downloadLogsSuccess}
+          openNotification={actions.notifications.open}
+          closeNotification={actions.notifications.closeActiveNotification}
+          icon="spinner"
+          hasEllipsis
+          hasCloseButton
+          order={notificationOrder}
+        >
+          {intl.formatMessage(messages.downloadLogsProgress)}
+        </GenericNotification>
+        <GenericNotification
+          id={DOWNLOAD_LOGS_SUCCESS_NOTIFICATION_ID}
+          duration={DOWNLOAD_LOGS_SUCCESS_DURATION}
+          show={stores.uiNotifications.isOpen(
+            DOWNLOAD_LOGS_SUCCESS_NOTIFICATION_ID
+          )}
+          actionToListenAndOpen={actions.profile.downloadLogsSuccess}
+          actionToListenAndClose={actions.profile.downloadLogs}
+          openNotification={actions.notifications.open}
+          closeNotification={actions.notifications.closeActiveNotification}
+          icon="success"
+          hasCloseButton
+          order={notificationOrder}
+        >
+          {intl.formatMessage(messages.downloadLogsSuccess)}
+        </GenericNotification>
+      </div>
     );
   }
 }
