58e93c09b17067f4581b8278698127d66c32e5a7
Danilo Prates <daniloprates@gmail.com>
2019-06-19 11:10:25 -0300

    [DDW-708] Hide other Storybook stories

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -2,34 +2,34 @@
 import '../../source/renderer/app/themes/index.global.scss';
 import './support/environment';
 
-// Wallets
-import './WalletScreens.stories';
-import './WalletSummary.stories';
-import './BlockConsolidationStatus.stories';
-import './AddWallet.stories';
-import './DeleteWalletConfirmationDialog.stories';
-import './WalletTransactionsList.stories';
-import './ExportWalletToFileDialog.stories';
-import './PaperWallets.stories';
-import './ManualUpdateOverlay.stories';
+// // Wallets
+// import './WalletScreens.stories';
+// import './WalletSummary.stories';
+// import './BlockConsolidationStatus.stories';
+// import './AddWallet.stories';
+// import './DeleteWalletConfirmationDialog.stories';
+// import './WalletTransactionsList.stories';
+// import './ExportWalletToFileDialog.stories';
+// import './PaperWallets.stories';
+// import './ManualUpdateOverlay.stories';
 
-// Staking
-import './Staking.stories';
+// // Staking
+// import './Staking.stories';
 
-// Ada Redemption
-import './AdaRedemptionDialog.stories';
+// // Ada Redemption
+// import './AdaRedemptionDialog.stories';
 
-// Settings
-import './LanguageSelectionForm.stories';
+// // Settings
+// import './LanguageSelectionForm.stories';
 
 // Loading
 import './Loading.stories';
 
-// Navigation
-import './Sidebar.stories';
-import './SidebarCategory.stories';
-import './SidebarWalletsMenu.stories';
-import './TopBar.stories';
+// // Navigation
+// import './Sidebar.stories';
+// import './SidebarCategory.stories';
+// import './SidebarWalletsMenu.stories';
+// import './TopBar.stories';
 
-// Widgets
-import './Widgets.stories';
+// // Widgets
+// import './Widgets.stories';
