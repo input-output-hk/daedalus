6966c3b611885a211b31b19e8258c10cb8e86a62
Tomislav Horaček <tomislav.horacek@thespian.hr>
2019-06-27 10:04:50 +0200

    [DDW-658] UI - Delegation setup wizard steps 0 & 1 (#1417)
    * [DDW-658] Introduce delegation wizard steps 0 & 1 UI components initial setup
    
    * [DDW-658] Introduce steps 0 & 1 with walk through logic, translations and theming
    
    * [DDW-658] CHANGELOG update
    
    * [DDW-658] Introduce external link icon
    
    * [DDW-658] React polymorph Stepper component and Select custom selection renderer integrations
    
    * [DDW-658] Styles alphabetical order
    
    * [DDW-658] Add missing translations and Delegation setup story improvement
    
    * [DDW-658] Resolve Flow errors
    
    * [DDW-658] Prettier format
    
    * [DDW-658] Improve dialog title translation ID
    
    * [DDW-658] Styles and translations fixes
    
    * [DDW-658] Styles alphabetical order and few small code improvements and cleanup
    
    * [DDW-658] Force netlify re-build
    
    * [DDW-658] Minor style improvements
    
    * [DDW-658] Add theme error color variable instead static hex for Select error

---
+++ source/renderer/app/components/staking/delegation-setup-wizard/DelegationStepsChooseWalletDialog.js
@@ -0,0 +1,210 @@
+// @flow
+import React, { Component } from 'react';
+import {
+  defineMessages,
+  intlShape,
+  FormattedHTMLMessage,
+  FormattedMessage,
+} from 'react-intl';
+import classNames from 'classnames';
+import { Select } from 'react-polymorph/lib/components/Select';
+import { SelectSkin } from 'react-polymorph/lib/skins/simple/SelectSkin';
+import { Stepper } from 'react-polymorph/lib/components/Stepper';
+import { StepperSkin } from 'react-polymorph/lib/skins/simple/StepperSkin';
+import styles from './DelegationStepsChooseWalletDialog.scss';
+import DialogCloseButton from '../../widgets/DialogCloseButton';
+import DialogBackButton from '../../widgets/DialogBackButton';
+import Dialog from '../../widgets/Dialog';
+
+const messages = defineMessages({
+  title: {
+    id: 'staking.delegationSetup.steps.dialog.title',
+    defaultMessage: '!!!Delegation Setup',
+    description:
+      'Title "Delegation Setup" on the delegation setup "choose wallet" step dialog.',
+  },
+  description: {
+    id: 'staking.delegationSetup.chooseWallet.step.dialog.description',
+    defaultMessage:
+      '!!!Choose a wallet with funds you would like to delegate to a stake pool. Selected wallet needs to have a minimum of <span>{minDelegationFunds} ada</span>.',
+    description:
+      'Description on the delegation setup "choose wallet" step dialog.',
+  },
+  selectWalletInputLabel: {
+    id:
+      'staking.delegationSetup.chooseWallet.step.dialog.selectWalletInputLabel',
+    defaultMessage: '!!!Wallet',
+    description:
+      'Label "Wallet" for select input on the delegation setup "choose wallet" step dialog.',
+  },
+  selectWalletInputPlaceholder: {
+    id:
+      'staking.delegationSetup.chooseWallet.step.dialog.selectWalletInputPlaceholder',
+    defaultMessage: '!!!Select Wallet',
+    description:
+      'Placeholder "Select Wallet" for select input on the delegation setup "choose wallet" step dialog.',
+  },
+  stepIndicatorLabel: {
+    id: 'staking.delegationSetup.chooseWallet.step.dialog.stepIndicatorLabel',
+    defaultMessage: '!!!STEP {currentStep} OF {totalSteps}',
+    description:
+      'Step indicator labe on the delegation setup "choose wallet" step dialog.',
+  },
+  errorMessage: {
+    id: 'staking.delegationSetup.chooseWallet.step.dialog.errorMessage',
+    defaultMessage:
+      '!!!This wallet does not have enough ada for delegation setup. Please choose a wallet with a minimum of <span>{minDelegationFunds} ada</span> and click continue.',
+    description:
+      'Error Label on the delegation setup "choose wallet" step dialog.',
+  },
+  continueButtonLabel: {
+    id: 'staking.delegationSetup.chooseWallet.step.dialog.continueButtonLabel',
+    defaultMessage: '!!!Continue',
+    description:
+      'Label for continue button on the delegation setup "choose wallet" step dialog.',
+  },
+});
+
+type WalletData = {
+  label: string,
+  value: string,
+  isAcceptableSetupWallet: boolean,
+};
+
+type Props = {
+  onClose: Function,
+  onContinue: Function,
+  onBack: Function,
+  wallets: Array<WalletData>,
+  stepsList: Array<string>,
+  minDelegationFunds: number,
+};
+
+type State = {
+  selectedWalletAmount: string,
+  walletChoiceError: boolean,
+};
+
+export default class DelegationStepsChooseWalletDialog extends Component<
+  Props,
+  State
+> {
+  static contextTypes = {
+    intl: intlShape.isRequired,
+  };
+
+  state = {
+    selectedWalletAmount: '',
+    walletChoiceError: false,
+  };
+
+  onWalletChange = (selectedWallet: WalletData) => {
+    this.setState({
+      walletChoiceError: !selectedWallet.isAcceptableSetupWallet,
+      selectedWalletAmount: selectedWallet.value,
+    });
+  };
+
+  render() {
+    const { intl } = this.context;
+    const { walletChoiceError, selectedWalletAmount } = this.state;
+    const {
+      wallets,
+      stepsList,
+      minDelegationFunds,
+      onClose,
+      onContinue,
+      onBack,
+    } = this.props;
+
+    const actions = [
+      {
+        className: 'continueButton',
+        label: intl.formatMessage(messages.continueButtonLabel),
+        onClick: onContinue,
+        primary: true,
+        disabled: !selectedWalletAmount || walletChoiceError,
+      },
+    ];
+
+    const walletSelectClasses = classNames([
+        styles.walletSelect,
+        walletChoiceError ? styles.error : null,
+      ]);
+
+    return (
+      <Dialog
+        title={intl.formatMessage(messages.title)}
+        actions={actions}
+        closeOnOverlayClick
+        onClose={onClose}
+        className={styles.delegationStepsChooseWalletDialogWrapper}
+        closeButton={<DialogCloseButton onClose={onClose} />}
+        backButton={<DialogBackButton onBack={onBack} />}
+      >
+        <div className={styles.delegationStepsIndicatorWrapper}>
+          <p className={styles.stepIndicatorLabel}>
+            <FormattedMessage
+              {...messages.stepIndicatorLabel}
+              values={{
+                currentStep: 2,
+                totalSteps: stepsList.length,
+              }}
+            />
+          </p>
+          <Stepper
+            steps={stepsList}
+            activeStep={2}
+            skin={StepperSkin}
+            labelDisabled
+          />
+        </div>
+
+        <div className={styles.content}>
+          <p className={styles.description}>
+            <FormattedHTMLMessage
+              {...messages.description}
+              values={{ minDelegationFunds }}
+            />
+          </p>
+          <Select
+            className={walletSelectClasses}
+            label={intl.formatMessage(messages.selectWalletInputLabel)}
+            options={wallets}
+            optionRenderer={option => {
+              return (
+                <div
+                  className={styles.customOptionStyle}
+                  role="presentation"
+                  onClick={this.onWalletChange.bind(this, option)}
+                >
+                  <div className={styles.optionLabel}>{option.label}</div>
+                  <div className={styles.optionValue}>{option.value}</div>
+                </div>
+              );
+            }}
+            selectionRenderer={option => (
+              <div className={styles.customValueStyle}>
+                <div className={styles.label}>{option.label}</div>
+                <div className={styles.value}>{option.value}</div>
+              </div>
+            )}
+            placeholder={intl.formatMessage(
+              messages.selectWalletInputPlaceholder
+            )}
+            skin={SelectSkin}
+            value={selectedWalletAmount}
+          />
+          {walletChoiceError && (
+            <p className={styles.errorMessage}>
+              <FormattedHTMLMessage
+                {...messages.errorMessage}
+                values={{ minDelegationFunds }}
+              />
+            </p>
+          )}
+        </div>
+      </Dialog>
+    );
+  }
+}
