ecc31ccc999a1ffe67377052a6e8234841e4ab79
Danilo Prates <daniloprates@gmail.com>
2019-07-10 15:25:41 -0300

    [DDW-736] Animated logo - manual canvas size adjustment

--- source/renderer/app/components/loading/syncing-connecting/LogosDisplay.js
+++ source/renderer/app/components/loading/syncing-connecting/LogosDisplay.js
@@ -1,37 +1,58 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
 import SVGInline from 'react-svg-inline';
 import classNames from 'classnames';
+import Lottie from 'react-lottie';
 import styles from './LogosDisplay.scss';
 import adaLogo from '../../../assets/images/ada-logo.inline.svg';
-import daedalusLogo from '../../../assets/images/daedalus-logo-loading-grey.inline.svg';
 import cardanoLogo from '../../../assets/images/cardano-logo.inline.svg';
+import animationData from './logo-animation-data.json';
 
 type Props = {
   isConnected: boolean,
 };
 
-const LogosDisplay = ({ isConnected }: Props) => {
-  const currencyLogoStyles = classNames([
-    styles['ada-logo'],
-    !isConnected ? styles.connectingLogo : styles.syncingLogo,
-  ]);
-  const daedalusLogoStyles = classNames([
-    styles.daedalusLogo,
-    !isConnected ? styles.connectingLogo : styles.syncingLogo,
-  ]);
-  const apiLogoStyles = classNames([
-    styles['ada-apiLogo'],
-    !isConnected ? styles.connectingLogo : styles.syncingLogo,
-  ]);
-
-  return (
-    <div className={styles.component}>
-      <SVGInline svg={adaLogo} className={currencyLogoStyles} />
-      <SVGInline svg={daedalusLogo} className={daedalusLogoStyles} />
-      <SVGInline svg={cardanoLogo} className={apiLogoStyles} />
-    </div>
-  );
+const logoAnimationOptionsLottie = {
+  loop: true,
+  autoplay: true,
+  animationData,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
 };
 
-export default LogosDisplay;
+export default class LogosDisplay extends Component<Props> {
+  componentDidMount() {
+    // Manual adjustment due to `logo-animation-data.json` canvas size
+    const svg = document.querySelector('.LogosDisplay_daedalusLogo svg');
+    if (svg instanceof HTMLElement) {
+      svg.setAttribute('viewBox', '534 250 212 220');
+    }
+  }
+
+  render() {
+    const { isConnected } = this.props;
+    const currencyLogoStyles = classNames([
+      styles['ada-logo'],
+      !isConnected ? styles.connectingLogo : styles.syncingLogo,
+    ]);
+    const daedalusLogoStyles = classNames([
+      styles.daedalusLogo,
+      !isConnected ? styles.connectingLogo : styles.syncingLogo,
+    ]);
+    const apiLogoStyles = classNames([
+      styles['ada-apiLogo'],
+      !isConnected ? styles.connectingLogo : styles.syncingLogo,
+    ]);
+
+    return (
+      <div className={styles.component}>
+        <SVGInline svg={adaLogo} className={currencyLogoStyles} />
+        <div className={daedalusLogoStyles}>
+          <Lottie options={logoAnimationOptionsLottie} />
+        </div>
+        <SVGInline svg={cardanoLogo} className={apiLogoStyles} />
+      </div>
+    );
+  }
+}
