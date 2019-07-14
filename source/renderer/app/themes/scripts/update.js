f86d445a4ff927d583dbb42863bc3212dbaec4f2
MarcusHurney <marcushurney@gmail.com>
2019-07-14 16:11:17 -0700

    [DDW-697] Removes CLI code from scripts/update.js and imports module instead

--- source/renderer/app/themes/scripts/update.js
+++ source/renderer/app/themes/scripts/update.js
@@ -1,55 +1,21 @@
 // @flow
-import chalk from 'chalk';
-import { findUpdates, updateThemes } from '../utils/updateThemes';
-import { CREATE_THEME_OBJ } from '../utils/constants';
+import { createTheme } from '../utils/createTheme';
+import { findUpdates } from '../utils/findUpdates';
+import { runUpdateThemesCLI } from '../utils/updateThemesCLI';
+import {
+  CREATE_CARDANO_THEME_PARAMS,
+  CREATE_LIGHT_BLUE_THEME_PARAMS,
+  CREATE_DARK_BLUE_THEME_PARAMS,
+} from '../utils/constants';
 
-const logInputOption = (option: string) => chalk.magenta.italic(`'${option}'`);
-const YES_OR_NO = `${logInputOption('yes')} / ${logInputOption('no')}`;
+const findUpdatesParams = {
+  cardano: createTheme(CREATE_CARDANO_THEME_PARAMS),
+  darkBlue: createTheme(CREATE_DARK_BLUE_THEME_PARAMS),
+  lightBlue: createTheme(CREATE_LIGHT_BLUE_THEME_PARAMS),
+};
 
-const readline = require('readline');
+const pendingUpdates = findUpdates(findUpdatesParams);
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: chalk.cyan(
-    `\nWould you like to write these updates to the theme files? ( ${YES_OR_NO} )\n`
-  ),
-});
-
-const pendingUpdates = findUpdates(CREATE_THEME_OBJ); // Todo: Enter correct params
 if (pendingUpdates !== null) {
-  rl.prompt();
-  rl.on('line', line => {
-    switch (line.trim()) {
-      case 'yes' || 'y':
-        chalk`\n{bold Upating themes...}\n`;
-        const updatedThemes = updateThemes(pendingUpdates);
-        for (const updatedTheme of Object.entries(updatedThemes)) {
-          const [file, themeObj] = updatedTheme;
-          // call fs module to write updates
-          //write the themeObj to the file using the fs module
-        }
-        chalk
-          .hex('#2cbb69')
-          .bold(
-            `\nThemes updated! Please check the result and commit the updates if neccessary.\n`
-          );
-        break;
-      case 'no' || 'n':
-        chalk.cyan('Exiting... See you later!');
-        process.exit(0);
-        break;
-      default:
-        chalk.cyan(
-          `Umm... Enter ( ${YES_OR_NO} ) to continue, or ${logInputOption(
-            'ctrl + c'
-          )} to exit.`
-        );
-        break;
-    }
-    rl.prompt();
-  }).on('close', () => {
-    chalk.cyan('Exiting... See you later!');
-    process.exit(0);
-  });
+  runUpdateThemesCLI(pendingUpdates);
 }
