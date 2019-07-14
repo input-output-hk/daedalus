393f08bc34501eb148fb42650cc05cf1a983bde4
MarcusHurney <marcushurney@gmail.com>
2019-07-13 22:25:23 -0700

    [DDW-697] Adds createTheme params for each existing theme to constants.js for use in themes:update script

--- source/renderer/app/themes/utils/constants.js
+++ source/renderer/app/themes/utils/constants.js
@@ -4,6 +4,8 @@ import {
   createReactPolymorphTheme,
   createDaedalusComponentsTheme,
 } from './createTheme';
+import type { PartialThemeParts } from './createTheme';
+import type { CreateThemeParams } from '../types';
 
 export const DEFAULT_FONTS = {
   black: 'NotoSans-Black, NotoSansCJKjp-Black',
@@ -18,7 +20,7 @@ export const DEFAULT_FONTS = {
   ultralight: 'NotoSans-ExtraLight, NotoSansCJKjp-Thin',
 };
 
-export const CREATE_THEME_MOCK_PARAMS = {
+export const CREATE_THEME_MOCK_PARAMS: PartialThemeParts = {
   colors: {
     border: '#eee',
     error: createErrorShades('#eee'),
@@ -38,4 +40,55 @@ export const CREATE_THEME_MOCK_PARAMS = {
 export const CREATE_THEME_OBJ = {
   ...createReactPolymorphTheme(CREATE_THEME_MOCK_PARAMS),
   ...createDaedalusComponentsTheme(CREATE_THEME_MOCK_PARAMS),
+};
+
+export const CREATE_CARDANO_THEME_PARAMS: CreateThemeParams = {
+  colors: {
+    background: {
+      primary: createBackgroundShades('#efefef'),
+      secondary: createBackgroundShades('#2cbb69'),
+    },
+    border: '#c6cdd6',
+    error: createErrorShades('#ea4c5b'),
+    focus: '#5e6066',
+    text: {
+      primary: '#5e6066',
+      secondary: '#fafbfc',
+    },
+  },
+  fonts: DEFAULT_FONTS,
+};
+
+export const CREATE_LIGHT_BLUE_THEME_PARAMS: CreateThemeParams = {
+  colors: {
+    background: {
+      primary: createBackgroundShades('#ebeff2'),
+      secondary: createBackgroundShades('#2f496e'),
+    },
+    border: '#c6cdd6',
+    error: createErrorShades('#ea4c5b'),
+    focus: '#5e6066',
+    text: {
+      primary: '#5e6066',
+      secondary: '#fafbfc',
+    },
+  },
+  fonts: DEFAULT_FONTS,
+};
+
+export const CREATE_DARK_BLUE_THEME_PARAMS: CreateThemeParams = {
+  colors: {
+    background: {
+      primary: createBackgroundShades('#263345'),
+      secondary: createBackgroundShades('#536370'),
+    },
+    border: 'rgba(102, 122, 138, 0.3)',
+    error: createErrorShades('#ea4c5b'),
+    focus: '#667a8a',
+    text: {
+      primary: '#e9f4fe',
+      secondary: '#fafbfc',
+    },
+  },
+  fonts: DEFAULT_FONTS,
 };
