a24ad1776798beb1dcbae0944d3d1c1465da05b0
MarcusHurney <marcushurney@gmail.com>
2019-07-14 16:14:37 -0700

    [DDW-697] Cleanup in updateThemes.js

--- source/renderer/app/themes/utils/updateThemes.js
+++ source/renderer/app/themes/utils/updateThemes.js
@@ -1,142 +1,22 @@
 /* eslint-disable no-console */
 // @flow
-import { has, isEmpty } from 'lodash';
-import chalk from 'chalk';
-import { findMissingDefinitions, findMissingCSSVars } from './checkCreateTheme';
+import { isEmpty } from 'lodash';
+import { updateTheme } from './createTheme';
+// these throw the error
 import { CARDANO_THEME_CONFIG } from '../daedalus/cardano';
 import { DARK_BLUE_THEME_CONFIG } from '../daedalus/dark-blue';
 import { LIGHT_BLUE_THEME_CONFIG } from '../daedalus/light-blue';
-import type {
-  DaedalusThemesUpdates,
-  FindUpdatesParams,
-  LogDifferencesParams,
-} from '../types';
+import type { PendingThemesUpdates } from '../types';
 
-const logDifferences = ({
-  color,
-  missingDefs,
-  themeName,
-}: LogDifferencesParams) => {
-  // $FlowFixMe
-  const message = chalk`\n{underline ${themeName}} is missing the following definitions that exist on the {bold createTheme} object:\n\n${JSON.stringify(
-    missingDefs,
-    0,
-    2
-  )}\n`;
-  return console.log(chalk.hex(color)(message));
-};
-
-// Checks for properties/CSS vars on createThemeObj that don't exist on existing themes
-export const findUpdates = (
-  createThemeOutputs: FindUpdatesParams
-): null | Object => {
-  const { cardano, darkBlue, lightBlue } = createThemeOutputs;
-  const daedalusThemesUpdates = {};
-
-  const cardanoDefsToAdd = {
-    ...findMissingDefinitions(cardano, CARDANO_THEME_CONFIG),
-    ...findMissingCSSVars(cardano, CARDANO_THEME_CONFIG),
-  };
-  const darkBlueDefsToAdd = {
-    ...findMissingDefinitions(darkBlue, DARK_BLUE_THEME_CONFIG),
-    ...findMissingCSSVars(darkBlue, DARK_BLUE_THEME_CONFIG),
-  };
-  const lightBlueDefsToAdd = {
-    ...findMissingDefinitions(lightBlue, LIGHT_BLUE_THEME_CONFIG),
-    ...findMissingCSSVars(lightBlue, LIGHT_BLUE_THEME_CONFIG),
-  };
-
-  if (!isEmpty(cardanoDefsToAdd)) {
-    logDifferences({
-      color: '#2cbb69',
-      missingDefs: cardanoDefsToAdd,
-      themeName: 'cardano.js',
-    });
-    daedalusThemesUpdates.cardanoUpdates = cardanoDefsToAdd;
-  }
-
-  if (!isEmpty(darkBlueDefsToAdd)) {
-    logDifferences({
-      color: '#2874A6',
-      missingDefs: darkBlueDefsToAdd,
-      themeName: 'dark-blue.js',
-    });
-    daedalusThemesUpdates.darkBlueUpdates = darkBlueDefsToAdd;
-  }
-
-  if (!isEmpty(lightBlueDefsToAdd)) {
-    logDifferences({
-      color: '#33C4FF',
-      missingDefs: lightBlueDefsToAdd,
-      themeName: 'light-blue.js',
-    });
-    daedalusThemesUpdates.lightBlueUpdates = lightBlueDefsToAdd;
-  }
-
-  if (
-    isEmpty(cardanoDefsToAdd) &&
-    isEmpty(darkBlueDefsToAdd) &&
-    isEmpty(lightBlueDefsToAdd)
-  ) {
-    console.log(
-      chalk.hex('#2cbb69')(
-        `\n${chalk.bold.inverse(
-          '*** All Daedalus themes are up to date with the createTheme object! ***'
-        )}`
-      )
-    );
-    return null;
-  }
-  return daedalusThemesUpdates;
-};
-
-export const updateTheme = (existingTheme: Object, themeUpdates: Object) => {
-  const updatedTheme = Object.entries(themeUpdates).reduce(
-    (theme: Object, newEntry: [string, Object]) => {
-      const [keyName, newCSSVars] = newEntry;
-      if (keyName && has(theme, keyName)) {
-        return {
-          ...theme,
-          [keyName]: {
-            ...theme[keyName],
-            ...newCSSVars,
-          },
-        };
-      }
-      if (keyName && !has(theme, keyName)) {
-        return {
-          ...theme,
-          [keyName]: {
-            ...newCSSVars,
-          },
-        };
-      }
-      return theme;
-    },
-    { ...existingTheme }
-  );
-  return updatedTheme;
-};
-
-export const updateThemes = (daedalusThemesUpdates: DaedalusThemesUpdates) => {
-  const {
-    cardanoUpdates,
-    darkBlueUpdates,
-    lightBlueUpdates,
-  } = daedalusThemesUpdates;
+export const updateThemes = (pendingUpdates: PendingThemesUpdates) => {
+  const { cardanoUpdates, darkBlueUpdates, lightBlueUpdates } = pendingUpdates;
   const updatedThemes = {};
   if (cardanoUpdates && !isEmpty(cardanoUpdates)) {
     const updatedCardanoTheme = updateTheme(
       CARDANO_THEME_CONFIG,
       cardanoUpdates
     );
-    updateThemes['cardano'] = updatedCardanoTheme;
-
-    // write updatedCardanoTheme theme object to cardano.js
-    // $FlowFixMe
-    // console.log(
-    //   `cardano theme updated!\n${JSON.stringify(updatedCardanoTheme, 0, 2)}`
-    // );
+    updateThemes.cardano = updatedCardanoTheme;
   }
 
   if (darkBlueUpdates && !isEmpty(darkBlueUpdates)) {
@@ -170,5 +50,5 @@ export const updateThemes = (daedalusThemesUpdates: DaedalusThemesUpdates) => {
     //   )}`
     // );
   }
-  return updateThemes;
+  return updatedThemes;
 };
