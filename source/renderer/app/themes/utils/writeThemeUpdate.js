5b485fa0d598ced4b67f52a22d6cc8df8fb1197d
MarcusHurney <marcushurney@gmail.com>
2019-07-16 16:24:01 -0700

    [DDW-697] Adds writeFileSync call to writeThemeUpdate.js

--- source/renderer/app/themes/utils/writeThemeUpdate.js
+++ source/renderer/app/themes/utils/writeThemeUpdate.js
@@ -1,14 +1,36 @@
 // @flow
 /* eslint-disable */
 import fs from 'fs';
-import chalk from 'chalk';
+import path from 'path';
 import type { WriteThemeUpdateParams } from '../types';
 
 export const writeThemeUpdate = ({
   fileName,
   updatedThemeObj,
 }: WriteThemeUpdateParams) => {
-  console.log(`fileName: ${fileName}`);
-  console.log(`updatedThemeObj: ${updatedThemeObj}`);
-  // create write file logic
+  const THEME_FILE = path.join(
+    __dirname,
+    `../../source/renderer/app/themes/daedalus/${fileName}.js`
+  );
+  const TEMP_THEME_FILE = path.join(__dirname, `../daedalus/${fileName}.js`);
+  const fileNameParts = fileName.split('-');
+  fs.writeFileSync(
+    TEMP_THEME_FILE,
+    `
+    // @flow
+    import chroma from 'chroma-js';
+    import { createTheme } from '../utils/createTheme';
+
+    export const CARDANO_THEME_CONFIG = ${JSON.stringify(
+      updatedThemeObj,
+      null,
+      2
+    )};
+  
+    export default createTheme({
+      config: CARDANO_THEME_CONFIG
+    });
+  `,
+    {}
+  );
 };
