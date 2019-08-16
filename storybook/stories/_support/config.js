f69a0b3230508e297437a9e26039796e67b2a01e
Danilo Prates <daniloprates@gmail.com>
2019-08-15 20:33:00 -0400

    [DDW-820] Daedalus Menu reorganized && general settings

---
+++ storybook/stories/_support/config.js
@@ -0,0 +1,66 @@
+// @flow
+import cardano from '../../../source/renderer/app/themes/daedalus/cardano.js';
+import darkBlue from '../../../source/renderer/app/themes/daedalus/dark-blue.js';
+import lightBlue from '../../../source/renderer/app/themes/daedalus/light-blue.js';
+import darkCardano from '../../../source/renderer/app/themes/daedalus/dark-cardano.js';
+import white from '../../../source/renderer/app/themes/daedalus/white.js';
+import yellow from '../../../source/renderer/app/themes/daedalus/yellow.js';
+
+export const themes = {
+  Cardano: cardano,
+  DarkBlue: darkBlue,
+  LightBlue: lightBlue,
+  DarkCardano: darkCardano,
+  Yellow: yellow,
+  White: white,
+};
+export const themeNames = Object.keys(themes);
+
+export const locales = {
+  English: 'en-US',
+  Japanese: 'ja-JP',
+};
+export const localeNames = Object.keys(locales);
+
+export const operatingSystems = {
+  Windows: 'windows',
+  Linux: 'linux',
+  Mac: 'mac',
+};
+export const osNames = Object.keys(operatingSystems);
+
+// These differences are due to the different menu heights on each OS
+export const osMinWindowHeights = {
+  Windows: '541px',
+  Linux: '560px',
+  Mac: '600px',
+};
+
+/* eslint-disable no-restricted-globals */
+const getParams = (param: string) => {
+  const { hash, search } = parent.window.location;
+  const queries = hash || search;
+  const params = new URLSearchParams(queries.slice(1));
+  return params.get(param);
+};
+
+export const getInitialState = () => {
+  const themeName =
+    getParams('themeName') ||
+    sessionStorage.getItem('themeName') ||
+    themeNames[0];
+
+  const localeName =
+    getParams('localeName') ||
+    sessionStorage.getItem('localeName') ||
+    localeNames[0];
+
+  const osName =
+    getParams('osName') || sessionStorage.getItem('osName') || osNames[0];
+
+  return {
+    themeName,
+    localeName,
+    osName,
+  };
+};
--- storybook/stories/settings/Settings-General.stories.js
+++ storybook/stories/settings/Settings-General.stories.js
@@ -2,27 +2,21 @@
 import React from 'react';
 import { boolean } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
-// import StoryDecorator from '../_support/StoryDecorator';
 import SettingsWrapper from './SettingsWrapper';
 import { LANGUAGE_OPTIONS } from '../../../source/renderer/app/config/profileConfig';
-import {
-  updateParam,
-  // onReceiveParam,
-} from '../../addons/DaedalusMenu';
+import { updateParam } from '../../addons/DaedalusMenu';
+import { locales } from '../_support/config';
 
 // Screens
 import GeneralSettings from '../../../source/renderer/app/components/settings/categories/GeneralSettings';
 
-// export const updateParam = (query: Object) =>
-//   channel.emit('daedalusMenu/updateParam', query);
-
-const locales = {
+const localeNames = {
   'en-US': 'English',
   'ja-JP': 'Japanese',
 };
 
 /* eslint-disable consistent-return */
-storiesOf('SETTINGS|General', module)
+storiesOf('SETTINGS|Settings', module)
   .addDecorator(SettingsWrapper)
 
   // ====== Stories ======
@@ -30,9 +24,9 @@ storiesOf('SETTINGS|General', module)
   .add('General', () => (
     <GeneralSettings
       languages={LANGUAGE_OPTIONS}
-      currentLocale="en-US"
+      currentLocale={locales[sessionStorage.localeName]}
       onSelectLanguage={({ locale }) =>
-        updateParam({ localeName: locales[locale] })
+        updateParam({ param: 'localeName', value: localeNames[locale] })
       }
       isSubmitting={boolean('isSubmitting', false)}
     />
