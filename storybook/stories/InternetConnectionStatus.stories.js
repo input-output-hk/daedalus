77cbcae008d5efbf69b7dc53678b57f048ff5ef6
Tomislav Horaček <tomislav.horacek@thespian.hr>
2019-08-28 13:40:56 +0200

    [DDW-823] Add story with re-check internet connection spinner

--- storybook/stories/InternetConnectionStatus.stories.js
+++ storybook/stories/InternetConnectionStatus.stories.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import StoryDecorator from './support/StoryDecorator';
 import InternetConnectionOfflineStatus from '../../source/renderer/app/components/status/InternetConnectionOfflineStatus';
 
@@ -9,13 +10,16 @@ storiesOf('Internet Connection Status', module)
 
   // ====== Stories ======
 
-  .add(
-    'Offline',
-    () => (
-      <InternetConnectionOfflineStatus
-        checking={false}
-        checkAgain={() => null}
-      />
-    ),
-    { id: 'offline' }
-  );
+  .add('Offline - default', () => (
+    <InternetConnectionOfflineStatus
+      checking={false}
+      checkAgain={action('checkAgain')}
+    />
+  ))
+
+  .add('Offline - with checking spinner', () => (
+    <InternetConnectionOfflineStatus
+      checking
+      checkAgain={action('checkAgain')}
+    />
+  ));
