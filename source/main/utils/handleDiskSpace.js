41b9f021c951ac0eae4a8b3dcdfa4f23bc1778d9
Tomislav Horaček <tomislav.horacek@thespian.hr>
2019-09-10 13:27:00 +0200

    [DDW-892] Add disk space checker into try<->catche to avoid app crash

--- source/main/utils/handleDiskSpace.js
+++ source/main/utils/handleDiskSpace.js
@@ -24,58 +24,76 @@ export const handleDiskSpace = (
 
   const handleCheckDiskSpace = async (forceDiskSpaceRequired?: number) => {
     const diskSpaceRequired = forceDiskSpaceRequired || DISK_SPACE_REQUIRED;
-    const {
-      free: diskSpaceAvailable,
-      size: diskTotalSpace,
-    } = await checkDiskSpace(stateDrive);
-    const diskSpaceMissing = Math.max(
-      diskSpaceRequired - diskSpaceAvailable,
-      0
-    );
-    const diskSpaceRecommended =
-      (diskTotalSpace * DISK_SPACE_RECOMMENDED_PERCENTAGE) / 100;
-    const diskSpaceRequiredMargin =
-      diskSpaceRequired -
-      (diskSpaceRequired * DISK_SPACE_REQUIRED_MARGIN_PERCENTAGE) / 100;
+    try {
+      const {
+        free: diskSpaceAvailable,
+        size: diskTotalSpace,
+      } = await checkDiskSpace(stateDrive);
+      const diskSpaceMissing = Math.max(
+        diskSpaceRequired - diskSpaceAvailable,
+        0
+      );
+      const diskSpaceRecommended =
+        (diskTotalSpace * DISK_SPACE_RECOMMENDED_PERCENTAGE) / 100;
+      const diskSpaceRequiredMargin =
+        diskSpaceRequired -
+        (diskSpaceRequired * DISK_SPACE_REQUIRED_MARGIN_PERCENTAGE) / 100;
 
-    if (diskSpaceAvailable <= diskSpaceRequiredMargin) {
-      if (!isNotEnoughDiskSpace) {
-        // State change: transitioning from enough to not-enough disk space
-        setDiskSpaceIntervalChecking(DISK_SPACE_CHECK_SHORT_INTERVAL);
-        isNotEnoughDiskSpace = true;
+      if (diskSpaceAvailable <= diskSpaceRequiredMargin) {
+        if (!isNotEnoughDiskSpace) {
+          // State change: transitioning from enough to not-enough disk space
+          setDiskSpaceIntervalChecking(DISK_SPACE_CHECK_SHORT_INTERVAL);
+          isNotEnoughDiskSpace = true;
+        }
+      } else if (diskSpaceAvailable >= diskSpaceRequired) {
+        const newDiskSpaceCheckIntervalLength =
+          diskSpaceAvailable >= diskSpaceRequired * 2
+            ? DISK_SPACE_CHECK_LONG_INTERVAL
+            : DISK_SPACE_CHECK_MEDIUM_INTERVAL;
+        if (isNotEnoughDiskSpace) {
+          // State change: transitioning from not-enough to enough disk space
+          setDiskSpaceIntervalChecking(newDiskSpaceCheckIntervalLength);
+          isNotEnoughDiskSpace = false;
+        } else if (
+          newDiskSpaceCheckIntervalLength !== diskSpaceCheckIntervalLength
+        ) {
+          // Interval change: transitioning from medium to long interval (or vice versa)
+          // This is a special case in which we adjust the disk space check polling interval:
+          // - more than 2x of available space than required: LONG interval
+          // - less than 2x of available space than required: MEDIUM interval
+          setDiskSpaceIntervalChecking(newDiskSpaceCheckIntervalLength);
+        }
       }
-    } else if (diskSpaceAvailable >= diskSpaceRequired) {
-      const newDiskSpaceCheckIntervalLength =
-        diskSpaceAvailable >= diskSpaceRequired * 2
-          ? DISK_SPACE_CHECK_LONG_INTERVAL
-          : DISK_SPACE_CHECK_MEDIUM_INTERVAL;
-      if (isNotEnoughDiskSpace) {
-        // State change: transitioning from not-enough to enough disk space
-        setDiskSpaceIntervalChecking(newDiskSpaceCheckIntervalLength);
-        isNotEnoughDiskSpace = false;
-      } else if (
-        newDiskSpaceCheckIntervalLength !== diskSpaceCheckIntervalLength
-      ) {
-        // Interval change: transitioning from medium to long interval (or vice versa)
-        // This is a special case in which we adjust the disk space check polling interval:
-        // - more than 2x of available space than required: LONG interval
-        // - less than 2x of available space than required: MEDIUM interval
-        setDiskSpaceIntervalChecking(newDiskSpaceCheckIntervalLength);
+
+      const response = {
+        isNotEnoughDiskSpace,
+        diskSpaceRequired: prettysize(diskSpaceRequired),
+        diskSpaceMissing: prettysize(diskSpaceMissing),
+        diskSpaceRecommended: prettysize(diskSpaceRecommended),
+        diskSpaceAvailable: prettysize(diskSpaceAvailable),
+      };
+      if (isNotEnoughDiskSpace)
+        Logger.info('Not enough disk space', { response });
+      if (typeof onCheckDiskSpace === 'function') onCheckDiskSpace(response);
+      getDiskSpaceStatusChannel.send(response, mainWindow.webContents);
+      return response;
+    } catch (error) {
+      // Remove diskSpaceCheckInterval if set
+      if (diskSpaceCheckInterval) {
+        clearInterval(diskSpaceCheckInterval);
+        // Reset to default check interval
+        diskSpaceCheckIntervalLength = DISK_SPACE_CHECK_LONG_INTERVAL;
       }
+      const response = {
+        isNotEnoughDiskSpace: false,
+        diskSpaceRequired: '? GB',
+        diskSpaceMissing: '? Bytes',
+        diskSpaceRecommended: '? GB',
+        diskSpaceAvailable: '? GB',
+      };
+      getDiskSpaceStatusChannel.send(response, mainWindow.webContents);
+      return response;
     }
-
-    const response = {
-      isNotEnoughDiskSpace,
-      diskSpaceRequired: prettysize(diskSpaceRequired),
-      diskSpaceMissing: prettysize(diskSpaceMissing),
-      diskSpaceRecommended: prettysize(diskSpaceRecommended),
-      diskSpaceAvailable: prettysize(diskSpaceAvailable),
-    };
-    if (isNotEnoughDiskSpace)
-      Logger.info('Not enough disk space', { response });
-    if (typeof onCheckDiskSpace === 'function') onCheckDiskSpace(response);
-    getDiskSpaceStatusChannel.send(response, mainWindow.webContents);
-    return response;
   };
 
   const setDiskSpaceIntervalChecking = interval => {
