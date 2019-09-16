34ae4e2df604e72c49a16b90307ae0a903c79a09
MarcusHurney <marcushurney@gmail.com>
2019-09-16 17:12:58 +0200

    [DDW-901] Fixes ESLint errors

--- storybook/stories/newsfeed/IncidentOverlay.stories.js
+++ storybook/stories/newsfeed/IncidentOverlay.stories.js
@@ -1,6 +1,9 @@
 // @flow
-// eslint-disable-file no-unused-vars
+
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import StoryDecorator from '../support/StoryDecorator';
+
+storiesOf('NewsFeed', module)
+  .addDecorator(story => <StoryDecorator>{story()}</StoryDecorator>)
+  .add('Incident Overlay', () => <div>hello</div>);
