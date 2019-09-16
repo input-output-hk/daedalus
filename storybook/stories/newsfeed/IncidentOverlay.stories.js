baba683ca0430b2e5a7d218e158691a3b2b70577
MarcusHurney <marcushurney@gmail.com>
2019-09-16 16:56:59 +0200

    [DDW-901] Disables no-unused-vars rule

--- storybook/stories/newsfeed/IncidentOverlay.stories.js
+++ storybook/stories/newsfeed/IncidentOverlay.stories.js
@@ -1,1 +1,6 @@
+// @flow
+// eslint-disable-file no-unused-vars
 import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import StoryDecorator from '../support/StoryDecorator';
