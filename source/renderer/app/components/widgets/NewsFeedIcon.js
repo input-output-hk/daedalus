4de1d3cab7ff1573a5f54a2518be0219da8e704b
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2019-09-17 18:43:53 +0200

    [DDW-901] News feed - creating News feed component - adding toggle button on loading screen

--- source/renderer/app/components/widgets/NewsFeedIcon.js
+++ source/renderer/app/components/widgets/NewsFeedIcon.js
@@ -1,19 +1,26 @@
 // @flow
 import React, { Component } from 'react';
 import SVGInline from 'react-svg-inline';
+import classNames from 'classnames';
+import newsFeedBellIcon from '../../assets/images/top-bar/bell.inline.svg';
 import styles from './NewsFeedIcon.scss';
 
 type Props = {
   onNewsFeedIconClick: Function,
-  newsFeedBellIcon: string,
+  newsFeedBellIconClass?: string,
 };
 
 export default class NewsFeedIcon extends Component<Props> {
   render() {
-    const { onNewsFeedIconClick, newsFeedBellIcon } = this.props;
+    const { onNewsFeedIconClick, newsFeedBellIconClass } = this.props;
+
+    const componentClasses = classNames([
+      styles.component,
+      newsFeedBellIconClass,
+    ]);
 
     return (
-      <button className={styles.component} onClick={onNewsFeedIconClick}>
+      <button className={componentClasses} onClick={onNewsFeedIconClick}>
         <SVGInline className={styles.icon} svg={newsFeedBellIcon} />
       </button>
     );
