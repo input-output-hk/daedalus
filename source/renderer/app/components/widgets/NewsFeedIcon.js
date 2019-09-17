9b9eaed418f1e3ddf2a57ae63afcdd513d067f6d
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2019-09-17 18:03:15 +0200

    [DDW-901] News feed - creating News feed component - adding toggle button on top bar

--- source/renderer/app/components/widgets/NewsFeedIcon.js
+++ source/renderer/app/components/widgets/NewsFeedIcon.js
@@ -1,7 +1,6 @@
 // @flow
 import React, { Component } from 'react';
 import SVGInline from 'react-svg-inline';
-import classNames from 'classnames';
 import styles from './NewsFeedIcon.scss';
 
 type Props = {
@@ -12,10 +11,9 @@ type Props = {
 export default class NewsFeedIcon extends Component<Props> {
   render() {
     const { onNewsFeedIconClick, newsFeedBellIcon } = this.props;
-    const componentClasses = classNames([styles.component]);
 
     return (
-      <button className={componentClasses} onClick={onNewsFeedIconClick}>
+      <button className={styles.component} onClick={onNewsFeedIconClick}>
         <SVGInline className={styles.icon} svg={newsFeedBellIcon} />
       </button>
     );
