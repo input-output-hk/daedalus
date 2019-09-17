5c208c9e59d1c0a465bc22ed4f0e9e688ccb7bc2
Tomislav Horaček <tomislav.horacek@thespian.hr>
2019-09-17 13:21:00 +0200

    [DDW-901] NewsFeed store improvement

--- source/renderer/app/stores/NewsFeedStore.js
+++ source/renderer/app/stores/NewsFeedStore.js
@@ -1,5 +1,6 @@
 // @flow
-import { observable, action } from 'mobx';
+import { observable, action, runInAction, computed } from 'mobx';
+import { map, get } from 'lodash';
 import Store from './lib/Store';
 import Request from './lib/LocalizedRequest';
 import { NEWS_POLL_INTERVAL } from '../config/timingConfig';
@@ -8,8 +9,10 @@ import type { GetNewsResponse } from '../api/news/types';
 const { isTest } = global.environment;
 
 export default class NewsFeedStore extends Store {
-  @observable newsFeed = [];
+  @observable newsData = null;
   @observable newsUpdatedAt = null;
+  // @TODO - jsut if we don't have a data - show error
+  @observable fetchingNewsFailed = false;
   @observable
   getNewsRequest: Request<GetNewsResponse> = new Request(
     this.api.ada.getNews
@@ -24,15 +27,36 @@ export default class NewsFeedStore extends Store {
     }
   }
 
-  getNews = async () => {
+  @action getNews = async () => {
     const result = await this.getNewsRequest.execute().promise;
+    console.debug('>>> execute newsData');
     if (result) {
-      this._setNewsFeed(result);
+      console.debug('>>> SET newsData');
+      runInAction('set news data', () => {
+        this.newsData = result;
+      });
     }
   };
 
-  @action _setNewsFeed = (newsFeed) => {
-    this.newsFeed = newsFeed.items;
-    this.newsUpdatedAt = newsFeed.updatedAt;
-  };
+  @computed get newsFeedData(): any {
+    console.debug('@computed: ', this.newsData);
+    const currentLocale = this.stores.profile.currentLocale;
+    const newsFeedItems = get(this.newsData, ['result', 'items']);
+    const newsCreatedAt = get(this.newsData, ['result', 'createdAt']);
+
+    // @TODO - check news stored in local storage, comppare dates and merge data if is needed
+
+    return map(newsFeedItems, (item) => ({
+      ...item,
+      title: item.title[currentLocale],
+      content: item.title[currentLocale],
+      action: {
+        ...item.action,
+        label: item.action.label[currentLocale],
+        url: get(item, ['action', 'url', currentLocale]),
+      },
+      read: false // @TODO - check in LC
+    }));
+
+  }
 }
