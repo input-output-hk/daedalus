d60950cb9732d5cff8cb5f9830889b786402177e
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2019-09-18 16:18:57 +0200

    [DDW-901] News feed - creating News feed component - News Item updates

--- source/renderer/app/themes/daedalus/cardano.js
+++ source/renderer/app/themes/daedalus/cardano.js
@@ -378,7 +378,8 @@ export const CARDANO_THEME_OUTPUT = {
     '--theme-news-item-action-button-border-color': '#fafbfc',
     '--theme-news-item-action-button-color': '#fafbfc',
     '--theme-news-item-alert-background-color': 'rgba(242, 162, 24, 0.5)',
-    '--theme-news-item-announcement-background-color': 'rgba(234, 76, 91, 0.25)',
+    '--theme-news-item-announcement-background-color':
+      'rgba(234, 76, 91, 0.25)',
     '--theme-news-item-badge-color': '#ea4c5b',
     '--theme-news-item-info-background-color': 'rgba(0, 0, 0, 0.1)',
     '--theme-news-item-title-color': '#fafbfc',
