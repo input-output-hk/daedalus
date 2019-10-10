75645e222ff1cdc328db867333fc7757e1278f58
MarcusHurney <marcushurney@gmail.com>
2019-10-10 16:09:20 -0400

    [DDW-820] Prevents 'No Preview Message' from popping up

--- storybook/stories/staking/Staking.stories.js
+++ storybook/stories/staking/Staking.stories.js
@@ -65,7 +65,7 @@ const decorator = (story, context) => {
             <StakingWithNavigation
               isActiveNavItem={item => item === getItemFromContext()}
               activeItem={getItemFromContext()}
-              onNavItemClick={linkTo('Staking', item => pageNames[item])}
+              onNavItemClick={() => {}}
             >
               {storyWithKnobs}
             </StakingWithNavigation>
