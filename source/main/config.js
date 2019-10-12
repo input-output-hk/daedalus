930309acbae4e0d2e30fa56876e05b7db4ebc121
darkomijic <eff547b3dbe3e08fb9d2813c2d5192771dafe9d9>
2019-10-12 15:22:15 +0200

    Set window size and position for demo.

--- source/main/config.js
+++ source/main/config.js
@@ -65,8 +65,8 @@ type WindowOptionsType = {
   icon?: string,
 };
 
-export const WINDOW_WIDTH = 1150;
-export const WINDOW_HEIGHT = 870;
+export const WINDOW_WIDTH = 1280;
+export const WINDOW_HEIGHT = 720 - 23;
 export const MIN_WINDOW_CONTENT_WIDTH = 905;
 export const MIN_WINDOW_CONTENT_HEIGHT = 600;
 
