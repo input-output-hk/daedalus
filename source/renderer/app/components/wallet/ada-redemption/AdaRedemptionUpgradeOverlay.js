e9c8ffc654287eac3b3b963baaa509e1ea37786e
MarcusHurney <marcushurney@gmail.com>
2019-10-16 23:08:48 -0400

    [DDW-955] Adds close button

--- source/renderer/app/components/wallet/ada-redemption/AdaRedemptionUpgradeOverlay.js
+++ source/renderer/app/components/wallet/ada-redemption/AdaRedemptionUpgradeOverlay.js
@@ -4,6 +4,8 @@ import moment from 'moment';
 import { observer } from 'mobx-react';
 import { get } from 'lodash';
 import SVGInline from 'react-svg-inline';
+import DialogCloseButton from '../../widgets/DialogCloseButton';
+import closeCrossThin from '../../../assets/images/close-cross-thin.inline.svg';
 import externalLinkIcon from '../../../assets/images/link-ic.inline.svg';
 import styles from './AdaRedemptionUpgradeOverlay.scss';
 
@@ -27,6 +29,10 @@ export default class AdaRedemptionUpgradeOverlay extends Component<Props> {
     }
   }
 
+  onClose = () => {
+    console.log('Should close overlay');
+  };
+
   renderInstructionsBtn = () => (
     <button
       className={styles.actionBtn}
@@ -40,6 +46,11 @@ export default class AdaRedemptionUpgradeOverlay extends Component<Props> {
   render() {
     return (
       <div className={styles.component}>
+        <DialogCloseButton
+          className={styles.closeButton}
+          icon={closeCrossThin}
+          onClose={this.onClose}
+        />
         <h1 className={styles.title}>Daedalus 0.14.0 for ada redemption</h1>
         <div
           className={styles.content}
