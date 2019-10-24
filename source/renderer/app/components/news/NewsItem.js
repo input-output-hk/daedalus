92dc8bf4a8efb809aca02eaf57054ab85f18b7e6
Dominik Guzei <dominik.guzei@gmail.com>
2019-10-24 08:30:31 +0200

    [DDW-980] Fix build mode of webpack auto dll plugin (#1606)
    * [DDW-980] Build auto dll plugin in dev mode for better debugging
    
    * [DDW-980] Add changelog entry
    
    * [DDW-980] Fix CI issues
    
    * [DDW-980] Make unit tests fast again
    
    The last major tests refactoring merged the unit and e2e tests into the same folder structure which resulted in the same cucumber setup for all tests and thus slow unit tests (because each test case had to wait for the UI to reload like in e2e).
    
    This change only reloads and waits for the UI for any scenarios tagged with e2e which solves that problem in an easy way while keeping the same folder structure (which is good IMO)
    
    * [DDW-980] Remove CI hack
    
    * [DDW-980] Keep default config of auto dll plugin in CI env

--- source/renderer/app/components/news/NewsItem.js
+++ source/renderer/app/components/news/NewsItem.js
@@ -152,7 +152,6 @@ export default class NewsItem extends Component<Props, State> {
                   'code',
                   'html',
                   'virtualHtml',
-                  'parsedHtml',
                 ]}
               />
             </div>
