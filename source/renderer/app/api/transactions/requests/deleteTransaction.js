6ad54c8511639c191ebe58f7a100d8c680e5d462
MarcusHurney <marcushurney@gmail.com>
2019-11-01 17:35:23 -0400

    [DDW-1006] Adds deleteTransaction request to api/transactions

---
+++ source/renderer/app/api/transactions/requests/deleteTransaction.js
@@ -0,0 +1,14 @@
+// @flow
+import type { RequestConfig } from '../../common/types';
+import type { DeleteTransactionRequest } from '../types';
+import { request } from '../../utils/request';
+
+export const deleteTransaction = (
+  config: RequestConfig,
+  { walletId, transactionId }: DeleteTransactionRequest
+): Promise<*> =>
+  request({
+    method: 'DELETE',
+    path: `/v2/wallets/${walletId}/transactions/${transactionId}`,
+    ...config,
+  });
