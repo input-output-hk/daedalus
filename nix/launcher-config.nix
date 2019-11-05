e7cc23d6e514cd9a21f489f430ceb97b2b2ec5d0
Michael Bishop <michael.bishop@iohk.io>
2019-11-05 19:39:18 -0400

    .

--- nix/launcher-config.nix
+++ nix/launcher-config.nix
@@ -55,7 +55,7 @@ let
     "--genesis-block-hash" "${jormungandrLib.environments.${environment}.genesisHash}"
     "--state-dir" dataDir.${os}
     "--"
-    "--config" "\${DAEDALUS_INSTALL_DIRECTORY}/jormungandr-config-${environment}.yaml"
+    "--config" "\${DAEDALUS_INSTALL_DIRECTORY}/jormungandr-config.yaml"
   ];
   walletArgs.selfnode.macos64 = [
     "launch"
