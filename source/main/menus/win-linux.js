daf300ee726a41dce72dad9c6aa4e7221e2d4840
Danilo Prates <daniloprates@gmail.com>
2019-11-08 18:55:23 -0300

    [DDW-1053] Remove 'Safe mode' application menu item

--- source/main/menus/win-linux.js
+++ source/main/menus/win-linux.js
@@ -10,7 +10,11 @@ import { showUiPartChannel } from '../ipc/control-ui-parts';
 import { generateSupportRequestLink } from '../../common/utils/reporting';
 
 const id = 'menu';
-const { isWindows, isBlankScreenFixActive } = environment;
+const {
+  isWindows,
+  isBlankScreenFixActive,
+  isIncentivizedTestnet,
+} = environment;
 
 export const winLinuxMenu = (
   app: App,
@@ -126,14 +130,16 @@ export const winLinuxMenu = (
           shell.openExternal(faqLink);
         },
       },
-      {
-        label: translation('helpSupport.blankScreenFix'),
-        type: 'checkbox',
-        checked: isBlankScreenFixActive,
-        click(item) {
-          actions.toggleBlankScreenFix(item);
-        },
-      },
+      !isIncentivizedTestnet
+        ? {
+            label: translation('helpSupport.blankScreenFix'),
+            type: 'checkbox',
+            checked: isBlankScreenFixActive,
+            click(item) {
+              actions.toggleBlankScreenFix(item);
+            },
+          }
+        : null,
       { type: 'separator' },
       {
         label: translation('helpSupport.safetyTips'),
