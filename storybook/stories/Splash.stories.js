4e3e4541a71965182073c32109cc7807ea680414
Yakov Karavelov <yakov.karavelov@iohk.io>
2019-11-08 04:47:02 -0800

    [DDW-1042]: Add network splash screen story

---
+++ storybook/stories/Splash.stories.js
@@ -0,0 +1,5 @@
+// @flow
+import { storiesOf } from '@storybook/react';
+import { SplashNetworkStory } from './Splash-Network.stories';
+
+storiesOf('Splash', module).add('Network', SplashNetworkStory);
