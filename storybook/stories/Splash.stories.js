69ff27fcdcc2c50f9a559d9fd6172a62af81b609
Yakov Karavelov <yakov.karavelov@iohk.io>
2019-11-08 07:03:16 -0800

    [DDW-1042]: Fix network splash screen story

--- storybook/stories/Splash.stories.js
+++ storybook/stories/Splash.stories.js
@@ -1,5 +1,9 @@
 // @flow
+import React from 'react';
 import { storiesOf } from '@storybook/react';
+import StoryDecorator from './support/StoryDecorator';
 import { SplashNetworkStory } from './Splash-Network.stories';
 
-storiesOf('Splash', module).add('Network', SplashNetworkStory);
+storiesOf('Splash', module)
+  .addDecorator(story => <StoryDecorator>{story()}</StoryDecorator>)
+  .add('Network', SplashNetworkStory);
