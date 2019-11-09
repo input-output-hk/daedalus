3c7468ea17a8a114386abad7f32f25bf1afa675b
Danilo Prates <daniloprates@gmail.com>
2019-11-09 13:03:33 -0300

    [DDW-1053] Correct network pretty names

--- source/common/types/environment.types.js
+++ source/common/types/environment.types.js
@@ -52,7 +52,8 @@ export const OS_NAMES = {
 };
 
 export const networkPrettyNames = {
-  production: 'Production',
+  staging: 'Staging',
+  testnet: 'Testnet',
   development: 'Development',
   itn_balance_check: 'Incentivized Testnet - Balance check',
 };
