c2b87ec68a844cb6dfd77044b0bf32e9cb6dce81
Danilo Prates <daniloprates@gmail.com>
2019-11-09 14:19:07 -0300

    DDW-1051 Update Cardano Explorer links

--- source/renderer/app/config/urlsConfig.js
+++ source/renderer/app/config/urlsConfig.js
@@ -3,6 +3,7 @@ export const MAINNET_EXPLORER_URL = 'cardanoexplorer.com';
 export const STAGING_EXPLORER_URL = 'cardano-explorer.awstest.iohkdev.io';
 export const TESTNET_EXPLORER_URL =
   'cardano-explorer.cardano-testnet.iohkdev.io';
+export const ITN_EXPLORER_URL = 'shelley-testnet-explorer-qa.netlify.com';
 
 export const DEVELOPMENT_EKG_URL = 'http://localhost:8085';
 export const STAGING_EKG_URL = 'http://localhost:8082';
@@ -28,6 +29,7 @@ export const ALLOWED_EXTERNAL_HOSTNAMES = [
   MAINNET_EXPLORER_URL,
   STAGING_EXPLORER_URL,
   TESTNET_EXPLORER_URL,
+  ITN_EXPLORER_URL,
   MAINNET_LATEST_VERSION_INFO_URL,
   TESTNET_LATEST_VERSION_INFO_URL,
   STAGING_LATEST_VERSION_INFO_URL,
