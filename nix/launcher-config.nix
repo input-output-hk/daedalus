de3e5fd215e5b5720a75131ce6c4bb4566626672
Samuel Leathers <samuel.leathers@iohk.io>
2019-11-13 12:30:22 -0500

    remove balance check from name

--- nix/launcher-config.nix
+++ nix/launcher-config.nix
@@ -14,7 +14,7 @@ let
   installDirectorySuffix.qa = "QA";
   installDirectorySuffix.selfnode = "SelfNode";
   installDirectorySuffix.nightly = "Nightly";
-  installDirectorySuffix.itn_balance_check = "Incentivized Testnet (Balance Check)";
+  installDirectorySuffix.itn_balance_check = "Incentivized Testnet";
 
   spacedName = "Daedalus ${installDirectorySuffix.${environment}}";
 
