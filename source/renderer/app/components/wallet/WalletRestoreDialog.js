5cef9231933b4cdd07b67b4074377fd83593da31
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2019-11-24 14:03:44 +0100

    [DDW-1077] - Restore a wallet wizard to be scrolled automatically when focused

--- source/renderer/app/components/wallet/WalletRestoreDialog.js
+++ source/renderer/app/components/wallet/WalletRestoreDialog.js
@@ -346,14 +346,18 @@ export default class WalletRestoreDialog extends Component<Props, State> {
   };
 
   autoScrollOnFocus = () => {
-      const scrollableDialogElement = document.querySelector(
-        SCROLLABLE_DOM_ELEMENT_SELECTOR
-      );
-      const autocompleteField = document.querySelector(
-        FOCUSED_DOM_ELEMENT_SELECTOR
-      );
-      if (!(scrollableDialogElement instanceof HTMLElement) || !(autocompleteField instanceof HTMLElement) ) return;
-      scrollableDialogElement.scrollTop = autocompleteField.offsetHeight;
+    const scrollableDialogElement = document.querySelector(
+      SCROLLABLE_DOM_ELEMENT_SELECTOR
+    );
+    const autocompleteField = document.querySelector(
+      FOCUSED_DOM_ELEMENT_SELECTOR
+    );
+    if (
+      !(scrollableDialogElement instanceof HTMLElement) ||
+      !(autocompleteField instanceof HTMLElement)
+    )
+      return;
+    scrollableDialogElement.scrollTop = autocompleteField.offsetHeight;
   };
 
   render() {
