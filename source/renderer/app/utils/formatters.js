bf159f7f4b43693ee80a4e5cd3353d05c27b7374
Nikola Glumac <niglumac@gmail.com>
2019-11-26 12:40:27 +0100

    [DDW-1080] Fix issues with number handling

--- source/renderer/app/utils/formatters.js
+++ source/renderer/app/utils/formatters.js
@@ -60,21 +60,20 @@ export const formattedWalletAmount = (
   return formattedAmount.toString();
 };
 
-export const formattedAmountToBigNumber = (amount: string) => {
-  const cleanedAmount = amount.replace(/,/g, '');
-  return new BigNumber(cleanedAmount !== '' ? cleanedAmount : 0);
-};
-
 export const formattedAmountToNaturalUnits = (amount: string): string => {
   const cleanedAmount = amount
-    .replace('.', '')
-    .replace(/,/g, '')
+    .replace(/\./g, '') // removes all the dot separators
+    .replace(/,/g, '') // removes all the comma separators
+    .replace(/\s/g, '') // removes all the space separators
     .replace(/^0+/, '');
   return cleanedAmount === '' ? '0' : cleanedAmount;
 };
 
-export const formattedAmountWithoutTrailingZeros = (amount: string): string =>
-  amount.replace(/0+$/, '').replace(/\.$/, '');
+// TODO: FIX!
+export const formattedAmountToBigNumber = (amount: string) => {
+  const cleanedAmount = amount.replace(/,/g, '');
+  return new BigNumber(cleanedAmount !== '' ? cleanedAmount : 0);
+};
 
 export const formattedAmountToLovelace = (amount: string): number =>
   parseInt(formattedAmountToBigNumber(amount).times(LOVELACES_PER_ADA), 10);
@@ -83,6 +82,7 @@ export const formattedLovelaceToAmount = (lovelace: number): number =>
   formattedAmountToBigNumber(String(lovelace))
     .dividedBy(LOVELACES_PER_ADA)
     .toNumber();
+// TODO: FIX!
 
 export const formattedBytesToSize = (bytes: number): string => {
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
