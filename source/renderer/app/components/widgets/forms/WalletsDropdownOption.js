c03655af3cba3d24c29fc6994888984813d3bf5e
MarcusHurney <marcushurney@gmail.com>
2019-11-26 03:55:05 -0500

    [DDW-1026] Renders active delegation slug w / correct color

--- source/renderer/app/components/widgets/forms/WalletsDropdownOption.js
+++ source/renderer/app/components/widgets/forms/WalletsDropdownOption.js
@@ -1,15 +1,32 @@
 // @flow
 import React, { Component } from 'react';
 import classnames from 'classnames';
+import { getColorFromRange } from '../../../utils/colors';
 import styles from './WalletsDropdownOption.scss';
+import type { StakePool } from '../../../api/staking/types';
 
 export type WalletOption = {
-  label: string,
+  activeDelegation?: StakePool,
   detail: string,
+  label: string,
+  numberOfStakePools?: number,
   selected?: boolean,
 };
 
 export default class WalletsDropdownOption extends Component<WalletOption> {
+  renderActiveDelegationSlug = () => {
+    const { activeDelegation, numberOfStakePools } = this.props;
+    if (!activeDelegation || !numberOfStakePools) return null;
+
+    const { ranking, slug } = activeDelegation;
+    const color = getColorFromRange(ranking, numberOfStakePools);
+    return (
+      <div style={{ color }} className={styles.slug}>
+        [{slug}]
+      </div>
+    );
+  };
+
   render() {
     const { label, detail, selected } = this.props;
     const componentStyles = classnames(styles.component, {
@@ -17,6 +34,7 @@ export default class WalletsDropdownOption extends Component<WalletOption> {
     });
     return (
       <div className={componentStyles}>
+        {this.renderActiveDelegationSlug()}
         <div className={styles.label}>{label}</div>
         <div className={styles.detail}>{detail}</div>
       </div>
