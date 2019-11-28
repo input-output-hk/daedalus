908159ad182a9475ebd21942a3004c59154e9635
Danilo <daniloprates@gmail.com>
2019-11-28 16:37:30 -0300

    [DDW-971] Implement new Wallet Receive screen (#1700)
    * [DDW-971] Init
    
    * [DDW-971] CHANGELOG Entry
    
    * [DDW-971] Wallet receive component - progress
    
    * [DDW-971] Translation manager
    
    * [DDW-971] Wallet receive tooltip - progress
    
    * [DDW-971] Copy button for ITN
    
    * [DDW-971] Invalid address tooltip
    
    * [DDW-971] Middle ellipsis
    
    * [DDW-971] Flow errors
    
    * [DDW-971] Translation manager
    
    * [DDW-971] Share address dialog - init
    
    * [DDW-971] Copy address and error message
    
    * [DDW-971] Save PDF - init
    
    * [DDW-971] Save PDF - progress
    
    * [DDW-971] PDF Content
    
    * [DDW-971] WalletReceiveDialog new layout
    
    * [DDW-971] Treat textarea value
    
    * [DDW-971] Updates PDF content
    
    * [DDW-971] Jp fonts and translations
    
    * [DDW-971] Styling adjustments, En copy and Jp translations
    
    * [DDW-971] PDF settings and locales
    
    * [DDW-971] Update E2E Tests
    
    * [DDW-971] Update E2E Tests
    
    * [DDW-971] Increase minimum document height
    
    * [DDW-971] Restore invalid address error mesage
    
    * [DDW-971] Keep active wallet in inner state to enable notification close when store removes them, fix window resize event listener on addresses
    
    * [DDW-971] Small code improvement for Address component if/else clause
    
    * [DDW-971] Fixes breakpoints on ITN
    
    * [DDW-971] Fix responsive issues
    
    * [DDW-971] Add PDF dialog storybook and remove incorrect border
    
    * [DDW-971] Fixes flickering mouse issue
    
    * [DDW-971] Prevents address selection with ellipsis

---
+++ source/renderer/app/components/wallet/receive/WalletReceiveDialog.js
@@ -0,0 +1,161 @@
+// @flow
+import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+import { defineMessages, intlShape } from 'react-intl';
+import CopyToClipboard from 'react-copy-to-clipboard';
+import SVGInline from 'react-svg-inline';
+import { TextArea } from 'react-polymorph/lib/components/TextArea';
+import { TextAreaSkin } from 'react-polymorph/lib/skins/simple/TextAreaSkin';
+import QRCode from 'qrcode.react';
+import Dialog from '../../widgets/Dialog';
+import DialogCloseButton from '../../widgets/DialogCloseButton';
+import WalletAddress from '../../../domains/WalletAddress';
+import globalMessages from '../../../i18n/global-messages';
+import styles from './WalletReceiveDialog.scss';
+import ReactToolboxMobxForm from '../../../utils/ReactToolboxMobxForm';
+import iconCopy from '../../../assets/images/clipboard-ic.inline.svg';
+
+const messages = defineMessages({
+  inputLabel: {
+    id: 'wallet.receive.dialog.inputLabel',
+    defaultMessage: '!!!PDF note',
+    description: 'placeholder on the wallet "Share Address" dialog',
+  },
+  inputPlaceholder: {
+    id: 'wallet.receive.dialog.inputPlaceholder',
+    defaultMessage: '!!!Add a note to the sender',
+    description: 'inputPlaceholder on the wallet "Share Address" dialog',
+  },
+  downloadPDFButton: {
+    id: 'wallet.receive.dialog.downloadPDFButton',
+    defaultMessage: '!!!Download as PDF',
+    description: 'downloadPDFButton on the wallet "Share Address" dialog',
+  },
+  dialogTitle: {
+    id: 'wallet.receive.dialog.dialogTitle',
+    defaultMessage: '!!!Share wallet address',
+    description: 'dialogTitle on the wallet "Share Address" dialog',
+  },
+  copyAddressLabel: {
+    id: 'wallet.receive.dialog.copyAddressLabel',
+    defaultMessage: '!!!Copy address',
+    description: 'Label for "Copy address" link on the wallet "Receive page"',
+  },
+});
+
+messages.fieldIsRequired = globalMessages.fieldIsRequired;
+
+type Props = {
+  address: WalletAddress,
+  onCopyAddress: Function,
+  onDownloadPDF: Function,
+  onClose: Function,
+};
+
+@observer
+export default class WalletReceiveDialog extends Component<Props> {
+  static contextTypes = {
+    intl: intlShape.isRequired,
+  };
+
+  form = new ReactToolboxMobxForm({
+    fields: {
+      noteInput: {
+        value: '',
+        label: this.context.intl.formatMessage(messages.inputLabel),
+        placeholder: this.context.intl.formatMessage(messages.inputPlaceholder),
+      },
+    },
+  });
+
+  submit = () => {
+    this.form.submit({
+      onSuccess: form => {
+        const { noteInput } = form.values();
+        const { onDownloadPDF, onClose } = this.props;
+        onDownloadPDF(noteInput);
+        onClose();
+      },
+      onError: err => {
+        throw new Error(err);
+      },
+    });
+  };
+
+  handleChange = (field: { value: string }) => {
+    field.value = field.value.replace(/\n/g, '');
+  };
+
+  render() {
+    const { address, onCopyAddress, onClose } = this.props;
+    const { intl } = this.context;
+    const noteInputField = this.form.$('noteInput');
+
+    const actions = [
+      {
+        className: 'downloadPDFButton',
+        label: intl.formatMessage(messages.downloadPDFButton),
+        onClick: this.submit,
+        primary: true,
+      },
+    ];
+
+    // Get QRCode color value from active theme's CSS variable
+    const qrCodeBackgroundColor = document.documentElement
+      ? document.documentElement.style.getPropertyValue(
+          '--theme-receive-qr-code-background-color'
+        )
+      : 'transparent';
+    const qrCodeForegroundColor = document.documentElement
+      ? document.documentElement.style.getPropertyValue(
+          '--theme-receive-qr-code-foreground-color'
+        )
+      : '#000';
+
+    return (
+      <Dialog
+        title={intl.formatMessage(messages.dialogTitle)}
+        actions={actions}
+        closeOnOverlayClick
+        onClose={onClose}
+        closeButton={<DialogCloseButton onClose={onClose} />}
+      >
+        <div className={styles.container}>
+          <div className={styles.qrCode}>
+            <QRCode
+              value={address.id}
+              bgColor={qrCodeBackgroundColor}
+              fgColor={qrCodeForegroundColor}
+              size={192}
+            />
+          </div>
+
+          <div className={styles.address}>{address.id}</div>
+
+          <CopyToClipboard
+            text={address.id}
+            onCopy={() => onCopyAddress(address.id)}
+          >
+            <span className={styles.copyAddress}>
+              <SVGInline svg={iconCopy} className={styles.copyIcon} />
+              <span className={styles.copyAddressLabel}>
+                {intl.formatMessage(messages.copyAddressLabel)}
+              </span>
+            </span>
+          </CopyToClipboard>
+
+          <TextArea
+            className={styles.noteInput}
+            skin={TextAreaSkin}
+            autoResize={false}
+            rows={3}
+            maxLength={201}
+            {...noteInputField.bind({
+              onChange: this.handleChange(noteInputField),
+            })}
+          />
+        </div>
+      </Dialog>
+    );
+  }
+}
