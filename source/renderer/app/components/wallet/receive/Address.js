fa6e55a64b146f70e5c78b6f60e5e8b03fbbbe0b
Danilo Prates <daniloprates@gmail.com>
2019-12-06 15:28:11 -0300

    [DDW-1108] CSS Only version

--- source/renderer/app/components/wallet/receive/Address.js
+++ source/renderer/app/components/wallet/receive/Address.js
@@ -8,7 +8,6 @@ import styles from './Address.scss';
 import iconQR from '../../../assets/images/qr-code.inline.svg';
 import iconCopy from '../../../assets/images/clipboard-ic.inline.svg';
 import WalletAddress from '../../../domains/WalletAddress';
-import { ellipsis } from '../../../utils/strings';
 
 type Props = {
   address: WalletAddress,
@@ -16,23 +15,21 @@ type Props = {
   onCopyAddress: Function,
   shareAddressLabel: string,
   copyAddressLabel: string,
-  currentLocale: string,
   isIncentivizedTestnet: boolean,
-  isShowingSubMenus: boolean,
-  minCharsInit: number,
-  minCharsEnd?: ?number,
 };
 
 @observer
 export default class Address extends Component<Props> {
-  get hasEllipsis() {
-    const {
-      isIncentivizedTestnet,
-      isShowingSubMenus,
-      currentLocale,
-    } = this.props;
-    return (
-      isShowingSubMenus || (isIncentivizedTestnet && currentLocale === 'ja-JP')
+  get address() {
+    return this.props.address.id;
+  }
+  get addressBegin() {
+    return this.address.substring(0, parseInt(this.address.length / 2, 10));
+  }
+  get addressEnd() {
+    return this.address.substring(
+      parseInt(this.address.length / 2, 10),
+      this.address.length
     );
   }
 
@@ -44,26 +41,23 @@ export default class Address extends Component<Props> {
       shareAddressLabel,
       copyAddressLabel,
       isIncentivizedTestnet,
-      isShowingSubMenus,
-      minCharsInit,
-      minCharsEnd,
     } = this.props;
     const addressClasses = classnames([
       `receiveAddress-${address.id}`,
       styles.component,
       address.used ? styles.usedWalletAddress : null,
     ]);
-    const addressIdClasses = classnames([
-      styles.addressId,
-      isIncentivizedTestnet ? styles.isIncentivizedTestnet : null,
-      isShowingSubMenus ? styles.isShowingSubMenus : null,
-    ]);
     return (
       <div className={addressClasses}>
-        <div className={addressIdClasses} id={`address-${address.id}`}>
-          {this.hasEllipsis
-            ? ellipsis(address.id, minCharsInit, minCharsEnd)
-            : address.id}
+        <div
+          className={styles.addressId}
+          ref={ref => {
+            this.addressContainerElement = ref;
+          }}
+          id={`address-${address.id}`}
+        >
+          <span className={styles.addressIdBegin}>{this.addressBegin}</span>
+          <span className={styles.addressIdEnd}>{this.addressEnd}</span>
         </div>
         <div className={styles.addressActions}>
           {!isIncentivizedTestnet ? (
