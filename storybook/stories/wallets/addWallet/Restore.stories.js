bb707bdf54a76a23b37774216eba6bdc346a68b3
Danilo Prates <daniloprates@gmail.com>
2019-12-20 09:57:43 -0300

    [DDW-1156] Storybook story

--- storybook/stories/wallets/addWallet/Restore.stories.js
+++ storybook/stories/wallets/addWallet/Restore.stories.js
@@ -1,24 +1,35 @@
 // @flow
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import { boolean } from '@storybook/addon-knobs';
+import { select } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 
 // Helpers
 import WalletsWrapper from '../_utils/WalletsWrapper';
+import {
+  WALLET_KINDS,
+  WALLET_DAEDALUS_KINDS,
+  WALLET_YOROI_KINDS,
+  WALLET_HARDWARE_KINDS,
+} from '../../../../source/renderer/app/config/walletRestoreConfig';
 
 // Screens
-import WalletRestoreDialog from '../../../../source/renderer/app/components/wallet/WalletRestoreDialog';
+import StepWalletTypeDialog from '../../../../source/renderer/app/components/wallet/wallet-restore/StepWalletTypeDialog';
 
 storiesOf('Wallets|Add Wallet', module)
   .addDecorator(WalletsWrapper)
-  .add('Restore', () => (
-    <WalletRestoreDialog
-      onSubmit={action('onSubmit')}
-      onCancel={action('onCancel')}
-      isSubmitting={boolean('isSubmitting', false)}
-      mnemonicValidator={action('mnemonicValidator')}
-      suggestedMnemonics={[]}
-      onChoiceChange={action('onChoiceChange')}
+  .add('Restore - Step 1', () => (
+    <StepWalletTypeDialog
+      onContinue={action('onContinue')}
+      onClose={action('onClose')}
+      onSetWalletKind={action('onSetWalletKind')}
+      walletKind={select('Wallet Kind', WALLET_KINDS, WALLET_KINDS.DAEDALUS)}
+      walletKindDaedalus={select(
+        'Daedalus Wallet Kind',
+        WALLET_DAEDALUS_KINDS,
+        null
+      )}
+      walletKindYoroi={select('Yoroi Wallet Kind', WALLET_YOROI_KINDS, null)}
+      walletKindHardware={select('Hardware Kind', WALLET_HARDWARE_KINDS, null)}
     />
   ));
