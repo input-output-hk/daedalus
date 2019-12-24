acc375bbe4e2852de7953a18688e6df94d623045
Danilo Prates <daniloprates@gmail.com>
2019-12-23 21:30:33 -0300

    [DDW-1156] - Success screen - Storybook

--- storybook/stories/wallets/addWallet/Restore.stories.js
+++ storybook/stories/wallets/addWallet/Restore.stories.js
@@ -17,6 +17,7 @@ import {
 import WalletTypeDialog from '../../../../source/renderer/app/components/wallet/wallet-restore/WalletTypeDialog';
 import MnemonicsDialog from '../../../../source/renderer/app/components/wallet/wallet-restore/MnemonicsDialog';
 import ConfigurationDialog from '../../../../source/renderer/app/components/wallet/wallet-restore/ConfigurationDialog';
+import SuccessDialog from '../../../../source/renderer/app/components/wallet/wallet-restore/SuccessDialog';
 
 storiesOf('Wallets|Add Wallet', module)
   .addDecorator(WalletsWrapper)
@@ -96,4 +97,34 @@ storiesOf('Wallets|Add Wallet', module)
       onClose={action('onClose')}
       onBack={action('onSetWalletKind')}
     />
-  ));
+  ))
+  .add('Restore - Step 4', () => {
+    const walletKindSelect = select(
+      'Wallet Kind',
+      WALLET_KINDS,
+      WALLET_KINDS.DAEDALUS
+    );
+    let selectItems;
+    if (walletKindSelect === WALLET_KINDS.YOROI)
+      selectItems = WALLET_YOROI_KINDS;
+    else if (walletKindSelect === WALLET_KINDS.HARDWARE)
+      selectItems = WALLET_HARDWARE_KINDS;
+    else selectItems = WALLET_DAEDALUS_KINDS;
+
+    let walletKindSpecificSelect;
+    if (walletKindSelect)
+      walletKindSpecificSelect = select(
+        `Wallet Kind - ${walletKindSelect || 'Daedalus'}`,
+        selectItems,
+        Object.values(WALLET_DAEDALUS_KINDS)[0]
+      );
+    return (
+      <SuccessDialog
+        onClose={action('onClose')}
+        walletKind={walletKindSelect}
+        walletKindDaedalus={walletKindSpecificSelect}
+        walletKindYoroi={walletKindSpecificSelect}
+        walletKindHardware={walletKindSpecificSelect}
+      />
+    );
+  });
