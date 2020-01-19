0446912bdb4cdc2bc4ff65ad832b4d1bc89b138a
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2020-01-19 20:22:17 +0100

    [DDW-1149] - Implement acceptance tests for stake pools loading

--- tests/stake-pools/e2e/features/stake-pools.feature
+++ tests/stake-pools/e2e/features/stake-pools.feature
@@ -3,78 +3,51 @@ Feature: Stake Pools Loading
 
   Background:
     Given I have completed the basic setup
+    Given I am on the Delegation Centre staking page
     And I have the following wallets:
       | name          |
       | Test Wallet |
 
   Scenario: "Loading stake pools" message is shown during initial loading of stake pool data
-    Given The sidebar shows "Delegation Center" staking page icon
-    When I click on the "Delegation Center" staking page button
-    And I see the "Delegation Center" staking page
-    And I click on the "Stake Pools" tab
-    And I see the "Stake Pools" page
     Then I should see the following loading message:
       | message                               |
       | staking.stakePools.loadingStakePoolsMessage |
 
-  @watch
   Scenario: Stake pools load error handling works as expected
-    Given The sidebar shows "Delegation Center" staking page icon
-    When I click on the "Delegation Center" staking page button
-    And I see the "Delegation Center" staking page
-    And I click on the "Stake Pools" tab
-    And I see the "Stake Pools" page
-    Then I should see the following loading message:
+    And I should see the following loading message:
       | message                               |
       | staking.stakePools.loadingStakePoolsMessage |
-    Then I should see loading stake pools error message
-    And I should not see any stake pools
+    And Stake pools loading failed
+    Then I should see loading stake pools error message:
+      | message                               |
+      | staking.stakePools.loadingStakePoolsMessage |
+    And I should not see any stake pool
 
   Scenario: Stake pools are correctly ordered by rank
-    Given The sidebar shows "Delegation Center" staking page icon
-    When I click on the "Delegation Center" staking page button
-    And I see the "Delegation Center" staking page
-    And I click on the "Stake Pools" tab
-    And I see the "Stake Pools" page
     Then I should see the following loading message:
       | message                               |
       | staking.stakePools.loadingStakePoolsMessage |
     And I should see "3" stake pools loaded by rank
 
   Scenario: Stake pools search works as expected
-    Given The sidebar shows "Delegation Center" staking page icon
-    When I click on the "Delegation Center" staking page button
-    And I see the "Delegation Center" staking page
-    And I click on the "Stake Pools" tab
-    And I see the "Stake Pools" page
-    And I see the stake pools search input field
-    And I execute search in the stake pool search input field //@todo - search for xyz
-    And There are results of the search //@todo - search for xyz
-    Then I should see filtered stake pools as results //@todo - search for xyz
+    When I see the stake pools search input field
+    And I enter "ROOT" in search input field
+    Then I should see message "Stake pools. Search results: (3)"
+    And I should see "3" stake pool with slug "ROOT"
 
+  @watch
   Scenario: Stake pools user is already delegating to are correctly displayed
-    Given The sidebar shows "Delegation Center" staking page icon
-    When I click on the "Delegation Center" staking page button
-    And I see the "Delegation Center" staking page
     And I see list of wallets
     And If the user is already delegating to any of stake pools
     Then I should see the "You are already delegating..." message next to the wallet which is already delegating
 
   Scenario: Stake pool tooltip is correctly displayed and shows correct data
-    Given The sidebar shows "Delegation Center" staking page icon
-    When I click on the "Delegation Center" staking page button
-    And I see the "Delegation Center" staking page
-    And I click on the "Stake Pools" tab
-    And I see the "Stake Pools" page
     And I see any stake pool
     And I click on any stake pool
     Then I should see stake pool tooltip
     And Stake pool tooltip shows correct data
 
   Scenario: Delegating to stake pool from "Stake pools" screen works as expected
-    Given The sidebar shows "Delegation Center" staking page icon
-    When I click on the "Delegation Center" staking page button
-    And I see the "Delegation Center" staking page
     And I see Not-delegated label
     And I click on "Delegate your stake"
     Then I should see "Delegate Wallet" modal
--- tests/stake-pools/e2e/steps/stake-pools.js
+++ tests/stake-pools/e2e/steps/stake-pools.js
@@ -1,48 +1,33 @@
 // @flow
 import { Given, When, Then } from 'cucumber';
 import { expect } from 'chai';
+import { delegationCentreStakingHelper } from './helpers';
 import type { Daedalus } from '../../../types';
+import {getVisibleElementsCountForSelector} from "../../../common/e2e/steps/helpers";
 
 declare var daedalus: Daedalus;
 
-Given(/^The sidebar shows "Delegation Center" staking page icon/, function () {
-  return this.client.waitForVisible('.SidebarCategory_stakingIcon');
-});
+const {
+  stakingButtonVisible,
+  clickStakingButton,
+  delegationCenterVisible,
+  clickStakePoolsTab,
+  stakingPoolVisible
+} = delegationCentreStakingHelper;
 
-When(/^I click on the "Delegation Center" staking page button/, function () {
-  return this.waitAndClick('.SidebarCategory_component.staking');
-});
+const LOADING_MESSAGE_SELECTOR = '.StakePools_component.StakePools_isLoading .StakePools_loadingBlockWrapper p';
+const STAKE_POOLS_LIST_SELECTOR = '.StakePoolsList_component';
+const STAKE_POOLS_SEARCH_SELECTOR = '.StakePoolsSearch_component .StakePoolsSearch_searchInput.SimpleFormField_root input';
+const SEARCH_RESULTS_LABEL_SELECTOR = '.StakePools_component h2 span';
+const STAKE_POOL_SELECTOR = '.StakePoolsList_component .StakePoolThumbnail_component';
+const STAKE_POOL_SLUG_SELECTOR = '.StakePoolsList_component .StakePoolThumbnail_component .StakePoolThumbnail_ticker';
 
-Then(/^I see the "Delegation Center" staking page/, function () {
-  return this.client.waitForVisible('.StakingWithNavigation_page');
-});
-
-When(/^I click on the "Stake Pools" tab/, function () {
-  return this.waitAndClick('.stake-pools.NavButton_component.NavButton_normal');
-});
-
-Then(/^I see the "Stake Pools" page/, function () {
-  return this.client.waitForVisible('.StakePools_component');
-});
-
-Then(/^I should see the following loading message:$/, async function (message) {
-  await this.client.executeAsync((done) => {
-      daedalus.actions.staking.fakeStakePoolLoading.trigger(true);
-      done();
-    }
-  );
-  const loadingMsg = message.hashes()[0];
-  const loadingSelector =
-    '.StakePools_component.StakePools_isLoading .StakePools_loadingBlockWrapper p';
-  await this.client.waitForText(loadingSelector);
-  const loadingMsgOnScreen = await this.client.getText(loadingSelector);
-  const expectedLoadingMsg = await this.intl(loadingMsg.message);
-  expect(loadingMsgOnScreen).to.equal(expectedLoadingMsg);
-  await this.client.executeAsync((done) => {
-      daedalus.actions.staking.fakeStakePoolLoading.trigger();
-      done();
-    }
-  );
+Given(/^I am on the Delegation Centre staking page/, async function () {
+  await stakingButtonVisible(this.client);
+  await clickStakingButton(this.client);
+  await delegationCenterVisible(this.client);
+  await clickStakePoolsTab(this.client);
+  await stakingPoolVisible(this.client);
 });
 
 Then(/^I should see "([^"]*)" stake pools loaded by rank$/, async function (numberOfStakePools) {
@@ -54,4 +39,74 @@ Then(/^I should see "([^"]*)" stake pools loaded by rank$/, async function (numb
   });
   const result = stakePools && stakePools.value ? stakePools.value : [];
   expect(result.length).to.equal(parseInt(numberOfStakePools));
+});
+
+Then(/^I should see the following loading message:$/, async function (message) {
+  await this.client.executeAsync((done) => {
+      daedalus.actions.staking.fakeStakePoolLoading.trigger();
+      done();
+    }
+  );
+  const loadingMsg = message.hashes()[0];
+  const loadingSelector = LOADING_MESSAGE_SELECTOR;
+  await this.client.waitForText(loadingSelector);
+  const loadingMsgOnScreen = await this.client.getText(loadingSelector);
+  const expectedLoadingMsg = await this.intl(loadingMsg.message);
+  expect(loadingMsgOnScreen).to.equal(expectedLoadingMsg);
+});
+
+When(/^Stake pools loading failed/, async function () {
+  const stakePools = await this.client.executeAsync(done => {
+    daedalus.stores.staking.stakePoolsRequest
+      .execute()
+      .then(done)
+      .catch(error => done(error));
+  });
+  const result = stakePools && stakePools.value ? stakePools.value : [];
+  expect(result.length).to.not.equal(0);
+});
+
+Then(/^I should see loading stake pools error message:$/, async function (message) {
+  await this.client.executeAsync((done) => {
+      daedalus.actions.staking.fakeStakePoolLoading.trigger(true);
+      done();
+    }
+  );
+  const loadingMsg = message.hashes()[0];
+  const loadingSelector = LOADING_MESSAGE_SELECTOR;
+  await this.client.waitForText(loadingSelector);
+  const loadingMsgOnScreen = await this.client.getText(loadingSelector);
+  const expectedLoadingMsg = await this.intl(loadingMsg.message);
+  expect(loadingMsgOnScreen).to.equal(expectedLoadingMsg);
+});
+
+Then(/^I should not see any stake pool/, function () {
+  return this.client.waitForVisible(STAKE_POOLS_LIST_SELECTOR, null, true);
+});
+
+When(/^I see the stake pools search input field/, function () {
+  return this.client.waitForVisible(STAKE_POOLS_SEARCH_SELECTOR);
+});
+
+When(/^I enter "([^"]*)" in search input field$/, function (data) {
+  return this.client.setValue(STAKE_POOLS_SEARCH_SELECTOR, data);
+});
+
+Then(/^I should see message "([^"]*)"$/, async function (message) {
+  const searchResultsLabelSelector = SEARCH_RESULTS_LABEL_SELECTOR;
+  await this.client.waitForText(searchResultsLabelSelector);
+  const searchResultsMessages = await this.client.getText(searchResultsLabelSelector);
+  expect(searchResultsMessages).to.equal(message);
+});
+
+Then(/^I should see "([^"]*)" stake pool with slug "([^"]*)"$/, async function (numberOfStakePools, slug) {
+  const stakePoolsCount = await getVisibleElementsCountForSelector(
+    this.client,
+    STAKE_POOL_SELECTOR
+  );
+  const stakePoolSlugSelector = STAKE_POOL_SLUG_SELECTOR;
+  await this.client.waitForText(stakePoolSlugSelector);
+  const stakePoolSlug = await this.client.getText(stakePoolSlugSelector);
+  expect(stakePoolsCount).to.equal(parseInt(numberOfStakePools));
+  expect(stakePoolSlug[0]).to.equal(slug);
 });
