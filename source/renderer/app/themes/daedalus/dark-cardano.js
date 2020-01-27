cae54b9783f03f6cbedd6aa7c0fc8d9c5e494689
MarcusHurney <marcushurney@gmail.com>
2020-01-27 15:11:57 -0500

    [DDW-927] Adds rpScrollBar CSS vars to dark-cardano theme

--- source/renderer/app/themes/daedalus/dark-cardano.js
+++ source/renderer/app/themes/daedalus/dark-cardano.js
@@ -511,6 +511,11 @@ export const DARK_CARDANO_THEME_OUTPUT = {
     '--rp-radio-label-text-color': '#ffffff',
     '--rp-radio-size': '22px',
   },
+  rpScrollBar: {
+    '--rp-scrollbar-thumb-bg-color:': 'rgba(255, 255, 255, 0.1)',
+    '--rp-scrollbar-thumb-bg-color-active': 'rgba(255, 255, 255, 0.3)',
+    '--rp-scrollbar-thumb-bg-color-hover': 'rgba(255, 255, 255, 0.3)',
+  },
   rpSelect: {
     '--rp-select-arrow-bg-color': 'rgba(255, 255, 255, 0.3)',
     '--rp-select-arrow-bg-color-open': 'rgba(255, 255, 255, 0.7)',
