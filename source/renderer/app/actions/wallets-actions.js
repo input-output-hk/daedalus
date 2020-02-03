03f63bd61511f52a142ad3db89d4c5b07bbbbbd6
Danilo Prates <daniloprates@gmail.com>
2020-02-03 17:15:37 -0300

    [DDW-1148] Lint issue

--- source/renderer/app/actions/wallets-actions.js
+++ source/renderer/app/actions/wallets-actions.js
@@ -1,6 +1,5 @@
 // @flow
 import Action from './lib/Action';
-import Wallet from '../domains/Wallet';
 import type { walletExportTypeChoices } from '../types/walletExportTypes';
 import type { CsvRecord } from '../../../common/types/rewards-csv-request.types';
 
