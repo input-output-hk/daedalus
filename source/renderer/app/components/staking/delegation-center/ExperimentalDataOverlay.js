3dbecbca5647d0e3aeb2e2df36a1ce0057f7bfff
Tomislav Horaček <tomislav.horacek@thespian.hr>
2020-02-11 10:24:51 +0100

    [DDW-1197] Add translations to i18n and run translations manager

--- source/renderer/app/components/staking/delegation-center/ExperimentalDataOverlay.js
+++ source/renderer/app/components/staking/delegation-center/ExperimentalDataOverlay.js
@@ -10,18 +10,18 @@ import styles from './ExperimentalDataOverlay.scss';
 
 const messages = defineMessages({
   title: {
-    id: 'experimental.data.overlay.title',
+    id: 'experimentalData.overlay.title',
     defaultMessage: '!!!Experimental features',
     description: 'Experimental features',
   },
   incentivizedTestnetDescription: {
-    id: 'experimental.data.overlay.description',
+    id: 'experimentalData.overlay.description',
     defaultMessage:
-      '<p>!!!As well as an environment for testing the core staking and delegation features of the Shelley era, the Incentivized Testnet is also a valuable opportunity to test a number of new features in Daedalus.</p><p>Our goal is to test and to iterate, and also to experiment. However, given the nature of the testnet - network inconsistencies, feature incompleteness etc. - this does mean that some of the features we shall be trying out in Daedalus for the Incentivized Testnet may not work reliably. For example, we are not currently able to calculate all parameters precisely, so this data should not be relied upon.</p><p>While we continue to work on resolving those issues, stake pool parameters which may contain inaccurate or unreliable data are marked with this icon <span className="icon"></span> in stake pool details.</p>',
+      '<p>!!!As well as providing an environment for testing the core staking and delegation features of the Shelley era, the Incentivized Testnet is also a valuable opportunity to try out a number of new Daedalus features.</p><p>Our goal is to test and to iterate, and also to experiment. However, given the nature of the testnet - network inconsistencies, feature incompleteness etc. - this does mean that some of the features we shall be trying out in Daedalus for the Incentivized Testnet may not work reliably. For example, we are not currently able to calculate all parameters precisely, so this data should not be relied upon.</p><p>We’ll continue to work on developing these features and resolving any issues behind the scenes, as we head towards releasing Shelley on the Cardano main net. Meanwhile, we have marked any stake pool parameters which may contain inaccurate or unreliable data with this icon <span class="experimentalIcon"></span> in the stake pool details.</p>',
     description: 'Experimental data description',
   },
   actionLabel: {
-    id: 'experimental.data.overlay.actionLabel',
+    id: 'experimentalData.overlay.actionLabel',
     defaultMessage: '!!!I understand',
     description: 'Experimental data action',
   },
