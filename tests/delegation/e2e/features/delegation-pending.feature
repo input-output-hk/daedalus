cb8f8de289d678a4e4449f91957b8f54c2b9393f
Danilo Prates <daniloprates@gmail.com>
2020-02-20 01:57:50 -0300

    [DDW-1206] Pending delegation tests

--- tests/delegation/e2e/features/delegation-pending.feature
+++ tests/delegation/e2e/features/delegation-pending.feature
@@ -1,31 +1,71 @@
 @e2e @watch
 Feature: Wallet Pending Delegations
 
-  Scenario Outline: Pending delegations
+  Scenario: Pending delegations
     Given I have completed the basic setup
+    # The wallets don't need to be sequentially created,
+    # as their data is changed by the patched api
+    # so it doesn't matter their original names & data
     And I have the following "Rewards" wallets:
-      | name        |
-      | Test Wallet |
+      | name           |
+      | Test Wallet 1  |
+      | Test Wallet 2  |
+      | Test Wallet 3  |
+      | Test Wallet 4  |
+      | Test Wallet 5  |
+      | Test Wallet 6  |
+      | Test Wallet 7  |
+      | Test Wallet 8  |
+      | Test Wallet 9  |
+      | Test Wallet 10 |
     And I am on the Delegation "delegation-center" screen
     And I mark experimental feature as read
-    Given the wallet has the following pending delegations: <DELEGATION_SCENARIO>
-    Then the wallet should correctly display the correct stake pool tickers
-    # Then I freeze
-
-    # And the ADA logo should be <ACTIVE_POOL_VISIBILITY>
-    # And the tooltips should be displayed as follows: <TOOLTIPS>
-    # And the wallet should display the correct <LINKS>
-    # Then I freeze
-
-    Examples:
-    | DELEGATION_SCENARIO                   | ACTIVE_POOL_VISIBILITY | TOOLTIPS                                  | LINKS                    |
-    | undelegated                           | hidden                 | none                                      | Delegate                 |
-    | undelegated > delegated               | hidden                 | none > from_epoch                         | Undelegate or Redelegate |
-    | undelegated > delegated > undelegated | hidden                 | none > from_epoch > from_epoch            | Delegate                 |
-    | undelegated > delegated > delegated   | hidden                 | none > from_epoch > from_epoch            | Undelegate or Redelegate |
-    # | delegated                             | visible                | earning_rewards                           | Undelegate or Redelegate |
-    # | delegated > undelegated               | visible                | earning_rewards > from_epoch              | Delegate                 |
-    # | delegated > undelegated > delegated   | visible                | earning_rewards > from_epoch > from_epoch  | Delegate                 |
-    # | delegated > delegated                 | visible                | earning_rewards > from_epoch              | Undelegate or Redelegate |
-    # | delegated > delegated > undelegated   | visible                | earning_rewards > from_epoch > from_epoch | Delegate                 |
-    # | delegated > delegated > delegated     | visible                | earning_rewards > from_epoch > from_epoch | Undelegate or Redelegate |
+    Given the wallets have the following pending delegations:
+      | DELEGATION_SCENARIO                   |
+      | undelegated                           |
+      | undelegated > delegated               |
+      | undelegated > delegated > undelegated |
+      | undelegated > delegated > delegated   |
+      | delegated                             |
+      | delegated > undelegated               |
+      | delegated > undelegated > delegated   |
+      | delegated > delegated                 |
+      | delegated > delegated > undelegated   |
+      | delegated > delegated > delegated     |
+    Then the wallets should correctly display the correct stake pool tickers
+    And the ADA logo should be displayed as follows:
+      | ADA_LOGO |
+      | hidden   |
+      | hidden   |
+      | hidden   |
+      | hidden   |
+      | visible  |
+      | visible  |
+      | visible  |
+      | visible  |
+      | visible  |
+      | visible  |
+    And the tooltips should be displayed as follows:
+      | TOOLTIPS                                  |
+      | none                                      |
+      | none > from_epoch                         |
+      | none > from_epoch > from_epoch            |
+      | none > from_epoch > from_epoch            |
+      | earning_rewards                           |
+      | earning_rewards > from_epoch              |
+      | earning_rewards > from_epoch > from_epoch |
+      | earning_rewards > from_epoch              |
+      | earning_rewards > from_epoch > from_epoch |
+      | earning_rewards > from_epoch > from_epoch |
+    And the wallets should display the links as follows:
+      | LINKS                    |
+      | Delegate                 |
+      | Undelegate or Redelegate |
+      | Delegate                 |
+      | Undelegate or Redelegate |
+      | Undelegate or Redelegate |
+      | Delegate                 |
+      | Delegate                 |
+      | Undelegate or Redelegate |
+      | Delegate                 |
+      | Undelegate or Redelegate |
--- tests/delegation/e2e/steps/delegation-pending.js
+++ tests/delegation/e2e/steps/delegation-pending.js
@@ -1,117 +1,142 @@
 // @flow
 import { Given, Then } from 'cucumber';
 import { expect } from 'chai';
-import { set } from 'lodash';
+import { set, last } from 'lodash';
 import type { Daedalus } from '../../../types';
 import type { WalletPendingDelegations } from '../../../../source/renderer/app/api/wallets/types';
 
 declare var daedalus: Daedalus;
 
+let walletsTickers = [];
 
-let tickers: Array<Object> = [];
-
-Given(/^the wallet has the following pending delegations: (.*)$/, async function(delegationScenario) {
-
-  const walletName = `Modified Wallet ${new Date().getTime()}`;
+Given(/^the wallets have the following pending delegations:$/, async function(delegationScenariosTable) {
+  const delegationScenarios = delegationScenariosTable.hashes();
 
   await this.client.waitUntil(async () => {
     const stakePools = await this.client.execute(() => daedalus.stores.staking.stakePools);
     return stakePools.value.length;
   });
-  const tickersInfo = await this.client.executeAsync((delegationScenario, walletName, done) => {
+
+  const walletsTickersInfo = await this.client.executeAsync((delegationScenarios, done) => {
     const statusOptions = {
       delegated: 'delegating',
       undelegated: 'not_delegating',
     };
-    const delegationQueue = delegationScenario.split(' > ');
-    const modifiedWallet: {
-      name: string,
-      delegatedStakePoolId?: string,
-      delegationStakePoolStatus?: string,
-      lastDelegationStakePoolId?: string,
-      pendingDelegations: Array<Object>,
-    } = {
-      name: walletName,
-      pendingDelegations: [],
-    };
-    const { stakePools } = daedalus.stores.staking;
-    const tickers = delegationQueue.map((delegationInfo, index) => {
-      const status = statusOptions[delegationInfo];
-      const stakePool = status === 'delegating' ? stakePools[index] : null;
-      const ticker = stakePool ? `[${stakePool.ticker}]` : 'UNDELEGATED';
-      const stakePoolId = stakePool ? stakePool.id : null;
-      return({
-        ticker,
-        stakePoolId,
-        status,
-      });
-    })
-    tickers.forEach(({ stakePoolId, status }, index) => {
-      if (index === 0) {
-        modifiedWallet.delegatedStakePoolId = stakePoolId;
-        modifiedWallet.delegationStakePoolStatus = status;
-      }
-      else {
-        modifiedWallet.lastDelegationStakePoolId = stakePoolId
-        modifiedWallet.pendingDelegations[index - 1] = {
-          status: status,
-          target: stakePoolId,
-          changes_at: {
-            epoch_start_time: '2020-02-02T02:02:57Z',
-            epoch_number: 123456789,
-          },
+    let walletsTickers = [];
+    let modifiedWallets = [];
+    for (let index = 0; index < delegationScenarios.length; index++) {
+      const delegationQueue = delegationScenarios[index].DELEGATION_SCENARIO.split(' > ');
+      console.log('delegationQueue', delegationQueue);
+      const modifiedWallet: {
+        name: string,
+        delegatedStakePoolId?: string,
+        delegationStakePoolStatus?: string,
+        lastDelegationStakePoolId?: string,
+        pendingDelegations: Array<Object>,
+        syncState: Object;
+      } = {
+        name: `Modified Wallet ${index + 1}`,
+        pendingDelegations: [],
+        syncState: {
+          status: 'ready'
+        },
+      };
+      const { stakePools } = daedalus.stores.staking;
+      const tickers = delegationQueue.map((delegationInfo, index) => {
+        const status = statusOptions[delegationInfo];
+        const stakePool = status === 'delegating' ? stakePools[index] : null;
+        const ticker = stakePool ? `[${stakePool.ticker}]` : 'UNDELEGATED';
+        const stakePoolId = stakePool ? stakePool.id : null;
+        return({
+          status,
+          ticker,
+          stakePoolId,
+        });
+      })
+      walletsTickers.push(tickers);
+      tickers.forEach(({ stakePoolId, status }, index) => {
+        if (index === 0) {
+          modifiedWallet.delegatedStakePoolId = stakePoolId;
+          modifiedWallet.delegationStakePoolStatus = status;
         }
-      }
-    });
-    daedalus.api.ada.setTestingWallet(modifiedWallet);
-    done(tickers);
-  }, delegationScenario, walletName);
-  tickers = tickersInfo.value;
+        else {
+          modifiedWallet.lastDelegationStakePoolId = stakePoolId
+          modifiedWallet.pendingDelegations[index - 1] = {
+            status: status,
+            target: stakePoolId,
+            changes_at: {
+              epoch_start_time: '2020-02-02T02:02:57Z',
+              epoch_number: 123456789,
+            },
+          }
+        }
+      });
+      modifiedWallets.push(modifiedWallet);
+    }
+    //$FlowFixMe
+    daedalus.api.ada.setTestingWallets(modifiedWallets);
+    done(walletsTickers);
+  }, delegationScenarios);
+  walletsTickers = walletsTickersInfo.value;
 });
 
-Then(/^the wallet should correctly display the correct stake pool tickers$/, async function() {
+Then(/^the wallets should correctly display the correct stake pool tickers$/, { timeout: 60000 }, async function() {
   const walletNameSelector = '.WalletRow_title';
+  const lastWalletTickers = last(walletsTickers);
   await this.client.waitForVisible(walletNameSelector);
   // Waits for the patchAdaApi to transform the wallet values
   await this.client.waitUntil(async () => {
-    const walletName = await this.client.getText(walletNameSelector);
-    return walletName === walletName;
+    const walletNames = await this.client.getText(walletNameSelector);
+    return last(walletNames) === `Modified Wallet ${walletsTickers.length}`;
   });
   const tickerSelector = '.tickerText';
   await this.client.waitForVisible(tickerSelector);
-  let tickerTexts = await this.client.getText(tickerSelector);
-  if (!Array.isArray(tickerTexts)) tickerTexts = [tickerTexts];
-  console.log('>>> ', tickerTexts.length === tickers.length);
-  if (tickerTexts.length !== tickers.length) {
-    console.log('------------------------------------------');
-    console.log('tickerTexts', tickerTexts);
-    console.log('tickers', tickers);
+  for (let index = 0; index < walletsTickers.length; index++) {
+    const expectedTickers = walletsTickers[index];
+    let tickerTexts = await this.client.getText(`.WalletRow_component:nth-child(${index + 1}) ${tickerSelector}`);
+    if (!Array.isArray(tickerTexts)) tickerTexts = [tickerTexts];
+    expect(tickerTexts.length).to.equal(expectedTickers.length);
+    tickerTexts.forEach((tickerText, index) => {
+      let { ticker: expectedTickerText } = expectedTickers[index];
+      expect(tickerText).to.equal(expectedTickerText);
+    })
   }
-  // expect(tickerTexts.length).to.equal(tickers.length);
-  // tickerTexts.forEach((tickerText, index) => {
-  //   let { ticker: expectedTickerText } = tickers[index];
-  //   expect(tickerText).to.equal(expectedTickerText);
-  // })
 });
 
-Then(/^the ADA logo should be (.*)$/, async function(visibility) {
-  const shouldBeVisible = visibility === 'visible';
-  await this.client.waitForVisible('.WalletRow_activeAdaSymbol', null, !shouldBeVisible);
+Then(/^the ADA logo should be displayed as follows:$/, async function(visibilityTable) {
+  const visibility = visibilityTable.hashes();
+  for (let index = 0; index < visibility.length; index++) {
+    const shouldBeVisible = visibility[index].ADA_LOGO === 'visible';
+    await this.client.waitForVisible(`.WalletRow_component:nth-child(${index + 1}) .WalletRow_activeAdaSymbol`, null, !shouldBeVisible);
+  }
 });
 
-Then(/^the tooltips should be displayed as follows: (.*)$/, async function(tooltipsStr) {
-  const expectedTooltips = tooltipsStr.split(' > ').filter(value => value !== 'none');
-  const tooltipTexts = await this.client.getHTML('.WalletRow_status .WalletRow_tooltipLabelWrapper span');
-  expect(tooltipTexts.length).to.equal(expectedTooltips.length);
-  expectedTooltips.forEach((expectedTooltip, index) => {
-    const tooltipText = tooltipTexts[index];
-    const expectedExcerpt = expectedTooltip === 'earning_rewards'
-      ? 'Earning rewards'
-      : 'From epoch';
-    expect(tooltipText).to.have.string(expectedExcerpt);
-  });
+Then(/^the tooltips should be displayed as follows:$/, async function(tooltipsTable) {
+  const tooltipsScenarios = tooltipsTable.hashes();
+  for (let index = 0; index < tooltipsScenarios.length; index++) {
+    const expectedTooltips = tooltipsScenarios[index].TOOLTIPS.split(' > ').filter(value => value !== 'none');
+    if (expectedTooltips.length) {
+      let tooltipTexts = await this.client.getHTML(`.WalletRow_component:nth-child(${index + 1}) .WalletRow_status .WalletRow_tooltipLabelWrapper span`);
+      if (!Array.isArray(tooltipTexts)) tooltipTexts = [tooltipTexts];
+      expect(tooltipTexts.length).to.equal(expectedTooltips.length);
+      expectedTooltips.forEach((expectedTooltip, index) => {
+        const tooltipText = tooltipTexts[index];
+        const expectedExcerpt = expectedTooltip === 'earning_rewards'
+          ? 'Earning rewards'
+          : 'From epoch';
+        expect(tooltipText).to.have.string(expectedExcerpt);
+      });
+    }
+  }
 });
 
-Then(/^the wallet should display the correct (.*)$/, async function(links) {
-  console.log('links', links);
+Then(/^the wallets should display the links as follows:$/, async function(linksTable) {
+  const linksScenarios = linksTable.hashes();
+  for (let index = 0; index < linksScenarios.length; index++) {
+    const expectedLinkExcerpts = linksScenarios[index].LINKS.split(' or ');
+    let linksHTML = await this.client.getHTML(`.WalletRow_component:nth-child(${index + 1}) .WalletRow_action`);
+    expectedLinkExcerpts.forEach(expectedLinkExcerpt => {
+      expect(linksHTML).to.have.string(expectedLinkExcerpt);
+    })
+  }
 });
