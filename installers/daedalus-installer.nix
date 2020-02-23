a97fb6b6181e553c1021de9361a39f5aa3a2345a
John Lotoski <john.lotoski@iohk.io>
2020-02-23 18:57:21 -0500

    Start a heredoc type string for the codesigning script

--- installers/daedalus-installer.nix
+++ installers/daedalus-installer.nix
@@ -2,7 +2,7 @@
 , filepath, foldl, github, Glob, hspec, lens-aeson, managed
 , megaparsec, microlens, network-uri, nsis, optional-args
 , optparse-applicative, optparse-generic, split, stdenv
-, system-filepath, temporary, text, turtle, universum
+, system-filepath, temporary, text, turtle, universum, raw-strings-qq
 , unordered-containers, wreq, yaml, zip-archive
 }:
 mkDerivation {
@@ -14,19 +14,20 @@ mkDerivation {
   doCheck = false;
   libraryHaskellDepends = [
     aeson base bytestring containers directory github Glob lens-aeson
-    megaparsec microlens network-uri nsis optional-args system-filepath
-    text turtle universum unordered-containers wreq yaml zip-archive
+    megaparsec microlens network-uri nsis optional-args raw-strings-qq
+    system-filepath text turtle universum unordered-containers wreq
+    yaml zip-archive
   ];
   executableHaskellDepends = [
     base bytestring containers directory filepath foldl optional-args
-    optparse-applicative optparse-generic split system-filepath
-    temporary text turtle universum yaml
+    optparse-applicative optparse-generic raw-strings-qq split
+    system-filepath temporary text turtle universum yaml
   ];
   testHaskellDepends = [
     aeson base bytestring containers directory filepath foldl github
     hspec lens-aeson managed megaparsec optional-args
-    optparse-applicative optparse-generic split system-filepath
-    temporary text turtle universum yaml
+    optparse-applicative optparse-generic raw-strings-qq split
+    system-filepath temporary text turtle universum yaml
   ];
   description = "Daedalus Installer Builder";
   license = stdenv.lib.licenses.asl20;
