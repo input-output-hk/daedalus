5ba29c6f4ffb8134386afff917d2d6488a823b3c
Nikola Glumac <niglumac@gmail.com>
2020-03-19 17:45:01 +0100

    [DDW-24] Fix macOS ITN buidls

--- source/main/webpack.config.js
+++ source/main/webpack.config.js
@@ -78,6 +78,9 @@ module.exports = {
           ? {
               // Only bake in NODE_ENV value for production builds.
               'process.env.NODE_ENV': '"production"',
+              'process.env.PATH': JSON.stringify(
+                [process.env.PATH, process.env.DAEDALUS_INSTALL_DIR].join(':')
+              ),
             }
           : {}
       )
