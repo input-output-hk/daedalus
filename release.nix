71031afdf6c6da486d79ca68b5d9994cb3f001ef
Michael Bishop <michael.bishop@iohk.io>
2020-03-19 15:38:33 -0300

    add ifd-pins to speed up eval

--- release.nix
+++ release.nix
@@ -23,6 +23,11 @@ let
   in
     table.${system};
 
+  mkPins = inputs: (daedalusPkgs {}).pkgs.runCommand "ifd-pins" {} ''
+    mkdir $out
+    cd $out
+    ${lib.concatMapStringsSep "\n" (input: "ln -sv ${input.value} ${input.key}") (lib.attrValues (lib.mapAttrs (key: value: { inherit key value; }) inputs))}
+  '';
   makeJobs = cluster: with daedalusPkgs { inherit cluster; }; {
     daedalus.x86_64-linux = daedalus;
     installer.x86_64-linux = wrappedBundle newBundle pkgs cluster daedalus-bridge.wallet-version;
@@ -41,11 +46,15 @@ let
   lib = (import ./. {}).pkgs.lib;
   clusters = lib.splitString " " (builtins.replaceStrings ["\n"] [""] (builtins.readFile ./installer-clusters.cfg));
   mapOverArches = supportedTree: lib.mapAttrsRecursive (path: value: lib.listToAttrs (map (arch: { name = arch; value = lib.attrByPath path null (daedalusPkgsWithSystem arch); }) value)) supportedTree;
+  sources = import ./nix/sources.nix;
 in {
   inherit shellEnvs;
   inherit ((daedalusPkgs {}).pkgs) mono;
   wine = (daedalusPkgs {}).pkgs.wine.override { wineBuild = "wine32"; };
   tests = (daedalusPkgs {}).tests;
+  ifd-pins = mkPins {
+    inherit (sources) iohk-nix cardano-wallet cardano-shell;
+  };
 } // (builtins.listToAttrs (map (x: { name = x; value = makeJobs x; }) clusters))
 // (mapOverArches {
   daedalus-installer = [ "x86_64-linux" "x86_64-darwin" ];
