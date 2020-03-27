b4fc0ff635f28af4a6b1b3b3985adacf4392ead6
Samuel Leathers <samuel.leathers@iohk.io>
2020-03-27 15:44:44 -0400

    fix shellcheck

--- scripts/build-installer-unix.sh
+++ scripts/build-installer-unix.sh
@@ -129,7 +129,7 @@ CARDANO_BRIDGE=$(nix-build --no-out-link -A daedalus-bridge --argstr nodeImpleme
 echo '~~~ Prebuilding jormungandr bridge'
 JORMUNGANDR_BRIDGE=$(nix-build --no-out-link -A daedalus-bridge --argstr nodeImplementation jormungandr)
 
-itnClusters="$(< $(nix-build --no-out-link -A itnClustersFile))"
+itnClusters="$(< "$(nix-build --no-out-link -A itnClustersFile)")"
 
 pushd installers
     echo '~~~ Prebuilding dependencies for cardano-installer, quietly..'
