659f216458d965814618bfb7d3a8c5cb5218c6c3
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2020-04-09 15:49:33 -0600

    [DDW-105] - Force setting password on passwordless wallets - removing forceSetPassword

--- source/renderer/app/components/wallet/settings/SetWalletPassword.js
+++ source/renderer/app/components/wallet/settings/SetWalletPassword.js
@@ -48,7 +48,7 @@ export default class SetWalletPassword extends Component<Props> {
     return (
       <>
         {isDialogOpen(ChangeSpendingPasswordDialog) ? (
-          <ChangeSpendingPasswordDialogContainer forceSetPassword />
+          <ChangeSpendingPasswordDialogContainer />
         ) : (
           false
         )}
