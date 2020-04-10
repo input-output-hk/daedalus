e18a893c7f6a58dc0262b30064f3025ccc9a7d86
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2020-04-10 08:25:56 -0600

    [DDW-105] - Force setting password on passwordless wallets - fixing storybook

--- storybook/stories/wallets/setPassword/SetWalletPassword.stories.js
+++ storybook/stories/wallets/setPassword/SetWalletPassword.stories.js
@@ -1,16 +1,20 @@
 // @flow
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { boolean, withKnobs } from '@storybook/addon-knobs';
 
 // Screens
+import { action } from '@storybook/addon-actions';
 import SetWalletPassword from '../../../../source/renderer/app/components/wallet/settings/SetWalletPassword';
 import StoryDecorator from '../../_support/StoryDecorator';
 
 storiesOf('Wallets|Set Password', module)
-  .addDecorator(story => <StoryDecorator>{story()}</StoryDecorator>)
-  .add('Send', () => (
+  .addDecorator((story, context) => (
+    <StoryDecorator>{withKnobs(story, context)}</StoryDecorator>
+  ))
+  .add('Set Wallet Password', () => (
     <SetWalletPassword
-      isSetWalletPasswordDialogOpen={false}
-      onSetWalletPassword={() => {}}
+      isSetWalletPasswordDialogOpen={boolean('isSetWalletPasswordDialogOpen', false)}
+      onSetWalletPassword={action('onSetWalletPassword')}
     />
   ));
