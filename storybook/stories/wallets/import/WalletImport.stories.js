ef402c3dc357526744e0c08c9e39191f19154e8b
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2020-04-14 01:39:16 -0600

    [DDW-106] - Wallet import feature

--- storybook/stories/wallets/import/WalletImport.stories.js
+++ storybook/stories/wallets/import/WalletImport.stories.js
@@ -6,9 +6,9 @@ import WalletsWrapper from '../_utils/WalletsWrapper';
 import VerticalFlexContainer from '../../../../source/renderer/app/components/layout/VerticalFlexContainer';
 import WalletImportFileDialog from '../../../../source/renderer/app/components/wallet/wallet-import/WalletImportFileDialog';
 
-storiesOf('Wallets|Import', module)
+storiesOf('Wallets|Import File', module)
   .addDecorator(WalletsWrapper)
-  .add('Import File', () => {
+  .add('Wallets Import File Dialog', () => {
     const stateDirectoryPath =
       '/Users/daedalus/Library/Application Support/Daedalus Demo';
     return (
