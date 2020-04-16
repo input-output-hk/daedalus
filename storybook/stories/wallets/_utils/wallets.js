e28f834a5c04526986a9ff79373d007684f6f9c4
Danilo Prates <daniloprates@gmail.com>
2020-04-16 09:36:48 -0300

    [DDW-106] Storybook story

--- storybook/stories/wallets/_utils/wallets.js
+++ storybook/stories/wallets/_utils/wallets.js
@@ -1,29 +1,5 @@
 // @flow
 
-// export type ExportedByronWallet = {
-//   // Export tool wallet props
-//   encrypted_root_private_key: string,
-//   name: ?string,
-//   id: string,
-//   passphrase_hash: string,
-//   is_passphrase_empty: boolean,
-
-//   // Daedalus derived wallet props
-//   hasName: boolean,
-//   import: {
-//     status: WalletImportStatus,
-//     error: ?LocalizableError,
-//   },
-// };
-
-// export type WalletImportStatus =
-//   | 'unstarted'
-//   | 'pending'
-//   | 'running'
-//   | 'completed'
-//   | 'exists'
-//   | 'errored';
-
 export const exportedByronWallets = [
   {
     encrypted_root_private_key: '',
