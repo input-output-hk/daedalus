62f6ec82ecfdad729d8a7a481f43fdaab844b8e1
Yakov Karavelov <yakov.karavelov@iohk.io>
2020-06-11 17:03:54 -0700

    [DDW-256]: Add missing japanese translations to common widgets story

--- storybook/stories/common/Widgets.stories.js
+++ storybook/stories/common/Widgets.stories.js
@@ -1,11 +1,14 @@
 // @flow
 import React from 'react';
+import { defineMessages, IntlProvider } from 'react-intl';
 import { storiesOf } from '@storybook/react';
 import { observable, action as mobxAction } from 'mobx';
 import { action } from '@storybook/addon-actions';
 import StoryDecorator from '../_support/StoryDecorator';
 import StoryProvider from '../_support/StoryProvider';
 import StoryLayout from '../_support/StoryLayout';
+import enMessages from '../../../source/renderer/app/i18n/locales/en-US.json';
+import jpMessages from '../../../source/renderer/app/i18n/locales/ja-JP.json';
 import BigButtonForDialogs from '../../../source/renderer/app/components/widgets/BigButtonForDialogs';
 import MnemonicInputWidget from '../../../source/renderer/app/components/widgets/forms/MnemonicInputWidget';
 import createIcon from '../../../source/renderer/app/assets/images/create-ic.inline.svg';
@@ -14,6 +17,66 @@ import joinSharedIcon from '../../../source/renderer/app/assets/images/join-shar
 import TinySwitch from '../../../source/renderer/app/components/widgets/forms/TinySwitch';
 import ButtonLink from '../../../source/renderer/app/components/widgets/ButtonLink';
 import NormalSwitch from '../../../source/renderer/app/components/widgets/forms/NormalSwitch';
+
+const { intl: enIntl } = new IntlProvider({
+  locale: 'en-US',
+  messages: enMessages,
+}).getChildContext();
+const { intl: jpIntl } = new IntlProvider({
+  locale: 'ja-JP',
+  messages: jpMessages,
+}).getChildContext();
+const intl = { 'en-US': enIntl, 'ja-JP': jpIntl };
+
+const messages = defineMessages({
+  create: {
+    id: 'global.labels.create',
+    defaultMessage: '!!!Create',
+    description: 'Create label.',
+  },
+  createNewWallet: {
+    id: 'wallet.add.dialog.create.description',
+    defaultMessage: '!!!Create a new wallet',
+    description: 'Create a new wallet description.',
+  },
+  join: {
+    id: 'wallet.add.dialog.join.label',
+    defaultMessage: '!!!Join',
+    description: 'Join label.',
+  },
+  joinSharedWallet: {
+    id: 'wallet.add.dialog.join.description',
+    defaultMessage: '!!!Join a shared wallet',
+    description: 'Join a shared wallet description.',
+  },
+  import: {
+    id: 'wallet.add.dialog.import.label',
+    defaultMessage: '!!!Import',
+    description: 'Import label.',
+  },
+  importExistingWallet: {
+    id: 'wallet.add.dialog.import.description',
+    defaultMessage:
+      '!!!Import wallets from an earlier version of Daedalus or the Daedalus state directory',
+    description:
+      'Import wallets from an earlier version of Daedalus or the Daedalus state directory description.',
+  },
+  recoveryPhrase: {
+    id: 'wallet.restore.dialog.mnemonicsStep',
+    defaultMessage: '!!!Recovery Phrase',
+    description: 'Recovery Phrase description.',
+  },
+  save: {
+    id: 'global.labels.save',
+    defaultMessage: '!!!Save',
+    description: 'Save description.',
+  },
+  followInstructions: {
+    id: 'manualUpdate.button.label',
+    defaultMessage: '!!!Follow instructions and manually update',
+    description: 'Follow instructions and manually update description.',
+  },
+});
 
 storiesOf('Common|Widgets', module)
   .addDecorator((story: any, context: any) => {
@@ -39,20 +102,24 @@ storiesOf('Common|Widgets', module)
 
   // ====== Stories ======
 
-  .add('BigButtonForDialogs', () => (
+  .add('BigButtonForDialogs', (props: { locale: string }) => (
     <div>
       <div style={{ width: '300px', height: '200px', display: 'flex' }}>
         <BigButtonForDialogs
-          description="Create new wallet"
-          label="Create"
+          description={intl[props.locale].formatMessage(
+            messages.createNewWallet
+          )}
+          label={intl[props.locale].formatMessage(messages.create)}
           icon={createIcon}
           onClick={() => {}}
         />
       </div>
       <div style={{ width: '300px', height: '200px', display: 'flex' }}>
         <BigButtonForDialogs
-          description="Join shared wallet up to 5 people"
-          label="Join"
+          description={intl[props.locale].formatMessage(
+            messages.joinSharedWallet
+          )}
+          label={intl[props.locale].formatMessage(messages.join)}
           icon={joinSharedIcon}
           onClick={() => {}}
           isDisabled
@@ -60,8 +127,10 @@ storiesOf('Common|Widgets', module)
       </div>
       <div style={{ width: '300px', height: '200px', display: 'flex' }}>
         <BigButtonForDialogs
-          description="Import existing wallet"
-          label="Import"
+          description={intl[props.locale].formatMessage(
+            messages.importExistingWallet
+          )}
+          label={intl[props.locale].formatMessage(messages.import)}
           icon={importIcon}
           onClick={() => {}}
         />
@@ -69,26 +138,30 @@ storiesOf('Common|Widgets', module)
     </div>
   ))
 
-  .add('MnemonicInputWidget - 9 words', () => {
+  .add('MnemonicInputWidget - 9 words', (props: { locale: string }) => {
     const tokens = observable(['', '', '', '', '', '', '', '', '']);
     return (
-      <MnemonicInputWidget
-        label="Your Passphrase"
-        tokens={tokens}
-        onTokenChanged={(index, token) => {
-          tokens[index] = token;
-        }}
-      />
+      <div style={{ padding: 20 }}>
+        <MnemonicInputWidget
+          label={intl[props.locale].formatMessage(messages.recoveryPhrase)}
+          tokens={tokens}
+          onTokenChanged={(index, token) => {
+            tokens[index] = token;
+          }}
+        />
+      </div>
     );
   })
 
   .add('TinySwitch', () => <TinySwitch />)
 
-  .add('TinySwitch - short label', () => <TinySwitch label="My switch" />)
+  .add('TinySwitch - short label', (props: { locale: string }) => (
+    <TinySwitch label={intl[props.locale].formatMessage(messages.save)} />
+  ))
 
-  .add('ButtonLink', () => (
+  .add('ButtonLink', (props: { locale: string }) => (
     <ButtonLink
-      label="Follow instructions and manually update"
+      label={intl[props.locale].formatMessage(messages.followInstructions)}
       onClick={action('onClick')}
     />
   ))
--- storybook/stories/news/AlertsOverlay.stories.js
+++ storybook/stories/news/AlertsOverlay.stories.js
@@ -6,6 +6,8 @@ import { action } from '@storybook/addon-actions';
 import { select, withKnobs } from '@storybook/addon-knobs';
 
 import StoryDecorator from '../_support/StoryDecorator';
+import enMessages from '../../../source/renderer/app/i18n/locales/en-US.json';
+import jpMessages from '../../../source/renderer/app/i18n/locales/ja-JP.json';
 import News from '../../../source/renderer/app/domains/News';
 import {
   DATE_ENGLISH_OPTIONS,
@@ -15,21 +17,11 @@ import AlertsOverlay from '../../../source/renderer/app/components/news/AlertsOv
 
 const { intl: enIntl } = new IntlProvider({
   locale: 'en-US',
-  messages: {
-    readMore: 'Read More',
-    failureAlert: 'Failure Alert',
-    content:
-      '# h1 Heading\nUt consequat semper viverra nam libero justo laoreet sit. Sagittis vitae et leo duis. Eget nullam non nisi est sit amet facilisis magna etiam. Nisl tincidunt eget nullam non nisi est sit amet facilisis. Auctor neque vitae tempus quam pellentesque. Vel facilisis volutpat est velit egestas dui id ornare arcu.\n\n## h2 Heading\n\nConsequat mauris nunc congue nisi vitae suscipit. Dictum non consectetur a erat nam. Laoreet non curabitur gravida arcu ac tortor dignissim. Eu augue ut lectus arcu bibendum at. Facilisis gravida neque convallis a cras semper. Ut consequat semper viverra nam libero justo laoreet sit. Sagittis vitae et leo duis. Eget nullam non nisi est sit amet facilisis magna etiam. Nisl tincidunt eget nullam non nisi est sit amet facilisis. Auctor neque vitae tempus quam pellentesque. Vel facilisis volutpat est velit egestas dui id ornare arcu. Nam aliquam sem et tortor consequat id porta nibh venenatis.\n\nViverra nam libero justo laoreet sit amet. Pharetra diam sit amet nisl. Quam viverra orci sagittis eu. Rhoncus dolor purus non enim. Posuere urna nec tincidunt praesent semper feugiat. Suspendisse in est ante in nibh mauris cursus. Sit amet consectetur adipiscing elit duis. Tortor id aliquet lectus proin nibh nisl condimentum id. At in tellus integer feugiat scelerisque. Maecenas sed enim ut sem viverra aliquet. Pellentesque pulvinar pellentesque habitant morbi. Ultrices neque ornare aenean euismod elementum nisi quis eleifend. Praesent tristique magna sit amet purus gravida. Diam volutpat commodo sed egestas egestas. Ut placerat orci nulla pellentesque dignissim enim. Ultrices in iaculis nunc sed augue lacus viverra. Etiam sit amet nisl purus.\n\n## Typographic replacements\n\nEnable typographer option to see result.\n\n(c) (C) (r) (R) (tm) (TM) (p) (P) +-\n\ntest.. test... test..... test?..... test!....\n\n!!!!!! ???? ,,  -- ---\n\n"Smartypants, double quotes" and \'single quotes\'\n\n## Emphasis\n\n**This is bold text**\n\n__This is bold text__\n\n*This is italic text*\n\n_This is italic text_\n\n## Lists\n\nUnordered\n\n+ Create a list by starting a line with +, -, or *\n+ Sub-lists are made by indenting 2 spaces:\n+ Very easy!\n\nOrdered\n\n1. Lorem ipsum dolor sit amet\n2. Consectetur adipiscing elit\n3. Integer molestie lorem at massa\n\n\n1. You can use sequential numbers...\n1. ...or keep all the numbers as `1.`\n\n## Links\n\n[link text](http://dev.nodeca.com)\n\n[link with title](http://nodeca.github.io/pica/demo/ "title text!")\n\nAutoconverted link https://github.com/nodeca/pica (enable linkify to see)\n\n### [Subscript](https://github.com/markdown-it/markdown-it-sub) / [Superscript](https://github.com/markdown-it/markdown-it-sup)\n\n- 19^th^\n- H~2~O\n',
-  },
+  messages: enMessages,
 }).getChildContext();
 const { intl: jpIntl } = new IntlProvider({
   locale: 'ja-JP',
-  messages: {
-    readMore: '続きを読む',
-    failureAlert: '障害警告',
-    content:
-      '# 見出し 1\nある初期状態が与えられればその後の全ての状態量の変化が決定される系を力学系と呼ぶ。特に、決定論に従う力学系を扱うことを強調して決定論的力学系とも呼ばれる。カオス理論において研究されるカオスと呼ばれる複雑で確率的なランダムにも見える振る舞いは、この決定論的力学系に従って生み出されるものである。この点を強調するためカオス理論が取り扱うカオスを決定論的カオス(deterministic chaos)とも呼ぶ[3]。複雑で高次元の系ではなくとも、1次元離散方程式や3次元連続方程式のような非常に簡単な低次元の系からでも、確率的ランダムに相当する振る舞いが生起される点が決定論的カオスの特徴といえる。この用語は、カオス理論以前から存在するボルツマンにより導入された分子カオスと呼び分ける意味合いもある。ボルツマンによるカオスは確率論的乱雑さを表しており、カオス理論におけるカオスとは概念が異なる。\n## 見出し 2\nある初期状態が与えられればその後の全ての状態量の変化が決定される系を力学系と呼ぶ。特に、決定論に従う力学系を扱うことを強調して決定論的力学系とも呼ばれる。カオス理論において研究されるカオスと呼ばれる複雑で確率的なランダムにも見える振る舞いは、この決定論的力学系に従って生み出されるものである。この点を強調するためカオス理論が取り扱うカオスを決定論的カオス(deterministic chaos)とも呼ぶ[3]。複雑で高次元の系ではなくとも、1次元離散方程式や3次元連続方程式のような非常に簡単な低次元の系からでも、確率的ランダムに相当する振る舞いが生起される点が決定論的カオスの特徴といえる。この用語は、カオス理論以前から存在するボルツマンにより導入された分子カオスと呼び分ける意味合いもある。ボルツマンによるカオスは確率論的乱雑さを表しており、カオス理論におけるカオスとは概念が異なる。',
-  },
+  messages: jpMessages,
 }).getChildContext();
 const intl = { 'en-US': enIntl, 'ja-JP': jpIntl };
 const dateOptionsIntl = {
@@ -39,17 +31,17 @@ const dateOptionsIntl = {
 
 const messages = defineMessages({
   readMore: {
-    id: 'readMore',
+    id: 'global.labels.readMore',
     defaultMessage: '!!!Read More',
     description: 'Read More button label.',
   },
   failureAlert: {
-    id: 'failureAlert',
+    id: 'global.errors.failureAlert',
     defaultMessage: '!!!Failure Alert',
     description: 'Failure Alert title.',
   },
   content: {
-    id: 'content',
+    id: 'static.dummy.markdown',
     defaultMessage: '!!!Content',
     description: 'Content.',
   },
