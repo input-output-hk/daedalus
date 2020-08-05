9dd07c5374c796e73b497e6757a32444953fbf3b
Danilo Prates <daniloprates@gmail.com>
2020-08-05 16:32:48 -0300

    [DDW-344] Enable the Redeem Rewards sidebar menu item

--- source/renderer/app/stores/SidebarStore.js
+++ source/renderer/app/stores/SidebarStore.js
@@ -98,7 +98,7 @@ export default class SidebarStore extends Store {
       [categories.PAPER_WALLET_CREATE_CERTIFICATE.name]: false,
       [categories.STAKING_DELEGATION_COUNTDOWN.name]: isShelleyPending,
       [categories.STAKING.name]: isShelleyActivated,
-      [categories.REDEEM_ITN_REWARDS.name]: isDev,
+      [categories.REDEEM_ITN_REWARDS.name]: true,
       [categories.SETTINGS.name]: true,
       [categories.NETWORK_INFO.name]:
         isFlight || isIncentivizedTestnet || isShelleyTestnet,
