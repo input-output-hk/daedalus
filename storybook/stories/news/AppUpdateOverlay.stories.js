1ce1f69a94f5cc2d1f42017732ffc050004c6274
Danilo Prates <daniloprates@gmail.com>
2020-08-13 10:57:31 -0300

    [DDW-269] Fix storybook styling

--- storybook/stories/news/AppUpdateOverlay.stories.js
+++ storybook/stories/news/AppUpdateOverlay.stories.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { number, withKnobs, boolean } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
+import StoryDecorator from '../_support/StoryDecorator';
 import AppUpdateOverlay from '../../../source/renderer/app/components/appUpdate/AppUpdateOverlay';
 import { update, version, availableAppVersion } from './_utils/fakeDataUpdate';
 import { rangeMap } from '../../../source/renderer/app/utils/numbers';
 
 storiesOf('News|Overlays', module)
+  .addDecorator(story => <StoryDecorator>{story()}</StoryDecorator>)
   .addDecorator(withKnobs)
   .add('Update', ({ locale }: { locale: string }) => {
     const downloadProgress = number('downloadProgress', 30, {
@@ -30,9 +32,9 @@ storiesOf('News|Overlays', module)
         availableAppVersion={availableAppVersion}
         currentAppVersion={version}
         downloadProgress={downloadProgress}
-        isUpdateDownloaded={boolean('isUpdateDownloaded', false)}
+        isUpdateDownloaded={boolean('isUpdateDownloaded', true)}
         isAutomaticUpdateFailed={boolean('isAutomaticUpdateFailed', false)}
-        displayManualUpdateLink={boolean('displayManualUpdateLink', false)}
+        displayManualUpdateLink={boolean('displayManualUpdateLink', true)}
         onClose={action('onClose')}
         onInstallUpdate={action('onInstallUpdate')}
         onExternalLinkClick={action('onExternalLinkClick')}
