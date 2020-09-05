2a0cfb4e9f8521dd5a4974c8e16b9b42c30c9fa4
Danilo Prates <daniloprates@gmail.com>
2020-09-04 21:23:26 -0300

    [DDW-269] Fix switched installers

--- source/renderer/app/api/news/dummyUpdate.js
+++ source/renderer/app/api/news/dummyUpdate.js
@@ -5,15 +5,15 @@ const UPDATE_VERSION = '2.2.0';
 const DARWIN_INSTALLER_URL =
   'https://update-cardano-mainnet.iohk.io/daedalus-2.2.0-mainnet-14276.pkg';
 const WIN32_INSTALLER_URL =
-  'https://update-cardano-mainnet.iohk.io/daedalus-2.2.0-mainnet-14276.bin';
-const LINUX_INSTALLER_URL =
   'https://update-cardano-mainnet.iohk.io/daedalus-2.2.0-mainnet-14276.exe';
+const LINUX_INSTALLER_URL =
+  'https://update-cardano-mainnet.iohk.io/daedalus-2.2.0-mainnet-14276.bin';
 const DARWIN_HASH =
   'b546db6f06065ddce601b1dd6b9afdce8a3b46c01e0058503207be99f3e4976b';
 const WIN32_HASH =
-  'e8d3879b6402062f2626e0fe16ea452992c68f70c639adbf083015d3b24c1f03';
-const LINUX_HASH =
   'a3826e1b05d211c1c21de4a28a967d46530a1c318ae970bb024001ad05684f74';
+const LINUX_HASH =
+  'e8d3879b6402062f2626e0fe16ea452992c68f70c639adbf083015d3b24c1f03';
 
 const { updatedAt, items } = news;
 
