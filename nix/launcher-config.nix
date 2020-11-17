38e3d372564a0b3af2cc8a879152b5880af25878
Samuel Leathers <samuel.leathers@iohk.io>
2020-11-17 11:16:12 -0500

    use testnet temporarily

--- nix/launcher-config.nix
+++ nix/launcher-config.nix
@@ -23,9 +23,9 @@
 let
   clusterOverrides = {
     mainnet_catalyst = {
-      cardanoEnv = cardanoLib.environments.mainnet;
-      cluster = "mainnet";
-      networkName = "mainnet";
+      cardanoEnv = cardanoLib.environments.testnet;
+      cluster = "testnet";
+      networkName = "testnet";
     };
     mainnet_flight = {
       cardanoEnv = cardanoLib.environments.mainnet;
