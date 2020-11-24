ae78a7999724bc0d32b206c6a9974da54c994272
Nikola Glumac <niglumac@gmail.com>
2020-11-24 10:59:17 +0100

    [DDW-197] Update permissions

--- installers/common/MacInstaller.hs
+++ installers/common/MacInstaller.hs
@@ -167,10 +167,6 @@ codeSignEntitlements = [r|<?xml version="1.0" encoding="UTF-8"?>
   <dict>
     <key>com.apple.security.cs.allow-unsigned-executable-memory</key>
     <true/>
-    <key>com.apple.security.cs.allow-dyld-environment-variables</key>
-    <true/>
-    <key>com.apple.security.cs.disable-library-validation</key>
-    <true/>
     <key>com.apple.security.device.usb</key>
     <true/>
   </dict>
