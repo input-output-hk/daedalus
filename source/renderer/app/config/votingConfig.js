29b19588498317490a8303a7b30fb453ee418374
Juan Christian Cieri <jcieri@atixlabs.com>
2020-11-30 15:43:13 -0400

    Change minimum amount to vote from 8000 to 100

--- source/renderer/app/config/votingConfig.js
+++ source/renderer/app/config/votingConfig.js
@@ -1,5 +1,5 @@
 // @flow
-export const MIN_VOTING_FUNDS = 8000; // 8000 ADA | unit: ada
+export const MIN_VOTING_FUNDS = 100; // 100 ADA | unit: ada
 export const VOTING_FEE_FOR_CALCULATE = 1; // 1 ADA | unit: ada
 export const PIN_CODE_LENGTH = 4;
 // Timers
