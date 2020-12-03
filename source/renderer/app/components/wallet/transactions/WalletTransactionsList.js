4d0cc707afeb5fa02358b370208ee540ec60544c
Nikola Glumac <niglumac@gmail.com>
2020-12-03 10:25:15 +0100

    [DDW-505] Fix lint issues and run translation manager

--- source/renderer/app/components/wallet/transactions/WalletTransactionsList.js
+++ source/renderer/app/components/wallet/transactions/WalletTransactionsList.js
@@ -7,7 +7,6 @@ import { Button } from 'react-polymorph/lib/components/Button';
 import { ButtonSkin } from 'react-polymorph/lib/skins/simple/ButtonSkin';
 import { defineMessages, intlShape } from 'react-intl';
 import moment from 'moment';
-import { set, omit, has } from 'lodash';
 import styles from './WalletTransactionsList.scss';
 import Transaction from './Transaction';
 import { WalletTransaction } from '../../../domains/WalletTransaction';
