3962d3b17dc32885268debe339f10768d6139a90
Juan Christian Cieri <jcieri@atixlabs.com>
2020-12-08 13:20:48 -0300

    Increase threshold to 7950 ADA and timeout to 3600 seconds

--- source/renderer/app/config/votingConfig.js
+++ source/renderer/app/config/votingConfig.js
@@ -1,9 +1,9 @@
 // @flow
-export const MIN_VOTING_FUNDS = 100; // 100 ADA | unit: ada
+export const MIN_VOTING_FUNDS = 7950; // 100 ADA | unit: ada
 export const VOTING_FEE_FOR_CALCULATE = 1; // 1 ADA | unit: ada
 export const PIN_CODE_LENGTH = 4;
 // Timers
 
 export const VOTING_REGISTRATION_TRANSACTION_CHECK_INTERVAL = 1 * 1000; // 1 second | unit: milliseconds;
 export const VOTING_REGISTRATION_TRANSACTION_CHECKER_TIMEOUT = 30 * 1000; // 30 seconds | unit: milliseconds;
-export const VOTING_COUNTDOWN_INTERVAL = 60; // 60 minutes | unit: seconds;
+export const VOTING_COUNTDOWN_INTERVAL = 60 * 60; // 60 minutes | unit: seconds;
