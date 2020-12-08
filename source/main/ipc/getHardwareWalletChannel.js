99c801c36ac6203960182ae234d4462468337a48
Nikola Glumac <niglumac@gmail.com>
2020-12-08 11:24:07 +0100

    [DDW-197] Usb events

--- source/main/ipc/getHardwareWalletChannel.js
+++ source/main/ipc/getHardwareWalletChannel.js
@@ -1,6 +1,7 @@
 // @flow
 import TransportNodeHid from '@ledgerhq/hw-transport-node-hid';
 import { getDevices } from '@ledgerhq/hw-transport-node-hid-noevents';
+import usb from 'usb';
 import AppAda, {
   cardano,
   utils,
@@ -361,6 +362,13 @@ export const handleHardwareWalletRequests = async (
     observer = new EventObserver(mainWindow);
     try {
       logger.info('[HW-DEBUG] OBSERVER INIT');
+      usb.on('attach', (device) => {
+        logger.info('[HW-DEBUG]:usb:attach', { device });
+      });
+      usb.on('detach', (device) => {
+        logger.info('[HW-DEBUG]:usb:dettach', { device });
+        // this.lastDetached = device;
+      });
       TransportNodeHid.setListenDevicesDebounce(1000); // Defaults to 500ms
       await TransportNodeHid.listen(observer);
       logger.info('[HW-DEBUG] OBSERVER INIT - listener started');
