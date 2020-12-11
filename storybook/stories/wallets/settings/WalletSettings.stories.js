85bcb0a414bac4996445ddd14189236126730bfd
Yakov Karavelov <yakov.karavelov@iohk.io>
2020-12-11 07:41:09 -0800

    [DDW-438]: Update wallet public key stories

--- storybook/stories/wallets/settings/WalletSettings.stories.js
+++ storybook/stories/wallets/settings/WalletSettings.stories.js
@@ -9,6 +9,7 @@ import WalletsWrapper from '../_utils/WalletsWrapper';
 import WalletSettingsScreen from './WalletSettingsScreen.stories.js';
 import './WalletDelete.stories';
 import './WalletRecoveryPhraseVerification.stories';
+import './WalletPublicKeyQRCode.stories';
 
 /* eslint-disable consistent-return */
 storiesOf('Wallets|Settings', module)
