a51faadb23323acff7c9939203ba6daaafc517ac
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2021-01-25 05:18:02 -0600

    DDW-500 Implement design for native tokens - Wallet token send confirmation dialog updates

--- storybook/stories/wallets/confirmations/WalletTokenSendConfirmationDialog.stories.js
+++ storybook/stories/wallets/confirmations/WalletTokenSendConfirmationDialog.stories.js
@@ -3,38 +3,43 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 // Helpers
+import BigNumber from 'bignumber.js';
 import StoryDecorator from '../../_support/StoryDecorator';
 
 // Screens
 import WalletTokenSendConfirmationDialog
   from '../../../../source/renderer/app/components/wallet/WalletTokenSendConfirmationDialog';
+import { DECIMAL_PLACES_IN_ADA } from '../../../../source/renderer/app/config/numbersConfig';
+import { HwDeviceStatuses} from '../../../../source/renderer/app/domains/Wallet';
+import { generateHash, generateWallet } from '../../_support/utils';
+import { formattedAmountToNaturalUnits } from '../../../../source/renderer/app/utils/formatters';
 
 storiesOf('Wallet Token Dialog|Send Confirmation', module)
   .addDecorator((story) => <StoryDecorator>{story()}</StoryDecorator>)
   .add('Wallet Token Send Confirmation Dialog', () => (
     <div>
       <WalletTokenSendConfirmationDialog
-        amount={amount}
-        receiver={receiver}
-        totalAmount={totalAmount}
-        transactionFee={transactionFee}
-        amountToNaturalUnits={amountToNaturalUnits}
-        onSubmit={this.handleWalletSendFormSubmit}
-        isSubmitting={isSubmitting}
-        isFlight={isFlight}
-        onCancel={() => {
-          actions.dialogs.closeActiveDialog.trigger();
-          sendMoneyRequest.reset();
-          resetHardwareWalletTransaction({ cancelDeviceAction: true });
-        }}
-        error={error}
-        currencyUnit={currencyUnit}
-        onExternalLinkClick={onExternalLinkClick}
-        hwDeviceStatus={hwDeviceStatus}
-        isHardwareWallet={isHardwareWallet}
-        onInitiateTransaction={this.handleInitiateTransaction}
-        walletName={activeWallet.name}
-        nativeTokens={nativeTokens}
+        amount={new BigNumber(100100).toFormat(DECIMAL_PLACES_IN_ADA)}
+        receiver={generateHash()}
+        totalAmount={new BigNumber(100100).add(new BigNumber(0.10100)).toFormat(DECIMAL_PLACES_IN_ADA)}
+        transactionFee={new BigNumber(0.10100).toFormat(DECIMAL_PLACES_IN_ADA)}
+        amountToNaturalUnits={formattedAmountToNaturalUnits(new BigNumber(100100).toString())}
+        onSubmit={() => null}
+        isSubmitting={false}
+        isFlight={false}
+        onCancel={() => null}
+        currencyUnit="USDC"
+        onExternalLinkClick={() => null}
+        hwDeviceStatus={HwDeviceStatuses.CONNECTING}
+        isHardwareWallet={false}
+        onInitiateTransaction={() => null}
+        walletName={generateWallet('TrueUSD', '15119903750165').name}
+        nativeTokens={[
+          generateWallet('ADA', '55119903750165'),
+          generateWallet('Tether', '25119903750165'),
+          generateWallet('TrueUSD', '15119903750165'),
+          generateWallet('USD Coin', '0'),
+        ]}
       />
     </div>
   ));
