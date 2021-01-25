dbc407ccd4534d46d7db73bb0bb0ced5997438e1
Nikola Glumac <niglumac@gmail.com>
2021-01-25 11:25:02 +0100

    [DDW-541] Code cleanup

--- source/renderer/app/actions/index.js
+++ source/renderer/app/actions/index.js
@@ -1,31 +1,31 @@
 // @flow
 import AddressesActions from './addresses-actions';
 import AppActions from './app-actions';
+import AppUpdateActions from './app-update-actions';
 import DialogsActions from './dialogs-actions';
 import HardwareWalletsActions from './hardware-wallets-actions';
 import NetworkStatusActions from './network-status-actions';
-import AppUpdateActions from './app-update-actions';
 import NotificationsActions from './notifications-actions';
 import ProfileActions from './profile-actions';
 import RouterActions from './router-actions';
 import SidebarActions from './sidebar-actions';
 import StakingActions from './staking-actions';
 import TransactionsActions from './transactions-actions';
+import VotingActions from './voting-actions';
 import WalletsActions from './wallets-actions';
 import WalletsLocalAction from './wallets-local-actions';
 import WalletBackupActions from './wallet-backup-actions';
 import WalletMigrationActions from './wallet-migration-actions';
 import WalletSettingsActions from './wallet-settings-actions';
 import WindowActions from './window-actions';
-import VotingActions from './voting-actions';
 
 export type ActionsMap = {
   addresses: AddressesActions,
   app: AppActions,
+  appUpdate: AppUpdateActions,
   dialogs: DialogsActions,
   hardwareWallets: HardwareWalletsActions,
   networkStatus: NetworkStatusActions,
-  appUpdate: AppUpdateActions,
   notifications: NotificationsActions,
   profile: ProfileActions,
   router: RouterActions,
@@ -44,10 +44,10 @@ export type ActionsMap = {
 const actionsMap: ActionsMap = {
   addresses: new AddressesActions(),
   app: new AppActions(),
+  appUpdate: new AppUpdateActions(),
   dialogs: new DialogsActions(),
   hardwareWallets: new HardwareWalletsActions(),
   networkStatus: new NetworkStatusActions(),
-  appUpdate: new AppUpdateActions(),
   notifications: new NotificationsActions(),
   profile: new ProfileActions(),
   router: new RouterActions(),
