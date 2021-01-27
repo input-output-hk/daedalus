2015aa2425cc45ddfebe86ec30efd41dec506b78
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2021-01-27 07:44:59 -0400

    DDW-500 Implement design for native tokens - Wallet token send form - adding dropdown field for native tokens

--- source/renderer/app/components/wallet/WalletTokenSendForm.js
+++ source/renderer/app/components/wallet/WalletTokenSendForm.js
@@ -641,6 +641,7 @@ export default class WalletTokenSendForm extends Component<Props, State> {
                                 />
                               </button>
                             </PopOver>
+                            <div className={styles.separator} />
                           </div>
                         )}
                         <div className={styles.walletsDropdownWrapper}>
