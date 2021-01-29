b5c68e1f0b8464911d527dc01948e78a10e44417
Michael Bishop <michael.bishop@iohk.io>
2021-01-29 15:45:34 -0400

    fix lint errors

--- source/renderer/app/stores/VotingStore.js
+++ source/renderer/app/stores/VotingStore.js
@@ -134,10 +134,10 @@ export default class VotingStore extends Store {
         index: '0',
       });
       stakeKey = await this.getHexFromBech32(stakeKey);
-      let addressHex = await this.getHexFromBech32(address.id);
+      const addressHex = await this.getHexFromBech32(address.id);
 
       const signature = await this.signMetadataRequest.execute({
-        addressHex: addressHex,
+        addressHex,
         walletId,
         passphrase,
         votingKey,
@@ -148,7 +148,7 @@ export default class VotingStore extends Store {
 
       const transaction = await this.votingSendTransactionRequest.execute({
         address: address.id,
-        addressHex: addressHex,
+        addressHex,
         amount,
         passphrase,
         walletId,
