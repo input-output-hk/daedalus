4ff5743d7f71c66d876311db1e9b6891af5cd7e5
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2021-03-04 05:49:42 -0600

    DDW-570 - Update view wallets public key feature - setting extended on true

--- source/renderer/app/stores/WalletsStore.js
+++ source/renderer/app/stores/WalletsStore.js
@@ -323,7 +323,7 @@ export default class WalletsStore extends Store {
     // @TODO Once the api is ready, index value should be configured properly
     const walletId = this.active.id;
     const index = '1852H';
-    const extended = false;
+    const extended = true;
 
     try {
       const walletPublicKey = await this.walletPublicKeyRequest.execute({
