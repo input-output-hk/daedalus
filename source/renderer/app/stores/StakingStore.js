30eefd011af56a8d9f00ebbc29035df448427407
Yakov Karavelov <yakov.karavelov@iohk.io>
2021-03-23 04:24:11 -0400

    [DDW-435] Update staking store

--- source/renderer/app/stores/StakingStore.js
+++ source/renderer/app/stores/StakingStore.js
@@ -83,7 +83,7 @@ export default class StakingStore extends Store {
   @observable cyclesWithoutIncreasingStakePools: number = 0;
 
   /* ----------  Stake Pools Filter  ---------- */
-  @observable _filterOptions = {};
+  @observable filterOptions: ?StakePoolFilterOptionsType = null;
 
   pollingStakePoolsInterval: ?IntervalID = null;
   refreshPolling: ?IntervalID = null;
