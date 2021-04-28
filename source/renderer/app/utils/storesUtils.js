cb5465ea3bee76ac5292ad7fa25e80c7025322b7
Danilo Prates <daniloprates@gmail.com>
2021-04-28 09:14:46 -0300

    [DDW-656] Revert flow rule

--- source/renderer/app/utils/storesUtils.js
+++ source/renderer/app/utils/storesUtils.js
@@ -17,7 +17,7 @@ export const getRequestKeys = (
   };
 };
 
-export const requestGetter = (req: Request, systemValue: string | boolean) => {
+export const requestGetter = (req: Request, systemValue: any) => {
   const { result } = req.execute();
   if (isRequestSet(req)) return result;
   return systemValue;
