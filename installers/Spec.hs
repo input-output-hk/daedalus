25f4a203acc4bf1a30f643cb1e8c7b030b8bb44f
Nikola Glumac <niglumac@gmail.com>
2021-05-31 13:57:16 +0200

    [DDW-615] Remove legacy code from nix/hs files

--- installers/Spec.hs
+++ installers/Spec.hs
@@ -40,7 +40,7 @@ macBuildSpec = do
                  { oOS = Win64
                  , oBackend = Cardano daedalusBridge
                  , oBuildJob = Just (BuildJob "test")
-                 , oCluster = Nightly
+                 , oCluster = Testnet
                  , oAppName = "Daedalus"
                  , oOutputDir = out
                  , oTestInstaller = testInstaller False
@@ -123,8 +123,8 @@ utilSpec = do
 
   describe "Package filename generation" $ do
     it "generates a good filename for windows" $ do
-      let f = packageFileName Win64 Nightly (Version "0.4.2") (Cardano "") "9.9" (Just "job.id")
-      f `shouldBe` (fromText "daedalus-0.4.2-cardano-sl-9.9-nightly-windows-job.id.exe")
+      let f = packageFileName Win64 Testnet (Version "0.4.2") (Cardano "") "9.9" (Just "job.id")
+      f `shouldBe` (fromText "daedalus-0.4.2-cardano-sl-9.9-testnet-windows-job.id.exe")
 
 ----------------------------------------------------------------------------
 -- Tests for Hydra downloading (yes it's in the AppVeyor module)
