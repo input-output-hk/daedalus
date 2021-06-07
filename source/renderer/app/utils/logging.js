fbb1ea9a058c24ca6186f841b3fb45f5b4f7492d
Daniel Main <daniel.main.cernhoff@icloud.com>
2021-06-07 09:05:08 +0200

    [DDW-414] Fixed ipc communication due electron breaking change

--- source/renderer/app/utils/logging.js
+++ source/renderer/app/utils/logging.js
@@ -33,7 +33,15 @@ const logToLevel = (level: LoggingLevel) => (
     formatContext({ ...messageContext, level }),
     {
       message,
-      data,
+      // The algorithm used to serialize objects sent over IPC on electron v9+
+      // does not support sending Functions, Promises, WeakMaps, WeakSets,
+      // or objects containing any such values.
+      // https://github.com/electron/electron/blob/main/docs/breaking-changes.md#behavior-changed-values-sent-over-ipc-are-now-serialized-with-structured-clone-algorithm
+      data: ((dataVal: ?Object) =>
+        !!dataVal &&
+        typeof dataVal === 'object' &&
+        typeof dataVal !== 'function' &&
+        JSON.parse(JSON.stringify(dataVal)))(data),
       environmentData,
     },
   ];
