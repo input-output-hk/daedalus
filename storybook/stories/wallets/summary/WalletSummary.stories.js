71a4bb3243b98ee8df0d14dd89a18c6bc044bfcd
Danilo Prates <daniloprates@gmail.com>
2021-06-10 09:49:16 -0300

    [DDW-587] Summary story adjustments

--- storybook/stories/wallets/summary/WalletSummary.stories.js
+++ storybook/stories/wallets/summary/WalletSummary.stories.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import BigNumber from 'bignumber.js';
 import { storiesOf } from '@storybook/react';
-import { boolean, number, select } from '@storybook/addon-knobs';
+import { boolean, number, select, text } from '@storybook/addon-knobs';
 
 // Assets and helpers
 import { action } from '@storybook/addon-actions';
@@ -164,7 +164,8 @@ storiesOf('Wallets|Summary', module)
         'Fetching rate': 'loading',
         'Disabled or unavailable': 'off',
       },
-      'fetched'
+      'fetched',
+      'Currency'
     );
 
     let currencyIsFetchingRate = false;
@@ -178,29 +179,69 @@ storiesOf('Wallets|Summary', module)
       currencyIsActive = false;
     }
 
-    const currencySelected = select('currencySelected', currenciesList, {
-      id: 'uniswap-state-dollar',
-      symbol: 'usd',
-      name: 'unified Stable Dollar',
-    });
+    const currencySelected = select(
+      'currencySelected',
+      currenciesList,
+      {
+        id: 'uniswap-state-dollar',
+        symbol: 'usd',
+        name: 'unified Stable Dollar',
+      },
+      'Currency'
+    );
+
+    const [firstAsset] = walletAssets;
 
     return (
       <WalletSummary
         wallet={generateWallet('Wallet name', '45119903750165', assets)}
-        numberOfTransactions={number('Number of transactions', 100)}
+        numberOfTransactions={number(
+          'Number of transactions',
+          100,
+          {},
+          'Header'
+        )}
         numberOfRecentTransactions={number(
           'Number of Recent transactions',
-          100
+          100,
+          {},
+          'Header'
         )}
-        numberOfPendingTransactions={number('Number of transactions', 3)}
-        isLoadingTransactions={boolean('isLoadingTransactions', false)}
+        numberOfPendingTransactions={number(
+          'Number of pending transactions',
+          0,
+          'Header'
+        )}
+        isLoadingTransactions={boolean(
+          'isLoadingTransactions',
+          false,
+          'Header'
+        )}
         currencyIsFetchingRate={currencyIsFetchingRate}
         currencyIsActive={currencyIsActive}
         currencySelected={currencySelected}
         currencyRate={0.321}
         currencyLastFetched={currencyLastFetched}
         onCurrencySettingClick={action('onCurrencySettingClick')}
-        assets={walletAssets}
+        assets={[
+          {
+            ...firstAsset,
+            quantity: new BigNumber(number('quantity', 100, {}, 'First Asset')),
+            decimals: number('decimals', 0, {}, 'First Asset'),
+            recommendedDecimals: number(
+              'recommendedDecimals',
+              0,
+              {},
+              'First Asset'
+            ),
+            metadata: {
+              name: text('Metadata - name', '', 'First Asset'),
+              ticker: text('Metadata - ticker', '', 'First Asset'),
+              description: text('Metadata - description', '', 'First Asset'),
+            },
+          },
+          ...walletAssets.slice(1),
+        ]}
         isLoadingAssets={boolean('isLoadingAssets', false)}
         onOpenAssetSend={action('onOpenAssetSend')}
         onCopyAssetItem={action('onCopyAsset')}
