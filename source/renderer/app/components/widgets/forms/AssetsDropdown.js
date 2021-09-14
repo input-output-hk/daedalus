2ee90afeed882c007939de570a56b89a4fe81cac
Danilo Prates <daniloprates@gmail.com>
2021-09-14 16:08:53 -0300

    [DDW-694] Fix Assets dropdown search

--- source/renderer/app/components/widgets/forms/AssetsDropdown.js
+++ source/renderer/app/components/widgets/forms/AssetsDropdown.js
@@ -1,9 +1,8 @@
 // @flow
 import React, { Component } from 'react';
-import { omit } from 'lodash';
+import { omit, filter, escapeRegExp } from 'lodash';
 import ItemsDropdown from './ItemsDropdown';
 import { formattedTokenWalletAmount } from '../../../utils/formatters';
-import { searchAssets } from '../../../utils/assets';
 import type { AssetToken } from '../../../api/assets/types';
 import Asset from '../../assets/Asset';
 import styles from './AssetsDropdown.scss';
@@ -21,13 +20,33 @@ type Props = {
   assets?: Array<$Shape<AssetToken>>,
 };
 
+export const onSearchAssetsDropdown = (
+  searchValue: string,
+  options: Array<any>
+) => {
+  return filter(options, ({ asset }) => {
+    if (searchValue.length < 3) {
+      return true;
+    }
+    const { policyId, assetName, fingerprint, metadata } = asset;
+    const { name, ticker, description } = metadata || {};
+    const checkList = [
+      policyId,
+      assetName,
+      fingerprint,
+      metadata,
+      name,
+      ticker,
+      description,
+    ];
+    const regex = new RegExp(escapeRegExp(searchValue), 'i');
+    return checkList.some((item) => regex.test(item));
+  });
+};
+
 export default class AssetsDropdown extends Component<Props> {
   static defaultProps = {
-    onSearch: (searchValue: string, assets: Array<any>) =>
-      searchAssets(
-        searchValue,
-        assets.map(({ asset }) => asset)
-      ),
+    onSearch: onSearchAssetsDropdown,
   };
 
   render() {
