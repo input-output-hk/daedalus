cf413e99d4decb9a1beeaebc6bbc1ace8d2ac742
Lucas Araujo <lucas.araujo@iohk.io>
2021-11-03 09:55:24 -0300

    [DDW-786] Fix default message

--- source/renderer/app/components/widgets/discreet-mode/DiscreetToggle.js
+++ source/renderer/app/components/widgets/discreet-mode/DiscreetToggle.js
@@ -13,13 +13,13 @@ import styles from './DiscreetToggle.scss';
 const messages = defineMessages({
   discreetModeOn: {
     id: 'widgets.discreetToggle.on',
-    defaultMessage: 'Toggle discreet mode on',
+    defaultMessage: '!!!Toggle discreet mode on',
     description:
       'Text for the tooltip on "discreet mode" button when mode is on',
   },
   discreetModeOff: {
     id: 'widgets.discreetToggle.off',
-    defaultMessage: 'Toggle discreet mode off',
+    defaultMessage: '!!!Toggle discreet mode off',
     description:
       'Text for the tooltip on "discreet mode" button when mode is off',
   },
