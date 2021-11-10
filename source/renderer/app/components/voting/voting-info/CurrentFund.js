401e4ca7c82dd0c34a82ee1e99843961fd6c0c3b
Lucas <lukas_barros@hotmail.com>
2021-11-10 04:52:25 -0300

    [DDW-794] Implement the Catalysts Fund7 screen (#2732)
    * [DDW-794] Move to folder
    
    * [DDW-794] Current Fund component
    
    * [DDW-794] App shell
    
    * [DDW-794] RegisterToVote component
    
    * [DDW-794] Font adjustments
    
    * [DDW-794] Fix build
    
    * [DDW-794] Translations file
    
    * [DDW-794] CHANGELOG
    
    * [DDW-794] Translations
    
    * [DDW-794] Fix button height
    
    * [DDW-794] Introduce fund number config variable
    
    * [DDW-794] Add long date time format
    
    * [DDW-794] English translations
    
    * fixup! [DDW-794] Introduce fund number config variable
    
    * [DDW-794] Fix external button links
    
    * [DDW-794] Update translations
    
    * [DDW-794] Fix text
    
    * [DDW-794] Minor UI fixes
    
    * [DDW-794] Fix learn more button layout
    
    * [DDW-794] Minor UI fixes
    
    * [DDW-794] Add number format for user config
    
    * [DDW-794] Build translations
    
    * fixup! [DDW-794] Minor UI fixes
    
    * [DDW-794] Add space between date time for Japanese
    
    * [DDW-794] Change verb tense
    
    * [DDW-794] Use config file for reward value
    
    * [DDW-794] Remove empty lines
    
    * [DDW-794] Use a variable for register condition
    
    * [DDW-794] Date time separator logic
    
    * [DDW-794] Decople from array order
    
    * [DDW-794] Move messages to separete file
    
    * [DDW-794] Move locale to .messages file
    
    * [DDW-794] remove  messages.js file
    
    * [DDW-794] Minor UI updates
    
    * [DDW-794] Update changelog
    
    * [DDW-794] Use checkbox label prop
    
    * [DDW-794] Update ExternalLinkButton
    
    * [DDW-794] Patch Checkbox component for Voting screen
    
    This commit should be removed when https://input-output.atlassian.net/browse/DDW-811 is done.
    
    * [DDW-794] Add new text
    
    * [DDW-794] Add translation
    
    * [DDW-794] UI fixes on register to vote component
    
    * [DDW-794] Add unit
    
    * [DDW-794] Remove blank lines
    
    * [DDW-794] Translations file
    
    * [DDW-794] Update translations
    
    * [DDW-794] Make button with auto
    
    * [DDW-794] Rename variable
    
    Co-authored-by: Renan Ferreira <renan.ferreira@iohk.io>
    Co-authored-by: Danilo Prates <daniloprates@gmail.com>

---
+++ source/renderer/app/components/voting/voting-info/CurrentFund.js
@@ -0,0 +1,70 @@
+// @flow
+import React from 'react';
+import { injectIntl } from 'react-intl';
+import {
+  VOTING_REGISTRATION_CAST_END_DATE,
+  CURRENT_VOTING_FUND_NUMBER,
+} from '../../../config/votingConfig';
+import {
+  formattedDateTime,
+  mapToLongDateTimeFormat,
+} from '../../../utils/formatters';
+import type { Locale } from '../../../../../common/types/locales.types';
+import { ExternalLinkButton } from '../../widgets/ExternalLinkButton';
+import type { Intl } from '../../../types/i18nTypes';
+import styles from './CurrentFund.scss';
+import { messages } from './CurrentFund.messages';
+
+type Props = {
+  currentLocale: Locale,
+  currentDateFormat: string,
+  currentTimeFormat: string,
+  onExternalLinkClick: Function,
+  intl: Intl,
+};
+
+function CurrentFund({
+  currentLocale,
+  currentDateFormat,
+  currentTimeFormat,
+  onExternalLinkClick,
+  intl,
+}: Props) {
+  const currentFundEndDate = formattedDateTime(
+    VOTING_REGISTRATION_CAST_END_DATE,
+    {
+      currentLocale,
+      ...mapToLongDateTimeFormat({
+        currentLocale,
+        currentDateFormat,
+        currentTimeFormat,
+      }),
+    }
+  );
+
+  return (
+    <section className={styles.component}>
+      <h1 className={styles.fundName}>
+        {intl.formatMessage(messages.name, {
+          currentVotingFundNumber: CURRENT_VOTING_FUND_NUMBER,
+        })}
+      </h1>
+
+      <div className={styles.endDateBlock}>
+        <span className={styles.endDateText}>
+          {intl.formatMessage(messages.headingForEndDate)}
+        </span>
+        <span className={styles.endDate}>{currentFundEndDate}</span>
+      </div>
+
+      <ExternalLinkButton
+        label={intl.formatMessage(messages.viewResultsLinkLabel)}
+        onClick={() =>
+          onExternalLinkClick(intl.formatMessage(messages.viewResultsLinkURL))
+        }
+      />
+    </section>
+  );
+}
+
+export default injectIntl(CurrentFund);
---
+++ source/renderer/app/components/voting/voting-info/RegisterToVote.js
@@ -0,0 +1,87 @@
+// @flow
+import React, { useState } from 'react';
+import { injectIntl } from 'react-intl';
+import { Button } from 'react-polymorph/lib/components/Button';
+import {
+  VOTING_REGISTRATION_END_DATE,
+  NEXT_VOTING_FUND_NUMBER,
+} from '../../../config/votingConfig';
+import {
+  formattedDateTime,
+  mapToLongDateTimeFormat,
+} from '../../../utils/formatters';
+import type { Locale } from '../../../../../common/types/locales.types';
+import type { Intl } from '../../../types/i18nTypes';
+import styles from './RegisterToVote.scss';
+import { PatchCheckbox } from '../widgets/PatchCheckbox';
+import { messages } from './RegisterToVote.messages';
+
+type Props = {
+  currentLocale: Locale,
+  currentDateFormat: string,
+  currentTimeFormat: string,
+  intl: Intl,
+  onRegisterToVoteClick: Function,
+};
+
+function RegisterToVote({
+  currentLocale,
+  currentDateFormat,
+  currentTimeFormat,
+  intl,
+  onRegisterToVoteClick,
+}: Props) {
+  const [step1, setStep1] = useState(false);
+  const [step2, setStep2] = useState(false);
+  const canRegister = step1 && step2;
+  const castEndDate = formattedDateTime(VOTING_REGISTRATION_END_DATE, {
+    currentLocale,
+    ...mapToLongDateTimeFormat({
+      currentLocale,
+      currentDateFormat,
+      currentTimeFormat,
+    }),
+  });
+
+  return (
+    <div className={styles.root}>
+      <span className={styles.title}>
+        {intl.formatMessage(messages.name, {
+          nextVotingFundNumber: NEXT_VOTING_FUND_NUMBER,
+        })}
+      </span>
+      <span className={styles.dateLabel}>
+        {intl.formatMessage(messages.dateLabel)}
+      </span>
+      <span className={styles.date}>{castEndDate}</span>
+      <div className={styles.separator} />
+      <span className={styles.stepsTitle}>
+        {intl.formatMessage(messages.stepsTitle)}
+      </span>
+      <div className={styles.step}>
+        <PatchCheckbox
+          className={styles.checkbox}
+          checked={step1}
+          onChange={setStep1}
+          label={intl.formatMessage(messages.step1CheckBoxLabel)}
+        />
+      </div>
+      <div className={styles.step}>
+        <PatchCheckbox
+          className={styles.checkbox}
+          checked={step2}
+          label={intl.formatMessage(messages.step2CheckBoxLabel)}
+          onChange={setStep2}
+        />
+      </div>
+      <Button
+        className={styles.button}
+        onClick={onRegisterToVoteClick}
+        label={intl.formatMessage(messages.buttonLabel)}
+        disabled={!canRegister}
+      />
+    </div>
+  );
+}
+
+export default injectIntl(RegisterToVote);
