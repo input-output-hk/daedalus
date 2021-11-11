c354445ed6be38e10e2de60a8371a9560da89720
Tomislav Horaček <tomislav@tt-media.hr>
2021-11-11 16:16:57 +0100

    Pass config packageOverrides to the default.nix pkgs

--- default.nix
+++ default.nix
@@ -30,7 +30,7 @@ let
       });
     };
   };
-  pkgs = localLib.iohkNix.pkgsDefault;
+  pkgs = localLib.iohkNix.getPkgsDefault { inherit config; };
   pkgsNodeJS = import sources.nixpkgs { inherit system config; };
   pkgsNative = localLib.iohkNix.getPkgsDefault {};
   sources = localLib.sources;
