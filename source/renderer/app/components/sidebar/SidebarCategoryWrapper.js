85b3d656823918b239d1e4dfbcb53032aa27d15c
Lucas Araujo <lucas.araujo@iohk.io>
2021-11-18 14:24:40 -0300

    [DDW-787] Fix tooltip showing after accessing security tab

--- source/renderer/app/components/sidebar/SidebarCategoryWrapper.js
+++ source/renderer/app/components/sidebar/SidebarCategoryWrapper.js
@@ -6,7 +6,6 @@ import { PopOver } from 'react-polymorph/lib/components/PopOver';
 import { CATEGORIES_BY_NAME } from '../../config/sidebarConfig';
 import NotificationDot from '../widgets/notification-dot/NotificationDot';
 import DiscreetModeFeatureInject from '../../features/discreet-mode/ui/DiscreetModeFeatureInject';
-
 import { messages } from './SidebarCategoryWrapper.messages';
 import styles from './SidebarCategoryWrapper.scss';
 
@@ -17,27 +16,28 @@ type Props = {
 
 const SidebarCategoryWrapper = ({ children, categoryName }: Props) => {
   if (categoryName === CATEGORIES_BY_NAME.SETTINGS.name) {
+    const popOverContent = (
+      <div className={styles.content}>
+        <FormattedHTMLMessage {...messages.label} />
+      </div>
+    );
     return (
-      <PopOver
-        placement="right"
-        className={styles.popOver}
-        content={
-          <div className={styles.content}>
-            <FormattedHTMLMessage {...messages.label} />
-          </div>
-        }
-      >
-        <DiscreetModeFeatureInject>
-          {({ isNotificationEnabled }) => (
+      <DiscreetModeFeatureInject>
+        {({ isNotificationEnabled }) => (
+          <PopOver
+            placement="right"
+            className={styles.popOver}
+            content={isNotificationEnabled ? popOverContent : null}
+          >
             <NotificationDot
               enabled={isNotificationEnabled}
               dotClassName={styles.dot}
             >
               {children}
             </NotificationDot>
-          )}
-        </DiscreetModeFeatureInject>
-      </PopOver>
+          </PopOver>
+        )}
+      </DiscreetModeFeatureInject>
     );
   }
 
