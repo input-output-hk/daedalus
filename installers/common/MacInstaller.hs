753a5387686c176f372d9a6f74497efee99ba0f7
Tomislav Horaček <tomislav@tt-media.hr>
2021-11-22 16:07:22 +0100

    [DDW-779] Add int64-buffer to MacInstaller

--- installers/common/MacInstaller.hs
+++ installers/common/MacInstaller.hs
@@ -270,6 +270,7 @@ buildElectronApp darwinConfig@DarwinConfig{dcAppName, dcAppNameApp} installerCon
       , "hmac-drbg"
       , "ieee754"
       , "inherits"
+      , "int64-buffer"
       , "js-chain-libs-node"
       , "json-stable-stringify"
       , "keccak"
