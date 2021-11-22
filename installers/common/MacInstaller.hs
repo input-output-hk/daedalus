1dc935b1e5f4c2aaa7c6da4b5385c31c2cfcb42b
Tomislav Horaček <tomislav@tt-media.hr>
2021-11-22 15:32:32 +0100

    [DDW-779] Add brorand to MacInstaller

--- installers/common/MacInstaller.hs
+++ installers/common/MacInstaller.hs
@@ -246,6 +246,7 @@ buildElectronApp darwinConfig@DarwinConfig{dcAppName, dcAppNameApp} installerCon
       , "blake-hash"
       , "blake2b-wasm"
       , "bn.js"
+      , "brorand"
       , "bs58"
       , "bs58check"
       , "bytebuffer-old-fixed-webpack"
