e8a87fe9bcc0c4c3521afa82e43416ba86ca121b
Tomislav Horaček <tomislav@tt-media.hr>
2021-11-22 15:15:49 +0100

    [DDW-779] Add minimalistic-crypto-utils to MacInstaller

--- installers/common/MacInstaller.hs
+++ installers/common/MacInstaller.hs
@@ -272,7 +272,8 @@ buildElectronApp darwinConfig@DarwinConfig{dcAppName, dcAppNameApp} installerCon
       , "keccak"
       , "long"
       , "ms"
-      , "minimalistic-assert"
+      , "minimalistic-assert",
+      , "minimalistic-crypto-utils"
       , "nanoassert"
       , "node-fetch"
       , "object-keys"
