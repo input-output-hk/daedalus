3421451943dc1ead8addce97af15249bc4e190ed
Tomislav Horaček <tomislav@tt-media.hr>
2021-11-22 14:45:41 +0100

    [DDW-779] Add elliptic to MacInstaller

--- installers/common/MacInstaller.hs
+++ installers/common/MacInstaller.hs
@@ -258,6 +258,7 @@ buildElectronApp darwinConfig@DarwinConfig{dcAppName, dcAppNameApp} installerCon
       , "create-hmac"
       , "cross-fetch"
       , "define-properties"
+      , "ecurve"
       , "elliptic"
       , "es-abstract"
       , "function-bind"
