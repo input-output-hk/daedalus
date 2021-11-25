c53e12f75d3d692709bc2e2e8e9551d2f8a54a1b
Renan Ferreira <renan.ferreira@iohk.io>
2021-11-25 12:13:29 -0300

    [DDW-839] Tests for public api

--- source/renderer/app/features/discreet-mode/integration-tests.spec.js
+++ source/renderer/app/features/discreet-mode/integration-tests.spec.js
@@ -1,9 +1,10 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
 import type { Node } from 'react';
 import { IntlProvider } from 'react-intl';
 import BigNumber from 'bignumber.js';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { observer } from 'mobx-react';
+import { render, fireEvent, screen, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import { BrowserLocalStorageBridge } from '../local-storage';
@@ -12,83 +13,150 @@ import {
   DiscreetWalletAmount,
   DiscreetModeToggle,
   DiscreetTokenWalletAmount,
+  DiscreetValue,
+  withDiscreetMode,
 } from './ui';
+import { DiscreetMode } from './feature';
 
 describe('Discreet Mode feature', () => {
-  function DiscreetModeFeatureDecorator({ children }: { children: Node }) {
+  afterEach(cleanup);
+
+  function TestDecorator({
+    children,
+    testId,
+  }: {
+    children: Node,
+    testId: string,
+  }) {
     return (
       <IntlProvider locale="en-US">
-        <BrowserLocalStorageBridge>{children}</BrowserLocalStorageBridge>
+        <BrowserLocalStorageBridge>
+          <DiscreetModeFeatureProvider>
+            <>
+              <DiscreetModeToggle />
+
+              <div data-testid={testId}>{children}</div>
+            </>
+          </DiscreetModeFeatureProvider>
+        </BrowserLocalStorageBridge>
       </IntlProvider>
     );
   }
 
-  test('should replace wallet amount by sensitive data symbol', () => {
+  const discreetWalletAmountCases = [
+    // testId, [amount, currency, withCurrency, long], [expected]
+    ['custom ticker', [new BigNumber(1), 'TEST', true, true], '1.000000 TEST'],
+    ['ada as ticker', [new BigNumber(1), undefined, true, false], '1 ADA'],
+    ['no currency', [new BigNumber(1), 'TEST', false, false], '1'],
+  ];
+
+  test.each(discreetWalletAmountCases)(
+    '<DiscreetWalletAmount /> should replace wallet amount with %s by sensitive data symbol',
+    async (testId, [amount, currency, withCurrency, long], expected) => {
+      expect.assertions(2);
+
+      render(
+        <TestDecorator testId={testId}>
+          <DiscreetWalletAmount
+            amount={amount}
+            currency={currency}
+            withCurrency={withCurrency}
+            long={long}
+          />
+        </TestDecorator>
+      );
+
+      expect(screen.getByTestId(testId)).toHaveTextContent(expected);
+
+      fireEvent.click(
+        screen.getByRole('button', { name: /discreetModeToggle/i })
+      );
+
+      expect(screen.getByTestId(testId)).toHaveTextContent('***');
+    }
+  );
+
+  const discreetTokenWalletAmountCases = [
+    // testId, [amount, metada, decimals], [expected]
+    ['without ticker', [new BigNumber(1), null, 0], '1'],
+    ['with ticker', [new BigNumber(1), { ticker: 'TEST' }, 5], '0.00001 TEST'],
+  ];
+
+  test.each(discreetTokenWalletAmountCases)(
+    '<DiscreetTokenWalletAmount /> should replace token wallet amount with %s by sensitive data symbol',
+    (testId, [amount, metadata, decimals], expected) => {
+      expect.assertions(2);
+
+      render(
+        <TestDecorator testId={testId}>
+          <DiscreetTokenWalletAmount
+            amount={amount}
+            metadata={metadata}
+            decimals={decimals}
+          />
+        </TestDecorator>
+      );
+
+      expect(screen.getByTestId(testId)).toHaveTextContent(expected);
+
+      fireEvent.click(
+        screen.getByRole('button', { name: /discreetModeToggle/i })
+      );
+
+      expect(screen.getByTestId(testId)).toHaveTextContent('***');
+    }
+  );
+
+  test('<DiscreetValue /> should replace value by sensitive data symbol', () => {
     expect.assertions(2);
 
-    const amount = new BigNumber(1);
-    const currency = 'TEST';
+    const value = 'test';
+    const testId = 'discreet-value';
 
     render(
-      <DiscreetModeFeatureDecorator>
-        <DiscreetModeFeatureProvider>
-          <>
-            <DiscreetModeToggle />
-
-            <div data-testid="walletAmount">
-              <DiscreetWalletAmount amount={amount} currency={currency} />
-            </div>
-          </>
-        </DiscreetModeFeatureProvider>
-      </DiscreetModeFeatureDecorator>
+      <TestDecorator testId={testId}>
+        <DiscreetValue>{value}</DiscreetValue>
+      </TestDecorator>
     );
 
-    expect(screen.getByTestId('walletAmount')).toHaveTextContent(
-      `1.000000 ${currency}`
-    );
+    expect(screen.getByTestId(testId)).toHaveTextContent(value);
 
     fireEvent.click(
       screen.getByRole('button', { name: /discreetModeToggle/i })
     );
 
-    expect(screen.getByTestId('walletAmount')).toHaveTextContent('***');
+    expect(screen.getByTestId(testId)).toHaveTextContent('***');
   });
 
-  test('should replace token wallet amount by sensitive data symbol', () => {
+  test('<withDiscreetMode /> high order component should replace value by sensitive data symbol', () => {
     expect.assertions(2);
 
-    const amount = new BigNumber(1);
-    const metadata = {
-      ticker: 'TEST',
-    };
-    const decimals = 1;
+    const value = 'test';
+    const testId = 'discreet-value';
+    const HighOrderComponentApi = withDiscreetMode(
+      observer(
+        class View extends Component<{ discreetModeFeature: DiscreetMode }> {
+          render() {
+            return this.props.discreetModeFeature.discreetValue({
+              value,
+            });
+          }
+        }
+      )
+    );
 
     render(
-      <DiscreetModeFeatureDecorator>
-        <DiscreetModeFeatureProvider>
-          <>
-            <DiscreetModeToggle />
-
-            <div data-testid="walletAmount">
-              <DiscreetTokenWalletAmount
-                amount={amount}
-                metadata={metadata}
-                decimals={decimals}
-              />
-            </div>
-          </>
-        </DiscreetModeFeatureProvider>
-      </DiscreetModeFeatureDecorator>
+      <TestDecorator testId={testId}>
+        <HighOrderComponentApi />
+      </TestDecorator>
     );
 
-    expect(screen.getByTestId('walletAmount')).toHaveTextContent(
-      `0.1 ${metadata.ticker}`
-    );
+    expect(screen.getByTestId(testId)).toHaveTextContent(value);
 
     fireEvent.click(
       screen.getByRole('button', { name: /discreetModeToggle/i })
     );
 
-    expect(screen.getByTestId('walletAmount')).toHaveTextContent('***');
+    expect(screen.getByTestId(testId)).toHaveTextContent('***');
   });
 });
