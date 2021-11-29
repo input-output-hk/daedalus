6def1565d9390b4166caa3baf06d8ab22c93a2d8
Daniel Main <daniel.main.cernhoff@icloud.com>
2021-11-29 18:26:51 +0100

    Adding hotfix from release 4.5.2 without rts flags

--- source/main/utils/detectSystemLocale.js
+++ source/main/utils/detectSystemLocale.js
@@ -1,10 +1,12 @@
 // @flow
 import { app } from 'electron';
+import { logger } from './logging';
 import { LOCALES } from '../../common/types/locales.types.js';
 import type { Locale } from '../../common/types/locales.types.js';
 
 export const detectSystemLocale = (): Locale => {
   const systemLocale = app.getLocale();
+  logger.info('Detected system locale', { systemLocale });
   if (systemLocale === 'ja') {
     return LOCALES.japanese;
   }
