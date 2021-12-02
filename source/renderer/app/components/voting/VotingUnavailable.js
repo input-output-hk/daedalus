0cafc2730cf129e10e4826fe9966d1a851f134be
Lucas Araujo <lucas.araujo@iohk.io>
2021-12-02 17:56:35 -0300

    [DDW-796] Fix lint issues

--- source/renderer/app/components/voting/VotingUnavailable.js
+++ source/renderer/app/components/voting/VotingUnavailable.js
@@ -2,40 +2,26 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 import { FormattedHTMLMessage } from 'react-intl';
+import BigNumber from 'bignumber.js';
 import globalMessages from '../../i18n/global-messages';
 import LoadingSpinner from '../widgets/LoadingSpinner';
 import styles from './VotingUnavailable.scss';
-import { formattedNumber } from '../../utils/formatters';
 
 type Props = {
   syncPercentage: number,
 };
 
-@observer
-export default class VotingUnavailable extends Component<Props> {
-  static contextTypes = {
-    intl: intlShape.isRequired,
-  };
-
-  render() {
-    const { intl } = this.context;
-    const {
-      syncPercentage,
-      isVotingRegistrationAvailable,
-      onExternalLinkClick,
-    } = this.props;
-
-    return (
-      <div className={styles.component}>
-        <LoadingSpinner big />
-        <div className={styles.description}>
-          <FormattedHTMLMessage
-            {...globalMessages.featureUnavailableWhileSyncing}
-            values={{
-              syncPercentage: new BigNumber(syncPercentage).toFormat(2),
-            }}
-          />
-        </div>
+const VotingUnavailable = ({ syncPercentage }: Props) => {
+  return (
+    <div className={styles.component}>
+      <LoadingSpinner big />
+      <div className={styles.description}>
+        <FormattedHTMLMessage
+          {...globalMessages.featureUnavailableWhileSyncing}
+          values={{
+            syncPercentage: new BigNumber(syncPercentage).toFormat(2),
+          }}
+        />
       </div>
     </div>
   );
