cb5fcb8b1fdd4c64d941e03f619c1b879105f543
Lucas Araujo <lucas.araujo@iohk.io>
2021-12-02 15:10:55 -0300

    [DDW-796] Remove closed voting logic

--- source/renderer/app/components/voting/VotingUnavailable.js
+++ source/renderer/app/components/voting/VotingUnavailable.js
@@ -11,17 +11,31 @@ type Props = {
   syncPercentage: number,
 };
 
-const VotingUnavailable = ({ syncPercentage }: Props) => {
-  return (
-    <div className={styles.component}>
-      <LoadingSpinner big />
-      <div className={styles.description}>
-        <FormattedHTMLMessage
-          {...globalMessages.featureUnavailableWhileSyncing}
-          values={{
-            syncPercentage: formattedNumber(syncPercentage, 2),
-          }}
-        />
+@observer
+export default class VotingUnavailable extends Component<Props> {
+  static contextTypes = {
+    intl: intlShape.isRequired,
+  };
+
+  render() {
+    const { intl } = this.context;
+    const {
+      syncPercentage,
+      isVotingRegistrationAvailable,
+      onExternalLinkClick,
+    } = this.props;
+
+    return (
+      <div className={styles.component}>
+        <LoadingSpinner big />
+        <div className={styles.description}>
+          <FormattedHTMLMessage
+            {...globalMessages.featureUnavailableWhileSyncing}
+            values={{
+              syncPercentage: new BigNumber(syncPercentage).toFormat(2),
+            }}
+          />
+        </div>
       </div>
     </div>
   );
