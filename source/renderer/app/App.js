b9e4c8c4a0d8fbc7977e9efca4bffd7281dd59d2
Lucas Araujo <lucas.araujo@iohk.io>
2021-12-07 16:17:11 -0300

    [DDW-796] Test only

--- source/renderer/app/App.js
+++ source/renderer/app/App.js
@@ -1,6 +1,7 @@
 // @flow
 import React, { Component, Fragment } from 'react';
 import { Provider, observer } from 'mobx-react';
+import { spy } from 'mobx';
 import { ThemeProvider } from 'react-polymorph/lib/components/ThemeProvider';
 import { SimpleSkins } from 'react-polymorph/lib/skins/simple';
 import { SimpleDefaults } from 'react-polymorph/lib/themes/simple';
@@ -28,6 +29,9 @@ export default class App extends Component<{
   history: Object,
 }> {
   componentDidMount() {
+    spy((event) => {
+      console.log(event);
+    });
     // Loads app's global environment variables into AppStore via ipc
     this.props.actions.app.initAppEnvironment.trigger();
   }
