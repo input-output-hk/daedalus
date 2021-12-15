e43ec44a8827d081be8b7d4a0ea1777fe5a3b6e0
Lucas Araujo <lucas.araujo@iohk.io>
2021-12-15 14:02:59 -0300

    [DDW-861] WalletToken refactor

---
+++ source/renderer/app/components/wallet/tokens/WalletTokenFooter.js
@@ -0,0 +1,90 @@
+// @flow
+import React from 'react';
+import { observer } from 'mobx-react';
+import { intlShape, injectIntl } from 'react-intl';
+import { Button } from 'react-polymorph/lib/components/Button';
+import { PopOver } from 'react-polymorph/lib/components/PopOver';
+import classNames from 'classnames';
+import SVGInline from 'react-svg-inline';
+import styles from './WalletTokenFooter.scss';
+import AssetAmount from '../../assets/AssetAmount';
+import type { AssetToken } from '../../../api/assets/types';
+import warningIcon from '../../../assets/images/asset-token-warning-ic.inline.svg';
+import { messages } from './WalletToken.messages';
+import { isRecommendedDecimal } from './helpers';
+
+type Props = {
+  asset: AssetToken,
+  intl: intlShape.isRequired,
+  isLoading: boolean,
+  onAssetSettings: Function,
+  onOpenAssetSend: Function,
+};
+
+const WalletTokenFooter = (props: Props) => {
+  const { asset, intl, isLoading, onAssetSettings, onOpenAssetSend } = props;
+  const { recommendedDecimals, decimals } = asset;
+  const hasWarning = isRecommendedDecimal({ decimals, recommendedDecimals });
+  const warningPopOverMessage =
+    typeof decimals === 'number'
+      ? messages.settingsWarningPopOverNotUsing
+      : messages.settingsWarningPopOverAvailable;
+
+  return (
+    <div className={styles.footer}>
+      <div className={styles.amount}>
+        <span className={styles.amountLabel}>
+          {intl.formatMessage(messages.amountLabel)}
+        </span>
+        <div className={styles.amountValue}>
+          <AssetAmount
+            amount={asset.quantity}
+            metadata={asset.metadata}
+            decimals={asset.decimals}
+            isLoading={isLoading}
+            className={styles.assetAmount}
+          />
+        </div>
+      </div>
+      <div className={styles.footerButtons}>
+        <PopOver
+          content={
+            hasWarning &&
+            intl.formatMessage(warningPopOverMessage, {
+              recommendedDecimals,
+            })
+          }
+        >
+          <Button
+            className={classNames([
+              'flat',
+              styles.button,
+              styles.settingsButton,
+            ])}
+            label={
+              <>
+                {intl.formatMessage(messages.settingsButtonLabel)}
+                {hasWarning && (
+                  <SVGInline className={styles.warningIcon} svg={warningIcon} />
+                )}
+              </>
+            }
+            onClick={() => onAssetSettings({ asset })}
+          />
+        </PopOver>
+        <Button
+          className={classNames([
+            'primary',
+            styles.button,
+            asset.quantity.isZero() && styles.disabled,
+          ])}
+          onClick={() => onOpenAssetSend(asset)}
+          label={intl.formatMessage(messages.tokenSendButton)}
+          disabled={asset.quantity.isZero()}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default injectIntl(observer(WalletTokenFooter));
---
+++ source/renderer/app/components/wallet/tokens/WalletTokenHeader.js
@@ -0,0 +1,86 @@
+// @flow
+import React from 'react';
+import { observer } from 'mobx-react';
+import classNames from 'classnames';
+import { get } from 'lodash';
+import SVGInline from 'react-svg-inline';
+import styles from './WalletTokenHeader.scss';
+import Asset from '../../assets/Asset';
+import AssetAmount from '../../assets/AssetAmount';
+import type { AssetToken } from '../../../api/assets/types';
+import arrow from '../../../assets/images/collapse-arrow-small.inline.svg';
+import starNotFilledIcon from '../../../assets/images/star-not-filled.inline.svg';
+import starFilledIcon from '../../../assets/images/star-filled.inline.svg';
+import { isRecommendedDecimal } from './helpers';
+
+type Props = {
+  anyAssetWasHovered: boolean,
+  asset: AssetToken,
+  assetSettingsDialogWasOpened: boolean,
+  isExpanded: boolean,
+  isFavorite: boolean,
+  isLoading: boolean,
+  onClick: Function,
+  onCopyAssetParam: Function,
+  onToggleFavorite: Function,
+};
+
+const WalletTokenHeader = (props: Props) => {
+  const {
+    anyAssetWasHovered,
+    asset,
+    assetSettingsDialogWasOpened,
+    isExpanded,
+    isFavorite,
+    isLoading,
+    onClick,
+    onCopyAssetParam,
+    onToggleFavorite,
+  } = props;
+  const { decimals, uniqueId, recommendedDecimals } = asset;
+  const starIcon = isFavorite ? starFilledIcon : starNotFilledIcon;
+  const hasWarning = isRecommendedDecimal({ decimals, recommendedDecimals });
+  const rootStyles = classNames(styles.root, isExpanded && styles.isExpanded);
+  const favoriteIconStyles = classNames(
+    styles.favoriteIcon,
+    props.isFavorite && styles.isFavorite
+  );
+
+  return (
+    <div className={rootStyles} onClick={onClick}>
+      <button
+        className={favoriteIconStyles}
+        onClick={(event) => {
+          event.persist();
+          event.stopPropagation();
+          onToggleFavorite({ uniqueId, isFavorite });
+        }}
+      >
+        <SVGInline className={styles.warningIcon} svg={starIcon} />
+      </button>
+
+      <Asset
+        asset={asset}
+        onCopyAssetParam={onCopyAssetParam}
+        metadataNameChars={get('name', asset.metadata, 0)}
+        assetSettingsDialogWasOpened={assetSettingsDialogWasOpened}
+        anyAssetWasHovered={anyAssetWasHovered}
+        className={styles.asset}
+        hidePopOver
+        fullFingerprint
+        hasWarning={hasWarning}
+      />
+      <AssetAmount
+        amount={asset.quantity}
+        metadata={asset.metadata}
+        decimals={asset.decimals}
+        isLoading={isLoading}
+        className={styles.assetAmount}
+        isShort
+      />
+      <SVGInline svg={arrow} className={styles.arrow} />
+    </div>
+  );
+};
+
+export default observer(WalletTokenHeader);
