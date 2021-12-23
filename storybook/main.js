fadfb7c03a19d4e72349c542b31aed17fb7d265f
Daniel Main <daniel.main.cernhoff@icloud.com>
2021-12-23 22:31:38 +0100

    [DDW-596] Upgraded to storybook 6

---
+++ storybook/main.js
@@ -0,0 +1,62 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  alias: {
+    Renderer: path.resolve(__dirname, 'source/renderer'),
+  },
+  stories: ['./stories/**/*.stories.@(js|mdx|ts)'],
+  addons: [
+    '@storybook/addon-knobs',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/builder-webpack5',
+    '@storybook/manager-webpack5',
+    './addons/DaedalusMenu/register',
+  ],
+  core: {
+    builder: 'webpack5',
+  },
+  // Make whatever fine-grained changes you need
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.scss/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: {
+              localIdentName: '[name]_[local]',
+            },
+            sourceMap: true,
+            importLoaders: true,
+          },
+        },
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true,
+            implementation: require.resolve('sass'),
+          },
+        },
+        {
+          loader: 'babel-loader',
+          options: {
+            cacheCompression: false,
+            cacheDirectory: true,
+            plugins: [require.resolve('react-refresh/babel')].filter(Boolean),
+          },
+        },
+      ],
+    });
+
+    // Return the altered config
+    return config;
+  },
+};
