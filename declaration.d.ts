912a0e7f9ff97b8caa82e7ac7e8b156f2e2eaec1
Michael Chappell <michael.chappell@iohk.io>
2022-01-05 13:04:41 +0000

    [DDW-596] Update enum map declaration

--- declaration.d.ts
+++ declaration.d.ts
@@ -22,7 +22,16 @@ type Daedalus = {
   };
 };
 
-//declare type EnumMap<K: string, V, O: Object = *> = O & { [K]: V & <O, K> };
+export type $ElementType<
+  T extends { [P in K & any]: any },
+  K extends keyof T | number
+> = T[K];
+
+export type EnumMap<
+  K extends string,
+  V,
+  O extends Record<string, any> = any
+> = O & Record<K, V & $ElementType<O, K>>;
 
 declare global {
   namespace NodeJS {
