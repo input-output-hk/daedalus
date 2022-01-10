2c5c6de54491564f7bdec2857c10a832d8194fc8
Michael Chappell <michael.chappell@iohk.io>
2022-01-10 10:51:59 +0000

    [DDW-596] Updated lockfile

--- utils/lockfile-checker/index.ts
+++ utils/lockfile-checker/index.ts
@@ -16,16 +16,35 @@ lodash.map(json.object, (entry, key) => {
     dependenciesWithIntegrity.push(key);
   }
 });
-const dependencyNamesToRemove = ['@types/react-syntax-highlighter', '@types/npmlog', '@types/reactcss', '@types/uglify-js', '@types/parse-json', '@types/html-minifier-terser', '@types/minimist', 'blake2b-wasm@https://github.com/BitGo/blake2b-wasm', 'blake2b@https://github.com/BitGo/blake2b', '@types/aria-query', '@types/istanbul-lib-report', '@types/micromatch', '@storybook/semver', '@types/glob-base', 'glob-base'];
-const dependenciesToRemove = Object.keys(json.object).filter(key => dependencyNamesToRemove.find(name => key.includes(name)));
-const affectedDependencies = Object.keys(json.object).filter(key => {
-  const {
-    dependencies
-  } = json.object[key];
-  return dependencies && dependencyNamesToRemove.find(name => dependencies[name]);
+const dependencyNamesToRemove = [
+  '@types/react-syntax-highlighter',
+  '@types/npmlog',
+  '@types/reactcss',
+  '@types/uglify-js',
+  '@types/parse-json',
+  '@types/html-minifier-terser',
+  '@types/minimist',
+  'blake2b-wasm@https://github.com/BitGo/blake2b-wasm',
+  'blake2b@https://github.com/BitGo/blake2b',
+  '@types/aria-query',
+  '@types/istanbul-lib-report',
+  '@types/micromatch',
+  '@types/glob-base',
+  'glob-base',
+];
+const dependenciesToRemove = Object.keys(json.object).filter((key) =>
+  dependencyNamesToRemove.find((name) => key.includes(name))
+);
+const affectedDependencies = Object.keys(json.object).filter((key) => {
+  const { dependencies } = json.object[key];
+  return (
+    dependencies && dependencyNamesToRemove.find((name) => dependencies[name])
+  );
 });
 const blake2bVersion = '2.1.3';
-const blake2bGitRefDependency = Object.keys(json.object).find(key => key.includes('blake2b@git+https://github.com'));
+const blake2bGitRefDependency = Object.keys(json.object).find((key) =>
+  key.includes('blake2b@git+https://github.com')
+);
 const shouldFix = process.argv.slice(2)[0] === '--fix';
 
 if (shouldFix) {
@@ -37,7 +56,12 @@ if (shouldFix) {
 function check() {
   console.log('\x1b[36m%s\x1b[0m', 'Checking yarn.lock file...\n');
 
-  if (!dependenciesWithIntegrity.length && !dependenciesToRemove.length && !affectedDependencies.length && !blake2bGitRefDependency) {
+  if (
+    !dependenciesWithIntegrity.length &&
+    !dependenciesToRemove.length &&
+    !affectedDependencies.length &&
+    !blake2bGitRefDependency
+  ) {
     console.log('\n \x1b[32m', 'All good, yarn.lock is clean!\n', '\x1b[0m');
     return;
   }
@@ -46,30 +70,60 @@ function check() {
 
   if (dependenciesWithIntegrity.length) {
     console.log(`\x1b[31mPlease check dependency integrity hashes!`, '\x1b[0m');
-    console.log(`\x1b[31mDependencies with integrity: ${lodash.join(dependenciesWithIntegrity, ', ')}\n`, '\x1b[0m');
+    console.log(
+      `\x1b[31mDependencies with integrity: ${lodash.join(
+        dependenciesWithIntegrity,
+        ', '
+      )}\n`,
+      '\x1b[0m'
+    );
   }
 
   if (dependenciesToRemove.length) {
     console.log(`\x1b[31mPlease check dependencies to remove!`, '\x1b[0m');
-    console.log(`\x1b[31mDependencies to remove: ${lodash.join(dependenciesToRemove, ', ')}\n`, '\x1b[0m');
+    console.log(
+      `\x1b[31mDependencies to remove: ${lodash.join(
+        dependenciesToRemove,
+        ', '
+      )}\n`,
+      '\x1b[0m'
+    );
   }
 
   if (affectedDependencies.length) {
     console.log(`\x1b[31mPlease check affected dependencies!`, '\x1b[0m');
-    console.log(`\x1b[31mAffected dependencies: ${lodash.join(affectedDependencies, ', ')}\n`, '\x1b[0m');
+    console.log(
+      `\x1b[31mAffected dependencies: ${lodash.join(
+        affectedDependencies,
+        ', '
+      )}\n`,
+      '\x1b[0m'
+    );
   }
 
   if (blake2bGitRefDependency) {
     console.log(`\x1b[31mPlease check blake2b dependency!`, '\x1b[0m');
-    console.log(`\x1b[31mblake2b dependency: ${blake2bGitRefDependency}\n`, '\x1b[0m');
+    console.log(
+      `\x1b[31mblake2b dependency: ${blake2bGitRefDependency}\n`,
+      '\x1b[0m'
+    );
   }
 
   console.log('To FIX issues run: \x1b[36m yarn lockfile:fix\n', '\x1b[0m');
 }
 
 function fix() {
-  if (!dependenciesWithIntegrity.length && !dependenciesToRemove.length && !affectedDependencies.length && !blake2bGitRefDependency) {
-    console.log('\n \x1b[32m', 'Nothing to fix, yarn.lock is clean!\n', '\x1b[0m');
+  if (
+    !dependenciesWithIntegrity.length &&
+    !dependenciesToRemove.length &&
+    !affectedDependencies.length &&
+    !blake2bGitRefDependency
+  ) {
+    console.log(
+      '\n \x1b[32m',
+      'Nothing to fix, yarn.lock is clean!\n',
+      '\x1b[0m'
+    );
     return;
   }
 
@@ -89,7 +143,9 @@ function fix() {
     const cleanedDependencies = obj.dependencies;
 
     if (cleanedDependencies) {
-      dependencyNamesToRemove.forEach(name => delete cleanedDependencies[name]);
+      dependencyNamesToRemove.forEach(
+        (name) => delete cleanedDependencies[name]
+      );
 
       if (cleanedDependencies.blake2b) {
         cleanedDependencies.blake2b = blake2bVersion;
@@ -99,14 +155,18 @@ function fix() {
     }
 
     Object.assign(fixedJSON, {
-      [key]: obj
+      [key]: obj,
     });
   });
 
   try {
     const fixedFileContent = lockfile.stringify(fixedJSON);
     fs.writeFileSync('yarn.lock', fixedFileContent);
-    console.log('\n \x1b[32m', 'yarn.lock file successfully FIXED!\n', '\x1b[0m');
+    console.log(
+      '\n \x1b[32m',
+      'yarn.lock file successfully FIXED!\n',
+      '\x1b[0m'
+    );
   } catch (err) {
     throw err;
   }
