49c0835728983af9bd0e1f5406e02eb32ab08988
Szymon Masłowski <szymon.maslowski@iohk.io>
2022-01-17 16:39:51 +0100

    [DDW-737] Improve code readability in the formatCpuInfo function

--- source/renderer/app/utils/formatCpuInfo.js
+++ source/renderer/app/utils/formatCpuInfo.js
@@ -5,6 +5,14 @@ import type {
 } from '../../../common/types/environment.types';
 import { formattedNumber } from './formatters';
 
+type FormatArgs = {
+  modelName: string,
+  speedValue: string,
+};
+
+const format = ({ modelName, speedValue }: FormatArgs) =>
+  `${modelName} @ ${speedValue}GHz`;
+
 const formatCpuInfo = (cpu: Cpu): string => {
   const { model, speed } = cpu?.[0] || ({}: CpuThreadData);
   if (!model) return '';
@@ -12,23 +20,23 @@ const formatCpuInfo = (cpu: Cpu): string => {
   const [modelName, modelSpeedSection] = model
     .split('@')
     .map((stringPart) => stringPart.trim().replace(/\s+/g, ' '));
-  if (!speed) {
-    const modelSpeedSectionExpressedInGHz = !!modelSpeedSection?.match(
-      /GHz/
-    )?.[0];
-    if (!modelSpeedSection && !modelSpeedSectionExpressedInGHz)
-      return modelName;
 
-    const rawSpeedValue = modelSpeedSection.match(/([\d,.]+)\s*GHz/)?.[1];
-    const parsedSpeedValue = parseFloat(rawSpeedValue);
-    if (!parsedSpeedValue) return modelName;
-
-    const speedValue = formattedNumber(parsedSpeedValue, 2);
-    return `${modelName} @ ${speedValue}GHz`;
+  if (speed) {
+    const speedValue = formattedNumber(speed / 1000, 2);
+    return format({ modelName, speedValue });
   }
 
-  const speedValue = formattedNumber(speed / 1000, 2);
-  return `${modelName} @ ${speedValue}GHz`;
+  const modelSpeedSectionExpressedInGHz = !!modelSpeedSection?.match(/GHz/);
+  if (!modelSpeedSection && !modelSpeedSectionExpressedInGHz) {
+    return modelName;
+  }
+
+  const rawSpeedValue = modelSpeedSection.match(/([\d,.]+)\s*GHz/)?.[1];
+  const parsedSpeedValue = parseFloat(rawSpeedValue);
+  if (!parsedSpeedValue) return modelName;
+
+  const speedValue = formattedNumber(parsedSpeedValue, 2);
+  return format({ modelName, speedValue });
 };
 
 export default formatCpuInfo;
