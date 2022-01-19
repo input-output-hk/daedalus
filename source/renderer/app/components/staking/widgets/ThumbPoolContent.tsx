f7148c4019bee9cd1564aeea4a3675a7c0dea24e
Renan Ferreira <renan.ferreira@iohk.io>
2022-01-19 16:59:51 -0300

    [DDW-788] Chakra-ui - cleaner version

--- source/renderer/app/components/staking/widgets/ThumbPoolContent.tsx
+++ source/renderer/app/components/staking/widgets/ThumbPoolContent.tsx
@@ -1,22 +1,20 @@
 import BigNumber from 'bignumber.js';
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
-import { Box, Text, Center, Flex } from '@chakra-ui/react';
-// @ts-ignore ts-migrate(2307) FIXME: Cannot find module '../../../assets/images/clock-c... Remove this comment to see the full error message
+import SVGInline from 'react-svg-inline';
+import classnames from 'classnames';
 import clockIcon from '../../../assets/images/clock-corner.inline.svg';
 // @ts-ignore ts-migrate(2307) FIXME: Cannot find module '../../../assets/images/no-data... Remove this comment to see the full error message
 import noDataDashBigImage from '../../../assets/images/no-data-dash-big.inline.svg';
-// @ts-ignore ts-migrate(2307) FIXME: Cannot find module './ThumbPoolContent.scss' or it... Remove this comment to see the full error message
+import styles from './ThumbPoolContent.scss';
 import { getColorFromRange, getSaturationColor } from '../../../utils/colors';
 import StakePool from '../../../domains/StakePool';
 import {
   IS_RANKING_DATA_AVAILABLE,
   IS_SATURATION_DATA_AVAILABLE,
 } from '../../../config/stakingConfig';
-// @ts-ignore ts-migrate(2307) FIXME: Cannot find module '../../../assets/images/ada-sym... Remove this comment to see the full error message
 import adaIcon from '../../../assets/images/ada-symbol.inline.svg';
 import { formattedWalletAmount } from '../../../utils/formatters';
-import { AdaIcon, ClockIcon, NoDataDashIcon } from './ThumbPoolContent.styles';
 
 type Props = {
   stakePool: StakePool;
@@ -61,101 +59,75 @@ class ThumbPoolContent extends Component<Props> {
     } = stakePool;
     const color = getColorFromRange(ranking, numberOfRankedStakePools);
 
+    const componentClassnames = classnames([
+      styles.component,
+      !IS_SATURATION_DATA_AVAILABLE ? styles.hideSaturation : null,
+    ]);
+
+    const saturationClassnames = classnames([
+      styles.saturationBar,
+      styles[getSaturationColor(saturation)],
+    ]);
+
     return (
-      <Flex
-        h="16"
-        w="20"
-        flexDirection="column"
-        pt={!IS_SATURATION_DATA_AVAILABLE ? '3' : '2'}
-        pos="relative"
-      >
-        <Box h="full">
-          <Center mb={!IS_SATURATION_DATA_AVAILABLE ? '1' : 'px'}>
-            <Text
-              fontWeight="semibold"
-              fontSize="sm"
-              sx={{ color: 'var(--theme-staking-stake-pool-ticker-color)' }}
-              lineHeight="none"
-            >
-              {ticker}
-            </Text>
-          </Center>
-          {isGridRewardsView &&
-            (IS_RANKING_DATA_AVAILABLE && nonMyopicMemberRewards ? (
-              <Center py="0.5" pos="relative" flex="1 1 auto">
-                <Text fontSize="sm" fontWeight="semibold">
-                  {this.formattedRewards(potentialRewards)}
-                </Text>
-                <AdaIcon svg={adaIcon} />
-              </Center>
-            ) : (
-              <Center flex="1 1 auto">
-                <NoDataDashIcon svg={noDataDashBigImage} />
-              </Center>
-            ))}
-          {!isGridRewardsView &&
-            (IS_RANKING_DATA_AVAILABLE ? (
-              <Center flex="1" style={{ color }} mt="1">
-                <Text fontSize="xl" fontWeight="bold" lineHeight="none">
-                  {nonMyopicMemberRewards ? (
-                    ranking
-                  ) : (
-                    <>
-                      {numberOfRankedStakePools + 1}
-                      <Text display="inline-block">*</Text>
-                    </>
-                  )}
-                </Text>
-              </Center>
-            ) : (
-              <Center flex="1 1 auto">
-                <NoDataDashIcon svg={noDataDashBigImage} />
-              </Center>
-            ))}
-          {IS_SATURATION_DATA_AVAILABLE && (
-            <Center my="1">
-              <Flex
-                h="1"
-                w="10"
-                sx={{
-                  background:
-                    'var(--theme-staking-stake-pool-saturation-background-color)',
-                }}
-                borderRadius="sm"
-              >
-                <Box
-                  as="span"
-                  h="1"
-                  sx={{
-                    // @ts-ignore ts-migrate(2345) FIXME: Argument of type 'number' is not assignable to par... Remove this comment to see the full error message
-                    width: `${parseFloat(saturation).toFixed(2)}%`,
-                  }}
-                  bg={`stakePoolSaturation.${getSaturationColor(saturation)}`}
-                />
-              </Flex>
-            </Center>
-          )}
-        </Box>
-        <Box alignSelf="flex-end" w="full">
-          {IS_RANKING_DATA_AVAILABLE ? (
-            <>
-              {retiring && (
-                <Box pos="absolute" right="0" top="0">
-                  <ClockIcon svg={clockIcon} />
-                </Box>
-              )}
-              <Box h="1" w="full" sx={{ background: color }} flexShrink="0" />
-            </>
+      <div className={componentClassnames}>
+        <div className={styles.ticker}>{ticker}</div>
+        {isGridRewardsView &&
+          (IS_RANKING_DATA_AVAILABLE && nonMyopicMemberRewards ? (
+            <div className={styles.rewards}>
+              {this.formattedRewards(potentialRewards)}
+              <SVGInline svg={adaIcon} className={styles.adaIcon} />
+            </div>
           ) : (
-            <Box
-              h="1"
-              w="full"
-              sx={{ background: 'var(--theme-staking-stake-pool-grey-color)' }}
-              flexShrink="0"
+            <div className={styles.noDataDash}>
+              <SVGInline svg={noDataDashBigImage} />
+            </div>
+          ))}
+        {!isGridRewardsView &&
+          (IS_RANKING_DATA_AVAILABLE ? (
+            <div className={styles.ranking} style={{ color }}>
+              {nonMyopicMemberRewards ? (
+                ranking
+              ) : (
+                <>
+                  {numberOfRankedStakePools + 1}
+                  <sup>*</sup>
+                </>
+              )}
+            </div>
+          ) : (
+            <div className={styles.noDataDash}>
+              <SVGInline svg={noDataDashBigImage} />
+            </div>
+          ))}
+        {IS_SATURATION_DATA_AVAILABLE && (
+          <div className={saturationClassnames}>
+            <span
+              style={{
+                // @ts-ignore Argument of type 'number' is not assignable to parameter of type 'string'.ts(2345)
+                width: `${parseFloat(saturation).toFixed(2)}%`,
+              }}
             />
-          )}
-        </Box>
-      </Flex>
+          </div>
+        )}
+        {IS_RANKING_DATA_AVAILABLE ? (
+          <>
+            {retiring && (
+              <div className={styles.clock}>
+                <SVGInline svg={clockIcon} className={styles.clockIcon} />
+              </div>
+            )}
+            <div
+              className={styles.colorBand}
+              style={{
+                background: color,
+              }}
+            />
+          </>
+        ) : (
+          <div className={styles.greyColorBand} />
+        )}
+      </div>
     );
   }
 }
