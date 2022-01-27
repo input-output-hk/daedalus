9c980f723938b81e74275269b765145c611f3c27
Michael Chappell <michael.chappell@iohk.io>
2022-01-27 14:30:53 +0000

    [DDW-773] created global declaration file

---
+++ declaration.d.ts
@@ -0,0 +1,46 @@
+declare module '*.svg' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: any;
+  export default content;
+}
+
+type Daedalus = {
+    actions: ActionsMap,
+    api: Api,
+    environment: Object,
+    reset: Function,
+    stores: StoresMap,
+    translations: Object,
+    utils: {
+      crypto: {
+        generateMnemonic: Function
+      }
+    },
+  };
+
+export type $ElementType<
+  T extends { [P in K & any]: any },
+  K extends keyof T | number
+> = T[K];
+
+export type EnumMap<
+  K extends string,
+  V,
+  O extends Record<string, any> = any
+> = O & Record<K, V & $ElementType<O, K>>;
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      WALLET_COUNT: number;
+    }
+  }
+  const daedalus: Daedalus;
+}
+
+export {};
+
