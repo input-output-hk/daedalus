f8fa42157cd93b04ce37fcacb001f956d54c9693
Marcin Mazurek <marcin.mazurek@iohk.io>
2022-02-01 13:54:00 +0100

    [DDW-844] Code review feedback

--- storybook/addons.ts
+++ storybook/addons.ts
@@ -1,4 +1,3 @@
-// date override must be imported and set up before any modules that rely on the Date object
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register'; // import './addons/DaedalusMenu/register';
