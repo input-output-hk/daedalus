2edbedc01ceb6bd141c7b3286bbf688e9f510bb2
Lucas Araujo <lucas.araujo@iohk.io>
2022-02-01 15:52:40 -0300

    [DDW-921] Remove discreet mode tooltip/notification

--- source/renderer/app/features/discreet-mode/api/index.ts
+++ source/renderer/app/features/discreet-mode/api/index.ts
@@ -14,16 +14,4 @@ export class DiscreetModeApi {
     this.localStorage.set(storageKeys.DISCREET_MODE_ENABLED, enabled);
   unsetDiscreetModeSettings = async () =>
     this.localStorage.unset(storageKeys.DISCREET_MODE_ENABLED);
-  getDiscreetModeSettingsTooltip = (): Promise<boolean> =>
-    this.localStorage.get(storageKeys.DISCREET_MODE_SETTINGS_TOOLTIP, true);
-  setDiscreetModeSettingsTooltip = (enabled: boolean): Promise<void> =>
-    this.localStorage.set(storageKeys.DISCREET_MODE_SETTINGS_TOOLTIP, enabled);
-  unsetDiscreetModeSettingsTooltip = (): Promise<void> =>
-    this.localStorage.unset(storageKeys.DISCREET_MODE_SETTINGS_TOOLTIP);
-  getDiscreetModeNotification = (): Promise<boolean> =>
-    this.localStorage.get(storageKeys.DISCREET_MODE_NOTIFICATION, true);
-  setDiscreetModeNotification = (enabled: boolean): Promise<void> =>
-    this.localStorage.set(storageKeys.DISCREET_MODE_NOTIFICATION, enabled);
-  unsetDiscreetModeNotification = (): Promise<void> =>
-    this.localStorage.unset(storageKeys.DISCREET_MODE_NOTIFICATION);
 }
