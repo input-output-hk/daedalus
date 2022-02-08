364035d31b2d8faa3fcd83fc140f19e7519ecccd
Renan Ferreira <renan.ferreira@iohk.io>
2022-02-08 14:58:16 -0300

    [DDW-944] Install typed-scss

---
+++ source/renderer/app/components/loading/syncing-connecting/LogosDisplay.scss.d.ts
@@ -0,0 +1,6 @@
+export const adaApiLogo: string;
+export const adaLogo: string;
+export const component: string;
+export const connectingLogo: string;
+export const daedalusLogo: string;
+export const syncingLogo: string;
---
+++ source/renderer/app/components/loading/syncing-connecting/SyncingConnectingStatus.scss.d.ts
@@ -0,0 +1,7 @@
+export const component: string;
+export const connecting: string;
+export const description: string;
+export const ellipsis: string;
+export const headline: string;
+export const syncing: string;
+export const withoutAnimation: string;
