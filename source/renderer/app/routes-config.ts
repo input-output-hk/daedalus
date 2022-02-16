34a91d7ba1beb24a944adf6935e5c27c6b422829
przemyslaw.wlodek <przem.wlodek.github@gmail.com>
2022-02-16 18:47:56 +0100

    [DDW-904] Remove Analytics settings page placeholder

--- source/renderer/app/routes-config.ts
+++ source/renderer/app/routes-config.ts
@@ -42,7 +42,6 @@ export const ROUTES = {
     STAKE_POOLS: '/settings/stake-pools',
     TERMS_OF_USE: '/settings/terms-of-service',
     SUPPORT: '/settings/support',
-    ANALYTICS: '/settings/analytics',
     DISPLAY: '/settings/display',
   },
 };
