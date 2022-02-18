41b02e23d66c844b421411955e64349659175ba4
Marcin Mazurek <marcin.mazurek@iohk.io>
2022-02-18 16:21:15 +0100

    [DDW-957] Add retry to buildkite agent upload calls

---
+++ scripts/utils.sh
@@ -0,0 +1,13 @@
+arg2nz() { test $# -ge 2 -a ! -z "$2" || usage "empty value for" "$1"; }
+fail() { echo "ERROR: $*" >&2; exit 1; }
+retry() {
+        local tries=$1; arg2nz "iteration count" "$1"; shift
+        for i in $(seq 1 "${tries}")
+        do if "$@"
+           then return 0
+           else echo "failed, retry #$i of ${tries}"
+           fi
+           sleep 5
+        done
+        fail "persistent failure to exec:  $*"
+}
