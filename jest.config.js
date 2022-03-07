06c7fbe70a047274026b81d3a5932f175fcedeed
Renan Ferreira <renan.ferreira@iohk.io>
2022-03-07 17:29:39 -0300

    revert jest

--- jest.config.js
+++ jest.config.js
@@ -156,7 +156,7 @@ module.exports = {
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
-  //   '/node_modules/',
+  //   "/node_modules/",
   // ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
