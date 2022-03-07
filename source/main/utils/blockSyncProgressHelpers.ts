1b42c937c34a61ff895234445b4f0a7c1a2e3589
Lucas Araujo <lucas.araujo@iohk.io>
2022-03-07 16:36:14 -0300

    [DDW-812] Remove interval and watch for new log file lines

--- source/main/utils/blockSyncProgressHelpers.ts
+++ source/main/utils/blockSyncProgressHelpers.ts
@@ -1,8 +1,7 @@
 import moment, { Moment } from 'moment';
 
 export function isItFreshLog(applicationStartDate: Moment, line: string) {
-  const [, logDate] = line.match(
-    /\[(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d+) UTC]/
-  );
+  const [, logDate] =
+    line.match(/\[(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d+) UTC]/) || [];
   return applicationStartDate.isBefore(moment.utc(logDate));
 }
