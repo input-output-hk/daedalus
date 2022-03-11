bf5d7becd73f1e1fad90f0ab5b3a799e0221f6d5
Lucas Araujo <lucas.araujo@iohk.io>
2022-03-11 15:50:29 -0300

    [DDW-812] Review: Clearer logic

--- source/main/utils/blockSyncProgressHelpers.ts
+++ source/main/utils/blockSyncProgressHelpers.ts
@@ -4,5 +4,9 @@ export function isItFreshLog(applicationStartDate: Moment, line: string) {
   const [, logDate] =
     line.match(/\[(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d+) UTC]/) || [];
 
-  return !!logDate && applicationStartDate.isBefore(moment.utc(logDate));
+  if (!logDate) {
+    return false;
+  }
+
+  return applicationStartDate.isBefore(moment.utc(logDate));
 }
