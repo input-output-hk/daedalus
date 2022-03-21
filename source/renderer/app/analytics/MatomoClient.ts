82cd6fae7c8d79f25a1219b54c2196aca9865f65
Marcin Mazurek <marcin.mazurek@iohk.io>
2022-03-21 21:56:07 +0100

    [DDW-1025] Wrap up POC implementation

--- source/renderer/app/analytics/MatomoClient.ts
+++ source/renderer/app/analytics/MatomoClient.ts
@@ -1,35 +1,27 @@
 import MatomoTracker from 'matomo-tracker';
 import { AnalyticsClient, MachineSpecPayload } from './types';
 
+const SITE_ID = 3;
+const API_ENDPOINT = 'https://mazurek.matomo.cloud/matomo.php';
+
 /**
  * Matomo API reference:
  * https://developer.matomo.org/api-reference/tracking-api
  */
-
 export class MatomoClient implements AnalyticsClient {
   private matomoTracker: MatomoTracker;
 
   constructor(private network: string, private userId: string) {
-    this.matomoTracker = new MatomoTracker(
-      2,
-      'https://mazurek.matomo.cloud/matomo.php'
-    );
+    // TODO: this should be configurable or at least extracted to constants
+    this.matomoTracker = new MatomoTracker(SITE_ID, API_ENDPOINT);
   }
 
-  /**
-   * This event collects machine spec.
-   *
-   * Important:
-   * Since it is not a real `event` by the design of Google Analytics (because it will be sent just once), we are using
-   * event `action` property as event `subcategory` to simplify reporting in GA web panel.
-   */
-  sendMachineSpec = async (payload: MachineSpecPayload) => {
-    // Sending device data as visit with custom dimensions
+  sendMachineSpec = async (payload: MachineSpecPayload) =>
     this.matomoTracker.track({
       _id: this.userId,
-      // Not required by API, however enforced by the matomo-tracker lib.
-      // In fully fledged implementation we might want to try to rewrite those calls as vanilla HTTP requests
-      // to avoid sending 'fake' unnecessary data.
+      // Not required by Matomo Tracking API, however enforced by the matomo-tracker lib.
+      // In fully fledged implementation we might want to try to rewrite those calls as vanilla
+      // HTTP requests (or contribute to matomo-tracker) to avoid sending 'fake' unnecessary data.
       url: 'http://daedalus.electron/',
       dimension1: this.network,
       dimension2: payload.cpuModel,
@@ -37,5 +29,4 @@ export class MatomoClient implements AnalyticsClient {
       dimension4: payload.os,
       dimension5: payload.osArch,
     });
-  };
 }
