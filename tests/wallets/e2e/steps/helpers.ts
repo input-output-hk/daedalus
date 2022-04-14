2463bb45f68e746913da4bec7cc57f6478401615
Szymon Masłowski <szymon.maslowski@iohk.io>
2022-04-14 08:13:23 +0200

    [DDW-596] Remove object exported as a default from wallet e2e tests helpers as it was not used anywhere

--- tests/wallets/e2e/steps/helpers.ts
+++ tests/wallets/e2e/steps/helpers.ts
@@ -467,43 +467,6 @@ export const addWalletPage = {
   clickImportButton: (client: Record<string, any>) =>
     waitAndClick(client, `${ADD_WALLET} ${IMPORT_WALLET_BUTTON}`),
 };
-export default {
-  waitForDialog: (
-    client: Record<string, any>,
-    {
-      isHidden,
-    }: {
-      isHidden?: boolean;
-    } = {}
-  ) => client.waitForVisible(IMPORT_WALLET_DIALOG, null, isHidden),
-  selectFile: (
-    client: Record<string, any>,
-    {
-      filePath,
-    }: {
-      filePath: string;
-    }
-  ) =>
-    client.chooseFile(
-      `${IMPORT_WALLET_DIALOG} .FileUploadWidget_dropZone input`,
-      filePath
-    ),
-  // @ts-ignore ts-migrate(2345) FIXME: Argument of type 'Record<string, any>' is not assi... Remove this comment to see the full error message
-  clickImport: (client: Record<string, any>) =>
-    waitAndClick(client, `${IMPORT_WALLET_DIALOG} .primary`),
-  expectError: (
-    client: Record<string, any>,
-    {
-      error,
-    }: {
-      error: string;
-    }
-  ) =>
-    expectTextInSelector(client, {
-      selector: `${IMPORT_WALLET_DIALOG}_error`,
-      text: error,
-    }),
-};
 export const waitForActiveRestoreNotification = (
   client: Record<string, any>,
   {
