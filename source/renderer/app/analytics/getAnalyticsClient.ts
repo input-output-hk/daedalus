ea485d82dcf0de8a29e386dec8f9bbffc92def84
Marcin Mazurek <marcin.mazurek@iohk.io>
2022-05-19 12:24:13 +0200

    [DDW-809] Move more changes to separate PR

--- source/renderer/app/analytics/getAnalyticsClient.ts
+++ source/renderer/app/analytics/getAnalyticsClient.ts
@@ -1,26 +1,9 @@
 import { Environment } from '../../../common/types/environment.types';
-import { AnalyticsAcceptanceStatus, AnalyticsClient } from './types';
 import { NoopAnalyticsClient } from './noopAnalyticsClient';
 import LocalStorageApi from '../api/utils/localStorage';
-import { MatomoClient } from './MatomoClient';
 
-let client: AnalyticsClient;
-
-const getAnalyticsClient = async (
-  localStorage: LocalStorageApi,
-  environment: Environment
-) => {
-  const analyticsAccepted =
-    (await localStorage.getAnalyticsAcceptance()) ===
-    AnalyticsAcceptanceStatus.ACCEPTED;
-
-  if (environment.analyticsFeatureEnabled && analyticsAccepted) {
-    if (!client) {
-      client = new MatomoClient(environment, await localStorage.getUserID());
-    }
-    return client;
-  }
-
+const getAnalyticsClient = async (_: LocalStorageApi, __: Environment) => {
+  // Matomo Client will be implemented in a separate PR
   return NoopAnalyticsClient;
 };
 
