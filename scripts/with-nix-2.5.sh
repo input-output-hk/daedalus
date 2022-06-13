b58ceba71c019c38688830249ca68c187583de28
Michal Rus <michal.rus@iohk.io>
2022-06-13 15:22:49 +0200

    [DDW-1109] Try to fix `aarch64-darwin` segfaults – once more

--- scripts/with-nix-2.5.sh
+++ scripts/with-nix-2.5.sh
@@ -13,8 +13,8 @@ nixUnstable=$(nix-build "$myDir"/../default.nix -A pkgs.nixUnstable)
 
 PATH="$nixUnstable/bin:$PATH"
 
-# *Maybe* prevent segfaults on `aarch64-darwin`:
-export NIX_DONT_GC=1
+# *Maybe* prevent segfaults on `aarch64-darwin` in `GC_*` code:
+export GC_DONT_GC=1 # <https://chromium.googlesource.com/chromiumos/third_party/gcc/+/f4131b9cddd80547d860a6424ee1644167a330d6/gcc/gcc-4.6.0/boehm-gc/doc/README.environment#151>
 
 export NIX_CONFIG='
   experimental-features = nix-command flakes
