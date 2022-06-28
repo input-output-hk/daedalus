4f192d8561dd17a73c9ea859f8097237e320b846
Marcin Mazurek <marcin.mazurek@iohk.io>
2022-06-28 20:20:08 +0200

    [DDW-809] Temporary settings for testing

--- source/renderer/app/config/analyticsConfig.ts
+++ source/renderer/app/config/analyticsConfig.ts
@@ -1,6 +1,7 @@
 import { Network } from '../../../common/types/environment.types';
 
-export const ANALYTICS_API_ENDPOINT = 'http://localhost:8080/matomo.php';
+export const ANALYTICS_API_ENDPOINT =
+  'http://daedalusqa.matomo.cloud/matomo.php';
 export const PRIVACY_POLICY_LINK =
   'https://static.iohk.io/terms/iog-privacy-policy.pdf';
 export const DEV_MODE_SITE_MAP_ID = 1;
@@ -8,9 +9,9 @@ export const NETWORK_TO_ANALYTICS_SITE_ID_MAP: Record<Network, number> = {
   mainnet: 4,
   mainnet_flight: 4,
   testnet: 3,
-  staging: 5,
-  shelley_qa: 5,
-  alonzo_purple: 5,
-  selfnode: 5,
-  development: 5,
+  staging: 1,
+  shelley_qa: 1,
+  alonzo_purple: 1,
+  selfnode: 1,
+  development: 1,
 };
