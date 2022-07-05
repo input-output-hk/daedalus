ce50ae013ef5052136f750629d8d91340b5c8433
Marcin Mazurek <marcin.mazurek@iohk.io>
2022-07-05 21:31:31 +0200

    [DDW-1111] Revert unrelated changes

--- source/renderer/app/stores/WalletsLocalStore.ts
+++ source/renderer/app/stores/WalletsLocalStore.ts
@@ -2,7 +2,7 @@ import { observable, computed } from 'mobx';
 import Store from './lib/Store';
 import Request from './lib/LocalizedRequest';
 import { asyncForEach } from '../utils/asyncForEach';
-import { WalletsLocalData } from '../types/localDataTypes';
+import type { WalletsLocalData } from '../api/utils/localStorage';
 
 export default class WalletsLocalStore extends Store {
   @observable
