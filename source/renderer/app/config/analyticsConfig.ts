2ddd60f8d17591f648e89813544a1f06200f3877
Marcin Mazurek <marcin.mazurek@iohk.io>
2022-08-01 15:08:27 +0200

    [DDW-809] Update test instance details

--- source/renderer/app/config/analyticsConfig.ts
+++ source/renderer/app/config/analyticsConfig.ts
@@ -1,14 +1,14 @@
 import { Network } from '../../../common/types/environment.types';
 
 export const ANALYTICS_API_ENDPOINT =
-  'https://daedalusqa.matomo.cloud/matomo.php';
+  'https://daedalusqa2.matomo.cloud/matomo.php';
 export const PRIVACY_POLICY_LINK =
   'https://static.iohk.io/terms/iog-privacy-policy.pdf';
 export const DEV_MODE_SITE_MAP_ID = 1;
 export const NETWORK_TO_ANALYTICS_SITE_ID_MAP: Record<Network, number> = {
   mainnet: 4,
   mainnet_flight: 4,
-  testnet: 3,
+  testnet: 2,
   staging: 1,
   shelley_qa: 1,
   alonzo_purple: 1,
