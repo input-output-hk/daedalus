deecded541640c8807e665454bcec5f1327b553d
VanessaRC <vanessa.cristobal@iohk.io>
2022-10-05 13:06:27 +0100

    Fix: Fixes store state working with storybook for Languages stories.

--- storybook/stories/settings/language/Language.stories.tsx
+++ storybook/stories/settings/language/Language.stories.tsx
@@ -1,40 +1,77 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { withState } from '@dump247/storybook-state';
 import StoryDecorator from '../../_support/StoryDecorator';
 import InitialSettings from '../../../../source/renderer/app/components/profile/initial-settings/InitialSettings';
-import {
-  DATE_ENGLISH_OPTIONS,
-  NUMBER_OPTIONS,
-  TIME_OPTIONS,
-} from '../../../../source/renderer/app/config/profileConfig';
+
+const onValueChange = (store, id, value) => {
+  let key;
+  switch (id) {
+    case 'dateFormat':
+      key = 'currentDateFormat';
+      break;
+    case 'numberFormat':
+      key = 'currentNumberFormat';
+      break;
+    case 'timeFormat':
+      key = 'currentTimeFormat';
+      break;
+    default:
+      key = 'currentLocale';
+  }
+
+  store.set({
+    [key]: value,
+  });
+};
 
 storiesOf('Settings / Language', module)
   .addDecorator((story) => <StoryDecorator>{story()}</StoryDecorator>) // ====== Stories ======
-  // @ts-ignore ts-migrate(2345) FIXME: Argument of type '({ locale }: { locale: string; }... Remove this comment to see the full error message
-  .add('Select Language - initial', (_, { locale }: { locale: string }) => (
-    <div>
-      <InitialSettings
-        onSubmit={action('submit')}
-        onChangeItem={action('onChangeItem')}
-        currentDateFormat={DATE_ENGLISH_OPTIONS[0].value}
-        currentLocale={locale}
-        currentNumberFormat={NUMBER_OPTIONS[0].value}
-        currentTimeFormat={TIME_OPTIONS[0].value}
-      />
-    </div>
-  ))
-  // @ts-ignore ts-migrate(2345) FIXME: Argument of type '({ locale }: { locale: string; }... Remove this comment to see the full error message
-  .add('Select Language - submitting', (_, { locale }: { locale: string }) => (
-    <div>
-      <InitialSettings
-        onSubmit={action('submit')}
-        onChangeItem={action('onChangeItem')}
-        currentDateFormat={DATE_ENGLISH_OPTIONS[0].value}
-        currentLocale={locale}
-        currentNumberFormat={NUMBER_OPTIONS[0].value}
-        currentTimeFormat={TIME_OPTIONS[0].value}
-        isSubmitting
-      />
-    </div>
-  ));
+  .add(
+    'Select Language - initial',
+    withState(
+      {
+        currentDateFormat: 'MM/DD/YYYY',
+        currentNumberFormat: 'number-1',
+        currentTimeFormat: 'hh:mm:ss A',
+        currentLocale: 'en-US',
+      },
+      (store) => (
+        <div>
+          <InitialSettings
+            onSubmit={action('submit')}
+            onChangeItem={(id, value) => onValueChange(store, id, value)}
+            currentDateFormat={store.state.currentDateFormat}
+            currentLocale={store.state.currentLocale}
+            currentNumberFormat={store.state.currentNumberFormat}
+            currentTimeFormat={store.state.currentTimeFormat}
+          />
+        </div>
+      )
+    )
+  )
+  .add(
+    'Select Language - submitting',
+    withState(
+      {
+        currentDateFormat: 'MM/DD/YYYY',
+        currentNumberFormat: 'number-1',
+        currentTimeFormat: 'hh:mm:ss A',
+        currentLocale: 'en-US',
+      },
+      (store) => (
+        <div>
+          <InitialSettings
+            onSubmit={action('submit')}
+            onChangeItem={(id, value) => onValueChange(store, id, value)}
+            currentDateFormat={store.state.currentDateFormat}
+            currentLocale={store.state.currentLocale}
+            currentNumberFormat={store.state.currentNumberFormat}
+            currentTimeFormat={store.state.currentTimeFormat}
+            isSubmitting
+          />
+        </div>
+      )
+    )
+  );
