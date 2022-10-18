35190b34ec939a228e3f7b8577685f6278285358
VanessaRC <vanessa.cristobal@iohk.io>
2022-10-18 19:54:29 +0100

    [DDW-1143] Adds a preselected store property on top of the property of the component and the storybook store.

--- storybook/stories/settings/language/Language.stories.tsx
+++ storybook/stories/settings/language/Language.stories.tsx
@@ -1,77 +1,77 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { withState } from '@dump247/storybook-state';
+import { withState, Store } from '@dump247/storybook-state';
 import StoryDecorator from '../../_support/StoryDecorator';
 import InitialSettings from '../../../../source/renderer/app/components/profile/initial-settings/InitialSettings';
 
-const onValueChange = (store, id, value) => {
-  let key;
-  switch (id) {
-    case 'dateFormat':
-      key = 'currentDateFormat';
-      break;
-    case 'numberFormat':
-      key = 'currentNumberFormat';
-      break;
-    case 'timeFormat':
-      key = 'currentTimeFormat';
-      break;
-    default:
-      key = 'currentLocale';
-  }
+interface StoryStore {
+  currentDateFormat: string;
+  currentNumberFormat: string;
+  currentTimeFormat: string;
+  currentLocale: string;
+}
+
+const mockedLanguageState = {
+  currentDateFormat: 'MM/DD/YYYY',
+  currentNumberFormat: 'number-1',
+  currentTimeFormat: 'hh:mm:ss A',
+  currentLocale: 'en-US',
+};
+
+const onValueChange = (
+  store: Store<StoryStore>,
+  id: string,
+  value: string
+): void => {
+  const fieldIdToStoreKeyMap = {
+    dateFormat: 'currentDateFormat',
+    numberFormat: 'currentNumberFormat',
+    timeFormat: 'currentTimeFormat',
+    locale: 'currentLocale',
+  };
 
   store.set({
-    [key]: value,
+    [fieldIdToStoreKeyMap[id]]: value,
   });
+
+  if (id === 'locale') {
+    if (value === mockedLanguageState.currentLocale) {
+      store.set({
+        currentDateFormat: mockedLanguageState.currentDateFormat,
+      });
+    } else {
+      store.set({
+        currentDateFormat: 'YYYY年MM月DD日',
+      });
+    }
+  }
 };
 
 storiesOf('Settings / Language', module)
   .addDecorator((story) => <StoryDecorator>{story()}</StoryDecorator>) // ====== Stories ======
   .add(
     'Select Language - initial',
-    withState(
-      {
-        currentDateFormat: 'MM/DD/YYYY',
-        currentNumberFormat: 'number-1',
-        currentTimeFormat: 'hh:mm:ss A',
-        currentLocale: 'en-US',
-      },
-      (store) => (
-        <div>
-          <InitialSettings
-            onSubmit={action('submit')}
-            onChangeItem={(id, value) => onValueChange(store, id, value)}
-            currentDateFormat={store.state.currentDateFormat}
-            currentLocale={store.state.currentLocale}
-            currentNumberFormat={store.state.currentNumberFormat}
-            currentTimeFormat={store.state.currentTimeFormat}
-          />
-        </div>
-      )
-    )
+    withState(mockedLanguageState, (store) => (
+      <div>
+        <InitialSettings
+          onSubmit={action('submit')}
+          onChangeItem={(id, value) => onValueChange(store, id, value)}
+          {...store.state}
+        />
+      </div>
+    ))
   )
   .add(
     'Select Language - submitting',
-    withState(
-      {
-        currentDateFormat: 'MM/DD/YYYY',
-        currentNumberFormat: 'number-1',
-        currentTimeFormat: 'hh:mm:ss A',
-        currentLocale: 'en-US',
-      },
-      (store) => (
-        <div>
-          <InitialSettings
-            onSubmit={action('submit')}
-            onChangeItem={(id, value) => onValueChange(store, id, value)}
-            currentDateFormat={store.state.currentDateFormat}
-            currentLocale={store.state.currentLocale}
-            currentNumberFormat={store.state.currentNumberFormat}
-            currentTimeFormat={store.state.currentTimeFormat}
-            isSubmitting
-          />
-        </div>
-      )
-    )
+    withState(mockedLanguageState, (store) => (
+      <div>
+        <InitialSettings
+          onSubmit={action('submit')}
+          onChangeItem={(id, value) => onValueChange(store, id, value)}
+          isSubmitting
+          {...store.state}
+        />
+      </div>
+    ))
   );
