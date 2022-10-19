7308c18855e569b1ef4987a32af640059c0c48dd
VanessaRC <vanessa.cristobal@iohk.io>
2022-10-19 11:05:01 +0100

    [DDW-1152] Fix General > General story to have it's own storybook state.

--- storybook/stories/settings/general/General.stories.tsx
+++ storybook/stories/settings/general/General.stories.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import { findKey } from 'lodash';
 import addons from '@storybook/addons';
 import { boolean, number, CHANGE } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { withState } from '@dump247/storybook-state';
+import { withState, Store } from '@dump247/storybook-state';
 import SettingsWrapper from '../utils/SettingsWrapper';
-import {
-  DATE_ENGLISH_OPTIONS,
-  LANGUAGE_OPTIONS,
-  NUMBER_OPTIONS,
-  TIME_OPTIONS,
-} from '../../../../source/renderer/app/config/profileConfig';
 import { updateParam } from '../../../addons/DaedalusMenu';
-import { locales, themesIds } from '../../_support/config';
+import { themesIds } from '../../_support/config';
 // Screens
 import ProfileSettingsForm from '../../../../source/renderer/app/components/widgets/forms/ProfileSettingsForm';
 import StakePoolsSettings from '../../../../source/renderer/app/components/settings/categories/StakePoolsSettings';
@@ -22,9 +15,58 @@ import SupportSettings from '../../../../source/renderer/app/components/settings
 import TermsOfUseSettings from '../../../../source/renderer/app/components/settings/categories/TermsOfUseSettings';
 import WalletsSettings from '../../../../source/renderer/app/components/settings/categories/WalletsSettings';
 import SecuritySettings from '../../../../source/renderer/app/components/settings/categories/SecuritySettings';
+import {
+  DATE_ENGLISH_OPTIONS,
+  LANGUAGE_OPTIONS,
+  NUMBER_OPTIONS,
+  TIME_OPTIONS,
+} from '../../../../source/renderer/app/config/profileConfig';
 // Assets and helpers
 import currenciesList from '../../../../source/renderer/app/config/currenciesList.json';
 import { getLocalizedCurrenciesList } from '../../../../source/renderer/app/config/currencyConfig';
+
+interface StoryStore {
+  currentDateFormat: string;
+  currentNumberFormat: string;
+  currentTimeFormat: string;
+  currentLocale: string;
+}
+
+const mockedGeneralState = {
+  currentDateFormat: DATE_ENGLISH_OPTIONS[0].value,
+  currentNumberFormat: NUMBER_OPTIONS[0].value,
+  currentTimeFormat: TIME_OPTIONS[0].value,
+  currentLocale: LANGUAGE_OPTIONS[0].value,
+};
+
+const onValueChange = (
+  store: Store<StoryStore>,
+  id: string,
+  value: string
+): void => {
+  const fieldIdToStoreKeyMap = {
+    dateFormat: 'currentDateFormat',
+    numberFormat: 'currentNumberFormat',
+    timeFormat: 'currentTimeFormat',
+    locale: 'currentLocale',
+  };
+
+  store.set({
+    [fieldIdToStoreKeyMap[id]]: value,
+  });
+
+  if (id === 'locale') {
+    if (value === mockedGeneralState.currentLocale) {
+      store.set({
+        currentDateFormat: mockedGeneralState.currentDateFormat,
+      });
+    } else {
+      store.set({
+        currentDateFormat: 'YYYY年MM月DD日',
+      });
+    }
+  }
+};
 
 const changeControl = (name: string, value: boolean) => {
   addons.getChannel().emit(CHANGE, {
@@ -39,24 +81,17 @@ const getParamName = (obj, itemName): any =>
 /* eslint-disable consistent-return */
 storiesOf('Settings / General', module)
   .addDecorator(SettingsWrapper) // ====== Stories ======
-  .add('General', () => (
-    <ProfileSettingsForm
-      isSubmitting={boolean('isSubmitting', false)}
-      onSubmit={action('submit')}
-      onChangeItem={(param: string, value: string) => {
-        if (param === 'locale') {
-          updateParam({
-            param: 'localeName',
-            value: findKey(locales, (item) => item === value),
-          });
-        }
-      }}
-      currentDateFormat={DATE_ENGLISH_OPTIONS[0].value}
-      currentLocale={LANGUAGE_OPTIONS[0].value}
-      currentNumberFormat={NUMBER_OPTIONS[0].value}
-      currentTimeFormat={TIME_OPTIONS[0].value}
-    />
-  ))
+  .add(
+    'General',
+    withState(mockedGeneralState, (store) => (
+      <ProfileSettingsForm
+        isSubmitting={boolean('isSubmitting', false)}
+        onSubmit={action('submit')}
+        onChangeItem={(id, value) => onValueChange(store, id, value)}
+        {...store.state}
+      />
+    ))
+  )
   .add(
     'Wallets',
     withState(
