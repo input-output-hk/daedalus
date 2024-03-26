ec0aad5212a4896dc7e2cc87dfa2096732945663
Michal Rus <michal.rus@iohk.io>
2024-03-26 15:52:00 +0100

    [LW-9778, DDW-1228] Fix the Windows installer

--- installers/common/WindowsInstaller.hs
+++ installers/common/WindowsInstaller.hs
@@ -247,6 +247,7 @@ writeInstallerNSIS outName (Version fullVersion') InstallerConfig{installDirecto
                 file [] "libstdc++-6.dll"
                 file [] "mcfgthread-12.dll"
                 file [] "libgcc_s_seh-1.dll"
+                file [] "zlib1.dll"
                 --file [] "cardano-x509-certificates.exe"
                 --file [] "log-config-prod.yaml"
                 --file [] "wallet-topology.yaml"
