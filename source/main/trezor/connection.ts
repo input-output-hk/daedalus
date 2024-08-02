eec6f36d1e5f2b0d501b2ad84151a1a496b43687
Tomislav Horaček <tomislav@tt-media.hr>
2024-08-02 10:29:36 +0200

    [LW-10808] remove unnecessary logs

--- source/main/trezor/connection.ts
+++ source/main/trezor/connection.ts
@@ -3,7 +3,6 @@ import { logger } from '../utils/logging';
 import { manifest } from './manifest';
 
 export const initTrezorConnect = async () => {
-  console.debug('>>> INIT TREZOR');
   try {
     await TrezorConnect.init({
       popup: false, // render your own UI
@@ -11,22 +10,17 @@ export const initTrezorConnect = async () => {
       debug: process.env.DEBUG_TREZOR === 'true',
       manifest,
     });
-    console.debug('>>> INIT TREZOR - done');
     logger.info('[TREZOR-CONNECT] Called TrezorConnect.init()');
   } catch (error) {
-    console.debug('>>> INIT TREZOR - error: ', error);
     logger.info('[TREZOR-CONNECT] Failed to call TrezorConnect.init()');
   }
 };
 
 export const reinitTrezorConnect = async () => {
-  console.debug('>>> Re-INIT TREZOR');
   try {
     logger.info('[TREZOR-CONNECT] Called TrezorConnect.dispose()');
     await TrezorConnect.dispose();
-    console.debug('>>> re-INIT TREZOR - done');
   } catch (error) {
-    console.debug('>>> re-INIT TREZOR - error: ', error);
     // ignore any TrezorConnect instance disposal errors
     logger.info('[TREZOR-CONNECT] Failed to call TrezorConnect.dispose()');
   }
